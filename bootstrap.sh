6c28e09662e820a3c2193e9061b7b90f4ccf1234
Brad Miller <brad@bradhouse.com>
2014-09-21 18:45:59 -0400

    Updated bootstrap file to install the new toolchains
    Change-Id: Ia9e254ed577517b191fee49bc55778636479be9c

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,46 +1,16 @@
 #!/usr/bin/env bash
 
 # Update the apt-get repository list
+apt --yes --force-yes install software-properties-common
+apt-add-repository --yes ppa:byteit101/frc-toolchain
 apt-get update
 
 
 # Download and install Java
-apt-get --yes --force-yes install default-jre
-apt-get --yes --force-yes install default-jdk
+apt-get --yes --force-yes install default-jre default-jdk maven python-pip python-setuptools g++-arm-frc-linux-gnueabi sshpass libc6-i386
 
-
-# Install Maven
-apt-get --yes --force-yes install maven
-
-
-# Install git-review
-apt-get --yes --force-yes install python-pip
-apt-get --yes --force-yes install python-setuptools
 easy_install pip
 yes | pip install git-review
-
-
-# Download and install the gcc linux arm-toolchains
-# Get the 32 bit c library
-apt-get --yes --force-yes install libc6-i386
-# Download the tool to unzip .zip files
-apt-get --yes --force-yes install unzip
-# The variables associated with the toolchains
-TOOL_VER="1.0.1"
-TOOL_DIR=~/../home/vagrant/wpilib/toolchains
-# Create the toolchains directory
-mkdir -p $TOOL_DIR
-# Download the toolchains to this directory
-wget -P ${TOOL_DIR}/ http://first.wpi.edu/FRC/c/maven/edu/wpi/first/wpilib/plugins/cpp/toolchains/linux/${TOOL_VER}/linux-${TOOL_VER}.zip
-# Unzip the file
-unzip -q ${TOOL_DIR}/linux-${TOOL_VER}.zip -d ${TOOL_DIR}/
-# Remove the unneeded zip file
-rm ${TOOL_DIR}/linux-${TOOL_VER}.zip
-# Change the name of the directory
-mv -f $TOOL_DIR/linux-${TOOL_VER} ${TOOL_DIR}/arm-none-linux-gnueabi-4.4.1
-chown -R vagrant $TOOL_DIR/arm-none-linux-gnueabi-4.4.1
-# Add the arm toolchains to the path variable
-echo 'PATH=$PATH:/home/vagrant/wpilib/toolchains/arm-none-linux-gnueabi-4.4.1/bin/' >> /home/vagrant/.bashrc
 
 
 # Install the jdk-linux-arm-vfp-sflt
@@ -49,9 +19,6 @@ mkdir -p /home/vagrant/jdk-linux-arm-vfp-sflt/
 # Unzip the jdk into this new directory
 tar -xzf /vagrant/jdk-7u45-linux-arm-vfp-sflt.gz -C /home/vagrant/jdk-linux-arm-vfp-sflt/
 
-
-# Install sshpass for the test system
-apt-get --yes --force-yes install sshpass
 
 # Keep this at the end of this file
 printf "\n\n"
