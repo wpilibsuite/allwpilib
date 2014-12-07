ac07142e4c4a75ba9538d9e135c65c9785f09ac5
PetaroMitaro <mitranopeter@gmail.com>
2014-12-07 14:36:25 -0500

    added PDP methods to javalib
    Change-Id: I60f3f615b01dba6e05721c78ba890859c7c9f027

--- wpilibj/wpilibJavaDevices/src/main/java/edu/wpi/first/wpilibj/hal/PDPJNI.java
+++ wpilibj/wpilibJavaDevices/src/main/java/edu/wpi/first/wpilibj/hal/PDPJNI.java
@@ -7,4 +7,9 @@ public class PDPJNI extends JNIWrapper {
 	public static native double getPDPTemperature(IntBuffer status);
 	public static native double getPDPVoltage(IntBuffer status);
 	public static native double getPDPChannelCurrent(byte channel, IntBuffer status);
+	public static native double getPDPTotalCurrent(IntBuffer status);
+	public static native double getPDPTotalPower(IntBuffer status);
+	public static native double getPDPTotalEnergy(IntBuffer status);
+	public static native void resetPDPTotalEnergy(IntBuffer status);
+	public static native void clearPDPStickyFaults(IntBuffer status);
 }
