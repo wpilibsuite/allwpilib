745489fec74050cb35bc49d6b5dac9e36fc2beff
Fredric Silberberg <fredric@fsilberberg.com>
2014-12-12 15:15:45 -0500

    Image v22
    Added the v22 libraries and headers, and updated the minimum version number to version 22
    
    Change-Id: Ie89dcde0212f164296c01e906b3ee9f332fb0b02

--- hal/lib/Athena/spilib/spi-lib.h
+++ hal/lib/Athena/spilib/spi-lib.h
@@ -1,12 +1,9 @@
 #ifndef __SPI_LIB_H__
 #define __SPI_LIB_H__
 
-#pragma once
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 int spilib_open(const char *device);
 void spilib_close(int handle);
 int spilib_setspeed(int handle, uint32_t speed);
@@ -15,11 +12,8 @@ int spilib_setopts(int handle, int msb_first, int sample_on_trailing, int clk_id
 int spilib_read(int handle, char *recv_buf, int32_t size);
 int spilib_write(int handle, const char *send_buf, int32_t size);
 int spilib_writeread(int handle, const char *send_buf, char *recv_buf, int32_t size);
-
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __SPI_LIB_H__ */
-
-
