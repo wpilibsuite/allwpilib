293d005432f8928060bacbe5ad501acdc6ffac3a
Peter Johnson <johnson.peter@gmail.com>
2015-08-13 22:14:17 -0700

    Turn on logging in unit tests.

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -7,8 +7,17 @@
 
 #include "gtest/gtest.h"
 
+#include "Log.h"
+
 int main(int argc, char **argv)
 {
+  nt::Logger::GetInstance().SetLogger(
+      [](unsigned int level, const char* file, unsigned int line,
+         const char* msg) {
+        std::fputs(msg, stderr);
+        std::fputc('\n', stderr);
+      });
+  nt::Logger::GetInstance().set_min_level(0);
     ::testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
     return ret;
