b4c058389666dee703a1a3308e67b7faf90ffd5d
Peter Johnson <johnson.peter@gmail.com>
2015-12-19 22:36:32 -0800

    ContainsKey: Compare to nullptr.
    This is a style change, not a functional change.

--- src/networktables/NetworkTable.cpp
+++ src/networktables/NetworkTable.cpp
@@ -207,7 +207,7 @@ bool NetworkTable::ContainsKey(StringRef key) const {
   llvm::SmallString<128> path(m_path);
   path += PATH_SEPARATOR_CHAR;
   path += key;
-  return nt::GetEntryValue(path) ? true : false;
+  return nt::GetEntryValue(path) != nullptr;
 }
 
 bool NetworkTable::ContainsSubTable(StringRef key) const {
