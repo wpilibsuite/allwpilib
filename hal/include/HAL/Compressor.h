dabab71f865f2da0c28d7683ce729aeaab538a9c
Tyler Veness <calcmogul@gmail.com>
2016-01-09 12:53:44 -0800

    artf2612: Added Python script for updating license in source files
    This is a follow-up of 3cd1253. A C++ program was written to automate the license update originally. That program was translated to Python so it can be kept in the repository and run when needed. It has been tested on Windows using the standard Python 3 installation and on Linux.
    
    The original version skipped files that had "//" at the beginning since most were files that should be excluded. The relevant files are now in an exclusion list and the rest are processed normally. The .hpp file extension has been added as well. The script rewrote CompressorJNI.cpp to remove the carriage returns from its line endings.

--- hal/include/HAL/Compressor.h
+++ hal/include/HAL/Compressor.h
@@ -1,7 +1,9 @@
-/**
- * Compressor.h
- * Methods for interacting with a compressor with the CAN PCM device
- */
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2016. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 #include <stdint.h>
 
