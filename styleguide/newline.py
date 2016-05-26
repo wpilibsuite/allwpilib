5554e43291ef4746f02163a15ec0a55f2c7ac09c
Tyler Veness <calcmogul@gmail.com>
2016-05-26 02:09:42 -0700

    format.py: added EOF newline management

--- styleguide/newline.py
+++ styleguide/newline.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from subprocess import call
 import os
 import re
 import sys
@@ -11,16 +10,14 @@ if sep == "\\":
     sep += "\\"
 
 # Files and directories which should be included in or excluded from the update
-regexInclude = re.compile("\.cpp$|\.h$|\.hpp$|\.inc$")
-folderExclude = "build" + sep + "|\.git" + sep + "|gradle" + sep + \
-                "|\.gradle" + sep + "|ni-libraries" + sep + "|ctre" + sep + \
-                "|frccansae" + sep + "|FRC_FPGA_ChipObject" + sep + \
-                "|gtest" + sep + "|i2clib" + sep + \
-                "|NetworkCommunication" + sep + "|spilib" + sep + \
-                "|visa" + sep + "|wpilibj" + sep
+folderExclude = "build" + sep + "|\.git" + sep + "|\.gradle" + sep + \
+                "|ni-libraries" + sep + "|ctre" + sep + "|frccansae" + sep + \
+                "|FRC_FPGA_ChipObject" + sep + "|gtest" + sep + \
+                "|i2clib" + sep + "|NetworkCommunication" + sep + \
+                "|spilib" + sep + "|visa" + sep
 regexExclude = re.compile(folderExclude +
                           "|NIIMAQdx\.h$|nivision\.h$|can_proto\.h$|"
-                          "CanTalonSRX\.h$")
+                          "CanTalonSRX\.h$|\.jar$|\.png$|\.so")
 
 # Handle running in either the root or styleguide directories
 configPath = ""
@@ -34,13 +31,7 @@ files = [os.path.join(dp, f) for dp, dn, fn in
          os.walk(os.path.expanduser(configPath)) for f in fn]
 
 # Apply regex filters to list
-files = [f for f in files if regexInclude.search(f)]
 files = [f for f in files if not regexExclude.search(f)]
-
-# Set clang-format name for platform
-clangExec = "clang-format"
-if sys.platform.startswith("win32"):
-    clangExec += ".exe"
 
 if not files:
     print("Error: no files found to format", file=sys.stderr)
@@ -51,5 +42,21 @@ for name in files:
     if len(sys.argv) > 1 and sys.argv[1] == "-v":
         print("Processing", name,)
 
-    # Run clang-format
-    call([clangExec, "-i", "-style=file", name])
+    newlines = 0
+
+    # Remove all EOF newlines, then append one
+    with open(name, "r+", encoding = "ISO-8859-1") as file:
+        file.seek(0, os.SEEK_END)
+        size = file.tell()
+        file.seek(size - 1)
+        while file.read(1) == os.linesep:
+            newlines = newlines + 1
+            size = size - 1
+            file.seek(size - 1)
+
+        if newlines < 1:
+            file.seek(size)
+            file.write(os.linesep)
+        elif newlines > 1:
+            file.seek(size)
+            file.truncate()
