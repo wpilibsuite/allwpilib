a3a3a74c02a7276afb923430d580206f056fd2db
Thad House <thadhouse1@gmail.com>
2016-05-26 17:44:35 -0700

    Adds support for Handles to the HAL
    Handles will be replacing opaque pointers in the HAL for better compatibility and easier error checking. This commits adds the initial resource manangement and definition for handles.

---
+++ hal/include/HAL/Handles.h
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2016. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <stdint.h>
+
+#define HAL_INVALID_HANDLE 0
+#define HAL_HANDLE_INDEX_OUT_OF_RANGE -1
+#define HAL_HANDLE_ALREADY_ALLOCATED -2
+#define HAL_HANDLE_INVALID_TYPE -3
+#define HAL_HANDLE_OUT_OF_HANDLES -4
+#define HAL_HANDLE_NEGATIVE_INDEX -5
+
+typedef int32_t HalHandle;
+
+enum HalHandleEnum { Undefined = 0, DIO = 1 };
---
+++ hal/lib/athena/handles/HandlesInternal.h
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2016. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <stdint.h>
+
+#include "HAL/Handles.h"
+
+/* General Handle Data Layout
+ * Bits 0-15:  Handle Index
+ * Bits 16-23: Unused
+ * Bits 24-30: Handle Type
+ * Bit 31:     1 if handle error, 0 if no error
+ *
+ * Other specialized handles will use different formats, however Bits 24-31 are
+ * always reserved for type and error handling.
+ */
+
+namespace hal {
+static inline int16_t getHandleIndex(HalHandle handle) {
+  // mask and return last 16 bits
+  return (int16_t)(handle & 0xffff);
+}
+static inline HalHandleEnum getHandleType(HalHandle handle) {
+  // mask first 8 bits and cast to enum
+  return (HalHandleEnum)((handle >> 24) & 0xff);
+}
+static inline bool isHandleType(HalHandle handle, HalHandleEnum handleType) {
+  return handleType == getHandleType(handleType);
+}
+static inline int16_t getHandleTypedIndex(HalHandle handle,
+                                          HalHandleEnum enumType) {
+  if (!isHandleType(handle, enumType)) return HAL_HANDLE_INVALID_TYPE;
+  return getHandleIndex(handle);
+}
+
+HalHandle createHandle(int16_t index, HalHandleEnum handleType);
+}
