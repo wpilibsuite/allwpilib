6bfc0086738043b26e6884465ed0e24ca5e51a8d
Tyler Veness <calcmogul@gmail.com>
2016-09-06 19:43:24 -0700

    Replaced snake case variable names with mixed case (#221)

--- hal/include/HAL/SPI.h
+++ hal/include/HAL/SPI.h
@@ -21,18 +21,18 @@ int32_t HAL_WriteSPI(int32_t port, uint8_t* dataToSend, int32_t sendSize);
 int32_t HAL_ReadSPI(int32_t port, uint8_t* buffer, int32_t count);
 void HAL_CloseSPI(int32_t port);
 void HAL_SetSPISpeed(int32_t port, int32_t speed);
-void HAL_SetSPIOpts(int32_t port, HAL_Bool msb_first,
-                    HAL_Bool sample_on_trailing, HAL_Bool clk_idle_high);
+void HAL_SetSPIOpts(int32_t port, HAL_Bool msbFirst, HAL_Bool sampleOnTrailing,
+                    HAL_Bool clkIdleHigh);
 void HAL_SetSPIChipSelectActiveHigh(int32_t port, int32_t* status);
 void HAL_SetSPIChipSelectActiveLow(int32_t port, int32_t* status);
 int32_t HAL_GetSPIHandle(int32_t port);
 void HAL_SetSPIHandle(int32_t port, int32_t handle);
 
 void HAL_InitSPIAccumulator(int32_t port, int32_t period, int32_t cmd,
-                            int32_t xfer_size, int32_t valid_mask,
-                            int32_t valid_value, int32_t data_shift,
-                            int32_t data_size, HAL_Bool is_signed,
-                            HAL_Bool big_endian, int32_t* status);
+                            int32_t xferSize, int32_t validMask,
+                            int32_t validValue, int32_t dataShift,
+                            int32_t dataSize, HAL_Bool isSigned,
+                            HAL_Bool bigEndian, int32_t* status);
 void HAL_FreeSPIAccumulator(int32_t port, int32_t* status);
 void HAL_ResetSPIAccumulator(int32_t port, int32_t* status);
 void HAL_SetSPIAccumulatorCenter(int32_t port, int32_t center, int32_t* status);
