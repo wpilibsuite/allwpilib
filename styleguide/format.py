423f7acc933a13bc87f351bd166498572ae53689
Thad House <thadhouse1@gmail.com>
2016-10-24 00:59:57 -0700

    Fixes format.py on windows

--- styleguide/format.py
+++ styleguide/format.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """This script invokes format.py in the wpilibsuite/styleguide repository.
 
 Set the WPI_FORMAT environment variable to its location on disk before use. For
@@ -12,17 +11,26 @@ import os
 import subprocess
 import sys
 
+
 def main():
     path = os.environ.get("WPI_FORMAT")
     if path == None:
         print("Error: WPI_FORMAT environment variable not set")
         sys.exit(1)
 
-    # Run main format.py script
-    args = ["python3", path + "/format.py"]
-    args.extend(sys.argv[1:])
-    proc = subprocess.Popen(args)
-    sys.exit(proc.wait())
+    try:
+        # Run main format.py script
+        args = ["python3", path + "/format.py"]
+        args.extend(sys.argv[1:])
+        proc = subprocess.Popen(args)
+        sys.exit(proc.wait())
+    except FileNotFoundError:
+        # Run main format.py script on windows
+        args = ["py", "-3", path + "/format.py"]
+        args.extend(sys.argv[1:])
+        proc = subprocess.Popen(args)
+        sys.exit(proc.wait())
+
 
 if __name__ == "__main__":
     main()
