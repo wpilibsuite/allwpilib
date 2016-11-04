0d77bc2564e93b78aa9db74a6c2de3ba9e5698b8
Thad House <thadhouse1@gmail.com>
2016-11-03 21:29:11 -0700

    Removes All chipobject files

--- ni-libraries/include/FRC_FPGA_ChipObject/RoboRIO_FRC_ChipObject_Aliases.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) National Instruments 2008.  All Rights Reserved.
-// Do Not Edit... this file is generated!
-
-#ifndef __RoboRIO_FRC_ChipObject_Aliases_h__
-#define __RoboRIO_FRC_ChipObject_Aliases_h__
-
-#define nRoboRIO_FPGANamespace nFRC_2017_17_0_2
-
-#endif // __RoboRIO_FRC_ChipObject_Aliases_h__
