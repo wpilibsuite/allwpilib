d82764f3db2f07a6abde42f11221ee7341780753
Dustin Spicuzza <dustin@virtualroadside.com>
2016-11-19 18:57:18 -0500

    Reorder the HAL handle types

--- hal/include/HAL/Types.h
+++ hal/include/HAL/Types.h
@@ -15,32 +15,32 @@ typedef int32_t HAL_Handle;
 
 typedef HAL_Handle HAL_PortHandle;
 
-typedef HAL_Handle HAL_InterruptHandle;
-
-typedef HAL_Handle HAL_NotifierHandle;
+typedef HAL_Handle HAL_AnalogInputHandle;
 
 typedef HAL_Handle HAL_AnalogOutputHandle;
 
-typedef HAL_Handle HAL_AnalogInputHandle;
-
 typedef HAL_Handle HAL_AnalogTriggerHandle;
 
-typedef HAL_Handle HAL_RelayHandle;
+typedef HAL_Handle HAL_CompressorHandle;
+
+typedef HAL_Handle HAL_CounterHandle;
 
 typedef HAL_Handle HAL_DigitalHandle;
 
 typedef HAL_Handle HAL_DigitalPWMHandle;
 
-typedef HAL_Handle HAL_CounterHandle;
-
-typedef HAL_Handle HAL_CompressorHandle;
-
-typedef HAL_Handle HAL_SolenoidHandle;
+typedef HAL_Handle HAL_EncoderHandle;
 
 typedef HAL_Handle HAL_FPGAEncoderHandle;
 
-typedef HAL_Handle HAL_EncoderHandle;
-
 typedef HAL_Handle HAL_GyroHandle;
+
+typedef HAL_Handle HAL_InterruptHandle;
+
+typedef HAL_Handle HAL_NotifierHandle;
+
+typedef HAL_Handle HAL_RelayHandle;
+
+typedef HAL_Handle HAL_SolenoidHandle;
 
 typedef int32_t HAL_Bool;
