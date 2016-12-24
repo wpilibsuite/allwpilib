4b16999fbc719d5f031115fb704028221bf444ac
Peter Johnson <johnson.peter@gmail.com>
2016-12-24 01:58:12 -0600

    Fix tests that are only built on cmake. (#168)
    Fixes #167.

--- test/rpc_speed.cpp
+++ test/rpc_speed.cpp
@@ -6,7 +6,7 @@
 #include "ntcore.h"
 
 std::string callback1(nt::StringRef name, nt::StringRef params_str,
-                      const ConnectionInfo& conn_info) {
+                      const nt::ConnectionInfo& conn_info) {
   auto params = nt::UnpackRpcValues(params_str, NT_DOUBLE);
   if (params.empty()) {
     std::fputs("empty params?\n", stderr);
