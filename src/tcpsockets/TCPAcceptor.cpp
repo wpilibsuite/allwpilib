95ad4783f165e0987aa3944b23d9a608014f6212
Peter Johnson <johnson.peter@gmail.com>
2016-12-26 13:55:21 -0800

    Revert "Fixes TCPAcceptor able to use an empty string (#172)"
    This doesn't actually do anything since the outer if statement checks
    for empty m_address already.
    
    This reverts commit 6ad9f45d9a89cb425bd52e99726ff420ae5e587f.

--- src/tcpsockets/TCPAcceptor.cpp
+++ src/tcpsockets/TCPAcceptor.cpp
@@ -90,7 +90,7 @@ int TCPAcceptor::start() {
 #else
     int res = inet_pton(PF_INET, m_address.c_str(), &(address.sin_addr));
 #endif
-    if (res != 1 && !m_address.empty()) {
+    if (res != 1) {
       WPI_ERROR(m_logger, "could not resolve " << m_address << " address");
       return -1;
     }
