6f18611eeb43f994fa26f2e2f4e35636267823e7
Austin Shalit <austinshalit@gmail.com>
2017-01-01 01:37:14 +0000

    Update license headers to 2017

--- hal/include/HAL/HAL.h
+++ hal/include/HAL/HAL.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2013-2016. All Rights Reserved.                        */
+/* Copyright (c) FIRST 2013-2017. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
