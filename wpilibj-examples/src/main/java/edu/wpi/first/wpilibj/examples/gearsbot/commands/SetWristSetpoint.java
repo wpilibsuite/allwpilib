2315fd409085d5cc122fa19851bbbd258ca49875
Austin Shalit <austinshalit@gmail.com>
2017-07-22 15:40:02 -0700

    Ran wpiformat

--- wpilibj-examples/src/main/java/edu/wpi/first/wpilibj/examples/gearsbot/commands/SetWristSetpoint.java
+++ wpilibj-examples/src/main/java/edu/wpi/first/wpilibj/examples/gearsbot/commands/SetWristSetpoint.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2017. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
--- wpilibj-examples/src/main/java/edu/wpi/first/wpilibj/examples/mecanumdrive/Robot.java
+++ wpilibj-examples/src/main/java/edu/wpi/first/wpilibj/examples/mecanumdrive/Robot.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2017. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package edu.wpi.first.wpilibj.examples.mecanumdrive;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -42,4 +49,3 @@ public class Robot extends IterativeRobot {
 		robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getZ(), 0.0);
 	}
 }
-
