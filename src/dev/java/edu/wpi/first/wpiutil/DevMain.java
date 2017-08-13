2fa41b23b9f8ce210d383fed9972dca3c270bb39
Thad House <ThadHouse@users.noreply.github.com>
2017-08-12 23:52:08 -0700

    Add a java version of the dev app. (#16)

---
+++ src/dev/java/edu/wpi/first/wpiutil/DevMain.java
@@ -0,0 +1,8 @@
+package edu.wpi.first.wpiutil;
+
+public class DevMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(RuntimeDetector.getPlatformPath());
+    }
+}
