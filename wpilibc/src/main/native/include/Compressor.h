06e4fb8322dc7361336e1a5cbde54c74d1e9fb52
sciencewhiz <git@rossesmail.com>
2017-08-18 20:27:04 -0700

    Improve C++ Compressor documentation based on Java's docs.

--- wpilibc/src/main/native/include/Compressor.h
+++ wpilibc/src/main/native/include/Compressor.h
@@ -18,7 +18,15 @@
 namespace frc {
 
 /**
- * PCM compressor
+ * Class for operating a compressor connected to a %PCM (Pneumatic Control Module). The %PCM will
+ * automatically run in closed loop mode by default whenever a Solenoid object is created.
+ * For most cases, a Compressor object does not need to be instantiated or used in a robot program.
+ * This class is only required in cases where the robot program needs a more detailed status of the
+ * compressor or to enable/disable closed loop control.
+ *
+ * Note: you cannot operate the compressor directly from this class as doing so would circumvent
+ * the safety provided by using the pressure switch and closed loop control. You can only turn off
+ * closed loop control, thereby stopping the compressor from operating.
  */
 class Compressor : public SensorBase,
                    public LiveWindowSendable,
