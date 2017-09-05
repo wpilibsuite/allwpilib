a5ef50c9e7c5042a967cb79e92c8caa3cae9750d
sciencewhiz <sciencewhiz@users.noreply.github.com>
2017-09-04 22:03:02 -0700

    Add documentation for PeriodMultiplier (#632)

--- wpilibc/src/main/native/include/PWM.h
+++ wpilibc/src/main/native/include/PWM.h
@@ -26,7 +26,7 @@ namespace frc {
  * The values supplied as arguments for PWM outputs range from -1.0 to 1.0. They
  * are mapped to the hardware dependent values, in this case 0-2000 for the
  * FPGA. Changes are immediately sent to the FPGA, and the update occurs at the
- * next FPGA cycle. There is no delay.
+ * next FPGA cycle (5.005ms). There is no delay.
  *
  * As of revision 0.1.10 of the FPGA, the FPGA interprets the 0-2000 values as
  * follows:
@@ -41,9 +41,21 @@ class PWM : public SensorBase,
             public ITableListener,
             public LiveWindowSendable {
  public:
+  /**
+   * Represents the amount to multiply the minimum servo-pulse pwm period by.
+   */
   enum PeriodMultiplier {
+    /**
+     * Don't skip pulses. PWM pulses occur every 5.005 ms
+     */
     kPeriodMultiplier_1X = 1,
+    /**
+     * Skip every other pulse. PWM pulses occur every 10.010 ms
+     */
     kPeriodMultiplier_2X = 2,
+    /**
+     * Skip three out of four pulses. PWM pulses occur every 20.020 ms
+     */
     kPeriodMultiplier_4X = 4
   };
 
