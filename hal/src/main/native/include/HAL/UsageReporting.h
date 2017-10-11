3a00ffd2f475873abe2ade1613707124a8f4cb67
Thad House <ThadHouse@users.noreply.github.com>
2017-10-11 15:47:47 -0700

    Updates to the 2018v5 image

--- hal/src/main/native/include/HAL/UsageReporting.h
+++ hal/src/main/native/include/HAL/UsageReporting.h
@@ -132,7 +132,7 @@ namespace nUsageReporting
 
     /**
      * Report the usage of a resource of interest.
-     * 
+     *
      * @param resource one of the values in the tResourceType above (max value 51).
      * @param instanceNumber an index that identifies the resource instance.
      * @param context an optional additional context number for some cases (such as module number).  Set to 0 to omit.
