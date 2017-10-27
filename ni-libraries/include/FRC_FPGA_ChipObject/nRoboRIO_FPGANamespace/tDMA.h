541753c814021e61f88ee83999a2c4a52e96dca8
Thad House <ThadHouse@users.noreply.github.com>
2017-10-27 00:47:56 -0700

    Updates to 2018 v9 image (#686)

--- ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/tDMA.h
+++ ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/tDMA.h
@@ -1,15 +1,15 @@
 // Copyright (c) National Instruments 2008.  All Rights Reserved.
 // Do Not Edit... this file is generated!
 
-#ifndef __nFRC_2018_18_0_1_DMA_h__
-#define __nFRC_2018_18_0_1_DMA_h__
+#ifndef __nFRC_2018_18_0_4_DMA_h__
+#define __nFRC_2018_18_0_4_DMA_h__
 
 #include "../tSystem.h"
 #include "../tSystemInterface.h"
 
 namespace nFPGA
 {
-namespace nFRC_2018_18_0_1
+namespace nFRC_2018_18_0_4
 {
 
 class tDMA
@@ -195,4 +195,4 @@ private:
 }
 }
 
-#endif // __nFRC_2018_18_0_1_DMA_h__
+#endif // __nFRC_2018_18_0_4_DMA_h__
