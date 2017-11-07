6a5ec372eb3bf5eee55536f807b9966c6d4bf2d2
Thad House <ThadHouse@users.noreply.github.com>
2017-11-07 14:31:12 -0800

    Updates image to 2018 v10 image

--- ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/tHMB.h
+++ ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/tHMB.h
@@ -1,15 +1,15 @@
 // Copyright (c) National Instruments 2008.  All Rights Reserved.
 // Do Not Edit... this file is generated!
 
-#ifndef __nFRC_2018_18_0_4_HMB_h__
-#define __nFRC_2018_18_0_4_HMB_h__
+#ifndef __nFRC_2018_18_0_5_HMB_h__
+#define __nFRC_2018_18_0_5_HMB_h__
 
 #include "../tSystem.h"
 #include "../tSystemInterface.h"
 
 namespace nFPGA
 {
-namespace nFRC_2018_18_0_4
+namespace nFRC_2018_18_0_5
 {
 
 class tHMB
@@ -146,4 +146,4 @@ private:
 }
 }
 
-#endif // __nFRC_2018_18_0_4_HMB_h__
+#endif // __nFRC_2018_18_0_5_HMB_h__
