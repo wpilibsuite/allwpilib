dae5e8586dbd4a502377f94c69a3239269d907fe
Thad House <ThadHouse@users.noreply.github.com>
2017-12-06 13:21:30 -0800

    Update to image 2018v13

--- ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/nInterfaceGlobals.h
+++ ni-libraries/include/FRC_FPGA_ChipObject/nRoboRIO_FPGANamespace/nInterfaceGlobals.h
@@ -1,15 +1,18 @@
 // Copyright (c) National Instruments 2008.  All Rights Reserved.
 // Do Not Edit... this file is generated!
 
-#ifndef __nFRC_2018_18_0_5_nInterfaceGlobals_h__
-#define __nFRC_2018_18_0_5_nInterfaceGlobals_h__
+#ifndef __nFRC_2018_18_0_7_nInterfaceGlobals_h__
+#define __nFRC_2018_18_0_7_nInterfaceGlobals_h__
 
 namespace nFPGA
 {
-namespace nFRC_2018_18_0_5
+namespace nFRC_2018_18_0_7
 {
    extern unsigned int g_currentTargetClass;
+
+   static const int g_SpiAutoData_index = 0;
+   static const int g_DMA_index = 1;
 }
 }
 
-#endif // __nFRC_2018_18_0_5_nInterfaceGlobals_h__
+#endif // __nFRC_2018_18_0_7_nInterfaceGlobals_h__
