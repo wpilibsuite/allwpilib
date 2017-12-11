596109e56c6b5cc4cecb040bb10925fb9688148d
Jaci Brunning <jaci.brunning@gmail.com>
2017-12-11 20:34:25 +0800

    Revert "Temporary workaround for Sim driver station issues"
    This reverts commit 6c2a6d211faf767a16ecc7d25ca97577cfd9271f.

--- hal/src/main/native/sim/DriverStation.cpp
+++ hal/src/main/native/sim/DriverStation.cpp
@@ -15,7 +15,6 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#include <thread>
 
 #include <support/condition_variable.h>
 #include <support/mutex.h>
@@ -258,7 +257,6 @@ HAL_Bool HAL_WaitForDSDataTimeout(double timeout) {
   std::unique_lock<wpi::mutex> lock(newDSDataAvailableMutex);
   int currentCount = newDSDataAvailableCounter;
   while (newDSDataAvailableCounter == currentCount) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
     if (timeout > 0) {
       auto timedOut = newDSDataAvailableCond->wait_until(lock, timeoutTime);
       if (timedOut == std::cv_status::timeout) {
