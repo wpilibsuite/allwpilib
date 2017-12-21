0f947613a904b5aeacc3f0d3076e30eeea877263
Peter Johnson <johnson.peter@gmail.com>
2017-12-20 19:26:48 -0800

    Prepare wpiutil for merge into allwpilib.

--- .travis-scripts/wpiformat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME != 'osx' ]]; then
-    python3.5 -m wpiformat -y 2018 -clang 5.0
-else
-    python3 -m wpiformat -y 2018
-fi
-
-git --no-pager diff --exit-code HEAD  # Ensure formatter made no changes
