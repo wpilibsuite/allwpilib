8ef7b5ae6b61e3dff1fcf29822ead0466c3915f3
Abiel <empoleom@gmail.com>
2017-12-22 21:27:07 -0600

    Fixed java solenoid functionality

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/solenoid/Robot.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/solenoid/Robot.java
@@ -25,36 +25,45 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 
 public class Robot extends TimedRobot {
-  private final Joystick m_stick = new Joystick(0);
+	private final Joystick m_stick = new Joystick(0);
 
-  // Solenoid corresponds to a single solenoid.
-  private final Solenoid m_solenoid = new Solenoid(0);
+	// Solenoid corresponds to a single solenoid.
+	private final Solenoid m_solenoid = new Solenoid(0);
 
-  // DoubleSolenoid corresponds to a double solenoid.
-  private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(1, 2);
+	// DoubleSolenoid corresponds to a double solenoid.
+	private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(1, 2);
 
-  private static final int kSolenoidButton = 1;
-  private static final int kDoubleSolenoidForward = 2;
-  private static final int kDoubleSolenoidReverse = 3;
+	private static final int kSolenoidButton = 1;
+	private static final int kDoubleSolenoidForward = 2;
+	private static final int kDoubleSolenoidReverse = 3;
 
-  @Override
-  public void teleopPeriodic() {
-    /*
-     * The output of GetRawButton is true/false depending on whether
-     * the button is pressed; Set takes a boolean for whether
-     * to use the default (false) channel or the other (true).
-     */
-    m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
+	@Override
+	public void teleopPeriodic() {
+		/*
+		 * The output of GetRawButton is true/false depending on whether
+		 * the button is pressed; Set takes a boolean for whether
+		 * to use the default (false) channel or the other (true).
+		 */
+		m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
 
-    /*
-     * In order to set the double solenoid, if just one button
-     * is pressed, set the solenoid to correspond to that button.
-     * If both are pressed, set the solenoid will be set to Forwards.
-     */
-    if (m_stick.getRawButton(kDoubleSolenoidForward)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-    } else if (m_stick.getRawButton(kDoubleSolenoidReverse)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
-    }
-  }
-}
+		/*
+		 * In order to set the double solenoid, we will say that if
+		 * neither button is pressed, it is off, if just one button
+		 * is pressed, set the solenoid to correspond to that button,
+		 * and if both are pressed, set the solenoid to Forwards.
+		 */
+		if (m_stick.getRawButton(kDoubleSolenoidForward)) {
+			m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+
+		} else if (m_stick.getRawButton(kDoubleSolenoidReverse)) {
+			m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+
+		} else if (m_stick.getRawButton(kDoubleSolenoidForward)
+				&& m_stick.getRawButton(kDoubleSolenoidReverse)) {
+			m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+
+		} else {
+			m_doubleSolenoid.set(DoubleSolenoid.Value.kOff);
+		}
+	}
+ }
