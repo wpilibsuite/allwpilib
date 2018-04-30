a28832e52fe441142bd98deff7572f2a05b828ee
Peter Johnson <johnson.peter@gmail.com>
2018-04-29 23:36:18 -0700

    Add backwards compatibility shims for old wpiutil headers.
    These generate a warning when included, then include the old header.
    
    Note for GCC, #warning is used; this requires -std=gnu++14 instead of
    -std=c++14 (otherwise the warning is treated as an error because #warning is
    a GNU extension).  On MSVC, #pragma message is used, which is a bit
    unsatisfactory as the message doesn't say where it was included from.
    
    The llvm shim headers also include a llvm namespace shim.

---
+++ wpiutil/src/main/native/include/llvm/iterator_range.h
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2016-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#ifdef _MSC_VER
+#pragma message "warning: llvm/iterator_range.h is deprecated; include wpi/iterator_range.h instead"
+#else
+#warning "llvm/iterator_range.h is deprecated; include wpi/iterator_range.h instead"
+#endif
+
+#include "wpi/iterator_range.h"
+
+namespace llvm = wpi;
