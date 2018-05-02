ed8dc87ffb921e7ad0d7066ee3c1611c194bc640
Peter Johnson <johnson.peter@gmail.com>
2018-05-02 00:28:56 -0700

    Update cscore examples for llvm to wpi rename.

--- cscore/examples/enum_usb/enum_usb.cpp
+++ cscore/examples/enum_usb/enum_usb.cpp
@@ -5,74 +5,74 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include <llvm/SmallString.h>
-#include <llvm/raw_ostream.h>
+#include <wpi/SmallString.h>
+#include <wpi/raw_ostream.h>
 
 #include "cscore.h"
 
 int main() {
   CS_Status status = 0;
-  llvm::SmallString<64> buf;
+  wpi::SmallString<64> buf;
 
   for (const auto& caminfo : cs::EnumerateUsbCameras(&status)) {
-    llvm::outs() << caminfo.dev << ": " << caminfo.path << " (" << caminfo.name
-                 << ")\n";
+    wpi::outs() << caminfo.dev << ": " << caminfo.path << " (" << caminfo.name
+                << ")\n";
     cs::UsbCamera camera{"usbcam", caminfo.dev};
 
-    llvm::outs() << "Properties:\n";
+    wpi::outs() << "Properties:\n";
     for (const auto& prop : camera.EnumerateProperties()) {
-      llvm::outs() << "  " << prop.GetName();
+      wpi::outs() << "  " << prop.GetName();
       switch (prop.GetKind()) {
         case cs::VideoProperty::kBoolean:
-          llvm::outs() << " (bool): "
-                       << "value=" << prop.Get()
-                       << " default=" << prop.GetDefault();
+          wpi::outs() << " (bool): "
+                      << "value=" << prop.Get()
+                      << " default=" << prop.GetDefault();
           break;
         case cs::VideoProperty::kInteger:
-          llvm::outs() << " (int): "
-                       << "value=" << prop.Get() << " min=" << prop.GetMin()
-                       << " max=" << prop.GetMax() << " step=" << prop.GetStep()
-                       << " default=" << prop.GetDefault();
+          wpi::outs() << " (int): "
+                      << "value=" << prop.Get() << " min=" << prop.GetMin()
+                      << " max=" << prop.GetMax() << " step=" << prop.GetStep()
+                      << " default=" << prop.GetDefault();
           break;
         case cs::VideoProperty::kString:
-          llvm::outs() << " (string): " << prop.GetString(buf);
+          wpi::outs() << " (string): " << prop.GetString(buf);
           break;
         case cs::VideoProperty::kEnum: {
-          llvm::outs() << " (enum): "
-                       << "value=" << prop.Get();
+          wpi::outs() << " (enum): "
+                      << "value=" << prop.Get();
           auto choices = prop.GetChoices();
           for (size_t i = 0; i < choices.size(); ++i) {
             if (choices[i].empty()) continue;
-            llvm::outs() << "\n    " << i << ": " << choices[i];
+            wpi::outs() << "\n    " << i << ": " << choices[i];
           }
           break;
         }
         default:
           break;
       }
-      llvm::outs() << '\n';
+      wpi::outs() << '\n';
     }
 
-    llvm::outs() << "Video Modes:\n";
+    wpi::outs() << "Video Modes:\n";
     for (const auto& mode : camera.EnumerateVideoModes()) {
-      llvm::outs() << "  PixelFormat:";
+      wpi::outs() << "  PixelFormat:";
       switch (mode.pixelFormat) {
         case cs::VideoMode::kMJPEG:
-          llvm::outs() << "MJPEG";
+          wpi::outs() << "MJPEG";
           break;
         case cs::VideoMode::kYUYV:
-          llvm::outs() << "YUYV";
+          wpi::outs() << "YUYV";
           break;
         case cs::VideoMode::kRGB565:
-          llvm::outs() << "RGB565";
+          wpi::outs() << "RGB565";
           break;
         default:
-          llvm::outs() << "Unknown";
+          wpi::outs() << "Unknown";
           break;
       }
-      llvm::outs() << " Width:" << mode.width;
-      llvm::outs() << " Height:" << mode.height;
-      llvm::outs() << " FPS:" << mode.fps << '\n';
+      wpi::outs() << " Width:" << mode.width;
+      wpi::outs() << " Height:" << mode.height;
+      wpi::outs() << " FPS:" << mode.fps << '\n';
     }
   }
 }
