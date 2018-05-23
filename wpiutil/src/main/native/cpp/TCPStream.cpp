5a16481a142b03ba3ddcfa773bde6a622b42f2b6
Peter Johnson <johnson.peter@gmail.com>
2018-05-22 20:20:29 -0700

    TCPStream.cpp: Fix include ordering.
    Make Windows includes lowercase for consistency.

--- wpiutil/src/main/native/cpp/TCPStream.cpp
+++ wpiutil/src/main/native/cpp/TCPStream.cpp
@@ -23,17 +23,18 @@
 
 #include "wpi/TCPStream.h"
 
-#include <errno.h>
 #include <fcntl.h>
 
 #ifdef _WIN32
-#include <WinSock2.h>
-#include <Ws2tcpip.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #else
 #include <arpa/inet.h>
 #include <netinet/tcp.h>
 #include <unistd.h>
 #endif
+
+#include <cerrno>
 
 using namespace wpi;
 
