dd5438fab70e495038da7add7ea5842155bb9acf
Thad House <ThadHouse@users.noreply.github.com>
2018-06-03 22:41:24 -0700

    add compressor, constants counter and dio

--- hal/src/main/native/include/HAL/DIO.h
+++ hal/src/main/native/include/HAL/DIO.h
@@ -17,103 +17,110 @@ extern "C" {
 
 /**
  * Create a new instance of a digital port.
+ *
+ * @param portHandle the port handle to create from
+ * @param input      true for input, false for output
+ * @return           the created digital handle
  */
 HAL_DigitalHandle HAL_InitializeDIOPort(HAL_PortHandle portHandle,
                                         HAL_Bool input, int32_t* status);
 
+/**
+ * Checks if a DIO channel is valid.
+ *
+ * @param channel the channel number to check
+ * @return        true if the channel is correct, otherwise false
+ */
 HAL_Bool HAL_CheckDIOChannel(int32_t channel);
 
 void HAL_FreeDIOPort(HAL_DigitalHandle dioPortHandle);
 
 /**
  * Allocate a DO PWM Generator.
- * Allocate PWM generators so that they are not accidentally reused.
  *
- * @return PWM Generator handle
+ * @return the allocated digital PWM handle
  */
 HAL_DigitalPWMHandle HAL_AllocateDigitalPWM(int32_t* status);
 
 /**
  * Free the resource associated with a DO PWM generator.
  *
- * @param pwmGenerator The pwmGen to free that was allocated with
- * allocateDigitalPWM()
+ * @param pwmGenerator the digital PWM handle
  */
 void HAL_FreeDigitalPWM(HAL_DigitalPWMHandle pwmGenerator, int32_t* status);
 
 /**
  * Change the frequency of the DO PWM generator.
  *
- * The valid range is from 0.6 Hz to 19 kHz.  The frequency resolution is
- * logarithmic.
+ * The valid range is from 0.6 Hz to 19 kHz.
  *
- * @param rate The frequency to output all digital output PWM signals.
+ *  The frequency resolution is logarithmic.
+ *
+ * @param rate the frequency to output all digital output PWM signals
  */
 void HAL_SetDigitalPWMRate(double rate, int32_t* status);
 
 /**
- * Configure the duty-cycle of the PWM generator
+ * Configure the duty-cycle of the PWM generator.
  *
- * @param pwmGenerator The generator index reserved by allocateDigitalPWM()
- * @param dutyCycle The percent duty cycle to output [0..1].
+ * @param pwmGenerator the digital PWM handle
+ * @param dutyCycle    the percent duty cycle to output [0..1]
  */
 void HAL_SetDigitalPWMDutyCycle(HAL_DigitalPWMHandle pwmGenerator,
                                 double dutyCycle, int32_t* status);
 
 /**
- * Configure which DO channel the PWM signal is output on
+ * Configure which DO channel the PWM signal is output on.
  *
- * @param pwmGenerator The generator index reserved by allocateDigitalPWM()
- * @param channel The Digital Output channel to output on
+ * @param pwmGenerator the digital PWM handle
+ * @param channel      the channel to output on
  */
 void HAL_SetDigitalPWMOutputChannel(HAL_DigitalPWMHandle pwmGenerator,
                                     int32_t channel, int32_t* status);
 
 /**
- * Write a digital I/O bit to the FPGA.
- * Set a single value on a digital I/O channel.
+ * Write a digital value to a DIO channel.
  *
- * @param channel The Digital I/O channel
- * @param value The state to set the digital channel (if it is configured as an
- * output)
+ * @param dioPortHandle the digital port handle
+ * @param value         the state to set the digital channel (if it is
+ * configured as an output)
  */
 void HAL_SetDIO(HAL_DigitalHandle dioPortHandle, HAL_Bool value,
                 int32_t* status);
 
 /**
- * Set direction of a DIO channel.
+ * Set the direction of a DIO channel.
  *
- * @param channel The Digital I/O channel
- * @param input true to set input, false for output
+ * @param dioPortHandle the digital port handle
+ * @param input         true to set input, false for output
  */
 void HAL_SetDIODirection(HAL_DigitalHandle dioPortHandle, HAL_Bool input,
                          int32_t* status);
 
 /**
- * Read a digital I/O bit from the FPGA.
- * Get a single value from a digital I/O channel.
+ * Read a digital value from a DIO channel.
  *
- * @param channel The digital I/O channel
- * @return The state of the specified channel
+ * @param dioPortHandle the digital port handle
+ * @return              the state of the specified channel
  */
 HAL_Bool HAL_GetDIO(HAL_DigitalHandle dioPortHandle, int32_t* status);
 
 /**
- * Read the direction of a the Digital I/O lines
- * A 1 bit means output and a 0 bit means input.
+ * Read the direction of a DIO channel.
  *
- * @param channel The digital I/O channel
- * @return The direction of the specified channel
+ * @param dioPortHandle the digital port handle
+ * @return              true for input, false for output
  */
 HAL_Bool HAL_GetDIODirection(HAL_DigitalHandle dioPortHandle, int32_t* status);
 
 /**
- * Generate a single pulse.
+ * Generate a single digital pulse.
+ *
  * Write a pulse to the specified digital output channel. There can only be a
  * single pulse going at any time.
  *
- * @param channel The Digital Output channel that the pulse should be output on
- * @param pulseLength The active length of the pulse (in seconds)
+ * @param dioPortHandle the digital port handle
+ * @param pulseLength   the active length of the pulse (in seconds)
  */
 void HAL_Pulse(HAL_DigitalHandle dioPortHandle, double pulseLength,
                int32_t* status);
@@ -121,35 +128,37 @@ void HAL_Pulse(HAL_DigitalHandle dioPortHandle, double pulseLength,
 /**
  * Check a DIO line to see if it is currently generating a pulse.
  *
- * @return A pulse is in progress
+ * @return true if a pulse is in progress, otherwise false
  */
 HAL_Bool HAL_IsPulsing(HAL_DigitalHandle dioPortHandle, int32_t* status);
 
 /**
  * Check if any DIO line is currently generating a pulse.
  *
- * @return A pulse on some line is in progress
+ * @return true if a pulse on some line is in progress
  */
 HAL_Bool HAL_IsAnyPulsing(int32_t* status);
 
 /**
  * Write the filter index from the FPGA.
+ *
  * Set the filter index used to filter out short pulses.
  *
- * @param dioPortHandle Handle to the digital I/O channel
- * @param filterIndex The filter index.  Must be in the range 0 - 3, where 0
- *                    means "none" and 1 - 3 means filter # filterIndex - 1.
+ * @param dioPortHandle the digital port handle
+ * @param filterIndex   the filter index (Must be in the range 0 - 3, where 0
+ * means "none" and 1 - 3 means filter # filterIndex - 1)
  */
 void HAL_SetFilterSelect(HAL_DigitalHandle dioPortHandle, int32_t filterIndex,
                          int32_t* status);
 
 /**
  * Read the filter index from the FPGA.
+ *
  * Get the filter index used to filter out short pulses.
  *
- * @param dioPortHandle Handle to the digital I/O channel
- * @return filterIndex The filter index.  Must be in the range 0 - 3,
- * where 0 means "none" and 1 - 3 means filter # filterIndex - 1.
+ * @param dioPortHandle the digital port handle
+ * @return filterIndex  the filter index (Must be in the range 0 - 3,
+ * where 0 means "none" and 1 - 3 means filter # filterIndex - 1)
  */
 int32_t HAL_GetFilterSelect(HAL_DigitalHandle dioPortHandle, int32_t* status);
 
@@ -160,9 +169,9 @@ int32_t HAL_GetFilterSelect(HAL_DigitalHandle dioPortHandle, int32_t* status);
  * filter index domains (MXP vs HDR), ignore that distinction for now since it
  * compilicates the interface.  That can be changed later.
  *
- * @param filterIndex The filter index, 0 - 2.
- * @param value The number of cycles that the signal must not transition to be
- * counted as a transition.
+ * @param filterIndex the filter index, 0 - 2
+ * @param value       the number of cycles that the signal must not transition
+ * to be counted as a transition.
  */
 void HAL_SetFilterPeriod(int32_t filterIndex, int64_t value, int32_t* status);
 
@@ -174,9 +183,9 @@ void HAL_SetFilterPeriod(int32_t filterIndex, int64_t value, int32_t* status);
  * compilicates the interface.  Set status to NiFpga_Status_SoftwareFault if the
  * filter values miss-match.
  *
- * @param filterIndex The filter index, 0 - 2.
- * @param value The number of cycles that the signal must not transition to be
- * counted as a transition.
+ * @param filterIndex the filter index, 0 - 2
+ * @param value       the number of cycles that the signal must not transition
+ * to be counted as a transition.
  */
 int64_t HAL_GetFilterPeriod(int32_t filterIndex, int32_t* status);
 #ifdef __cplusplus
