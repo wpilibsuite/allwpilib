fb56b10584afdb085adc97f757f96622d0b51c77
KadenK <kaden.keep@gmail.com>
2018-06-13 13:34:01 -1000

    Native Deploy HelloWorld Eclipse Project folder added

---
+++ Kauailabs/NativeDeployHelloWorld/HelloWorld/src/com/kauailabs/vmx/wpilib/Examples/HelloWorld/Robot.java
@@ -0,0 +1,15 @@
+package com.kauailabs.vmx.wpilib.Examples.HelloWorld;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+
+public class Robot extends SampleRobot {
+
+     public void robotInit() {
+    	 System.out.println("Hello World!");
+     }
+     
+     public void disabled() {
+     }
+}
+
