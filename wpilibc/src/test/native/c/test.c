57fc61407461e6834622118d397a2951b33e319a
Thad House <ThadHouse@users.noreply.github.com>
2018-07-08 15:42:59 -0700

    Compile C headers in C mode, and fix cscore C (#1179)

---
+++ wpilibc/src/test/native/c/test.c
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <HAL/HAL.h>
+
+#include "cscore.h"
+#include "ntcore.h"
