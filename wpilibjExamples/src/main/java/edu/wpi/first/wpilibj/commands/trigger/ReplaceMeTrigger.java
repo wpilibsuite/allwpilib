34879addbacbcdfc33066b0a0e1dc3caa5b98e97
Thad House <thadhouse1@gmail.com>
2018-07-13 11:10:52 -0700

    Updates license for templates and examples

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/commands/trigger/ReplaceMeTrigger.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/commands/trigger/ReplaceMeTrigger.java
@@ -1,8 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* Use and modify this template how you wish, it's public domain.             */
 /*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.commands.trigger;
