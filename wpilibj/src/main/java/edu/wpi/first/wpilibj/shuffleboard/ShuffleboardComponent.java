1e60f811767626738f710643a58747eb024a73c5
Sam Carlberg <sam.carlberg@gmail.com>
2018-07-18 09:23:08 -0400

    Allow size and position of components to be set

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/shuffleboard/ShuffleboardComponent.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/shuffleboard/ShuffleboardComponent.java
@@ -24,6 +24,10 @@ public abstract class ShuffleboardComponent<C extends ShuffleboardComponent<C>>
   private String m_type;
   private Map<String, Object> m_properties;
   private boolean m_metadataDirty = false; // NOPMD redundant field initializer
+  private int m_column = -1;
+  private int m_row = -1;
+  private int m_width = -1;
+  private int m_height = -1;
 
   protected ShuffleboardComponent(ShuffleboardContainer parent, String title, String type) {
     m_parent = Objects.requireNonNull(parent, "Parent cannot be null");
@@ -73,15 +77,66 @@ public abstract class ShuffleboardComponent<C extends ShuffleboardComponent<C>>
     return (C) this;
   }
 
+  /**
+   * Sets the position of this component in the tab. This has no effect if this component is inside
+   * a layout.
+   *
+   * <p>If the position of a single component is set, it is recommended to set the positions of
+   * <i>all</i> components inside a tab to prevent Shuffleboard from automatically placing another
+   * component there before the one with the specific position is sent.
+   *
+   * @param columnIndex the column in the tab to place this component
+   * @param rowIndex    the row in the tab to place this component
+   * @return this component
+   */
+  public final C withPosition(int columnIndex, int rowIndex) {
+    m_column = columnIndex;
+    m_row = rowIndex;
+    m_metadataDirty = true;
+    return (C) this;
+  }
+
+  /**
+   * Sets the size of this component in the tab. This has no effect if this component is inside a
+   * layout.
+   *
+   * @param width  how many columns wide the component should be
+   * @param height how many rows high the component should be
+   * @return this component
+   */
+  public final C withSize(int width, int height) {
+    m_width = width;
+    m_height = height;
+    m_metadataDirty = true;
+    return (C) this;
+  }
+
   protected final void buildMetadata(NetworkTable metaTable) {
     if (!m_metadataDirty) {
       return;
     }
+    // Component type
     if (getType() == null) {
       metaTable.getEntry("PreferredComponent").delete();
     } else {
       metaTable.getEntry("PreferredComponent").forceSetString(getType());
     }
+
+    // Tile size
+    if (m_width < 0 || m_height < 0) {
+      metaTable.getEntry("Size").delete();
+    } else {
+      metaTable.getEntry("Size").setDoubleArray(new double[]{m_width, m_height});
+    }
+
+    // Tile position
+    if (m_column < 0 || m_row < 0) {
+      metaTable.getEntry("Position").delete();
+    } else {
+      metaTable.getEntry("Position").setDoubleArray(new double[]{m_column, m_row});
+    }
+
+    // Custom properties
     if (getProperties() != null) {
       NetworkTable propTable = metaTable.getSubTable("Properties");
       getProperties().forEach((name, value) -> propTable.getEntry(name).setValue(value));
