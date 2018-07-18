906f669720e4f185e02a294e2d478d22b29db7b8
PJ Reiniger <pj.reiniger@gmail.com>
2018-07-17 23:44:14 -0400

    Code review updates

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/hal/MatchInfoDataTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/hal/MatchInfoDataTest.java
@@ -16,24 +16,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MatchInfoDataTest {
   @Test
-  void matchInfoDataDoesNotThrow() {
+  void matchInfoDataEventName() {
     HAL.initialize(500, 0);
 
     MatchInfoData inMatchInfo = new MatchInfoData();
     inMatchInfo.eventName = "Event Name";
-    inMatchInfo.gameSpecificMessage = "Game Message";
-    inMatchInfo.matchNumber = 174;
-    inMatchInfo.matchType = MatchType.Qualification.ordinal();
-    inMatchInfo.replayNumber = 191;
     DriverStationDataJNI.setMatchInfo(inMatchInfo);
 
     MatchInfoData outMatchInfo = new MatchInfoData();
     HAL.getMatchInfo(outMatchInfo);
 
     assertEquals("Event Name", outMatchInfo.eventName);
-    assertEquals(MatchType.Qualification.ordinal(), outMatchInfo.matchType);
-    assertEquals(174, outMatchInfo.matchNumber);
-    assertEquals(191, outMatchInfo.replayNumber);
+  }
+
+  @Test
+  void matchInfoDataGameMessage() {
+    HAL.initialize(500, 0);
+
+    MatchInfoData inMatchInfo = new MatchInfoData();
+    inMatchInfo.gameSpecificMessage = "Game Message";
+    DriverStationDataJNI.setMatchInfo(inMatchInfo);
+
+    MatchInfoData outMatchInfo = new MatchInfoData();
+    HAL.getMatchInfo(outMatchInfo);
+
     assertEquals("Game Message", outMatchInfo.gameSpecificMessage);
+  }
+
+  @Test
+  void matchInfoDataMatchnumber() {
+    HAL.initialize(500, 0);
+
+    MatchInfoData inMatchInfo = new MatchInfoData();
+    inMatchInfo.matchNumber = 174;
+    DriverStationDataJNI.setMatchInfo(inMatchInfo);
+
+    MatchInfoData outMatchInfo = new MatchInfoData();
+    HAL.getMatchInfo(outMatchInfo);
+
+    assertEquals(174, outMatchInfo.matchNumber);
+  }
+
+  @Test
+  void matchInfoDataMatchType() {
+    HAL.initialize(500, 0);
+
+    MatchInfoData inMatchInfo = new MatchInfoData();
+    inMatchInfo.matchType = MatchType.Qualification.ordinal();
+    DriverStationDataJNI.setMatchInfo(inMatchInfo);
+
+    MatchInfoData outMatchInfo = new MatchInfoData();
+    HAL.getMatchInfo(outMatchInfo);
+
+    assertEquals(MatchType.Qualification.ordinal(), outMatchInfo.matchType);
+  }
+
+  @Test
+  void matchInfoDataReplayNumber() {
+    HAL.initialize(500, 0);
+
+    MatchInfoData inMatchInfo = new MatchInfoData();
+    inMatchInfo.replayNumber = 191;
+    DriverStationDataJNI.setMatchInfo(inMatchInfo);
+
+    MatchInfoData outMatchInfo = new MatchInfoData();
+    HAL.getMatchInfo(outMatchInfo);
+
+    assertEquals(191, outMatchInfo.replayNumber);
   }
 }
