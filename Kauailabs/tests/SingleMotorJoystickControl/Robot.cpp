c5b79782e831f3a3573607433b6ca8b56f046b41
Kauai Labs <scott@kauailabs.com>
2018-08-02 14:31:07 -1000

    Added trivial Single Motor Joystick Test FRC program.

---
+++ Kauailabs/tests/SingleMotorJoystickControl/Robot.cpp
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <IterativeRobot.h>
+#include <Joystick.h>
+#include <Talon.h>
+#include <Timer.h>
+
+using namespace frc;
+
+class Robot : public IterativeRobot {
+public:
+	Robot() {
+	}
+
+	void AutonomousInit() override {
+	}
+
+	void AutonomousPeriodic() override {
+		Wait(0.02);
+	}
+
+	void TeleopInit() override {}
+
+	void TeleopPeriodic() override {
+		m_motor.Set(m_stick.GetX());
+		Wait(0.005);
+	}
+
+	void TestPeriodic() override {}
+
+private:
+	Talon m_motor{0};
+
+	Joystick m_stick{0};
+};
+
+START_ROBOT_CLASS(Robot)
