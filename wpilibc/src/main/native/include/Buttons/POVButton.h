f1ca643d967cb71e0c5e286a00645ba626b447c4
Redrield <redrield@gmail.com>
2018-10-20 17:31:31 -0400

    Add copyright notice

--- wpilibc/src/main/native/include/Buttons/POVButton.h
+++ wpilibc/src/main/native/include/Buttons/POVButton.h
@@ -1,10 +1,14 @@
-//
-// Created by redrield on 18/10/18.
-//
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 #pragma once
 
 // clang-format off
+
 #ifdef _MSC_VER
 #pragma message "warning: buttons/JoystickButton.h is deprecated; include frc/buttons/JoystickButton.h instead"
 #else
