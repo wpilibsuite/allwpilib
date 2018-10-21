36e698a2d0e5d9ecff653aef7b8a6e4b48290cc9
Redrield <redrield@gmail.com>
2018-10-20 22:44:49 -0400

    xd

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/buttons/POVButton.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/buttons/POVButton.java
@@ -14,14 +14,14 @@ import edu.wpi.first.wpilibj.GenericHID;
  */
 public class POVButton extends Button {
   private final GenericHID m_joystick;
-  private final int m_povNumber;
   private final int m_angle;
+  private final int m_povNumber;
 
   /**
    * Creates a POV button for triggering commands.
    *
    * @param joystick The GenericHID object that has the POV
-   * @param angle The desired angle (e.g. 90, 270)
+   * @param angle The desired angle in degrees(e.g. 90, 270)
    * @param povNumber The POV number (see {@link GenericHID#getPOV(int)})
    */
   public POVButton(GenericHID joystick, int angle, int povNumber) {
