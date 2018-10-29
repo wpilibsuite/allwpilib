de97b18b096f00590ab50109e6827b9b2109c1df
Thad House <thadhouse1@gmail.com>
2018-10-28 22:30:23 -0700

    Another hal header fix

--- hal/src/main/native/athena/AnalogInput.cpp
+++ hal/src/main/native/athena/AnalogInput.cpp
@@ -10,7 +10,7 @@
 #include <FRC_NetworkCommunication/AICalibration.h>
 #include <wpi/mutex.h>
 
-#include "AnalogAccumulator.h"
+#include "hal/AnalogAccumulator.h"
 #include "AnalogInternal.h"
 #include "HALInitializer.h"
 #include "PortsInternal.h"
