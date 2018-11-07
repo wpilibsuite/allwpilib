7fa1a65dab5c3b1745d80a367982ebe2c8f59def
Peter Johnson <johnson.peter@gmail.com>
2018-11-07 00:01:27 -0800

    cscore: Add Shutdown() function
    This allows ordered shutdown / destruction of sources and sinks prior to
    global singleton destruction.

--- cscore/src/main/native/cpp/jni/CameraServerJNI.cpp
+++ cscore/src/main/native/cpp/jni/CameraServerJNI.cpp
@@ -87,6 +87,8 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {
+  cs::Shutdown();
+
   JNIEnv* env;
   if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK)
     return;
