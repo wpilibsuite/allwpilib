fdc04d85002eaa49411240fd71fd402ec0b22da8
Thad House <thadhouse1@gmail.com>
2019-03-01 23:48:18 -0800

    Fixes receive side of LabVIEW USB streams
    They only accept %20 instead of + for parameters, they only send '\n' at the boundaries,
    And they include the -- when sending the initial boundary.  This solves those parts.
    
    This is not fully enough to fix shuffleboard and others, as the NT format for paths is not the correct path

--- cscore/src/main/native/cpp/HttpCameraImpl.cpp
+++ cscore/src/main/native/cpp/HttpCameraImpl.cpp
@@ -149,7 +149,7 @@ wpi::HttpConnection* HttpCameraImpl::DeviceStreamConnect(
 
   if (!m_active || !stream) return nullptr;
 
-  auto connPtr = wpi::make_unique<wpi::HttpConnection>(std::move(stream), 1);
+  auto connPtr = wpi::make_unique<wpi::HttpConnection>(std::move(stream), 2);
   wpi::HttpConnection* conn = connPtr.get();
 
   // update m_streamConn
@@ -189,6 +189,9 @@ wpi::HttpConnection* HttpCameraImpl::DeviceStreamConnect(
     std::tie(key, value) = keyvalue.split('=');
     if (key.trim() == "boundary") {
       value = value.trim().trim('"');  // value may be quoted
+      if (value.startswith("--")) {
+        value = value.slice(2, wpi::StringRef::npos);
+	  }
       boundary.append(value.begin(), value.end());
     }
   }
@@ -219,11 +222,16 @@ void HttpCameraImpl::DeviceStream(wpi::raw_istream& is,
     if (!FindMultipartBoundary(is, boundary, nullptr)) break;
 
     // Read the next two characters after the boundary (normally \r\n)
+	// Handle just \n for LabVIEW however
     char eol[2];
-    is.read(eol, 2);
+    is.read(eol, 1);
     if (!m_active || is.has_error()) break;
-    // End-of-stream is indicated with trailing --
-    if (eol[0] == '-' && eol[1] == '-') break;
+    if (eol[0] != '\n') {
+      is.read(eol + 1, 1);
+      if (!m_active || is.has_error()) break;
+      // End-of-stream is indicated with trailing --
+      if (eol[0] == '-' && eol[1] == '-') break;
+    }
 
     if (!DeviceStreamFrame(is, imageBuf))
       ++numErrors;
