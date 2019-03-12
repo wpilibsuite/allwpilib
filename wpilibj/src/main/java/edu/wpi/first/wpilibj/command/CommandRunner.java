3b12ea046918b07e560da13970ddee5d4f703ecd
Eli <emichaelbarnett@gmail.com>
2019-03-12 00:28:38 -0400

    first draft of commandrunner

---
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/command/CommandRunner.java
@@ -0,0 +1,56 @@
+package edu.wpi.first.wpilibj.command;
+
+import java.util.*;
+
+public class CommandRunner {
+
+    Map<ICommand, CommandState> m_scheduledCommands = new HashMap<>();
+    Map<Subsystem, ICommand> m_requirements = new HashMap<>();
+
+    public void scheduleCommand(ICommand command, boolean interruptable) {
+
+        Collection<Subsystem> requirements = command.getRequirements();
+
+        if (Collections.disjoint(m_requirements.keySet(), requirements)) {
+            command.initialize();
+            for (Subsystem requirement : requirements) {
+                CommandState scheduledCommand = new CommandState(interruptable);
+                m_scheduledCommands.put(command, scheduledCommand);
+                m_requirements.put(requirement, command);
+            }
+        } else {
+            boolean allInterruptable = true;
+            for (Subsystem requirement : requirements) {
+                if (m_requirements.keySet().contains(requirement)) {
+                    allInterruptable &= m_scheduledCommands.get(m_requirements.get(requirement)).isInterruptible();
+                }
+            }
+            if (allInterruptable) {
+                for (Subsystem requirement : requirements) {
+                    ICommand toInterrupt = m_requirements.get(requirement);
+                    toInterrupt.interrupted();
+                    m_scheduledCommands.remove(toInterrupt);
+                    m_requirements.keySet().removeAll(toInterrupt.getRequirements());
+                }
+            }
+        }
+    }
+
+    public void run() {
+        for (ICommand command : m_scheduledCommands.keySet()) {
+            command.execute();
+            if (command.isFinished()) {
+                command.end();
+                m_scheduledCommands.remove(command);
+                m_requirements.keySet().removeAll(command.getRequirements());
+            }
+        }
+    }
+
+    public void cancelCommand(ICommand command) {
+        command.interrupted();
+        m_scheduledCommands.remove(command);
+        m_requirements.keySet().removeAll(command.getRequirements());
+
+    }
+}
