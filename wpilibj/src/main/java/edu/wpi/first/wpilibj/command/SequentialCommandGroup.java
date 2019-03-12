dc8e334c11c1a15453e2a4659b8387c7a3fc8fb4
Eli <emichaelbarnett@gmail.com>
2019-03-12 03:55:24 -0400

    fix for repeated execution

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/command/SequentialCommandGroup.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/command/SequentialCommandGroup.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class SequentialCommandGroup implements ICommand {
 
-    private final Queue<ICommand> m_commands = new ArrayDeque<>();
+    private final List<ICommand> m_commands = new ArrayList<>();
+    private Queue<ICommand> m_commandQueue;
 
     public SequentialCommandGroup(ICommand... commands) {
         m_commands.addAll(List.of(commands));
@@ -12,23 +13,24 @@ public class SequentialCommandGroup implements ICommand {
 
     @Override
     public void initialize() {
-        m_commands.peek().initialize();
+        m_commandQueue = new ArrayDeque<>(m_commands);
+        m_commandQueue.peek().initialize();
     }
 
     @Override
     public void execute() {
-        m_commands.peek().execute();
-        if (m_commands.peek().isFinished()) {
-            m_commands.remove().end();
-            if(!m_commands.isEmpty()) {
-                m_commands.peek().initialize();
+        m_commandQueue.peek().execute();
+        if (m_commandQueue.peek().isFinished()) {
+            m_commandQueue.remove().end();
+            if (!m_commandQueue.isEmpty()) {
+                m_commandQueue.peek().initialize();
             }
         }
     }
 
     @Override
     public void interrupted() {
-        m_commands.peek().interrupted();
+        m_commandQueue.peek().interrupted();
     }
 
     @Override
@@ -37,13 +39,13 @@ public class SequentialCommandGroup implements ICommand {
 
     @Override
     public boolean isFinished() {
-        return m_commands.isEmpty();
+        return m_commandQueue.isEmpty();
     }
 
     @Override
     public Collection<Subsystem> getRequirements() {
         Collection<Subsystem> requirements = new HashSet<>();
-        m_commands.forEach(command -> requirements.addAll(command.getRequirements()));
+        m_commandQueue.forEach(command -> requirements.addAll(command.getRequirements()));
         return requirements;
     }
 }
