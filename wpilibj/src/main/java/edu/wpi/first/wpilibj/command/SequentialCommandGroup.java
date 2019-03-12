d1dc778617a063fd2307148af75dca51c95d0c20
Eli <emichaelbarnett@gmail.com>
2019-03-12 03:50:23 -0400

    write sequentialcommandgroup

---
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/command/SequentialCommandGroup.java
@@ -0,0 +1,46 @@
+package edu.wpi.first.wpilibj.command;
+
+import java.util.*;
+
+public class SequentialCommandGroup implements ICommand {
+
+    private final Queue<ICommand> m_commands = new ArrayDeque<>();
+
+    public SequentialCommandGroup(ICommand... commands) {
+        m_commands.addAll(List.of(commands));
+    }
+
+    @Override
+    public void initialize() {
+        m_commands.peek().initialize();
+    }
+
+    @Override
+    public void execute() {
+        m_commands.peek().execute();
+        if (m_commands.peek().isFinished()) {
+            m_commands.remove().end();
+        }
+    }
+
+    @Override
+    public void interrupted() {
+        m_commands.peek().interrupted();
+    }
+
+    @Override
+    public void end() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return m_commands.isEmpty();
+    }
+
+    @Override
+    public Collection<Subsystem> getRequirements() {
+        Collection<Subsystem> requirements = new HashSet<>();
+        m_commands.forEach(command -> requirements.addAll(command.getRequirements()));
+        return requirements;
+    }
+}
