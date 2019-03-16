6238f6918d7baf4f627e1d6dc74b406db724d5df
Eli <emichaelbarnett@gmail.com>
2019-03-16 10:57:10 -0400

    fix requirements

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/ConditionalCommand.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/ConditionalCommand.java
@@ -1,5 +1,6 @@
 package edu.wpi.first.wpilibj.experimental.command;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.BooleanSupplier;
 
@@ -9,11 +10,25 @@ public class ConditionalCommand implements Command {
   private final Command m_onFalse;
   private final BooleanSupplier m_condition;
   private Command m_selectedCommand;
+  private final Set<Subsystem> m_requirements = new HashSet<>();
 
+  /**
+   * Runs one of two commands, depending on the value of the given condition when this command is
+   * initialized.  Does not actually schedule the selected command - rather, the command is run
+   * through this command; this ensures that the command will behave as expected if used as
+   * part of a CommandGroup.  Requires the requirements of both commands, again to ensure proper
+   * functioning when used in a CommandGroup.
+   *
+   * @param onTrue the command to run if the condition is true
+   * @param onFalse the command to run if the condition is false
+   * @param condition the condition to determine which command to run
+   */
   public ConditionalCommand(Command onTrue, Command onFalse, BooleanSupplier condition) {
     m_onTrue = onTrue;
     m_onFalse = onFalse;
     m_condition = condition;
+    m_requirements.addAll(m_onTrue.getRequirements());
+    m_requirements.addAll(m_onFalse.getRequirements());
   }
 
   @Override
@@ -48,11 +63,7 @@ public class ConditionalCommand implements Command {
 
   @Override
   public Set<Subsystem> getRequirements() {
-    if (m_condition.getAsBoolean()) {
-      return m_onTrue.getRequirements();
-    } else {
-      return m_onFalse.getRequirements();
-    }
+    return m_requirements;
   }
 
   @Override
