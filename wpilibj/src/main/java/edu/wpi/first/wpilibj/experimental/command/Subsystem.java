ee0fffd038c0ee332896013991d5d0a20a04d260
Eli <emichaelbarnett@gmail.com>
2019-03-16 20:46:31 -0400

    decouple Subsystem from Sendable, move Sendable impl into SubsystemBase, rename accordingly

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/Subsystem.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/Subsystem.java
@@ -1,9 +1,6 @@
 package edu.wpi.first.wpilibj.experimental.command;
 
-import edu.wpi.first.wpilibj.Sendable;
-import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
-
-public interface Subsystem extends Sendable {
+public interface Subsystem {
 
   default void periodic() {}
 
@@ -15,33 +12,4 @@ public interface Subsystem extends Sendable {
     return CommandScheduler.getInstance().requiring(this);
   }
 
-  @Override
-  default String getName() {
-    return "";
-  }
-
-  @Override
-  default void setName(String name) {
-  }
-
-  @Override
-  default String getSubsystem() {
-    return getName();
-  }
-
-  @Override
-  default void setSubsystem(String subsystem) {
-  }
-
-  @Override
-  default void initSendable(SendableBuilder builder) {
-    builder.setSmartDashboardType("Subsystem");
-
-    builder.addBooleanProperty(".hasDefault", () -> getDefaultCommand() != null, null);
-    builder.addStringProperty(".default",
-        () -> getDefaultCommand() != null ? getDefaultCommand().getName() : "none", null);
-    builder.addBooleanProperty(".hasCommand", () -> getCurrentCommand() != null, null);
-    builder.addStringProperty(".command",
-        () -> getCurrentCommand() != null ? getCurrentCommand().getName() : "none", null);
-  }
 }
