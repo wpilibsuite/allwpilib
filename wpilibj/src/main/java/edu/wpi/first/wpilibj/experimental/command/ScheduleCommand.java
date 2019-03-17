a65ae29b2db76b81f7645a6ee8e6475d1ebb4e9a
Eli <emichaelbarnett@gmail.com>
2019-03-17 16:43:14 -0400

    make ScheduleCommand vararg, write more tests

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/ScheduleCommand.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/ScheduleCommand.java
@@ -1,26 +1,28 @@
 package edu.wpi.first.wpilibj.experimental.command;
 
 /**
- * Schedules a given command when this command is initialized.  Useful for forking off from
+ * Schedules the given commands when this command is initialized.  Useful for forking off from
  * CommandGroups.  Note that if run from a CommandGroup, the group will not know about the status
- * of the scheduled command, and will treat this command as finishing instantly.
+ * of the scheduled commands, and will treat this command as finishing instantly.
  */
 public class ScheduleCommand extends SendableCommandBase {
 
-  private final Command m_toSchedule;
+  private final Command[] m_toSchedule;
 
   /**
-   * Creates a new ScheduleCommand that schedules the given command when initialized.
+   * Creates a new ScheduleCommand that schedules the given commands when initialized.
    *
-   * @param toSchedule the command to schedule
+   * @param toSchedule the commands to schedule
    */
-  public ScheduleCommand(Command toSchedule) {
+  public ScheduleCommand(Command... toSchedule) {
     m_toSchedule = toSchedule;
   }
 
   @Override
   public void initialize() {
-    m_toSchedule.schedule();
+    for (Command command : m_toSchedule) {
+      command.schedule();
+    }
   }
 
   @Override
