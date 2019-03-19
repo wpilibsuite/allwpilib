6c0e4a9c571e0d600d08ba78e3f69bebdad55f08
Eli <emichaelbarnett@gmail.com>
2019-03-18 20:12:00 -0400

    add runnable bindings to button and trigger, update docs

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/buttons/Button.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/buttons/Button.java
@@ -19,19 +19,36 @@ import edu.wpi.first.wpilibj.experimental.command.Command;
  * operator interface as a common use case of the more generalized Trigger objects. This is a simple
  * wrapper around Trigger with the method names renamed to fit the Button object use.
  */
+@SuppressWarnings("PMD.TooManyMethods")
 public abstract class Button extends Trigger {
 
   /**
    * Starts the given command whenever the button is newly pressed.
    *
-   * @param command the command to start
+   * @param command       the command to start
+   * @param interruptible whether the command is interruptible
    */
   public void whenPressed(final Command command, boolean interruptible) {
     whenActive(command, interruptible);
   }
 
+  /**
+   * Starts the given command whenever the button is newly pressed. The command is set to be
+   * interruptible.
+   *
+   * @param command the command to start
+   */
   public void whenPressed(final Command command) {
     whenActive(command);
+  }
+
+  /**
+   * Runs the given runnable whenever the button is newly pressed.
+   *
+   * @param toRun the runnable to run
+   */
+  public void whenPressed(final Runnable toRun) {
+    whenActive(toRun);
   }
 
   /**
@@ -40,39 +57,101 @@ public abstract class Button extends Trigger {
    * {@link Command#schedule(boolean)} will be called repeatedly while the button is held, and will
    * be canceled when the button is released.
    *
-   * @param command the command to start
+   * @param command       the command to start
+   * @param interruptible whether the command is interruptible
    */
   public void whileHeld(final Command command, boolean interruptible) {
-    whileActive(command, interruptible);
+    whileActiveContinuous(command, interruptible);
   }
 
+  /**
+   * Constantly starts the given command while the button is held.
+   *
+   * {@link Command#schedule(boolean)} will be called repeatedly while the button is held, and will
+   * be canceled when the button is released.  The command is set to be interruptible.
+   *
+   * @param command the command to start
+   */
   public void whileHeld(final Command command) {
-    whileActive(command);
+    whileActiveContinuous(command);
+  }
+
+  /**
+   * Constantly runs the given runnable while the button is held.
+   *
+   * @param toRun the runnable to run
+   */
+  public void whileHeld(final Runnable toRun) {
+    whileActiveContinuous(toRun);
+  }
+
+  /**
+   * Starts the given command when the button is first pressed, and cancels it when it is released,
+   * but does not start it again if it ends or is otherwise interrupted.
+   *
+   * @param command       the command to start
+   * @param interruptible whether the command is interruptible
+   */
+  public void whenHeld(final Command command, boolean interruptible) {
+    whileActiveOnce(command, interruptible);
+  }
+
+  /**
+   * Starts the given command when the button is first pressed, and cancels it when it is released,
+   * but does not start it again if it ends or is otherwise interrupted.  The command is set
+   * to be interruptible.
+   *
+   * @param command the command to start
+   */
+  public void whenHeld(final Command command) {
+    whileActiveOnce(command, true);
   }
 
 
   /**
    * Starts the command when the button is released.
    *
-   * @param command the command to start
+   * @param command       the command to start
+   * @param interruptible whether the command is interruptible
    */
   public void whenReleased(final Command command, boolean interruptible) {
     whenInactive(command, interruptible);
   }
 
+  /**
+   * Starts the command when the button is released.  The command is set to be interruptible.
+   *
+   * @param command the command to start
+   */
   public void whenReleased(final Command command) {
     whenInactive(command);
   }
 
   /**
+   * Runs the given runnable when the button is released.
+   *
+   * @param toRun the runnable to run
+   */
+  public void whenReleased(final Runnable toRun) {
+    whenInactive(toRun);
+  }
+
+  /**
    * Toggles the command whenever the button is pressed (on then off then on).
    *
-   * @param command the command to start
+   * @param command       the command to start
+   * @param interruptible whether the command is interruptible
    */
   public void toggleWhenPressed(final Command command, boolean interruptible) {
     toggleWhenActive(command, interruptible);
   }
 
+  /**
+   * Toggles the command whenever the button is pressed (on then off then on).  The command is
+   * set to be interruptible.
+   *
+   * @param command the command to start
+   */
   public void toggleWhenPressed(final Command command) {
     toggleWhenActive(command);
   }
