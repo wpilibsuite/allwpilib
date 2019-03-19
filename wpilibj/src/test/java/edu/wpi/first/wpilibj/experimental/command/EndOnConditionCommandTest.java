2bcda5c2d0e77f1430648c08cc54041ee1343093
Eli <emichaelbarnett@gmail.com>
2019-03-19 11:09:45 -0400

    create tests for endonconditioncommand and withinterruptcondition

---
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/experimental/command/EndOnConditionCommandTest.java
@@ -0,0 +1,40 @@
+package edu.wpi.first.wpilibj.experimental.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EndOnConditionCommandTest extends CommandTestBase {
+  @Test
+  void endOnConditionTest() {
+    CommandScheduler scheduler = new CommandScheduler();
+
+    ConditionHolder condition = new ConditionHolder();
+
+    Command command = new EndOnConditionCommand(condition::getCondition);
+
+    scheduler.scheduleCommand(command, true);
+    scheduler.run();
+    assertTrue(scheduler.isScheduled(command));
+    condition.setCondition(true);
+    scheduler.run();
+    assertFalse(scheduler.isScheduled(command));
+  }
+
+  @Test
+  void withInterruptConditionTest() {
+    CommandScheduler scheduler = new CommandScheduler();
+
+    ConditionHolder condition = new ConditionHolder();
+
+    Command command = new WaitCommand(10).withInterruptCondition(condition::getCondition);
+
+    scheduler.scheduleCommand(command, true);
+    scheduler.run();
+    assertTrue(scheduler.isScheduled(command));
+    condition.setCondition(true);
+    scheduler.run();
+    assertFalse(scheduler.isScheduled(command));
+  }
+}
