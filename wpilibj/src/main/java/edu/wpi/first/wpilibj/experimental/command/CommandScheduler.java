e189a98914552b13bd40d383636615da4e533345
Eli <emichaelbarnett@gmail.com>
2019-03-18 21:46:33 -0400

    PMD

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/CommandScheduler.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/CommandScheduler.java
@@ -7,7 +7,16 @@
 
 package edu.wpi.first.wpilibj.experimental.command;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
