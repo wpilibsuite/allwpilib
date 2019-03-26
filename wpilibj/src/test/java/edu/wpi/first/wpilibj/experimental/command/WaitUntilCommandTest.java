bac5a9702482f99a2fc6a919257dc1b5ba686f6e
Eli <emichaelbarnett@gmail.com>
2019-03-26 10:32:23 -0400

    minor cleanup

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/experimental/command/WaitUntilCommandTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/experimental/command/WaitUntilCommandTest.java
@@ -5,30 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class EndOnConditionCommandTest extends CommandTestBase {
+class WaitUntilCommandTest extends CommandTestBase {
   @Test
-  void endOnConditionTest() {
+  void waitUntilTest() {
     CommandScheduler scheduler = new CommandScheduler();
 
     ConditionHolder condition = new ConditionHolder();
 
     Command command = new WaitUntilCommand(condition::getCondition);
-
-    scheduler.scheduleCommand(command, true);
-    scheduler.run();
-    assertTrue(scheduler.isScheduled(command));
-    condition.setCondition(true);
-    scheduler.run();
-    assertFalse(scheduler.isScheduled(command));
-  }
-
-  @Test
-  void withInterruptConditionTest() {
-    CommandScheduler scheduler = new CommandScheduler();
-
-    ConditionHolder condition = new ConditionHolder();
-
-    Command command = new WaitCommand(10).interruptOn(condition::getCondition);
 
     scheduler.scheduleCommand(command, true);
     scheduler.run();
