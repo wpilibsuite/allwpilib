22139386f0c4f760bb1b1dc643f37757f238e964
Eli <emichaelbarnett@gmail.com>
2019-03-25 22:36:06 -0400

    add perpetually() decorator

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/PerpetualCommand.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/PerpetualCommand.java
@@ -1,7 +1,12 @@
 package edu.wpi.first.wpilibj.experimental.command;
 
 /**
- * A command that runs another command in perpetuity, ignoring that command's end conditions.
+ * A command that runs another command in perpetuity, ignoring that command's end conditions.  While
+ * this class does not extend {@link CommandGroupBase}, it is still considered a CommandGroup, as
+ * it allows one to compose another command within it; that command is added to the list of grouped
+ * commands, and the ordinary restrictions apply.
+ *
+ * <p>As a rule, CommandGroups require the union of the requirements of their component commands.
  */
 public class PerpetualCommand extends SendableCommandBase {
 
@@ -16,6 +21,7 @@ public class PerpetualCommand extends SendableCommandBase {
   public PerpetualCommand(Command command) {
     m_command = command;
     m_requirements.addAll(command.getRequirements());
+    CommandGroupBase.registerGroupedCommands(command);
   }
 
   @Override
