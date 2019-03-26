d3a44daec812ca74dbcb0eb562560537333848df
Eli <emichaelbarnett@gmail.com>
2019-03-25 22:36:06 -0400

    add perpetually() decorator

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/Command.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/experimental/command/Command.java
@@ -205,6 +205,22 @@ public interface Command {
   }
 
   /**
+   * Decorates this command to run perpetually, ignoring its ordinary end conditions.  The decorated
+   * command can still be interrupted or canceled.
+   *
+   * <p>Note: This decorator works by composing this command within a CommandGroup.  The command
+   * cannot be used independently after being decorated, or be re-decorated with a different
+   * decorator, unless it is manually cleared from the list of grouped commands with
+   * {@link CommandGroupBase#clearGroupedCommand(Command)}.  The decorated command can, however, be
+   * further decorated without issue.
+   *
+   * @return the decorated command
+   */
+  default Command perpetually() {
+    return new PerpetualCommand(this);
+  }
+
+  /**
    * Schedules this command.
    *
    * @param interruptible whether this command can be interrupted by another command that
