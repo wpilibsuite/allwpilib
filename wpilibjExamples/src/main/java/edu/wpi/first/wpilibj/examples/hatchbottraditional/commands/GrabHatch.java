c05cd88ea751764b812ef267d37efd08553639d9
Eli <emichaelbarnett@gmail.com>
2019-04-05 12:34:30 -0400

    add static factories for command groups, for convenience

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/hatchbottraditional/commands/GrabHatch.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/hatchbottraditional/commands/GrabHatch.java
@@ -1,13 +1,30 @@
 package edu.wpi.first.wpilibj.examples.hatchbottraditional.commands;
 
 import edu.wpi.first.wpilibj.examples.hatchbottraditional.subsystems.HatchSubsystem;
-import edu.wpi.first.wpilibj.experimental.command.InstantCommand;
+import edu.wpi.first.wpilibj.experimental.command.SendableCommandBase;
 
 /**
- * A command that grabs the hatch.
+ * A simple command that grabs a hatch with the {@link HatchSubsystem}.  Written explicitly for
+ * pedagogical purposes.  Actual code should inline a command this simple with
+ * {@link edu.wpi.first.wpilibj.experimental.command.InstantCommand}.
  */
-public class GrabHatch extends InstantCommand {
+public class GrabHatch extends SendableCommandBase {
+
+  // The subsystem the command runs on
+  private final HatchSubsystem m_hatchSubsystem;
+
   public GrabHatch(HatchSubsystem subsystem) {
-    super(subsystem::grabHatch, subsystem);
+    m_hatchSubsystem = subsystem;
+    addRequirements(m_hatchSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    m_hatchSubsystem.grabHatch();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
   }
 }
