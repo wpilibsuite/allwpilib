d31da947158739f1370557cf72a880eebfe9f0c2
Eli <emichaelbarnett@gmail.com>
2019-04-05 12:34:30 -0400

    add static factories for command groups, for convenience

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/hatchbottraditional/commands/DefaultDrive.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/hatchbottraditional/commands/DefaultDrive.java
@@ -3,11 +3,28 @@ package edu.wpi.first.wpilibj.examples.hatchbottraditional.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.examples.hatchbottraditional.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj.experimental.command.RunCommand;
+import edu.wpi.first.wpilibj.experimental.command.SendableCommandBase;
 
-public class DefaultDrive extends RunCommand {
+/**
+ * A command to drive the robot with joystick input (passed in as {@link DoubleSupplier}s).
+ * Written explicitly for pedagogical purposes - actual code should inline a command this simple
+ * with {@link edu.wpi.first.wpilibj.experimental.command.RunCommand}.
+ */
+public class DefaultDrive extends SendableCommandBase {
 
-  public DefaultDrive(DriveSubsystem drive, DoubleSupplier fwd, DoubleSupplier rot){
-    super(() -> drive.arcadeDrive(fwd.getAsDouble(), rot.getAsDouble()), drive);
+  private final DriveSubsystem m_drive;
+  private final DoubleSupplier m_forward;
+  private final DoubleSupplier m_rotation;
+
+  public DefaultDrive(DriveSubsystem subsystem, DoubleSupplier forward, DoubleSupplier rotation){
+    m_drive = subsystem;
+    m_forward = forward;
+    m_rotation = rotation;
+    addRequirements(m_drive);
+  }
+
+  @Override
+  public void execute() {
+    m_drive.arcadeDrive(m_forward.getAsDouble(), m_rotation.getAsDouble());
   }
 }
