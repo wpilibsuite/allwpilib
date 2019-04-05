89ebc3ab12ff45ce3b1e6cd96abe5103a1331991
Thad House <thadhouse1@gmail.com>
2019-04-05 16:19:59 -0700

    Add a parallel repeating TCP connector

---
+++ wpiutil/src/main/native/cpp/uv/ParallelRepeatTCPConnector.cpp
@@ -0,0 +1,117 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "wpi/uv/ParallelRepeatTCPConnector.h"
+
+namespace wpi {
+namespace uv {
+
+void ParallelRepeatTCPConnector::Start() {
+  auto async = Async<>::Create(m_loop);
+  m_asyncEnter = async;
+  async->wakeup.connect([self = shared_from_this()] {
+    int oldConnectCount = 0;
+    {
+      std::lock_guard<wpi::mutex> lock(self->m_changeAddressesMutex);
+      oldConnectCount = self->m_connectingAddresses.size();
+      self->m_connectingAddresses.clear();
+      self->m_connectingAddresses.append(self->m_changeAddresses.begin(),
+                                         self->m_changeAddresses.end());
+    }
+    if (oldConnectCount == 0) {
+      self->Disconnect();
+      return;
+    }
+    if (!self->m_disconnectOnNewAddresses) return;
+    self->Disconnect();
+  });
+
+  async->closed.connect([self = shared_from_this()] {
+    self->closed();
+    self->m_this = nullptr;
+    self->Disconnect();
+  });
+}
+
+void ParallelRepeatTCPConnector::Close() {
+  if (auto lock = m_asyncEnter.lock()) {
+    lock->Close();
+  } else {
+    // Async is closed. Do the equivelent but make sure to hold this.
+    auto localThis = shared_from_this();
+    m_this = nullptr;
+    Disconnect();
+  }
+}
+
+void ParallelRepeatTCPConnector::Connect() {
+  if (!m_this || m_connectingAddresses.empty()) {
+    return;
+  }
+
+  auto timer = Timer::Create(m_loop);
+  auto timerLocal = timer.get();
+
+  m_attemptingConnections.reserve(m_connectingAddresses.size());
+  for (auto&& attempt : m_connectingAddresses) {
+    // Create a TCP per address
+    auto tcp = Tcp::Create(m_loop);
+    auto tcpLocal = tcp.get();
+    m_attemptingConnections.emplace_back(tcpLocal);
+    auto addrInfoReq = std::make_shared<GetAddrInfoReq>();
+    std::weak_ptr<GetAddrInfoReq> addrInfoReqWeak = addrInfoReq;
+
+    tcp->closed.connect(
+        [ self = shared_from_this(), timer, tcpLocal, addrInfoReqWeak ] {
+          auto found = std::find(self->m_attemptingConnections.begin(),
+                                 self->m_attemptingConnections.end(), tcpLocal);
+          if (found != self->m_attemptingConnections.end()) {
+            self->m_attemptingConnections.erase(found);
+          }
+          if (auto lock = addrInfoReqWeak.lock()) {
+            lock->Cancel();
+          }
+          if (self->m_attemptingConnections.empty()) {
+            self->Disconnect();
+          }
+        });
+
+    addrInfoReq->resolved.connect([ tcp, self = shared_from_this(),
+                                    timer ](const addrinfo& info) {
+      if (tcp->IsClosing()) return;
+      std::array<char, 16> name;
+      uv_ip4_name(reinterpret_cast<struct sockaddr_in*>(info.ai_addr),
+                  name.data(), 16);
+      tcp->Connect(*info.ai_addr, [ self, timer, tcpLocal = tcp.get(), name ] {
+        timer->Close();
+        for (auto&& toClose : self->m_attemptingConnections) {
+          if (toClose != tcpLocal) {
+            toClose->Close();
+          }
+        }
+        self->m_attemptingConnections.clear();
+        self->m_attemptingConnections.emplace_back(tcpLocal);
+        self->connected(*tcpLocal, StringRef{name.data(), name.size()});
+      });
+    });
+
+    addrinfo hints;
+    hints.ai_family = PF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = 0;
+
+    GetAddrInfo(m_loop, addrInfoReq, attempt.first, Twine{attempt.second},
+                &hints);
+  }
+
+  timer->timeout.connect([self = shared_from_this()] { self->Disconnect(); });
+
+  timer->Start(m_reconnectTime);
+}
+}  // namespace uv
+}  // namespace wpi
---
+++ wpiutil/src/main/native/include/wpi/uv/ParallelRepeatTCPConnector.h
@@ -0,0 +1,158 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#ifndef WPIUTIL_WPI_UV_PARALLELREPEATTCPCONNECTOR_H_
+#define WPIUTIL_WPI_UV_PARALLELREPEATTCPCONNECTOR_H_
+
+#include <stdint.h>
+#include <wpi/uv/Async.h>
+#include <wpi/uv/GetAddrInfo.h>
+#include <wpi/uv/Loop.h>
+#include <wpi/uv/Tcp.h>
+#include <wpi/uv/Timer.h>
+#include <wpi/uv/util.h>
+
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace wpi {
+namespace uv {
+
+/**
+ * Parallel Repeating TCP Connector
+ * This is used to connect to a TCP server from a client, with multiple IP
+ * addresses, and repeats on failure or connection closed.
+ */
+class ParallelRepeatTCPConnector
+    : public std::enable_shared_from_this<ParallelRepeatTCPConnector> {
+  struct private_init {};
+
+ public:
+  ParallelRepeatTCPConnector(Loop& loop, bool disconnectOnNewAddresses,
+                          Timer::Time reconnectTime, const private_init&)
+      : m_loop{loop},
+        m_disconnectOnNewAddresses{disconnectOnNewAddresses},
+        m_reconnectTime{reconnectTime} {}
+
+  /**
+   * Create a Parallel Repeat Connector.
+   *
+   * @param loop Loop object where this handle runs.
+   * @param disconnectOnNewAddress true to force a disconnect on an address
+   * change.
+   * @param reconnectTime the time to wait before reattempting a connection.
+   */
+  static std::shared_ptr<ParallelRepeatTCPConnector> Create(
+      Loop& loop, bool disconnectOnNewAddress, Timer::Time reconnectTime) {
+    auto ptr = std::make_shared<ParallelRepeatTCPConnector>(
+        loop, disconnectOnNewAddress, reconnectTime, private_init{});
+    ptr->m_this = ptr;
+    return ptr;
+  }
+
+  /**
+   * Create a Parallel Repeat Connector.
+   *
+   * @param loop Loop object where this handle runs.
+   * @param disconnectOnNewAddress true to force a disconnect on an address
+   * change.
+   * @param reconnectTime the time to wait before reattempting a connection.
+   */
+  static std::shared_ptr<ParallelRepeatTCPConnector> Create(
+      const std::shared_ptr<Loop>& loop, bool disconnectOnNewAddress,
+      Timer::Time reconnectTime) {
+    return Create(*loop, disconnectOnNewAddress, reconnectTime);
+  }
+
+  /**
+   * Start the repeating connection.
+   *
+   * This must be called before any addresses are added.
+   */
+  void Start();
+
+  /**
+   * Close the repeating connection.
+   */
+  void Close();
+
+  /**
+   * Signal generated when this connector is closed.
+   */
+  sig::Signal<> closed;
+
+  /**
+   * Signal generated when the client connects.
+   *
+   * The string parameter is the IP address of the server.
+   */
+  sig::Signal<Tcp&, StringRef> connected;
+
+  /**
+   * Add an address to connect to.
+   *
+   * This does not remove any existing addresses.
+   */
+  void AddAddress(const Twine& addr, uint32_t port) {
+    {
+      std::lock_guard<wpi::mutex> lock(m_changeAddressesMutex);
+      m_changeAddresses.emplace_back(std::make_pair(addr.str(), port));
+    }
+    if (auto lock = m_asyncEnter.lock()) {
+      lock->Send();
+    }
+  }
+
+  /**
+   * Add a list of addresses to connect to.
+   *
+   * This list will replace the existing list of addresses.
+   */
+  void SetAddresses(ArrayRef<std::pair<std::string, uint32_t>> addresses) {
+    {
+      std::lock_guard<wpi::mutex> lock(m_changeAddressesMutex);
+      m_changeAddresses.clear();
+      m_changeAddresses.append(addresses.begin(), addresses.end());
+    }
+    if (auto lock = m_asyncEnter.lock()) {
+      lock->Send();
+    }
+  }
+
+ private:
+  void Connect();
+
+  void Disconnect() {
+    if (m_attemptingConnections.empty()) {
+      Connect();
+      return;
+    }
+    for (auto&& tcp : m_attemptingConnections) {
+      tcp->Close();
+    }
+  }
+
+  Loop& m_loop;
+  bool m_disconnectOnNewAddresses;
+  Timer::Time m_reconnectTime;
+
+  SmallVector<Tcp*, 4> m_attemptingConnections;
+
+  std::weak_ptr<Async<>> m_asyncEnter;
+  SmallVector<std::pair<std::string, uint32_t>, 1> m_connectingAddresses;
+
+  wpi::mutex m_changeAddressesMutex;
+  SmallVector<std::pair<std::string, uint32_t>, 1> m_changeAddresses;
+
+  std::shared_ptr<ParallelRepeatTCPConnector> m_this;
+};
+
+}  // namespace uv
+}  // namespace wpi
+
+#endif  // WPIUTIL_WPI_UV_PARALLELREPEATTCPCONNECTOR_H_
