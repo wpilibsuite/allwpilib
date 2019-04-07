70d5d0d39fa884973fe8a8e3f1ba79ab5be3505e
Thad House <thadhouse1@gmail.com>
2019-04-07 16:46:15 -0700

    Add Pipe connector

---
+++ wpiutil/src/main/native/cpp/uv/RepeatPipeConnector.cpp
@@ -0,0 +1,78 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "wpi/uv/RepeatPipeConnector.h"
+
+namespace wpi {
+namespace uv {
+void RepeatPipeConnector::Start() {
+  auto async = Async<>::Create(m_loop);
+  m_asyncEnter = async;
+  async->wakeup.connect([self = shared_from_this()] {
+    {
+      std::lock_guard<wpi::mutex> lock(self->m_addressMutex);
+      self->m_currentAddress = self->m_changeAddress;
+    }
+    self->Disconnect();
+  });
+
+  async->closed.connect([self = shared_from_this()] {
+    self->closed();
+    self->m_this = nullptr;
+    self->Disconnect();
+  });
+}
+
+void RepeatPipeConnector::Close() {
+  if (auto lock = m_asyncEnter.lock()) {
+    lock->Close();
+  } else {
+    // Async is closed. Do the equivelent but make sure to hold this.
+    auto localThis = shared_from_this();
+    closed();
+    m_this = nullptr;
+    Disconnect();
+  }
+}
+
+void RepeatPipeConnector::Connect() {
+  if (!m_this || m_currentAddress.empty()) {
+    return;
+  }
+
+  auto timer = Timer::Create(m_loop);
+
+  auto pipe = Pipe::Create(m_loop, m_ipc);
+  auto pipeLocal = pipe.get();
+  m_connectingPipe = pipeLocal;
+
+  pipe->closed.connect([ self = shared_from_this(), timer, pipeLocal ] {
+    self->m_connectingPipe = nullptr;
+    if (!timer->IsClosing()) {
+      timer->Close();
+    }
+    self->Disconnect();
+  });
+
+  pipe->Connect(m_currentAddress,
+                [ pipeLocal, self = shared_from_this(), timer ] {
+                  if (!timer->IsClosing()) {
+                    timer->Close();
+                  }
+                  self->connected(*pipeLocal);
+                });
+
+  timer->timeout.connect(
+      [ self = shared_from_this(), timerLocal = timer.get() ] {
+        timerLocal->Close();
+        self->Disconnect();
+      });
+
+  timer->Start(m_reconnectTime);
+}
+}  // namespace uv
+}  // namespace wpi
---
+++ wpiutil/src/main/native/include/wpi/uv/RepeatPipeConnector.h
@@ -0,0 +1,95 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#ifndef WPIUTIL_WPI_UV_REPEATPIPECONNECTOR_H_
+#define WPIUTIL_WPI_UV_REPEATPIPECONNECTOR_H_
+
+#include <wpi/raw_ostream.h>
+#include <wpi/uv/Async.h>
+#include <wpi/uv/Loop.h>
+#include <wpi/uv/Pipe.h>
+#include <wpi/uv/Timer.h>
+#include <wpi/uv/util.h>
+
+#include <memory>
+#include <string>
+
+namespace wpi {
+namespace uv {
+
+class RepeatPipeConnector
+    : public std::enable_shared_from_this<RepeatPipeConnector> {
+  struct private_init {};
+
+ public:
+  RepeatPipeConnector(Loop& loop, Timer::Time reconnectTime, bool ipc,
+                      const private_init&)
+      : m_loop{loop}, m_ipc{ipc}, m_reconnectTime{reconnectTime} {}
+
+  static std::shared_ptr<RepeatPipeConnector> Create(Loop& loop,
+                                                     Timer::Time reconnectTime,
+                                                     bool ipc = false) {
+    auto ptr = std::make_shared<RepeatPipeConnector>(loop, reconnectTime, ipc,
+                                                     private_init{});
+    ptr->m_this = ptr;
+    return ptr;
+  }
+
+  static std::shared_ptr<RepeatPipeConnector> Create(
+      const std::shared_ptr<Loop>& loop, Timer::Time reconnectTime,
+      bool ipc = false) {
+    return Create(*loop, reconnectTime, ipc);
+  }
+
+  void Start();
+
+  void Close();
+
+  sig::Signal<> closed;
+
+  sig::Signal<Pipe&> connected;
+
+  void SetAddress(const Twine& addr) {
+    {
+      std::lock_guard<wpi::mutex> lock(m_addressMutex);
+      m_changeAddress = addr.str();
+    }
+    if (auto lock = m_asyncEnter.lock()) {
+      lock->Send();
+    }
+  }
+
+ private:
+  void Connect();
+
+  void Disconnect() {
+    if (!m_connectingPipe) {
+      Connect();
+      return;
+    }
+    m_connectingPipe->Close();
+  }
+
+  std::weak_ptr<Async<>> m_asyncEnter;
+
+  Timer::Time m_reconnectTime;
+
+  std::string m_currentAddress;
+
+  wpi::mutex m_addressMutex;
+  std::string m_changeAddress;
+
+  Pipe* m_connectingPipe{nullptr};
+  Loop& m_loop;
+  bool m_ipc;
+  std::shared_ptr<RepeatPipeConnector> m_this;
+};
+
+}  // namespace uv
+}  // namespace wpi
+
+#endif  // WPIUTIL_WPI_UV_REPEATPIPECONNECTOR_H_
