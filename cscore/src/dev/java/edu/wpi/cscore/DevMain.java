a55df4d03b9865f682be7677e6255e65a1a6b930
Thad House <thadhouse1@gmail.com>
2019-05-05 09:52:35 -0700

    Move tmp examples to a non building folder

--- cscore/src/dev/java/edu/wpi/cscore/DevMain.java
+++ cscore/src/dev/java/edu/wpi/cscore/DevMain.java
@@ -7,34 +7,9 @@
 
 package edu.wpi.cscore;
 
-import java.nio.ByteBuffer;
-
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-
-import edu.wpi.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.wpiutil.RuntimeDetector;
 
 public final class DevMain {
-  public static int GetCVFormat(PixelFormat pixelFormat) {
-    int type = 0;
-    switch (pixelFormat) {
-    case kYUYV:
-    case kRGB565:
-      type = CvType.CV_8UC2;
-      break;
-    case kBGR:
-      type = CvType.CV_8UC3;
-      break;
-    case kGray:
-    case kMJPEG:
-    default:
-      type = CvType.CV_8UC1;
-      break;
-    }
-    return type;
-  }
-
   /**
    * Main method.
    */
@@ -42,88 +17,6 @@ public final class DevMain {
     System.out.println("Hello World!");
     System.out.println(RuntimeDetector.getPlatformPath());
     System.out.println(CameraServerJNI.getHostname());
-
-    UsbCamera usbCameraObj = new UsbCamera("Cam", 0);
-
-    CameraServerCvJNI.forceLoad();
-
-
-    usbCameraObj.setPixelFormat(PixelFormat.kYUYV);
-
-    RawCVMatSink rawSink = new RawCVMatSink("Sink");
-
-    MjpegServer server = new MjpegServer("Hello", 1181);
-
-    RawCVMatSource cvSource = new RawCVMatSource("CvSource", PixelFormat.kGray, 640, 360, 30);
-
-    server.setSource(cvSource);
-
-    rawSink.setSource(usbCameraObj);
-
-    Mat mat = new Mat();
-
-    while (true) {
-      if (rawSink.grabFrame(mat) > 0) {
-        cvSource.putFrame(mat);
-      }
-    }
-
-
-
-    // int usbCamera = usbCameraObj.m_handle;
-
-    // //int usbCamera = CameraServerJNI.createUsbCameraDev("Camera", 0);
-
-    // int rawSink = CameraServerJNI.createRawSink("Raw Sink");
-
-    // int mjpeg = CameraServerJNI.createMjpegServer("Camera", "", 1181);
-
-    // int cvSource = CameraServerJNI.createCvSource("Raw Sink", PixelFormat.kGray.getValue(), 640, 360, 30);
-
-    // int mjpeg2 = CameraServerJNI.createMjpegServer("Camera", "", 1182);
-
-    // int rawSource = CameraServerJNI.createRawSource("Raw Sink", PixelFormat.kGray.getValue(), 640, 360, 30);
-
-    // CameraServerJNI.setSinkSource(rawSink, usbCamera);
-
-    // CameraServerJNI.setSinkSource(mjpeg, cvSource);
-    // CameraServerJNI.setSinkSource(mjpeg2, rawSource);
-
-    // System.out.println(cvSource);
-
-    // //CameraServerJNI.setSourceVideoMode(usbCamera, PixelFormat.kBGR.getValue(), 640, 360, 30);
-
-
-    // RawFrame raw = new RawFrame();
-
-    // ByteBuffer origByteBuffer = null;
-
-    // Mat mat = null;
-
-    // int width = 0;
-    // int height = 0;
-    // int pixelFormat = 0;
-
-
-    // while (true) {
-    //   raw.setPixelFormat(PixelFormat.kBGR.getValue());
-    //   raw.setWidth(0);
-    //   raw.setHeight(0);
-    //   long timeout = CameraServerJNI.grabSinkFrame(rawSink, raw);
-    //   if (timeout > 0) {
-    //     if (raw.getDataByteBuffer() != origByteBuffer || width != raw.getWidth() || height != raw.getHeight() || pixelFormat != raw.getPixelFormat()) {
-    //       origByteBuffer = raw.getDataByteBuffer();
-    //       height = raw.getHeight();
-    //       width = raw.getWidth();
-    //       pixelFormat = raw.getPixelFormat();
-    //       mat = new Mat(raw.getHeight(), raw.getWidth(), GetCVFormat(VideoMode.getPixelFormatFromInt(pixelFormat)), origByteBuffer);
-    //     }
-    //     CameraServerJNI.putSourceFrame(cvSource, mat.nativeObj);
-
-    //     CameraServerJNI.putRawSourceFrame(rawSource, raw);
-    //   }
-    // }
-
   }
 
   private DevMain() {
--- cscore/src/dev/native/cpp/main.cpp
+++ cscore/src/dev/native/cpp/main.cpp
@@ -7,52 +7,10 @@
 
 #include <iostream>
 
-#include <opencv/cv.hpp>
-
 #include "cscore.h"
-#include "cscore_raw.h"
 
 using namespace cv;
 
 int main() {
-  CS_Status status = 0;
-
-  auto usbSource = CS_CreateUsbCameraDev("Camera", 0, &status);
-
-  auto sinkHandle = CS_CreateRawSink("RawSink", &status);
-
-  CS_VideoMode videoMode;
-  videoMode.fps = 30;
-  videoMode.width = 640;
-  videoMode.height = 360;
-  videoMode.pixelFormat = CS_PixelFormat::CS_PIXFMT_GRAY;
-
-  auto sourceHandle = CS_CreateRawSource("RawSource", &videoMode, &status);
-
-  auto mjpegHandle = CS_CreateMjpegServer("Server", "", 1181, &status);
-
-  CS_SetSinkSource(mjpegHandle, sourceHandle, &status);
-
-  CS_SetSinkSource(sinkHandle, usbSource, &status);
-
-  namedWindow("window", WINDOW_AUTOSIZE);
-
-  cs::RawFrame frame;
-  frame.pixelFormat = CS_PIXFMT_UNKNOWN;
-  // frame->pixelFormat = CS_PixelFormat::CS_PIXFMT_BGR;
-
-  while (true) {
-    auto timeout = cs::GrabSinkFrameTimeout(sinkHandle, frame, 0.225, &status);
-
-    if (timeout > 0) {
-      cs::PutSourceFrame(sourceHandle, frame, &status);
-
-      cv::Mat mat{frame.height, frame.width, CV_8UC1, frame.data};
-      imshow("window", mat);
-      waitKey(1);
-    }
-    std::cout << timeout << std::endl;
-  }
-
   std::cout << cs::GetHostname() << std::endl;
 }
