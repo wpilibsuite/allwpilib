ca194fd355110db1c58fe7e51aaf80e1a8a92d96
Eli <emichaelbarnett@gmail.com>
2019-05-16 16:04:03 -0400

    fix typos

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/util/ErrorMessages.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/util/ErrorMessages.java
@@ -29,7 +29,7 @@ public final class ErrorMessages {
    */
   public static <T> T requireNonNullParam(T obj, String paramName, String methodName) {
     return requireNonNull(obj,
-        "Parameter " + paramName + " in method " + methodName + "was null " + "when it"
+        "Parameter " + paramName + " in method " + methodName + " was null when it"
             + " should not have been!  Check the stacktrace to find the responsible line of code - "
             + "usually, it is the first line of user-written code indicated in the stacktrace.  "
             + "Make sure all objects passed to the method in question were properly initialized -"
