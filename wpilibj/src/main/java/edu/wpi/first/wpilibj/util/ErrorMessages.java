8a246d500215f52d1b359a7f3f3152d529b96ca9
Eli <emichaelbarnett@gmail.com>
2019-05-16 16:19:26 -0400

    change "online" to "additional"

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/util/ErrorMessages.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/util/ErrorMessages.java
@@ -34,7 +34,7 @@ public final class ErrorMessages {
             + "usually, it is the first line of user-written code indicated in the stacktrace.  "
             + "Make sure all objects passed to the method in question were properly initialized -"
             + " note that this may not be obvious if it is being called under "
-            + "dynamically-changing conditions!  Please do not seek online technical assistance "
+            + "dynamically-changing conditions!  Please do not seek additional technical assistance "
             + " without doing this first!");
   }
 }
