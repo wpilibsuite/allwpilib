908cebcdbbc677a1904ff23dd3708420ab092503
Eli <emichaelbarnett@gmail.com>
2019-05-28 13:30:53 -0400

    add test for new null check method

---
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/util/NullCheckTest.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.util;
+
+import org.junit.jupiter.api.Test;
+
+import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NullCheckTest {
+  @Test
+  public void requireNonNullParamTest() {
+    assertThrows(NullPointerException.class, () -> requireNonNullParam(null, "testParam",
+        "testMethod"));
+  }
+}
