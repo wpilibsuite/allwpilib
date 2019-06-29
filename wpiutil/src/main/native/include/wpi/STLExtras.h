2fac1f09ac162ca3afcc678829e707fd1c4faab1
Peter Johnson <johnson.peter@gmail.com>
2019-06-29 09:59:02 -0700

    Fix include sort order

--- wpiutil/src/main/native/include/wpi/STLExtras.h
+++ wpiutil/src/main/native/include/wpi/STLExtras.h
@@ -30,8 +30,8 @@
 #include <initializer_list>
 #include <iterator>
 #include <limits>
-#include <optional>
 #include <memory>
+#include <optional>
 #include <tuple>
 #include <type_traits>
 #include <utility>
