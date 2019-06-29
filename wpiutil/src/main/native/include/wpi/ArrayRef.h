4b1b92bb74924e4d7733287ae2235137d5c33f51
Peter Johnson <johnson.peter@gmail.com>
2019-06-29 11:20:22 -0700

    Replace wpi::optional with C++17 std::optional (#1732)
    Keep wpi/optional.h as a shim with deprecated warnings.

--- wpiutil/src/main/native/include/wpi/ArrayRef.h
+++ wpiutil/src/main/native/include/wpi/ArrayRef.h
@@ -11,7 +11,6 @@
 #define WPIUTIL_WPI_ARRAYREF_H
 
 #include "wpi/Hashing.h"
-#include "wpi/optional.h"
 #include "wpi/SmallVector.h"
 #include "wpi/STLExtras.h"
 #include "wpi/Compiler.h"
@@ -22,6 +21,7 @@
 #include <initializer_list>
 #include <iterator>
 #include <memory>
+#include <optional>
 #include <type_traits>
 #include <vector>
 
@@ -62,7 +62,7 @@ namespace wpi {
     /*implicit*/ ArrayRef() = default;
 
     /// Construct an empty ArrayRef from nullopt.
-    /*implicit*/ ArrayRef(nullopt_t) {}
+    /*implicit*/ ArrayRef(std::nullopt_t) {}
 
     /// Construct an ArrayRef from a single element.
     /*implicit*/ ArrayRef(const T &OneElt)
@@ -298,7 +298,7 @@ namespace wpi {
     /*implicit*/ MutableArrayRef() = default;
 
     /// Construct an empty MutableArrayRef from nullopt.
-    /*implicit*/ MutableArrayRef(nullopt_t) : ArrayRef<T>() {}
+    /*implicit*/ MutableArrayRef(std::nullopt_t) : ArrayRef<T>() {}
 
     /// Construct an MutableArrayRef from a single element.
     /*implicit*/ MutableArrayRef(T &OneElt) : ArrayRef<T>(OneElt) {}
