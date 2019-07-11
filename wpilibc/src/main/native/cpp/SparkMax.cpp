44fbf9eb59a6ee2e7949741e19d5285b1f74106d
Austin Shalit <austinshalit@gmail.com>
2019-07-11 11:26:03 -0400

    formatting

--- wpilibc/src/main/native/cpp/SparkMax.cpp
+++ wpilibc/src/main/native/cpp/SparkMax.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2008-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -12,7 +12,7 @@
 using namespace frc;
 
 SparkMax::SparkMax(int channel) : PWMSpeedController(channel) {
-  /* Note that the Spark uses the following bounds for PWM values.
+  /* Note that the SparkMax uses the following bounds for PWM values.
    *
    *   2.003ms = full "forward"
    *   1.55ms = the "high end" of the deadband range
--- wpilibj/src/main/java/edu/wpi/first/wpilibj/SparkMax.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/SparkMax.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2008-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -34,7 +34,7 @@ public class SparkMax extends PWMSpeedController {
     setPeriodMultiplier(PeriodMultiplier.k1X);
     setSpeed(0.0);
     setZeroLatch();
-    
+
     HAL.report(tResourceType.kResourceType_RevSparkMaxPWM, getChannel());
     setName("SparkMax", getChannel());
   }
