a396cd641471fe487fea1beee0195f7323533ff1
Thad House <thadhouse1@gmail.com>
2019-07-14 11:44:44 -0700

    Styleguide

--- hal/src/main/native/athena/FRCDriverStation.cpp
+++ hal/src/main/native/athena/FRCDriverStation.cpp
@@ -486,7 +486,9 @@ static int& GetThreadLocalLastCount() {
   return lastCount;
 }
 
-void HAL_WaitForCachedControlData(void) {HAL_WaitForCachedControlDataTimeout(0);}
+void HAL_WaitForCachedControlData(void) {
+  HAL_WaitForCachedControlDataTimeout(0);
+}
 
 HAL_Bool HAL_WaitForCachedControlDataTimeout(double timeout) {
   int& lastCount = GetThreadLocalLastCount();
--- hal/src/main/native/include/hal/DriverStation.h
+++ hal/src/main/native/include/hal/DriverStation.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2013-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2013-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -196,14 +196,17 @@ int32_t HAL_GetMatchInfo(HAL_MatchInfo* info);
 void HAL_ReleaseDSMutex(void);
 
 /**
- * Checks if new control data has arrived since the last HAL_WaitForCachedControlData or HAL_IsNewControlData call.
- * If new data has not arrived, waits for new data to arrive. Otherwise, returns immediately.
+ * Checks if new control data has arrived since the last
+ * HAL_WaitForCachedControlData or HAL_IsNewControlData call. If new data has
+ * not arrived, waits for new data to arrive. Otherwise, returns immediately.
  */
 void HAL_WaitForCachedControlData(void);
 
 /**
- * Checks if new control data has arrived since the last HAL_WaitForCachedControlData or HAL_IsNewControlData call.
- * If new data has not arrived, waits for new data to arrive, or a timeout. Otherwise, returns immediately.
+ * Checks if new control data has arrived since the last
+ * HAL_WaitForCachedControlData or HAL_IsNewControlData call. If new data has
+ * not arrived, waits for new data to arrive, or a timeout. Otherwise, returns
+ * immediately.
  * 
  * @param timeout timeout in seconds
  * @return        true for new data, false for timeout
