18bd9a3ef0740a9918fe2c96cec962c3acfc141e
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-07-16 10:05:18 -0400

    Add Transform2d docs

--- wpilibc/src/main/native/include/frc/experimental/geometry/Transform2d.h
+++ wpilibc/src/main/native/include/frc/experimental/geometry/Transform2d.h
@@ -14,15 +14,34 @@ namespace experimental {
 
 class Pose2d;
 
+/**
+ * Represents a transformation for a Pose2d.
+ */
 class Transform2d {
  public:
+  /**
+   * Constructs the transform that maps the initial pose to the final pose.
+   */
   Transform2d(Pose2d initial, Pose2d final);
+
+  /**
+   * Constructs a transform with the given translation and rotation components.
+   */
   Transform2d(Translation2d translation, Rotation2d rotation);
+
+  /**
+   * Constructs the identity transform -- maps an initial pose to itself.
+   */
   Transform2d();
 
-  Transform2d Inverse() const;
-
+  /**
+   * Returns the translation component of the transformation.
+   */
   const Translation2d& Translation() const { return m_translation; }
+
+  /**
+   * Returns the rotational component of the transformation.
+   */
   const Rotation2d& Rotation() const { return m_rotation; }
 
  private:
