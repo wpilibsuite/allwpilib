f1e8e7910f5ae89d18a042bfefe7e58c573181bf
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-07-24 16:27:48 -0400

    Run wpiformat

--- wpiutil/src/test/native/cpp/EigenTest.cpp
+++ wpiutil/src/test/native/cpp/EigenTest.cpp
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #include "Eigen/Core"
 #include "Eigen/LU"
 #include "gtest/gtest.h"
