eac569e5c6d8d8120ca817c3b537249f225c9375
Thad House <thadhouse1@gmail.com>
2019-09-07 11:17:41 -0700

    more formatting

--- hal/src/main/native/sim/SerialPort.cpp
+++ hal/src/main/native/sim/SerialPort.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -16,29 +16,29 @@ void InitializeSerialPort() {}
 }  // namespace hal
 
 extern "C" {
-HAL_SerialPortHandle HAL_InitializeSerialPort(HAL_SerialPort port, int32_t* status) {
+HAL_SerialPortHandle HAL_InitializeSerialPort(HAL_SerialPort port,
+                                              int32_t* status) {
   hal::init::CheckInit();
   return HAL_kInvalidHandle;
 }
 
-HAL_SerialPortHandle HAL_InitializeSerialPortDirect(HAL_SerialPort port, const char* portName,
-                                    int32_t* status) {
+HAL_SerialPortHandle HAL_InitializeSerialPortDirect(HAL_SerialPort port,
+                                                    const char* portName,
+                                                    int32_t* status) {
   hal::init::CheckInit();
   return HAL_kInvalidHandle;
 }
 
-int HAL_GetSerialFD(HAL_SerialPortHandle handle, int32_t* status) {
-  return -1;
-}
+int HAL_GetSerialFD(HAL_SerialPortHandle handle, int32_t* status) { return -1; }
 
-void HAL_SetSerialBaudRate(HAL_SerialPortHandle handle, int32_t baud, int32_t* status) {
-}
+void HAL_SetSerialBaudRate(HAL_SerialPortHandle handle, int32_t baud,
+                           int32_t* status) {}
 
-void HAL_SetSerialDataBits(HAL_SerialPortHandle handle, int32_t bits, int32_t* status) {
-}
+void HAL_SetSerialDataBits(HAL_SerialPortHandle handle, int32_t bits,
+                           int32_t* status) {}
 
-void HAL_SetSerialParity(HAL_SerialPortHandle handle, int32_t parity, int32_t* status) {
-}
+void HAL_SetSerialParity(HAL_SerialPortHandle handle, int32_t parity,
+                         int32_t* status) {}
 
 void HAL_SetSerialStopBits(HAL_SerialPortHandle handle, int32_t stopBits,
                            int32_t* status) {}
@@ -55,7 +55,8 @@ void HAL_SetSerialTimeout(HAL_SerialPortHandle handle, double timeout,
 void HAL_EnableSerialTermination(HAL_SerialPortHandle handle, char terminator,
                                  int32_t* status) {}
 
-void HAL_DisableSerialTermination(HAL_SerialPortHandle handle, int32_t* status) {}
+void HAL_DisableSerialTermination(HAL_SerialPortHandle handle,
+                                  int32_t* status) {}
 
 void HAL_SetSerialReadBufferSize(HAL_SerialPortHandle handle, int32_t size,
                                  int32_t* status) {}
@@ -63,7 +64,8 @@ void HAL_SetSerialReadBufferSize(HAL_SerialPortHandle handle, int32_t size,
 void HAL_SetSerialWriteBufferSize(HAL_SerialPortHandle handle, int32_t size,
                                   int32_t* status) {}
 
-int32_t HAL_GetSerialBytesReceived(HAL_SerialPortHandle handle, int32_t* status) {
+int32_t HAL_GetSerialBytesReceived(HAL_SerialPortHandle handle,
+                                   int32_t* status) {
   return 0;
 }
 
@@ -72,8 +74,8 @@ int32_t HAL_ReadSerial(HAL_SerialPortHandle handle, char* buffer, int32_t count,
   return 0;
 }
 
-int32_t HAL_WriteSerial(HAL_SerialPortHandle handle, const char* buffer, int32_t count,
-                        int32_t* status) {
+int32_t HAL_WriteSerial(HAL_SerialPortHandle handle, const char* buffer,
+                        int32_t count, int32_t* status) {
   return 0;
 }
 
