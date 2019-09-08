561cbbd1441e9a5c0c14953507fc97717d1fa69e
Tyler Veness <calcmogul@gmail.com>
2019-09-07 21:02:44 -0700

    Deprecate Filter class for removal (#1876)

--- wpilibc/src/main/native/include/frc/filters/Filter.h
+++ wpilibc/src/main/native/include/frc/filters/Filter.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2015-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2015-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,6 +8,8 @@
 #pragma once
 
 #include <memory>
+
+#include <wpi/deprecated.h>
 
 #include "frc/PIDSource.h"
 
@@ -18,7 +20,9 @@ namespace frc {
  */
 class Filter : public PIDSource {
  public:
+  WPI_DEPRECATED("This class is no longer used.")
   explicit Filter(PIDSource& source);
+  WPI_DEPRECATED("This class is no longer used.")
   explicit Filter(std::shared_ptr<PIDSource> source);
   virtual ~Filter() = default;
 
