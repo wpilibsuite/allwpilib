0e46291fcaa254ad4cc68d1089230825ae7b6ca5
Thad House <thadhouse1@gmail.com>
2019-10-08 13:30:23 -0700

    Add messaging to extension loading in the HAL

--- hal/src/main/native/sim/Extensions.cpp
+++ hal/src/main/native/sim/Extensions.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,6 +9,7 @@
 
 #include <wpi/SmallString.h>
 #include <wpi/StringRef.h>
+#include <wpi/raw_ostream.h>
 
 #include "hal/HAL.h"
 
@@ -43,6 +44,8 @@ extern "C" {
 
 int HAL_LoadOneExtension(const char* library) {
   int rc = 1;  // It is expected and reasonable not to find an extra simulation
+  wpi::outs() << "Attempting to load: " << basename(library) << "\n";
+  wpi::outs().flush();
   HTYPE handle = DLOPEN(library);
 #if !defined(WIN32) && !defined(_WIN32)
   if (!handle) {
@@ -53,17 +56,30 @@ int HAL_LoadOneExtension(const char* library) {
 #else
     libraryName += ".so";
 #endif
+    wpi::outs() << "Trying modified name: " << basename(libraryName.c_str());
+    wpi::outs().flush();
     handle = DLOPEN(libraryName.c_str());
   }
 #endif
-  if (!handle) return rc;
+  if (!handle) {
+    wpi::outs() << "Failed to load library\n";
+    wpi::outs().flush();
+    return rc;
+  }
 
   auto init = reinterpret_cast<halsim_extension_init_func_t*>(
       DLSYM(handle, "HALSIM_InitExtension"));
 
   if (init) rc = (*init)();
 
-  if (rc != 0) DLCLOSE(handle);
+  if (rc != 0) {
+    wpi::outs() << "Failed to load extension\n";
+    wpi::outs().flush();
+    DLCLOSE(handle);
+  } else {
+    wpi::outs() << "Successfully loaded extension\n";
+    wpi::outs().flush();
+  }
   return rc;
 }
 
@@ -71,7 +87,11 @@ int HAL_LoadExtensions(void) {
   int rc = 1;
   wpi::SmallVector<wpi::StringRef, 2> libraries;
   const char* e = std::getenv("HALSIM_EXTENSIONS");
-  if (!e) return rc;
+  if (!e) {
+    wpi::outs() << "No extensions found\n";
+    wpi::outs().flush(); 
+    return rc;
+  }
   wpi::StringRef env{e};
   env.split(libraries, DELIM, -1, false);
   for (auto& libref : libraries) {
