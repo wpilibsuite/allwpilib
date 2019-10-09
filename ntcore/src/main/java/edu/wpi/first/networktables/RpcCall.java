0e3b0f3da75e24021ba9eda4b1dffc811dbf410a
Thad House <ThadHouse@users.noreply.github.com>
2019-10-08 21:30:33 -0700

    Remove deprecated free() calls (#1925)
    Replaced with the standard close()

--- ntcore/src/main/java/edu/wpi/first/networktables/RpcCall.java
+++ ntcore/src/main/java/edu/wpi/first/networktables/RpcCall.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -20,11 +20,6 @@ public final class RpcCall implements AutoCloseable {
   public RpcCall(NetworkTableEntry entry, int call) {
     m_entry = entry;
     m_call = call;
-  }
-
-  @Deprecated
-  public void free() {
-    close();
   }
 
   /**
