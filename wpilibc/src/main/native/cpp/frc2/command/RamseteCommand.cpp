82a49b9d2c0e0f33688a1856e95bdf4034968c23
oblarg <emichaelbarnett@gmail.com>
2019-10-17 16:17:19 -0400

    wpiformat

--- wpilibc/src/main/native/cpp/frc2/command/RamseteCommand.cpp
+++ wpilibc/src/main/native/cpp/frc2/command/RamseteCommand.cpp
@@ -1,46 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #include "frc2/command/RamseteCommand.h"
 
 using namespace frc2;
-using namespace units::literals;
+using namespace units;
 
 template <typename T>
 int sgn(T val) {
-    return (T(0) < val) - (val < T(0));
+  return (T(0) < val) - (val < T(0));
 }
 
-RamseteCommand::RamseteCommand(frc::Trajectory trajectory,
-                               std::function<frc::Pose2d()> pose,
-                               frc::RamseteController follower,
-                               units::voltage::volt_t ks,
-                               units::unit_t<voltsecondspermeter> kv,
-                               units::unit_t<voltsecondssquaredpermeter> ka,
-                               frc::DifferentialDriveKinematics kinematics,
-                               std::function<units::meters_per_second_t()> leftSpeed,
-                               std::function<units::meters_per_second_t()> rightSpeed,
-                               frc2::PIDController leftController,
-                               frc2::PIDController rightController,
-                               std::function<void(voltage::volt_t, voltage::volt_t)> output)
-                                : m_trajectory(trajectory),
-                                m_pose(pose),
-                                m_follower(follower),
-                                m_ks(ks),
-                                m_kv(kv),
-                                m_ka(ka),
-                                m_kinematics(kinematics),
-                                m_leftSpeed(leftSpeed),
-                                m_rightSpeed(rightSpeed),
-                                m_leftController(leftController),
-                                m_rightController(rightController),
-                                m_output(output) {}
+RamseteCommand::RamseteCommand(
+    frc::Trajectory trajectory, std::function<frc::Pose2d()> pose,
+    frc::RamseteController follower, units::voltage::volt_t ks,
+    units::unit_t<voltsecondspermeter> kv,
+    units::unit_t<voltsecondssquaredpermeter> ka,
+    frc::DifferentialDriveKinematics kinematics,
+    std::function<units::meters_per_second_t()> leftSpeed,
+    std::function<units::meters_per_second_t()> rightSpeed,
+    frc2::PIDController leftController, frc2::PIDController rightController,
+    std::function<void(voltage::volt_t, voltage::volt_t)> output)
+    : m_trajectory(trajectory),
+      m_pose(pose),
+      m_follower(follower),
+      m_ks(ks),
+      m_kv(kv),
+      m_ka(ka),
+      m_kinematics(kinematics),
+      m_leftSpeed(leftSpeed),
+      m_rightSpeed(rightSpeed),
+      m_leftController(leftController),
+      m_rightController(rightController),
+      m_output(output) {}
 
 void RamseteCommand::Initialize() {
   m_prevTime = 0_s;
   auto initialState = m_trajectory.Sample(0_s);
   m_prevSpeeds = m_kinematics.ToWheelSpeeds(
-    frc::ChassisSpeeds{
-      initialState.velocity, 
-      0_m/1_s, 
-      initialState.velocity * initialState.curvature});
+      frc::ChassisSpeeds{initialState.velocity, 0_m / 1_s,
+                         initialState.velocity * initialState.curvature});
   m_timer.Reset();
   m_timer.Start();
 }
@@ -50,23 +53,25 @@ void RamseteCommand::Execute() {
   auto dt = curTime - m_prevTime;
 
   auto targetWheelSpeeds = m_kinematics.ToWheelSpeeds(
-    m_follower.Calculate(m_pose(), m_trajectory.Sample(curTime)));
+      m_follower.Calculate(m_pose(), m_trajectory.Sample(curTime)));
 
-  auto leftFeedforward = m_ks * sgn(targetWheelSpeeds.left)
-      + m_kv * targetWheelSpeeds.left
-      + m_ka * (targetWheelSpeeds.left - m_prevSpeeds.left) / dt;
+  auto leftFeedforward =
+      m_ks * sgn(targetWheelSpeeds.left) + m_kv * targetWheelSpeeds.left +
+      m_ka * (targetWheelSpeeds.left - m_prevSpeeds.left) / dt;
 
-  auto rightFeedforward = m_ks * sgn(targetWheelSpeeds.right)
-      + m_kv * targetWheelSpeeds.right
-      + m_ka * (targetWheelSpeeds.right - m_prevSpeeds.right) / dt;
+  auto rightFeedforward =
+      m_ks * sgn(targetWheelSpeeds.right) + m_kv * targetWheelSpeeds.right +
+      m_ka * (targetWheelSpeeds.right - m_prevSpeeds.right) / dt;
 
-  auto leftOutput = voltage::volt_t(
-    m_leftController.Calculate(targetWheelSpeeds.left.to<double>(), 
-                               m_leftSpeed().to<double>())) + leftFeedforward;
+  auto leftOutput =
+      voltage::volt_t(m_leftController.Calculate(
+          targetWheelSpeeds.left.to<double>(), m_leftSpeed().to<double>())) +
+      leftFeedforward;
 
-  auto rightOutput = voltage::volt_t(
-    m_rightController.Calculate(targetWheelSpeeds.right.to<double>(), 
-                               m_rightSpeed().to<double>())) + rightFeedforward;
+  auto rightOutput =
+      voltage::volt_t(m_rightController.Calculate(
+          targetWheelSpeeds.right.to<double>(), m_rightSpeed().to<double>())) +
+      rightFeedforward;
 
   m_output(leftOutput, rightOutput);
 
@@ -74,9 +79,7 @@ void RamseteCommand::Execute() {
   m_prevSpeeds = targetWheelSpeeds;
 }
 
-void RamseteCommand::End(bool interrupted) {
-  m_timer.Stop();
-}
+void RamseteCommand::End(bool interrupted) { m_timer.Stop(); }
 
 bool RamseteCommand::IsFinished() {
   return second_t(m_timer.Get()) - m_trajectory.TotalTime() >= 0_s;
--- wpilibc/src/main/native/include/frc2/command/RamseteCommand.h
+++ wpilibc/src/main/native/include/frc2/command/RamseteCommand.h
@@ -1,44 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #include <functional>
+
 #include <units/units.h>
 
 #include "CommandBase.h"
 #include "CommandHelper.h"
-#include "frc/trajectory/Trajectory.h"
-#include "frc/geometry/Pose2d.h"
-#include "frc/controller/RamseteController.h"
-#include "frc/kinematics/DifferentialDriveKinematics.h"
-#include "frc/controller/PIDController.h"
 #include "frc/Timer.h"
+#include "frc/controller/PIDController.h"
+#include "frc/controller/RamseteController.h"
+#include "frc/geometry/Pose2d.h"
+#include "frc/kinematics/DifferentialDriveKinematics.h"
+#include "frc/trajectory/Trajectory.h"
 
 #pragma once
 
-using namespace units;
-
-namespace frc2{
+namespace frc2 {
 class RamseteCommand : public CommandHelper<CommandBase, RamseteCommand> {
-using voltsecondspermeter = compound_unit<voltage::volt, second, inverse<meter>>;
-using voltsecondssquaredpermeter = compound_unit<voltage::volt, squared<second>, inverse<meter>>;
+  using voltsecondspermeter =
+      units::compound_unit<units::voltage::volt, units::second,
+                           units::inverse<units::meter>>;
+  using voltsecondssquaredpermeter =
+      units::compound_unit<units::voltage::volt, units::squared<units::second>,
+                           units::inverse<units::meter>>;
 
  public:
-  RamseteCommand(frc::Trajectory trajectory,
-                 std::function<frc::Pose2d()> pose,
-                 frc::RamseteController follower,
-                 
-                voltage::volt_t ks,
-                 
-                unit_t<voltsecondspermeter> kv,
-                 
-                unit_t<voltsecondssquaredpermeter> ka,
-                 frc::DifferentialDriveKinematics kinematics,
-                 std::function<
-                meters_per_second_t()> leftSpeed,
-                 std::function<
-                meters_per_second_t()> rightSpeed,
-                 frc2::PIDController leftController,
-                 frc2::PIDController rightController,
-                 std::function<void(
-                  voltage::volt_t, 
-                voltage::volt_t)> output);
+  RamseteCommand(
+      frc::Trajectory trajectory, std::function<frc::Pose2d()> pose,
+      frc::RamseteController follower, units::voltage::volt_t ks,
+      units::unit_t<voltsecondspermeter> kv,
+      units::unit_t<voltsecondssquaredpermeter> ka,
+      frc::DifferentialDriveKinematics kinematics,
+      std::function<units::meters_per_second_t()> leftSpeed,
+      std::function<units::meters_per_second_t()> rightSpeed,
+      frc2::PIDController leftController, frc2::PIDController rightController,
+      std::function<void(units::voltage::volt_t, units::voltage::volt_t)>
+          output);
 
   void Initialize() override;
 
@@ -52,26 +54,18 @@ using voltsecondssquaredpermeter = compound_unit<voltage::volt, squared<second>,
   frc::Trajectory m_trajectory;
   std::function<frc::Pose2d()> m_pose;
   frc::RamseteController m_follower;
-  const 
-voltage::volt_t m_ks;
-  const 
-unit_t<voltsecondspermeter> m_kv;
-  const 
-unit_t<voltsecondssquaredpermeter> m_ka;
+  const units::voltage::volt_t m_ks;
+  const units::unit_t<voltsecondspermeter> m_kv;
+  const units::unit_t<voltsecondssquaredpermeter> m_ka;
   frc::DifferentialDriveKinematics m_kinematics;
-  std::function<
-meters_per_second_t()> m_leftSpeed;
-  std::function<
-meters_per_second_t()> m_rightSpeed;
+  std::function<units::meters_per_second_t()> m_leftSpeed;
+  std::function<units::meters_per_second_t()> m_rightSpeed;
   frc2::PIDController m_leftController;
   frc2::PIDController m_rightController;
-  std::function<void(
-  voltage::volt_t, 
-voltage::volt_t)> m_output;
+  std::function<void(units::voltage::volt_t, units::voltage::volt_t)> m_output;
 
   frc::Timer m_timer;
-  
-second_t m_prevTime;
+  units::second_t m_prevTime;
   frc::DifferentialDriveWheelSpeeds m_prevSpeeds;
 };
-}
+}  // namespace frc2
