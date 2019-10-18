63cfa64fb36ec6fe3c71b522d17d7dc169b663de
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-10-18 10:58:22 -0400

    Add getters for pose in odometry classes (#1943)

--- wpilibc/src/main/native/include/frc/kinematics/MecanumDriveOdometry.h
+++ wpilibc/src/main/native/include/frc/kinematics/MecanumDriveOdometry.h
@@ -46,6 +46,12 @@ class MecanumDriveOdometry {
   }
 
   /**
+   * Returns the position of the robot on the field.
+   * @return The pose of the robot.
+   */
+  const Pose2d& GetPose() const { return m_pose; }
+
+  /**
    * Updates the robot's position on the field using forward kinematics and
    * integration of the pose over time. This method takes in the current time as
    * a parameter to calculate period (difference between two timestamps). The
