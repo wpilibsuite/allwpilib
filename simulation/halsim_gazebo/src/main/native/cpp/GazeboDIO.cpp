086ae58c83837a5d27580b820c0eab53f02dc6bf
Peter Johnson <johnson.peter@gmail.com>
2019-10-19 10:16:22 -0700

    Fix Gazebo sim plugin build

--- simulation/halsim_gazebo/src/main/native/cpp/GazeboDIO.cpp
+++ simulation/halsim_gazebo/src/main/native/cpp/GazeboDIO.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2014-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2014-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,8 +9,8 @@
 
 #include <string>
 
+#include <hal/Value.h>
 #include <mockdata/DIOData.h>
-#include <mockdata/HAL_Value.h>
 #include <mockdata/NotifyListener.h>
 
 static void init_callback(const char* name, void* param,
