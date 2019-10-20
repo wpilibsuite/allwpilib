b924b811c6f8d8cbabaf7d3f34e873fccc71fa33
Tyler Veness <calcmogul@gmail.com>
2019-10-20 13:28:41 -0700

    Deprecate old PID classes
    PIDBase was only used by the old PIDController, which is deprecated.
    PIDInterface is only used by PIDBase, and that's deprecated by this
    commit.

--- wpilibc/src/main/native/include/frc/PIDInterface.h
+++ wpilibc/src/main/native/include/frc/PIDInterface.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2016-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2016-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,10 +7,13 @@
 
 #pragma once
 
+#include <wpi/deprecated.h>
+
 namespace frc {
 
 class PIDInterface {
  public:
+  WPI_DEPRECATED("All APIs which use this have been deprecated.")
   PIDInterface() = default;
   PIDInterface(PIDInterface&&) = default;
   PIDInterface& operator=(PIDInterface&&) = default;
