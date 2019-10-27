dd42c3b1c2daa86f17a41206e80d529b946c1c38
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-10-27 09:53:04 -0400

    Use FPGA Time instead of wall clock time for odometry

--- wpilibc/src/main/native/include/frc/kinematics/SwerveDriveOdometry.h
+++ wpilibc/src/main/native/include/frc/kinematics/SwerveDriveOdometry.h
@@ -16,6 +16,7 @@
 #include "SwerveDriveKinematics.h"
 #include "SwerveModuleState.h"
 #include "frc/geometry/Pose2d.h"
+#include "frc2/Timer.h"
 
 namespace frc {
 
@@ -95,9 +96,8 @@ class SwerveDriveOdometry {
   template <typename... ModuleStates>
   const Pose2d& Update(const Rotation2d& angle,
                        ModuleStates&&... moduleStates) {
-    const auto now = std::chrono::system_clock::now().time_since_epoch();
-    units::second_t time{now};
-    return UpdateWithTime(time, angle, moduleStates...);
+    return UpdateWithTime(frc2::Timer::GetFPGATimestamp(), angle,
+                          moduleStates...);
   }
 
  private:
