35904328099550c2239a1496bb24217156a03859
Peter Johnson <johnson.peter@gmail.com>
2019-11-02 17:07:25 -0700

    Revert accidental change to MyRobot.cpp

--- myRobot/src/main/native/cpp/MyRobot.cpp
+++ myRobot/src/main/native/cpp/MyRobot.cpp
@@ -7,42 +7,12 @@
 
 #include <frc/TimedRobot.h>
 
-#include <hal/CAN.h>
-#include <hal/DIO.h>
-#include <hal/DutyCycle.h>
-#include <hal/HALBase.h>
-
-#include "frc/smartdashboard/SmartDashboard.h"
-
-HAL_DigitalHandle inputHandle;
-HAL_DutyCycleHandle dutyCycleHandle;
-
 class MyRobot : public frc::TimedRobot {
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
-  void RobotInit() override {
-    int32_t status = 0;
-    inputHandle = HAL_InitializeDIOPort(HAL_GetPort(9), true, &status);
-    std::cout << "DIO Status: " << status << std::endl;
-    dutyCycleHandle = HAL_InitializeDutyCycle(
-        inputHandle, HAL_AnalogTriggerType::HAL_Trigger_kInWindow, &status);
-    std::cout << "Duty Cycle Status: " << status << std::endl;
-
-    // float pbs = 0;
-    // uint32_t boc = 0;
-    // uint32_t txfc = 0;
-    // uint32_t rec = 0;
-    // uint32_t tec = 0;
-    status = 0;
-    auto start = HAL_GetFPGATime(&status);
-    // HAL_CAN_GetCANStatus(&pbs, &boc, &txfc, &rec, &tec, &status);
-    uint8_t data = 0;
-    HAL_CAN_SendMessage(0, &data, 1, HAL_CAN_SEND_PERIOD_NO_REPEAT, &status);
-    auto end = HAL_GetFPGATime(&status);
-    std::cout << "Start " << start << " end " << end << std::endl;
-  }
+  void RobotInit() override {}
 
   /**
    * This function is run once each time the robot enters autonomous mode
@@ -72,15 +42,7 @@ class MyRobot : public frc::TimedRobot {
   /**
    * This function is called periodically during all modes
    */
-  void RobotPeriodic() override {
-    int32_t status = 0;
-    auto freq = HAL_GetDutyCycleFrequency(dutyCycleHandle, &status);
-    auto raw = HAL_GetDutyCycleOutputRaw(dutyCycleHandle, &status);
-    auto percentage = HAL_GetDutyCycleOutput(dutyCycleHandle, &status);
-    frc::SmartDashboard::PutNumber("Freq", freq);
-    frc::SmartDashboard::PutNumber("Raw", raw);
-    frc::SmartDashboard::PutNumber("Percentage", percentage);
-  }
+  void RobotPeriodic() override {}
 };
 
 int main() { return frc::StartRobot<MyRobot>(); }
