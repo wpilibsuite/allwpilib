94843adb8f4326194366298f9811174339b4f308
sciencewhiz <sciencewhiz@users.noreply.github.com>
2019-11-09 09:14:53 -0800

    Standardize documentation of Speed Controllers bounds (#2043)
    Some were in constructor docs, some in init function docs, and some
    inline in code. Move them all to class docs.

--- wpilibc/src/main/native/cpp/Victor.cpp
+++ wpilibc/src/main/native/cpp/Victor.cpp
@@ -14,20 +14,6 @@
 using namespace frc;
 
 Victor::Victor(int channel) : PWMSpeedController(channel) {
-  /* Note that the Victor uses the following bounds for PWM values.  These
-   * values were determined empirically and optimized for the Victor 888. These
-   * values should work reasonably well for Victor 884 controllers as well but
-   * if users experience issues such as asymmetric behaviour around the deadband
-   * or inability to saturate the controller in either direction, calibration is
-   * recommended. The calibration procedure can be found in the Victor 884 User
-   * Manual available from IFI.
-   *
-   *   2.027ms = full "forward"
-   *   1.525ms = the "high end" of the deadband range
-   *   1.507ms = center of the deadband range (off)
-   *   1.49ms = the "low end" of the deadband range
-   *   1.026ms = full "reverse"
-   */
   SetBounds(2.027, 1.525, 1.507, 1.49, 1.026);
   SetPeriodMultiplier(kPeriodMultiplier_2X);
   SetSpeed(0.0);
