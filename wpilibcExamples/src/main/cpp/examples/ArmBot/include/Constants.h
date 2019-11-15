f86c146165166aefe0abd1035918b6e8ef93ae6c
oblarg <emichaelbarnett@gmail.com>
2019-11-15 15:12:40 -0500

    use wpi::math::pi

--- wpilibcExamples/src/main/cpp/examples/ArmBot/include/Constants.h
+++ wpilibcExamples/src/main/cpp/examples/ArmBot/include/Constants.h
@@ -9,6 +9,8 @@
 
 #include <units/units.h>
 
+#include <wpi/math>
+
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
  * numerical or bool constants.  This should not be used for any other purpose.
@@ -33,7 +35,7 @@ constexpr int kEncoderCPR = 1024;
 constexpr double kWheelDiameterInches = 6;
 constexpr double kEncoderDistancePerPulse =
     // Assumes the encoders are directly mounted on the wheel shafts
-    (kWheelDiameterInches * 3.142) / static_cast<double>(kEncoderCPR);
+    (kWheelDiameterInches * wpi::math::pi) / static_cast<double>(kEncoderCPR);
 }  // namespace DriveConstants
 
 namespace ArmConstants {
@@ -53,7 +55,7 @@ constexpr auto kMaxAcceleration = 10_rad / (1_s * 1_s);
 
 constexpr int kEncoderPorts[]{4, 5};
 constexpr int kEncoderPPR = 256;
-constexpr auto kEncoderDistancePerPulse = 2.0_rad * 3.1415 / kEncoderPPR;
+constexpr auto kEncoderDistancePerPulse = 2.0_rad * wpi::math::pi / kEncoderPPR;
 
 // The offset of the arm from the horizontal in its neutral position,
 // measured from the horizontal
