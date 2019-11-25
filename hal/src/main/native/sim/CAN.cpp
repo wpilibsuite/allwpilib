ee4b3a4368cc64ab9393d45d8098b2c420c70abe
Thad House <thadhouse1@gmail.com>
2019-11-25 12:54:30 -0800

    Formatting

--- hal/src/main/native/sim/CAN.cpp
+++ hal/src/main/native/sim/CAN.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2016-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2016-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -26,7 +26,8 @@ void HAL_CAN_SendMessage(uint32_t messageID, const uint8_t* data,
 void HAL_CAN_ReceiveMessage(uint32_t* messageID, uint32_t messageIDMask,
                             uint8_t* data, uint8_t* dataSize,
                             uint32_t* timeStamp, int32_t* status) {
-  // Use a data size of 42 as call check. Difficult to add check to invoke handler
+  // Use a data size of 42 as call check. Difficult to add check to invoke
+  // handler
   *dataSize = 42;
   auto tmpStatus = *status;
   SimCanData->receiveMessage(messageID, messageIDMask, data, dataSize,
