089d1148750adb6eb1cf4a00a4f445218aff15e6
oblarg <emichaelbarnett@gmail.com>
2019-11-26 15:31:24 -0500

    wpiformat

--- wpilibc/src/test/native/cpp/MedianFilterTest.cpp
+++ wpilibc/src/test/native/cpp/MedianFilterTest.cpp
@@ -9,38 +9,38 @@
 #include "gtest/gtest.h"
 
 TEST(MedianFilterTest, MedianFilterNotFullTestEven) {
-   frc::MedianFilter filter{10};
+  frc::MedianFilter filter{10};
 
-   filter.Calculate(3);
-   filter.Calculate(0);
-   filter.Calculate(4);
+  filter.Calculate(3);
+  filter.Calculate(0);
+  filter.Calculate(4);
 
-   EXPECT_EQ(filter.Calculate(1000), 3.5);
- }
+  EXPECT_EQ(filter.Calculate(1000), 3.5);
+}
 
- TEST(MedianFilterTest, MedianFilterNotFullTestOdd) {
-   frc::MedianFilter filter{10};
+TEST(MedianFilterTest, MedianFilterNotFullTestOdd) {
+  frc::MedianFilter filter{10};
 
-   filter.Calculate(3);
-   filter.Calculate(0);
-   filter.Calculate(4);
-   filter.Calculate(7);
+  filter.Calculate(3);
+  filter.Calculate(0);
+  filter.Calculate(4);
+  filter.Calculate(7);
 
-   EXPECT_EQ(filter.Calculate(1000), 4);
- }
+  EXPECT_EQ(filter.Calculate(1000), 4);
+}
 
- TEST(MedianFilterTest, MedianFilterFullTestEven) {
-   frc::MedianFilter filter{6};
+TEST(MedianFilterTest, MedianFilterFullTestEven) {
+  frc::MedianFilter filter{6};
 
-   filter.Calculate(3);
-   filter.Calculate(0);
-   filter.Calculate(0);
-   filter.Calculate(5);
-   filter.Calculate(4);
-   filter.Calculate(1000);
+  filter.Calculate(3);
+  filter.Calculate(0);
+  filter.Calculate(0);
+  filter.Calculate(5);
+  filter.Calculate(4);
+  filter.Calculate(1000);
 
-   EXPECT_EQ(filter.Calculate(99), 4.5);
- }
+  EXPECT_EQ(filter.Calculate(99), 4.5);
+}
 
 TEST(MedianFilterTest, MedianFilterFullTestOdd) {
   frc::MedianFilter filter{5};
