cae0f1e1cb7e4a67c0086339d3d827a7305d7ec7
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-12-21 16:14:09 -0500

    WPIFormat

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/trajectory/TrajectoryTransformTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/trajectory/TrajectoryTransformTest.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package edu.wpi.first.wpilibj.trajectory;
 
 import java.util.List;
@@ -12,7 +19,6 @@ import edu.wpi.first.wpilibj.geometry.Translation2d;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TrajectoryTransformTest {
-
   @Test
   void testTransformBy() {
     var config = new TrajectoryConfig(3, 3);
