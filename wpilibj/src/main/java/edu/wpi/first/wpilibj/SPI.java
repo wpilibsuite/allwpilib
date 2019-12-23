12e81d0791e0919dfa6831a6ecaf18c7345a0d91
Thad House <thadhouse1@gmail.com>
2019-12-23 14:35:25 -0800

    Fix docs gen

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/SPI.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/SPI.java
@@ -427,8 +427,6 @@ public class SPI implements AutoCloseable {
   /**
    * Configure the Auto SPI Stall time between reads.
    *
-   * @param port The number of the port to use. 0-3 for Onboard CS0-CS2, 4 for
-   * MXP.
    * @param csToSclkTicks the number of ticks to wait before asserting the cs pin
    * @param stallTicks the number of ticks to stall for
    * @param pow2BytesPerRead the number of bytes to read before stalling
