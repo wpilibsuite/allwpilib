333655dc508c786299c446656772367cd3ae5fed
Oblarg <emichaelbarnett@gmail.com>
2020-01-12 06:44:27 -0500

    fix povbutton

--- wpilibNewCommands/src/main/native/include/frc2/command/button/POVButton.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/button/POVButton.h
@@ -28,7 +28,7 @@ class POVButton : public Button {
    */
   POVButton(frc::GenericHID* joystick, int angle, int povNumber = 0)
       : Button([joystick, angle, povNumber] {
-          m_joystick->GetPOV(m_povNumber) == m_angle;
+          joystick->GetPOV(m_povNumber) == m_angle;
         }) {}
 };
 }  // namespace frc2
