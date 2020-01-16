a61fcbd68d287773fee2fcd42f1aa6808e7af877
Chris Russell <375294+crussell52@users.noreply.github.com>
2020-01-15 21:59:51 -0500

    Make Button class concrete (#2244)

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/button/Button.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/button/Button.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2008-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -23,7 +23,7 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * wrapper around Trigger with the method names renamed to fit the Button object use.
  */
 @SuppressWarnings("PMD.TooManyMethods")
-public abstract class Button extends Trigger {
+public class Button extends Trigger {
   /**
    * Default constructor; creates a button that is never pressed (unless {@link Button#get()} is
    * overridden).
