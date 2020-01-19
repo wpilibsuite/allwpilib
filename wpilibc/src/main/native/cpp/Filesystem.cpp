e0109d0a1a96a42ffad01ec034e9fdc7a32be45e
Peter Johnson <johnson.peter@gmail.com>
2020-01-19 00:25:36 -0800

    Change sim Filesystem.getDeployDirectory() to src/main/deploy

--- wpilibc/src/main/native/cpp/Filesystem.cpp
+++ wpilibc/src/main/native/cpp/Filesystem.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -27,5 +27,11 @@ void frc::filesystem::GetOperatingDirectory(
 
 void frc::filesystem::GetDeployDirectory(wpi::SmallVectorImpl<char>& result) {
   frc::filesystem::GetOperatingDirectory(result);
-  wpi::sys::path::append(result, "deploy");
+  if (RobotBase::IsReal()) {
+    wpi::sys::path::append(result, "deploy");
+  } else {
+    wpi::sys::path::append(result, "src");
+    wpi::sys::path::append(result, "main");
+    wpi::sys::path::append(result, "deploy");
+  }
 }
