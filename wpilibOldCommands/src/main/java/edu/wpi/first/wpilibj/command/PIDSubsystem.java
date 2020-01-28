0d2805b8977ecec441571a1e646f0757e9f91f4f
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-01-28 10:26:04 -0800

    Update PIDSubsystem.java

--- wpilibOldCommands/src/main/java/edu/wpi/first/wpilibj/command/PIDSubsystem.java
+++ wpilibOldCommands/src/main/java/edu/wpi/first/wpilibj/command/PIDSubsystem.java
@@ -66,7 +66,7 @@ public abstract class PIDSubsystem extends Subsystem {
   }
 
   /**
-   * Instantiates a {@link PIDSubsystem} that will use the given p, i and d values.
+   * Instantiates a {@link PIDSubsystem} that will use the given p, i, d, and f values.
    *
    * @param name the name
    * @param p    the proportional value
@@ -82,7 +82,7 @@ public abstract class PIDSubsystem extends Subsystem {
   }
 
   /**
-   * Instantiates a {@link PIDSubsystem} that will use the given p, i and d values. It will also
+   * Instantiates a {@link PIDSubsystem} that will use the given p, i, d, and f values. It will also
    * space the time between PID loop calculations to be equal to the given period.
    *
    * @param name   the name
@@ -114,7 +114,7 @@ public abstract class PIDSubsystem extends Subsystem {
   }
 
   /**
-   * Instantiates a {@link PIDSubsystem} that will use the given p, i and d values. It will use the
+   * Instantiates a {@link PIDSubsystem} that will use the given p, i, d, and f values. It will use the
    * class name as its name. It will also space the time between PID loop calculations to be equal
    * to the given period.
    *
