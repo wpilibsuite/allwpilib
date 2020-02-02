5a3edc939514cd15763aa4363192b5d717067629
Banks T <btrout.dhrs@gmail.com>
2020-02-02 00:13:42 -0500

    Fix package in AddressableLEDColorOrder, fix copyright year

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/AddressableLEDColorOrder.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/AddressableLEDColorOrder.java
@@ -1,11 +1,11 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package edu.wpi.first.wpilibj2;
+package edu.wpi.first.wpilibj;
 
 /**
  * Defines the order in which the LED strip displays colors
