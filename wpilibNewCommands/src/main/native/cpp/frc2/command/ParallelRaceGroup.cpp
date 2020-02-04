c2f05c1729b91ccfda8fe9914ee473c4637a1145
Joe <sciencewhiz@users.noreply.github.com>
2020-02-03 22:15:19 -0800

    Fix running ParallelRaceGroup multiple times

--- wpilibNewCommands/src/main/native/cpp/frc2/command/ParallelRaceGroup.cpp
+++ wpilibNewCommands/src/main/native/cpp/frc2/command/ParallelRaceGroup.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -19,6 +19,7 @@ void ParallelRaceGroup::Initialize() {
     commandRunning->Initialize();
   }
   isRunning = true;
+  m_finished = false;
 }
 
 void ParallelRaceGroup::Execute() {
