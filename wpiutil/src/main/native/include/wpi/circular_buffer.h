05b7593e66c8b0dc39f7f32878c660943b9cd5e8
Oblarg <emichaelbarnett@gmail.com>
2020-02-08 16:35:21 -0500

    C++ circular_buffer: support types not implicitly convertible from int (#2350)
    Also fixes two cases of returning a reference to a constant.

--- wpiutil/src/main/native/include/wpi/circular_buffer.h
+++ wpiutil/src/main/native/include/wpi/circular_buffer.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2015-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2015-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -46,6 +46,8 @@ class circular_buffer {
 
  private:
   std::vector<T> m_data;
+
+  T zero_val{0};
 
   // Index of element at front of buffer
   size_t m_front = 0;
