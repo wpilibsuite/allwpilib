e4ed243bfdf884d233d91eb7c0ae227f432fb956
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2020-03-15 22:59:11 +0200

    styleguide cleanup

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/CommandBase.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/CommandBase.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -55,7 +55,7 @@ public abstract class CommandBase implements Sendable, Command {
   public void setName(String name) {
     SendableRegistry.setName(this, name);
   }
-  
+
   /**
   * Decorates this Command with a name.
   * Is an inline function for #setName(String);
