48a38f5c597a02b5b01e9fa9dd5f8c443ec9599c
Matt <matthew.morley.ca@gmail.com>
2020-03-16 09:55:37 -0700

    Update DrakeJNI.cpp

--- wpiutil/src/main/native/cpp/jni/DrakeJNI.cpp
+++ wpiutil/src/main/native/cpp/jni/DrakeJNI.cpp
@@ -21,21 +21,6 @@ using namespace wpi::java;
 
 extern "C" {
 
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    // Check to ensure the JNI version is valid
-
-    JNIEnv* env;
-    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK)
-        return JNI_ERR;
-
-    // In here is also where you store things like class references
-    // if they are ever needed
-
-    return JNI_VERSION_1_6;
-}
-
-JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {}
-
 /*
  * Class:     edu_wpi_first_wpiutil_math_DrakeJNI
  * Method:    discreteAlgebraicRiccatiEquation
