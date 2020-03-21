91f684732f433c2ef0de040308c764f7d0bd26a5
Matt <matthew.morley.ca@gmail.com>
2020-03-20 17:17:48 -0700

    Add vector and vecbuilder methods to make construction easier

--- wpiutil/src/main/java/edu/wpi/first/wpiutil/math/Vector.java
+++ wpiutil/src/main/java/edu/wpi/first/wpiutil/math/Vector.java
@@ -7,13 +7,22 @@
 
 package edu.wpi.first.wpiutil.math;
 
-import edu.wpi.first.wpiutil.math.numbers.N1;
 import org.ejml.simple.SimpleMatrix;
+
+import edu.wpi.first.wpiutil.math.numbers.N1;
 
 public class Vector<R extends Num> extends Matrix<R, N1> {
 
-    public Vector(SimpleMatrix storage) {
-        super(storage);
-    }
+  public Vector(SimpleMatrix storage) {
+    super(storage);
+  }
+
+  public Vector(Nat<R> size, double... data) {
+    super(new SimpleMatrix(size.getNum(), 1, true, data));
+  }
+
+  public static Vector<N1> d1(double data) {
+    return new Vector<>(Nat.N1(), data);
+  }
 
 }
