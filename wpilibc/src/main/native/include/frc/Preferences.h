e2d8b38d76928a03be75daffbde9e8b6a02f1fc9
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-03-22 18:17:16 -0400

    Run WPIFormat

--- wpilibc/src/main/native/include/frc/Preferences.h
+++ wpilibc/src/main/native/include/frc/Preferences.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2011-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2011-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -120,7 +120,6 @@ class Preferences : public ErrorBase {
    * @param value the value
    */
   void PutString(wpi::StringRef key, wpi::StringRef value);
-
 
   /**
    * Puts the given string into the preferences table if it doesn't
