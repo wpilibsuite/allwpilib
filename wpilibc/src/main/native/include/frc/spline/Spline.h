feaa5418ad7159961795348d49136570356526ca
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-03-22 21:39:53 -0400

    Run WPIFormat

--- wpilibc/src/main/native/include/frc/spline/Spline.h
+++ wpilibc/src/main/native/include/frc/spline/Spline.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,11 +10,10 @@
 #include <array>
 #include <utility>
 #include <vector>
-#include <units/units.h>
-
-#include <wpi/deprecated.h>
 
 #include <Eigen/Core>
+#include <units/units.h>
+#include <wpi/deprecated.h>
 
 #include "frc/geometry/Pose2d.h"
 
--- wpilibc/src/main/native/include/frc/trajectory/constraint/TrajectoryConstraint.h
+++ wpilibc/src/main/native/include/frc/trajectory/constraint/TrajectoryConstraint.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -72,7 +72,8 @@ class TrajectoryConstraint {
    *
    * @return The min and max acceleration bounds.
    */
-  virtual MinMax MinMaxAcceleration(const Pose2d& pose, units::curvature_t curvature,
+  virtual MinMax MinMaxAcceleration(const Pose2d& pose,
+                                    units::curvature_t curvature,
                                     units::meters_per_second_t speed) = 0;
 };
 }  // namespace frc
