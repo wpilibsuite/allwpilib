c2d7845c873a6b7afa86f8226515b43a538c61a0
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2020-03-24 12:15:21 +0200

    Added documentation to prevent looping end() by scheduling commands,
    helps #2441

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/Command.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/Command.java
@@ -36,6 +36,7 @@ public interface Command {
   /**
    * The action to take when the command ends.  Called when either the command finishes normally,
    * or when it interrupted/canceled.
+   *<p>Do not scedule commands here that share requirements with this command. Use {@link #andThen(Command)} instead.
    *
    * @param interrupted whether the command was interrupted/canceled
    */
