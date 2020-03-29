deb624e2fea11169535c74406a9a364ec7a435a0
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-03-29 15:56:16 -0400

    Run WPIFormat

--- wpilibc/src/main/native/cpp/Tracer.cpp
+++ wpilibc/src/main/native/cpp/Tracer.cpp
@@ -1,7 +1,14 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #include "frc/Tracer.h"
 
-#include <wpi/raw_ostream.h>
 #include <wpi/Format.h>
+#include <wpi/raw_ostream.h>
 
 using namespace frc;
 
--- wpilibc/src/main/native/include/frc/Tracer.h
+++ wpilibc/src/main/native/include/frc/Tracer.h
@@ -1,11 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #pragma once
 
 #include <chrono>
 
-#include <wpi/StringMap.h>
-#include <wpi/StringRef.h>
 #include <hal/cpp/fpga_clock.h>
 #include <units/units.h>
+#include <wpi/StringMap.h>
+#include <wpi/StringRef.h>
 
 namespace frc {
 /**
