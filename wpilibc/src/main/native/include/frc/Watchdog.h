bb2ee525c703e47629b47a16d089449b151860ae
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-03-29 16:01:55 -0400

    Remove StringMap include from Watchdog.h

--- wpilibc/src/main/native/include/frc/Watchdog.h
+++ wpilibc/src/main/native/include/frc/Watchdog.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -11,14 +11,13 @@
 #include <functional>
 #include <utility>
 
-#include "frc/Tracer.h"
-
 #include <hal/cpp/fpga_clock.h>
 #include <units/units.h>
 #include <wpi/SafeThread.h>
-#include <wpi/StringMap.h>
 #include <wpi/StringRef.h>
 #include <wpi/deprecated.h>
+
+#include "frc/Tracer.h"
 
 namespace frc {
 
