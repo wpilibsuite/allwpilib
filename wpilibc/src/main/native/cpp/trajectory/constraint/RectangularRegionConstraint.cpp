96c4854241efa03741d6a024cd84749ed3de25b0
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-04-02 23:34:59 -0400

    Add C++ constraints

---
+++ wpilibc/src/main/native/cpp/trajectory/constraint/RectangularRegionConstraint.cpp
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "frc/trajectory/constraint/RectangularRegionConstraint.h"
+
+#include <limits>
+
+using namespace frc;
+
+frc::RectangularRegionConstraint::RectangularRegionConstraint(
+    const Translation2d& bottomLeftPoint, const Translation2d& topRightPoint,
+    TrajectoryConstraint* constraint)
+    : m_bottomLeftPoint(bottomLeftPoint),
+      m_topRightPoint(topRightPoint),
+      m_constraint(constraint) {}
+
+units::meters_per_second_t RectangularRegionConstraint::MaxVelocity(
+    const Pose2d& pose, units::curvature_t curvature,
+    units::meters_per_second_t velocity) {
+  if (IsPoseInRegion(pose)) {
+    return m_constraint->MaxVelocity(pose, curvature, velocity);
+  } else {
+    return units::meters_per_second_t(std::numeric_limits<double>::infinity());
+  }
+}
+
+TrajectoryConstraint::MinMax RectangularRegionConstraint::MinMaxAcceleration(
+    const Pose2d& pose, units::curvature_t curvature,
+    units::meters_per_second_t speed) {
+  if (IsPoseInRegion(pose)) {
+    return m_constraint->MinMaxAcceleration(pose, curvature, speed);
+  } else {
+    return {};
+  }
+}
+
+bool frc::RectangularRegionConstraint::IsPoseInRegion(const Pose2d& pose) {
+  return pose.Translation().X() >= m_bottomLeftPoint.X() &&
+         pose.Translation().X() <= m_topRightPoint.X() &&
+         pose.Translation().Y() >= m_bottomLeftPoint.Y() &&
+         pose.Translation().Y() <= m_topRightPoint.Y();
+}
