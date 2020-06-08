4b76adf15bdc0d747bfc59bd06ed8aa4501c29a6
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-06-08 10:37:27 -0700

    [wpilibc] Remove incorrect timer rollover (#2523)
    If the 64 bit FPGA timer rolls over, a 32 bit value is added for
    the rollover, an artifact of when it was a 32 bit timer.
    The 64 bit microsecond timer won't rollover for 500k years so remove the
    check for simplicity.
    Fixes #2504

--- wpilibc/src/main/native/include/frc/Timer.h
+++ wpilibc/src/main/native/include/frc/Timer.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2008-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -131,9 +131,6 @@ class Timer {
    * @return Time remaining in current match period (auto or teleop)
    */
   static double GetMatchTime();
-
-  // The time, in seconds, at which the 32-bit FPGA timestamp rolls over to 0
-  static const double kRolloverTime;
 
  private:
   frc2::Timer m_timer;
