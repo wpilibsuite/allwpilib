bcad3f907e1c46eafd4f509d96c4f7c4cde5bc6f
Joe <sciencewhiz@users.noreply.github.com>
2020-06-15 21:22:21 -0700

    [command] Add GetSetpoint to PIDSubsystem

--- wpilibNewCommands/src/main/native/include/frc2/command/PIDSubsystem.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/PIDSubsystem.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -36,6 +36,13 @@ class PIDSubsystem : public SubsystemBase {
    * @param setpoint the setpoint for the subsystem
    */
   void SetSetpoint(double setpoint);
+
+  /**
+   * Gets the setpoint for the subsystem.
+   *
+   * @return the setpoint for the subsystem
+   */
+  double GetSetpoint();
 
   /**
    * Enables the PID control.  Resets the controller.
