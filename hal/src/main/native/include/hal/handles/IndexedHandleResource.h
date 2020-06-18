8507f62eb4587e63a538cc25a7cca49ab4bae51c
Peter Johnson <johnson.peter@gmail.com>
2020-06-17 23:40:19 -0700

    [hal] Add GetIndex() to handles classes
    This provides a lock-free way to get just the handle index.

--- hal/src/main/native/include/hal/handles/IndexedHandleResource.h
+++ hal/src/main/native/include/hal/handles/IndexedHandleResource.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2016-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2016-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -43,6 +43,9 @@ class IndexedHandleResource : public HandleBase {
   IndexedHandleResource& operator=(const IndexedHandleResource&) = delete;
 
   THandle Allocate(int16_t index, int32_t* status);
+  int16_t GetIndex(THandle handle) {
+    return getHandleTypedIndex(handle, enumValue, m_version);
+  }
   std::shared_ptr<TStruct> Get(THandle handle);
   void Free(THandle handle);
   void ResetHandles() override;
@@ -76,7 +79,7 @@ template <typename THandle, typename TStruct, int16_t size,
 std::shared_ptr<TStruct>
 IndexedHandleResource<THandle, TStruct, size, enumValue>::Get(THandle handle) {
   // get handle index, and fail early if index out of range or wrong handle
-  int16_t index = getHandleTypedIndex(handle, enumValue, m_version);
+  int16_t index = GetIndex(handle);
   if (index < 0 || index >= size) {
     return nullptr;
   }
@@ -91,7 +94,7 @@ template <typename THandle, typename TStruct, int16_t size,
 void IndexedHandleResource<THandle, TStruct, size, enumValue>::Free(
     THandle handle) {
   // get handle index, and fail early if index out of range or wrong handle
-  int16_t index = getHandleTypedIndex(handle, enumValue, m_version);
+  int16_t index = GetIndex(handle);
   if (index < 0 || index >= size) return;
   // lock and deallocated handle
   std::scoped_lock lock(m_handleMutexes[index]);
