510f572cc03eaf0bb4d94b2ed72d18dbecb77b16
Peter Johnson <johnson.peter@gmail.com>
2020-06-27 15:22:45 -0700

    [cscore] Fix USB camera test failure
    Introduced by camera filtering in #2532.

--- cscore/src/test/java/edu/wpi/cscore/UsbCameraTest.java
+++ cscore/src/test/java/edu/wpi/cscore/UsbCameraTest.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -56,6 +56,6 @@ class UsbCameraTest {
   private static int getNonexistentCameraDev() {
     return Arrays.stream(CameraServerJNI.enumerateUsbCameras())
         .mapToInt(info -> info.dev)
-        .max().orElse(-1) + 1;
+        .max().orElse(-1) + 20;
   }
 }
