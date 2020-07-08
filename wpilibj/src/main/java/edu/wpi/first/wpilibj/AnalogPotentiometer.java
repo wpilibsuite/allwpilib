4d4decaf54cc6481bd52a2345dff235794febaf7
Joe <sciencewhiz@users.noreply.github.com>
2020-07-08 06:38:31 -0700

    Fix line length

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/AnalogPotentiometer.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/AnalogPotentiometer.java
@@ -47,8 +47,8 @@ public class AnalogPotentiometer implements Potentiometer, Sendable, AutoCloseab
    * AnalogPotentiometer constructor.
    *
    * <p>Use the fullRange and offset values so that the output produces meaningful values. I.E: you
-   * have a 270 degree potentiometer and you want the output to be degrees with the halfway point as
-   * 0 degrees. The fullRange value is 270.0(degrees) and the offset is -135.0 since the halfway
+   * have a 270 degree potentiometer and you want the output to be degrees with the halfway point
+   * as 0 degrees. The fullRange value is 270.0(degrees) and the offset is -135.0 since the halfway
    * point after scaling is 135 degrees. This will calculate the result from the fullRange times
    * the fraction of the supply voltage, plus the offset.
    *
@@ -71,8 +71,8 @@ public class AnalogPotentiometer implements Potentiometer, Sendable, AutoCloseab
    * AnalogPotentiometer constructor.
    *
    * <p>Use the scale value so that the output produces meaningful values. I.E: you
-   * have a 270 degree potentiometer and you want the output to be degrees with the starting point as
-   * 0 degrees. The scale value is 270.0(degrees).
+   * have a 270 degree potentiometer and you want the output to be degrees with the starting point
+   * as 0 degrees. The scale value is 270.0(degrees).
    *
    * @param channel The analog input channel this potentiometer is plugged into. 0-3 are
    *                on-board and 4-7 are on the MXP port.
@@ -86,8 +86,8 @@ public class AnalogPotentiometer implements Potentiometer, Sendable, AutoCloseab
    * AnalogPotentiometer constructor.
    *
    * <p>Use the fullRange and offset values so that the output produces meaningful values. I.E: you
-   * have a 270 degree potentiometer and you want the output to be degrees with the starting point as
-   * 0 degrees. The scale value is 270.0(degrees).
+   * have a 270 degree potentiometer and you want the output to be degrees with the starting point
+   * as 0 degrees. The scale value is 270.0(degrees).
    *
    * @param input The {@link AnalogInput} this potentiometer is plugged into.
    * @param scale The scaling to multiply the voltage by to get a meaningful unit.
