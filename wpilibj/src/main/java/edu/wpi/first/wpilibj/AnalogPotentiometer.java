ac4b0a311815cca43b538b8dd33bcf09db3d36c0
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-07-10 00:29:23 -0400

    [wpilib] AnalogPotentiometer: Use average voltage (#2583)
    Using GetAverageVoltage will reduce the noise in potentiometer reading. Potentiometers are unlikely to be used where the minimal lag averaging introduces would impact control loop stability.

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/AnalogPotentiometer.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/AnalogPotentiometer.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2008-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -122,7 +122,8 @@ public class AnalogPotentiometer implements Potentiometer, Sendable, AutoCloseab
     if (m_analogInput == null) {
       return m_offset;
     }
-    return (m_analogInput.getVoltage() / RobotController.getVoltage5V()) * m_fullRange + m_offset;
+    return (m_analogInput.getAverageVoltage() / RobotController.getVoltage5V())
+        * m_fullRange + m_offset;
   }
 
   @Override
