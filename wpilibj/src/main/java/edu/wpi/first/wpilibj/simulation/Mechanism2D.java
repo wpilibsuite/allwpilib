8841f48e826bac0c2ecd8f96f963d6410a448d25
Gabe Deml <gddeml@wpi.edu>
2020-07-29 21:31:01 -0400

    Fixed formatting problem?

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/simulation/Mechanism2D.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/simulation/Mechanism2D.java
@@ -9,17 +9,14 @@ package edu.wpi.first.wpilibj.simulation;
 
 import edu.wpi.first.hal.SimDevice;
 import edu.wpi.first.hal.SimDouble;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
-import java.util.HashMap;
+
 
 public class Mechanism2D {
   SimDevice m_device = SimDevice.create("Mechanism2D");
-  HashMap<String, SimDouble> createdItems = new HashMap<String, SimDouble>();
+  HashMap<String, SimDouble> CreatedItems = new HashMap<String, SimDouble>();
 
   /**
-     * Set/Create the angle of a ligament
+     * Set/Create the angle of a ligament.
      *
      * @param ligamentPath json path to the ligament
      * @param angle        to set the ligament
@@ -27,28 +24,28 @@ public class Mechanism2D {
   public void setLigamentAngle(String ligamentPath, float angle) {
       ligamentPath = ligamentPath + "angle/";
     if (m_device != null) {
-//        System.out.println("java: " + ligamentPath + " " + angle);
-        if(!createdItems.containsKey(ligamentPath)){
-            createdItems.put(ligamentPath, m_device.createDouble(ligamentPath, false, angle));
+        if(!CreatedItems.containsKey(ligamentPath)){
+            CreatedItems.put(ligamentPath, m_device.createDouble(ligamentPath, false, angle));
         }
-            createdItems.get(ligamentPath).set(angle);
+
+        CreatedItems.get(ligamentPath).set(angle);
     }
+
   }
 
     /**
-     * Set/Create the length of a ligament
+     * Set/Create the length of a ligament.
      *
      * @param ligamentPath json path to the ligament
      * @param length       to set the ligament
      */
   public void setLigamentLength(String ligamentPath, float length) {
       ligamentPath = ligamentPath + "length/";
-//      System.out.println("java length : " + ligamentPath + " " + length);
     if (m_device != null) {
-        if(!createdItems.containsKey(ligamentPath)){
-            createdItems.put(ligamentPath, m_device.createDouble(ligamentPath, false, length));
+        if(!CreatedItems.containsKey(ligamentPath)){
+            CreatedItems.put(ligamentPath, m_device.createDouble(ligamentPath, false, length));
         }
-            createdItems.get(ligamentPath).set(length);
+        CreatedItems.get(ligamentPath).set(length);
     }
   }
 }
