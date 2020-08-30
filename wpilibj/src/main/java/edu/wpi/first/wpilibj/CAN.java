28b4efb107da8f0f7f0b95edaa37b10cec2b05f6
Thad House <thadhouse1@gmail.com>
2020-08-29 21:53:16 -0700

    Fix dates and pmd

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/CAN.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/CAN.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -25,6 +25,7 @@ import edu.wpi.first.hal.HAL;
  * read methods and the byte[] passed into the write methods need to not
  * be modified for the duration of their respective calls.
  */
+@SuppressWarnings("PMD.TooManyMethods")
 public class CAN implements Closeable {
   public static final int kTeamManufacturer = 8;
   public static final int kTeamDeviceType = 10;
