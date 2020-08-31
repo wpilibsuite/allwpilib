ad6c8b882e85bab2bd591c8aae72e51375228e4d
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-08-31 00:31:30 -0700

    [hal] Fix PDP Energy Calculation (#2672)
    Fixes #2642

--- hal/src/main/native/athena/PDP.cpp
+++ hal/src/main/native/athena/PDP.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2016-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2016-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -438,7 +438,7 @@ double HAL_GetPDPTotalEnergy(HAL_PDPHandle handle, int32_t* status) {
   energyJoules *=
       pdpStatus.bits
           .TmeasMs_likelywillbe20ms_; /* multiplied by TmeasMs = joules */
-  return 0.125 * raw;
+  return energyJoules;
 }
 
 void HAL_ResetPDPTotalEnergy(HAL_PDPHandle handle, int32_t* status) {
