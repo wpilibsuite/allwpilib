1c9e751a3029fcdde235297434a6deda0d4fc191
Matt <matthew.morley.ca@gmail.com>
2020-09-14 23:31:16 -0400

    format gdi

--- wpimath/src/main/native/include/frc/system/plant/LinearSystemId.h
+++ wpimath/src/main/native/include/frc/system/plant/LinearSystemId.h
@@ -187,8 +187,8 @@ class LinearSystemId {
    * @param J Moment of inertia.
    */
   static LinearSystem<2, 2, 2> DrivetrainVelocitySystem(
-      const DCMotor& motor, units::kilogram_t m, units::meter_t r, units::meter_t rb,
-      units::kilogram_square_meter_t J, double G) {
+      const DCMotor& motor, units::kilogram_t m, units::meter_t r,
+      units::meter_t rb, units::kilogram_square_meter_t J, double G) {
     auto C1 = -std::pow(G, 2) * motor.Kt /
               (motor.Kv * motor.R * units::math::pow<2>(r));
     auto C2 = G * motor.Kt / (motor.R * r);
