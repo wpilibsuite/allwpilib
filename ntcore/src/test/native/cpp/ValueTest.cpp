09986ddfc38e9b6e255c53c7b59d6283b23df10d
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-10-21 13:31:27 -0400

    Run WPIFormat

--- ntcore/src/test/native/cpp/ValueTest.cpp
+++ ntcore/src/test/native/cpp/ValueTest.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2015-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2015-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
