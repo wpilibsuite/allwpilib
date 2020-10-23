5fe8f9017fc411e2e32f5959db552e19a25504c3
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-10-23 00:52:24 -0400

    [build] Refactor CMake flags (#2788)
    The CMake enable/disable flags as currently structured are a confusing mix of
    WITH, WITHOUT, and USE with odd defaults.  This changes the flags to consistently
    use WITH and default the build options to everything enabled.

--- ntcore/src/test/native/cpp/ValueTest.cpp
+++ ntcore/src/test/native/cpp/ValueTest.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2015-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2015-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -241,7 +241,11 @@ TEST_F(ValueTest, StringArray) {
   NT_DisposeValue(&cv);
 }
 
+#ifdef NDEBUG
+TEST_F(ValueDeathTest, DISABLED_GetAssertions) {
+#else
 TEST_F(ValueDeathTest, GetAssertions) {
+#endif
   Value v;
   ASSERT_DEATH((void)v.GetBoolean(), "type == NT_BOOLEAN");
   ASSERT_DEATH((void)v.GetDouble(), "type == NT_DOUBLE");
