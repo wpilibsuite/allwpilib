8cd42478e1e44b63b9d9d759ac02dea95a5b37a7
Peter Johnson <johnson.peter@gmail.com>
2020-10-26 00:15:21 -0700

    [wpilib] SendableBuilder: Make GetTable() visible

--- wpilibc/src/main/native/include/frc/smartdashboard/SendableBuilder.h
+++ wpilibc/src/main/native/include/frc/smartdashboard/SendableBuilder.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -12,6 +12,7 @@
 #include <string>
 #include <vector>
 
+#include <networktables/NetworkTable.h>
 #include <networktables/NetworkTableEntry.h>
 #include <networktables/NetworkTableValue.h>
 #include <wpi/ArrayRef.h>
@@ -23,6 +24,12 @@ namespace frc {
 class SendableBuilder {
  public:
   virtual ~SendableBuilder() = default;
+
+  /**
+   * Get the network table.
+   * @return The network table
+   */
+  virtual std::shared_ptr<nt::NetworkTable> GetTable() = 0;
 
   /**
    * Set the string representation of the named data type that will be used
