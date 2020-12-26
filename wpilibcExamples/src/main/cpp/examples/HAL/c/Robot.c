9f0d487ceb9c30d66c8b2b7ad93541e829eec76f
Peter Johnson <johnson.peter@gmail.com>
2020-12-26 09:32:56 -0800

    Update copyright header across all files

--- wpilibcExamples/src/main/cpp/examples/HAL/c/Robot.c
+++ wpilibcExamples/src/main/cpp/examples/HAL/c/Robot.c
@@ -1,9 +1,6 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 /*
 This example shows how to use the HAL directly, and what is needed to run a
