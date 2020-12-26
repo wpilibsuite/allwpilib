800edc50a4b62fd3c01b839bf87aeff45afbcc13
Peter Johnson <johnson.peter@gmail.com>
2020-12-26 14:21:53 -0800

    [glass] Fix styleguide

--- glass/src/libnt/native/cpp/NetworkTables.cpp
+++ glass/src/libnt/native/cpp/NetworkTables.cpp
@@ -1,11 +1,10 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 #include "glass/networktables/NetworkTables.h"
+
+#include <networktables/NetworkTableValue.h>
 
 #include <cinttypes>
 #include <cstdio>
@@ -15,7 +14,6 @@
 #include <vector>
 
 #include <imgui.h>
-#include <networktables/NetworkTableValue.h>
 #include <ntcore_cpp.h>
 #include <wpi/ArrayRef.h>
 #include <wpi/Format.h>
