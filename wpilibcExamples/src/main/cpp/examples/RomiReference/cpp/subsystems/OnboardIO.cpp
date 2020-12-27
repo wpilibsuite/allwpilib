86c44adeb3d114832e9c90e1c88acd35850165c3
Starlight220 <yotamshlomi@gmail.com>
2020-12-27 14:01:40 +0200

    implement OnBoardIO

--- wpilibcExamples/src/main/cpp/examples/RomiReference/cpp/subsystems/OnboardIO.cpp
+++ wpilibcExamples/src/main/cpp/examples/RomiReference/cpp/subsystems/OnboardIO.cpp
@@ -1,1 +1,75 @@
-#include "subsystems/OnboardIO.h"
+#include "subsystems/OnBoardIO.h"
+#include "frc/DriverStation.h"
+
+OnBoardIO::OnBoardIO(OnBoardIO::ChannelMode dio1, OnBoardIO::ChannelMode dio2)
+{
+  if(dio1 == ChannelMode::INPUT)
+  {
+    m_buttonB {1};
+  } else
+  {
+    m_greenLed {1};
+  }
+  if (dio2 == ChannelMode::INPUT)
+  {
+    m_buttonC {2};
+    m_redLed {2};
+  }
+}
+
+bool OnBoardIO::GetButtonAPressed() { return m_buttonA.Get(); }
+
+bool OnBoardIO::GetButtonBPressed() {
+  if (m_buttonB != nullptr) {
+    return m_buttonB.Get();
+  }
+
+  double currentTime = frc2::Timer::GetFPGATimestamp();
+  if (currentTime > m_nextMessageTime) {
+    DriverStation::ReportError("Button B was not configured");
+    m_nextMessageTime = currentTime + MESSAGE_INTERVAL;
+  }
+  return false;
+}
+
+bool OnBoardIO::GetButtonCPressed() {
+  if (m_buttonC != nullptr) {
+    return m_buttonC.Get();
+  }
+
+  double currentTime = frc2::Timer::GetFPGATimestamp();
+  if (currentTime > m_nextMessageTime) {
+    DriverStation::ReportError("Button C was not configured");
+    m_nextMessageTime = currentTime + MESSAGE_INTERVAL;
+  }
+  return false;
+}
+
+void OnBoardIO::SetGreenLed(bool value) {
+  if (m_greenLed != nullptr) {
+    m_greenLed.Set(value);
+  } else {
+    double currentTime = frc2::Timer::GetFPGATimestamp();
+    if (currentTime > m_nextMessageTime) {
+      DriverStation::ReportError("Green LED was not configured");
+      m_nextMessageTime = currentTime + MESSAGE_INTERVAL;
+    }
+  }
+}
+
+void OnBoardIO::SetRedLed(bool value) {
+  if (m_redLed != nullptr) {
+    m_redLed.Set(value);
+  } else {
+    double currentTime = frc2::Timer::GetFPGATimestamp();
+    if (currentTime > m_nextMessageTime) {
+      DriverStation::ReportError("Red LED was not configured");
+      m_nextMessageTime = currentTime + MESSAGE_INTERVAL;
+    }
+  }
+}
+
+void OnBoardIO::SetYellowLed(bool value) {
+  m_yellowLed.Set(value);
+}
+
--- wpilibcExamples/src/main/cpp/examples/RomiReference/include/subsystems/OnboardIO.h
+++ wpilibcExamples/src/main/cpp/examples/RomiReference/include/subsystems/OnboardIO.h
@@ -1,3 +1,12 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
 /**
  * This class represents the onboard IO of the Romi
  * reference robot. This includes the pushbuttons and
@@ -8,6 +17,53 @@
  * DIO 2 - Button C (input) or Red LED (output)
  * DIO 3 - Yellow LED (output only)
  */
-class OnboardIO : public frc2::SubsystemBase {
+class OnBoardIO : public frc2::SubsystemBase {
+ public:
+  enum ChannelMode { INPUT, OUTPUT };
+  OnBoardIO(OnBoardIO::ChannelMode dio1, ChannelMode dio2);
 
+  static constexpr double MESSAGE_INTERVAL = 1.0;
+  double m_nextMessageTime;
+
+  /**
+   * Gets if the A button is pressed.
+   */
+  bool GetButtonAPressed();
+
+  /**
+   * Gets if the B button is pressed.
+   */
+  bool GetButtonBPressed();
+
+  /**
+   * Gets if the C button is pressed.
+   */
+  bool GetButtonCPressed();
+
+  /**
+   * Sets the green LED.
+   */
+  void SetGreenLed(bool value);
+
+  /**
+   * Sets the red LED.
+   */
+  void SetRedLed(bool value);
+
+  /**
+   * Sets the yellow LED.
+   */
+  void SetYellowLed(bool value);
+
+ private:
+  frc::DigitalInput m_buttonA{0};
+  frc::DigitalOutput m_yellowLed{3};
+
+  // DIO 1
+  frc::DigitalInput m_buttonB;
+  frc::DigitalOutput m_greenLed;
+
+  // DIO 2
+  frc::DigitalInput m_buttonC;
+  frc::DigitalOutput m_redLed;
 };
