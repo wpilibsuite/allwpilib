08ae0f588074189a5fd413983350077137405b37
jpokornyiii <jpokornyiii@gmail.com>
2020-12-29 15:04:46 -0500

    Fixing build errors

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/romireference/commands/DriveDistance.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/romireference/commands/DriveDistance.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package edu.wpi.first.wpilibj.examples.romireference.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.examples.romireference.Drivetrain;
 
 public class DriveDistance extends CommandBase {
   private final Drivetrain m_drive;
