8651aa73e8336b8c126d5ba26f69369bdf9a995e
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-12-30 19:12:15 -0500

    [examples] Enable NT Flush in Field2d examples (#3013)

--- wpilibcExamples/src/main/cpp/examples/StateSpaceDifferentialDriveSimulation/cpp/Robot.cpp
+++ wpilibcExamples/src/main/cpp/examples/StateSpaceDifferentialDriveSimulation/cpp/Robot.cpp
@@ -9,7 +9,11 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() {}
+void Robot::RobotInit() {
+  // Flush NetworkTables every loop. This ensures that robot pose and other
+  // values are sent during every iteration.
+  SetNetworkTablesFlushEnabled(true);
+}
 
 /**
  * This function is called every robot packet, no matter the mode. Use
