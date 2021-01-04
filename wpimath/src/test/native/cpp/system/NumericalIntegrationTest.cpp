7472ab783c3252712c2279a56b2094b856920980
Matt <matthew.morley.ca@gmail.com>
2021-01-04 15:28:39 -0800

    eeeeeeee

--- wpimath/src/test/native/cpp/system/NumericalIntegrationTest.cpp
+++ wpimath/src/test/native/cpp/system/NumericalIntegrationTest.cpp
@@ -38,18 +38,18 @@ TEST(NumericalIntegrationTest, ExponentialWithU) {
   EXPECT_NEAR(y1(0), std::exp(0.1) - std::exp(0), 1e-3);
 }
 
-// Tests that integrating dx/dt = e^x works with RKF45
-TEST(NumericalIntegrationTest, ExponentialAdaptive) {
+// Tests that integrating dx/dt = e^x works when we provide a U.
+TEST(NumericalIntegrationTest, ExponentialWithUAdaptive) {
   Eigen::Matrix<double, 1, 1> y0;
   y0(0) = 0.0;
 
-  Eigen::Matrix<double, 1, 1> y1 = frc::RungeKuttaAdaptive<>(
-      [](Eigen::Matrix<double, 1, 1> x) {
+  Eigen::Matrix<double, 1, 1> y1 = frc::RungeKuttaAdaptive(
+      [](Eigen::Matrix<double, 1, 1> x, Eigen::Matrix<double, 1, 1> u) {
         Eigen::Matrix<double, 1, 1> y;
-        y(0) = std::exp(x(0));
+        y(0) = std::exp(u(0) * x(0));
         return y;
       },
-      y0, 0.1_s);
+      y0, (Eigen::Matrix<double, 1, 1>() << 1.0).finished(), 0.1_s);
   EXPECT_NEAR(y1(0), std::exp(0.1) - std::exp(0), 1e-3);
 }
 
