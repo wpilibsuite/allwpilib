3cc08a03d7ef93882398295a9e58a1c608c15a42
Matt <matthew.morley.ca@gmail.com>
2021-01-08 20:36:46 -0800

    Update wpimath/src/main/native/include/frc/kinematics/SwerveModuleState.h
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

--- wpimath/src/main/native/include/frc/kinematics/SwerveModuleState.h
+++ wpimath/src/main/native/include/frc/kinematics/SwerveModuleState.h
@@ -25,10 +25,10 @@ struct SwerveModuleState {
   Rotation2d angle;
 
   /**
-   * Optimize a desired state by deciding weather to reverse the direction the
-   * wheel spins in order to minimize travel time. This means that the furthest
-   * a wheel will ever rotate is 90 degrees, if used in conjunction with the
-   * PIDController class' continuous input functionality.
+   * Minimize the change in heading the desired swerve module state would
+   * require by potentially reversing the direction the wheel spins. If this is
+   * used with the PIDController class's continuous input functionality, the
+   * furthest a wheel will ever rotate is 90 degrees.
    *
    * @param desiredState The desired state.
    * @param currentAngle The current module angle.
