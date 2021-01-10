1cc21944a2545a378a73fe26bf3fcccc75016ec6
Matt <matthew.morley.ca@gmail.com>
2021-01-10 13:22:46 -0800

    Fix javadoc

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/swervebot/SwerveModule.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/swervebot/SwerveModule.java
@@ -79,7 +79,7 @@ public class SwerveModule {
   /**
    * Sets the desired state for the module.
    *
-   * @param state Desired state with speed and angle.
+   * @param desiredState Desired state with speed and angle.
    */
   public void setDesiredState(SwerveModuleState desiredState) {
     // Optimize the reference state to avoid spinning further than 90 degrees
