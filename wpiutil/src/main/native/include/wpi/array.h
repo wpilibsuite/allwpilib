d8395aa9bd18f9f95e5226969fa796fd05df6a74
Tyler Veness <calcmogul@gmail.com>
2021-01-14 13:01:18 -0800

    Add default constructors and assignment operators

--- wpiutil/src/main/native/include/wpi/array.h
+++ wpiutil/src/main/native/include/wpi/array.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <array>
+#include <cstddef>
 #include <utility>
 
 namespace wpi {
@@ -16,13 +17,18 @@ namespace wpi {
  * std::array's implicit constructor can lead result in uninitialized elements
  * if the number of arguments doesn't match the std::array size.
  */
-template <typename T, int N>
+template <typename T, size_t N>
 class array : public std::array<T, N> {
  public:
   template <typename... Ts>
   array(Ts&&... args) : std::array<T, N>{std::forward<Ts>(args)...} {  // NOLINT
     static_assert(sizeof...(args) == N, "Dimension mismatch");
   }
+
+  array(const array<T, N>&) = default;
+  array& operator=(const array<T, N>&) = default;
+  array(array<T, N>&&) = default;
+  array& operator=(array<T, N>&&) = default;
 };
 
 }  // namespace wpi
