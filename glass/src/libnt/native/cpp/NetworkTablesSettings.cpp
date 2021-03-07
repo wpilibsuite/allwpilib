ca8baffaf1153ca98ff0f6c11a7e513742478635
Peter Johnson <johnson.peter@gmail.com>
2021-03-07 14:15:55 -0800

    [glass] NetworkTablesSettings: Don't block GUI
    On some systems, StopClient et al can take a long time to execute.
    Instead run these on a separate thread to avoid blocking the GUI.

--- glass/src/libnt/native/cpp/NetworkTablesSettings.cpp
+++ glass/src/libnt/native/cpp/NetworkTablesSettings.cpp
@@ -17,14 +17,60 @@
 
 using namespace glass;
 
+void NetworkTablesSettings::Thread::Main() {
+  while (m_active) {
+    // wait to be woken up
+    std::unique_lock lock(m_mutex);
+    m_cond.wait(lock, [&] { return !m_active || m_restart; });
+    if (!m_active) {
+      break;
+    }
+
+    // clear restart flag
+    m_restart = false;
+
+    // if just changing servers in client mode, no need to stop and restart
+    unsigned int curMode = nt::GetNetworkMode(m_inst);
+
+    if (m_mode != 1 || (curMode & NT_NET_MODE_SERVER) != 0) {
+      // release lock while stopping to avoid blocking GUI
+      lock.unlock();
+      nt::StopClient(m_inst);
+      nt::StopServer(m_inst);
+      nt::StopLocal(m_inst);
+      lock.lock();
+    }
+
+    if (m_mode == 1) {
+      wpi::StringRef serverTeam{m_serverTeam};
+      unsigned int team;
+      if (!serverTeam.contains('.') && !serverTeam.getAsInteger(10, team)) {
+        nt::StartClientTeam(m_inst, team, NT_DEFAULT_PORT);
+      } else {
+        wpi::SmallVector<wpi::StringRef, 4> serverNames;
+        wpi::SmallVector<std::pair<wpi::StringRef, unsigned int>, 4> servers;
+        serverTeam.split(serverNames, ',', -1, false);
+        for (auto&& serverName : serverNames) {
+          servers.emplace_back(serverName, NT_DEFAULT_PORT);
+        }
+        nt::StartClient(m_inst, servers);
+      }
+    } else if (m_mode == 2) {
+      nt::StartServer(m_inst, m_iniName.c_str(), m_listenAddress.c_str(),
+                      NT_DEFAULT_PORT);
+    }
+  }
+}
+
 NetworkTablesSettings::NetworkTablesSettings(NT_Inst inst,
-                                             const char* storageName)
-    : m_inst{inst} {
+                                             const char* storageName) {
   auto& storage = glass::GetStorage(storageName);
   m_pMode = storage.GetIntRef("mode");
   m_pIniName = storage.GetStringRef("iniName", "networktables.ini");
   m_pServerTeam = storage.GetStringRef("serverTeam");
   m_pListenAddress = storage.GetStringRef("listenAddress");
+
+  m_thread.Start(inst);
 }
 
 void NetworkTablesSettings::Update() {
@@ -32,27 +78,15 @@ void NetworkTablesSettings::Update() {
     return;
   }
   m_restart = false;
-  nt::StopClient(m_inst);
-  nt::StopServer(m_inst);
-  nt::StopLocal(m_inst);
-  if (*m_pMode == 1) {
-    wpi::StringRef serverTeam{*m_pServerTeam};
-    unsigned int team;
-    if (!serverTeam.contains('.') && !serverTeam.getAsInteger(10, team)) {
-      nt::StartClientTeam(m_inst, team, NT_DEFAULT_PORT);
-    } else {
-      wpi::SmallVector<wpi::StringRef, 4> serverNames;
-      wpi::SmallVector<std::pair<wpi::StringRef, unsigned int>, 4> servers;
-      serverTeam.split(serverNames, ',', -1, false);
-      for (auto&& serverName : serverNames) {
-        servers.emplace_back(serverName, NT_DEFAULT_PORT);
-      }
-      nt::StartClient(m_inst, servers);
-    }
-  } else if (*m_pMode == 2) {
-    nt::StartServer(m_inst, m_pIniName->c_str(), m_pListenAddress->c_str(),
-                    NT_DEFAULT_PORT);
-  }
+
+  // do actual operation on thread
+  auto thr = m_thread.GetThread();
+  thr->m_restart = true;
+  thr->m_mode = *m_pMode;
+  thr->m_iniName = *m_pIniName;
+  thr->m_serverTeam = *m_pServerTeam;
+  thr->m_listenAddress = *m_pListenAddress;
+  thr->m_cond.notify_one();
 }
 
 bool NetworkTablesSettings::Display() {
--- glass/src/libnt/native/include/glass/networktables/NetworkTablesSettings.h
+++ glass/src/libnt/native/include/glass/networktables/NetworkTablesSettings.h
@@ -7,6 +7,7 @@
 #include <string>
 
 #include <ntcore_cpp.h>
+#include <wpi/SafeThread.h>
 
 namespace wpi {
 template <typename T>
@@ -25,12 +26,26 @@ class NetworkTablesSettings {
   bool Display();
 
  private:
-  NT_Inst m_inst;
   bool m_restart = true;
   int* m_pMode;
   std::string* m_pIniName;
   std::string* m_pServerTeam;
   std::string* m_pListenAddress;
+
+  class Thread : public wpi::SafeThread {
+   public:
+    explicit Thread(NT_Inst inst) : m_inst{inst} {}
+
+    void Main() override;
+
+    NT_Inst m_inst;
+    bool m_restart = false;
+    int m_mode;
+    std::string m_iniName;
+    std::string m_serverTeam;
+    std::string m_listenAddress;
+  };
+  wpi::SafeThreadOwner<Thread> m_thread;
 };
 
 }  // namespace glass
