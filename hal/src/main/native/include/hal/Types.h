3cbdd350e52e349df29efe7f66686924d2ff6f4a
David Vo <auscompgeek@users.noreply.github.com>
2021-04-10 19:23:46 +1000

    [hal] Use enums with fixed underlying type in clang C

--- hal/src/main/native/include/hal/Types.h
+++ hal/src/main/native/include/hal/Types.h
@@ -68,6 +68,11 @@ typedef int32_t HAL_Bool;
 
 #ifdef __cplusplus
 #define HAL_ENUM(name) enum name : int32_t
+#elif defined(__clang__)
+#define HAL_ENUM(name)    \
+  enum name : int32_t;    \
+  typedef enum name name; \
+  enum name : int32_t
 #else
 #define HAL_ENUM(name)  \
   typedef int32_t name; \
