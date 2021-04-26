e19de3429901453ef9f86e12723d1225296496ac
Noam Zaks <noamzaks@outlook.com>
2021-04-26 17:31:34 +0300

    Move SlewRateLimiter test

--- wpilibj/src/test/java/edu/wpi/first/math/filter/SlewRateLimiterTest.java
+++ wpilibj/src/test/java/edu/wpi/first/math/filter/SlewRateLimiterTest.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj;
+package edu.wpi.first.math.filter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.Timer;
 import org.junit.jupiter.api.Test;
 
 public class SlewRateLimiterTest {
