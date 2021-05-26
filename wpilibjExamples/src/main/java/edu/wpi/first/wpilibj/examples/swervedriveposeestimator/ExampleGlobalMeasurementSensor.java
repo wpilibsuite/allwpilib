0a0003c110c9149c121fc67a7bf1ad8f6a6cd1c0
Tyler Veness <calcmogul@gmail.com>
2021-05-25 20:55:24 -0700

    [wpilibjExamples] Fix name of Java swerve drive pose estimator example (#3382)

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/swervedriveposeestimator/ExampleGlobalMeasurementSensor.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/swervedriveposeestimator/ExampleGlobalMeasurementSensor.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj.examples.swervesdriveposeestimator;
+package edu.wpi.first.wpilibj.examples.swervedriveposeestimator;
 
 import edu.wpi.first.math.StateSpaceUtil;
 import edu.wpi.first.math.VecBuilder;
