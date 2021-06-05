f91cfcd4fb78e78a0063b43361d4799f8978e238
Tyler Veness <calcmogul@gmail.com>
2021-06-05 15:14:42 -0700

    [wpiutil] Add fmtlib formatter overloads for Eigen::Matrix and units
    This allows using Eigen matrices or units natively with fmt::format() or
    fmt::print().

---
+++ wpimath/src/test/native/cpp/FormatterTest.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <fmt/format.h>
+
+#include "frc/fmt/Eigen.h"
+#include "frc/fmt/Units.h"
+#include "gtest/gtest.h"
+#include "units/velocity.h"
+
+TEST(FormatterTest, Eigen) {
+  Eigen::Matrix<double, 2, 2> A;
+  A << 1.0, 2.0, 3.0, 4.0;
+  EXPECT_EQ(
+      "  1.000000  2.000000\n"
+      "  3.000000  4.000000",
+      fmt::format("{}", A));
+}
+
+TEST(FormatterTest, Units) {
+  EXPECT_EQ("4 m s^-1", fmt::format("{}", 4_mps));
+}
