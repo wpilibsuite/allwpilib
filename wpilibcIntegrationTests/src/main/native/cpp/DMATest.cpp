1a1770f85a43fe2edcc8968bce42f17910960180
Thad House <thadhouse1@gmail.com>
2021-06-08 11:48:31 -0700

    Value types

--- wpilibcIntegrationTests/src/main/native/cpp/DMATest.cpp
+++ wpilibcIntegrationTests/src/main/native/cpp/DMATest.cpp
@@ -19,133 +19,120 @@ static constexpr auto kDelayTime = 100_ms;
 
 class DMATest : public testing::Test {
  protected:
-  DMA* m_dma;
-  AnalogInput* m_analogInput;
-  AnalogOutput* m_analogOutput;
-  DigitalOutput* m_manualTrigger;
-  PWMMotorController* m_pwm;
+  AnalogInput m_analogInput{TestBench::kAnalogOutputChannel};
+  AnalogOutput m_analogOutput{TestBench::kFakeAnalogOutputChannel};
+  DigitalOutput m_manualTrigger{TestBench::kLoop1InputChannel};
+  Jaguar m_pwm{TestBench::kFakePwmOutput};
+  DMA m_dma;
 
   void SetUp() override {
-    m_analogOutput = new AnalogOutput(TestBench::kAnalogOutputChannel);
-    m_analogInput = new AnalogInput(TestBench::kFakeAnalogOutputChannel);
-    m_manualTrigger = new DigitalOutput(TestBench::kLoop1InputChannel);
-    m_pwm = new Jaguar(TestBench::kFakePwmOutput);
-    m_dma = new DMA();
 
-    m_dma->AddAnalogInput(m_analogInput);
-    m_dma->SetExternalTrigger(m_manualTrigger, false, true);
-    m_manualTrigger->Set(true);
-  }
-
-  void TearDown() override {
-    delete m_pwm;
-    delete m_manualTrigger;
-    delete m_analogOutput;
-    delete m_analogInput;
-    delete m_dma;
+    m_dma.AddAnalogInput(&m_analogInput);
+    m_dma.SetExternalTrigger(&m_manualTrigger, false, true);
+    m_manualTrigger.Set(true);
   }
 };
 
 TEST_F(DMATest, PausingWorks) {
-  m_dma->Start(1024);
-  m_dma->SetPause(true);
-  m_manualTrigger->Set(false);
+  m_dma.Start(1024);
+  m_dma.SetPause(true);
+  m_manualTrigger.Set(false);
 
   frc::DMASample sample;
   int32_t remaining = 0;
   int32_t status = 0;
 
-  auto timedOut = sample.Update(m_dma, 5_ms, &remaining, &status);
+  auto timedOut = sample.Update(&m_dma, 5_ms, &remaining, &status);
 
   ASSERT_EQ(DMASample::DMAReadStatus::kTimeout, timedOut);
 }
 
 TEST_F(DMATest, RemovingTriggersWorks) {
-  m_dma->ClearExternalTriggers();
-  m_dma->Start(1024);
-  m_manualTrigger->Set(false);
+  m_dma.ClearExternalTriggers();
+  m_dma.Start(1024);
+  m_manualTrigger.Set(false);
 
   frc::DMASample sample;
   int32_t remaining = 0;
   int32_t status = 0;
 
-  auto timedOut = sample.Update(m_dma, 5_ms, &remaining, &status);
+  auto timedOut = sample.Update(&m_dma, 5_ms, &remaining, &status);
 
   ASSERT_EQ(DMASample::DMAReadStatus::kTimeout, timedOut);
 }
 
 TEST_F(DMATest, ManualTriggerOnlyHappensOnce) {
-  m_dma->Start(1024);
-  m_manualTrigger->Set(false);
+  m_dma.Start(1024);
+  m_manualTrigger.Set(false);
 
   frc::DMASample sample;
   int32_t remaining = 0;
   int32_t status = 0;
 
-  auto timedOut = sample.Update(m_dma, 5_ms, &remaining, &status);
+  auto timedOut = sample.Update(&m_dma, 5_ms, &remaining, &status);
 
   ASSERT_EQ(DMASample::DMAReadStatus::kOk, timedOut);
   ASSERT_EQ(0, remaining);
-  timedOut = sample.Update(m_dma, 5_ms, &remaining, &status);
+  timedOut = sample.Update(&m_dma, 5_ms, &remaining, &status);
   ASSERT_EQ(DMASample::DMAReadStatus::kTimeout, timedOut);
 }
 
 TEST_F(DMATest, AnalogIndividualTriggers) {
-  m_dma->Start(1024);
+  m_dma.Start(1024);
   for (double i = 0; i < 5; i += 0.5) {
     frc::DMASample sample;
     int32_t remaining = 0;
     int32_t status = 0;
 
-    m_analogOutput->SetVoltage(i);
+    m_analogOutput.SetVoltage(i);
     frc::Wait(kDelayTime);
-    m_manualTrigger->Set(false);
-    auto timedOut = sample.Update(m_dma, 1_ms, &remaining, &status);
-    m_manualTrigger->Set(true);
+    m_manualTrigger.Set(false);
+    auto timedOut = sample.Update(&m_dma, 1_ms, &remaining, &status);
+    m_manualTrigger.Set(true);
     ASSERT_EQ(DMASample::DMAReadStatus::kOk, timedOut);
     ASSERT_EQ(0, status);
     ASSERT_EQ(0, remaining);
-    ASSERT_DOUBLE_EQ(m_analogInput->GetVoltage(),
-                     sample.GetAnalogInputVoltage(m_analogInput, &status));
+    ASSERT_DOUBLE_EQ(m_analogInput.GetVoltage(),
+                     sample.GetAnalogInputVoltage(&m_analogInput, &status));
   }
 }
 
 TEST_F(DMATest, AnalogMultipleTriggers) {
-  m_dma->Start(1024);
+  m_dma.Start(1024);
   std::vector<double> values;
   for (double i = 0; i < 5; i += 0.5) {
     values.push_back(i);
-    m_analogOutput->SetVoltage(i);
+    m_analogOutput.SetVoltage(i);
     frc::Wait(kDelayTime);
-    m_manualTrigger->Set(false);
+    m_manualTrigger.Set(false);
     frc::Wait(kDelayTime);
-    m_manualTrigger->Set(true);
+    m_manualTrigger.Set(true);
   }
 
   for (size_t i = 0; i < values.size(); i++) {
     frc::DMASample sample;
     int32_t remaining = 0;
     int32_t status = 0;
-    auto timedOut = sample.Update(m_dma, 1_ms, &remaining, &status);
+    auto timedOut = sample.Update(&m_dma, 1_ms, &remaining, &status);
     ASSERT_EQ(DMASample::DMAReadStatus::kOk, timedOut);
     ASSERT_EQ(0, status);
     ASSERT_EQ(values.size() - i - 1, (uint32_t)remaining);
     ASSERT_DOUBLE_EQ(values[i],
-                     sample.GetAnalogInputVoltage(m_analogInput, &status));
+                     sample.GetAnalogInputVoltage(&m_analogInput, &status));
   }
 }
 
 TEST_F(DMATest, TimedTriggers) {
-  m_dma->SetTimedTrigger(10_ms);
-  m_dma->Start(1024);
+  m_dma.SetTimedTrigger(10_ms);
+  m_dma.Start(1024);
   frc::Wait(kDelayTime);
-  m_dma->SetPause(true);
+  m_dma.SetPause(true);
 
   frc::DMASample sample;
   int32_t remaining = 0;
   int32_t status = 0;
 
-  auto timedOut = sample.Update(m_dma, 1_ms, &remaining, &status);
+  auto timedOut = sample.Update(&m_dma, 1_ms, &remaining, &status);
   ASSERT_EQ(DMASample::DMAReadStatus::kOk, timedOut);
   ASSERT_EQ(0, status);
   ASSERT_GT(remaining, 5);
@@ -153,17 +140,17 @@ TEST_F(DMATest, TimedTriggers) {
 }
 
 TEST_F(DMATest, PWMTimedTriggers) {
-  m_dma->ClearExternalTriggers();
-  m_dma->SetPwmEdgeTrigger(m_pwm, true, false);
-  m_dma->Start(1024);
+  m_dma.ClearExternalTriggers();
+  m_dma.SetPwmEdgeTrigger(&m_pwm, true, false);
+  m_dma.Start(1024);
   frc::Wait(kDelayTime);
-  m_dma->SetPause(true);
+  m_dma.SetPause(true);
 
   frc::DMASample sample;
   int32_t remaining = 0;
   int32_t status = 0;
 
-  auto timedOut = sample.Update(m_dma, 1_ms, &remaining, &status);
+  auto timedOut = sample.Update(&m_dma, 1_ms, &remaining, &status);
   ASSERT_EQ(DMASample::DMAReadStatus::kOk, timedOut);
   ASSERT_EQ(0, status);
   ASSERT_GT(remaining, 5);
