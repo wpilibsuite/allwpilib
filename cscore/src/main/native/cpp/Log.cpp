422a9866490ca50e7d8c7fd0298ac5fdad919158
Tyler Veness <calcmogul@gmail.com>
2021-06-21 09:57:52 -0400

    Upgrade to fmtlib 8.0.0

--- cscore/src/main/native/cpp/Log.cpp
+++ cscore/src/main/native/cpp/Log.cpp
@@ -8,8 +8,8 @@ void cs::NamedLogV(wpi::Logger& logger, unsigned int level, const char* file,
                    unsigned int line, std::string_view name,
                    fmt::string_view format, fmt::format_args args) {
   fmt::memory_buffer out;
-  fmt::format_to(out, "{}: ", name);
-  fmt::vformat_to(out, format, args);
+  fmt::format_to(fmt::appender{out}, "{}: ", name);
+  fmt::vformat_to(fmt::appender{out}, format, args);
   out.push_back('\0');
   logger.DoLog(level, file, line, out.data());
 }
