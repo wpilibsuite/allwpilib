e0c6cd3dcc2f123cfbe2f89e3f012d829dc52538
Tyler Veness <calcmogul@gmail.com>
2021-08-23 20:22:48 -0700

    [wpimath] Add an operator for composing two Transform2ds (#3527)

--- wpimath/src/main/native/include/frc/geometry/Transform2d.h
+++ wpimath/src/main/native/include/frc/geometry/Transform2d.h
@@ -82,6 +82,13 @@ class Transform2d {
   }
 
   /**
+   * Composes two transformations.
+   *
+   * @param other The transform to compose with this one.
+   */
+  Transform2d operator+(const Transform2d& other) const;
+
+  /**
    * Checks equality between this Transform2d and another object.
    *
    * @param other The other object.
