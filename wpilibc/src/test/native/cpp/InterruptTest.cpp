ada9f7be54a14f7565934085958bc8a13944d598
Thad House <thadhouse1@gmail.com>
2021-09-10 22:42:56 -0700

    Nore formatting

--- wpilibc/src/test/native/cpp/InterruptTest.cpp
+++ wpilibc/src/test/native/cpp/InterruptTest.cpp
@@ -15,31 +15,31 @@
 namespace frc {
 using namespace frc::sim;
 TEST(InterruptTest, AsynchronousInterrupt) {
-    HAL_Initialize(500, 0);
+  HAL_Initialize(500, 0);
 
-    std::atomic_int counter{0};
-    std::atomic_bool hasFired{false};
+  std::atomic_int counter{0};
+  std::atomic_bool hasFired{false};
 
-    DigitalInput di{0};
-    AsynchronousInterrupt interrupt{di, [&](bool rising, bool falling){
-                                        counter++;
-                                        hasFired = true;
-                                    }};
+  DigitalInput di{0};
+  AsynchronousInterrupt interrupt{di, [&](bool rising, bool falling) {
+                                    counter++;
+                                    hasFired = true;
+                                  }};
 
-    interrupt.Enable();
-    frc::Wait(0.5_s);
-    DIOSim digitalSim{di};
-    digitalSim.SetValue(false);
-    frc::Wait(20_ms);
-    digitalSim.SetValue(true);
-    frc::Wait(10_ms);
+  interrupt.Enable();
+  frc::Wait(0.5_s);
+  DIOSim digitalSim{di};
+  digitalSim.SetValue(false);
+  frc::Wait(20_ms);
+  digitalSim.SetValue(true);
+  frc::Wait(10_ms);
 
-    int count = 0;
-    while (!hasFired) {
-        frc::Wait(5_ms);
-        count++;
-        ASSERT_TRUE(count < 1000);
-    }
-    ASSERT_EQ(1, counter.load());
+  int count = 0;
+  while (!hasFired) {
+    frc::Wait(5_ms);
+    count++;
+    ASSERT_TRUE(count < 1000);
+  }
+  ASSERT_EQ(1, counter.load());
 }
-}
+}  // namespace frc
--- wpilibj/src/test/java/edu/wpi/first/wpilibj/InterruptTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/InterruptTest.java
@@ -13,32 +13,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
 
 public class InterruptTest {
-    @Test
-    void testAsynchronousInterrupt() {
-        AtomicBoolean hasFired = new AtomicBoolean(false);
-        AtomicInteger counter = new AtomicInteger(0);
+  @Test
+  void testAsynchronousInterrupt() {
+    AtomicBoolean hasFired = new AtomicBoolean(false);
+    AtomicInteger counter = new AtomicInteger(0);
 
-        try (DigitalInput di = new DigitalInput(0);
-                AsynchronousInterrupt interrupt = new AsynchronousInterrupt(di, (a, b) -> {
-                    counter.incrementAndGet();
-                    hasFired.set(true);
-                })) {
-            interrupt.enable();
-            Timer.delay(0.5);
-            DIOSim digitalSim = new DIOSim(di);
-            digitalSim.setValue(false);
-            Timer.delay(0.01);
-            digitalSim.setValue(true);
-            Timer.delay(0.01);
+    try (DigitalInput di = new DigitalInput(0);
+        AsynchronousInterrupt interrupt = new AsynchronousInterrupt(di, (a, b) -> {
+          counter.incrementAndGet();
+          hasFired.set(true);
+        })) {
+      interrupt.enable();
+      Timer.delay(0.5);
+      DIOSim digitalSim = new DIOSim(di);
+      digitalSim.setValue(false);
+      Timer.delay(0.01);
+      digitalSim.setValue(true);
+      Timer.delay(0.01);
 
-            int count = 0;
-            while (!hasFired.get()) {
-                Timer.delay(0.005);
-                count++;
-                assertTrue(count < 1000);
-            }
-            assertEquals(1, counter.get(),
-                "The interrupt did not fire the expected number of times");
-        }
+      int count = 0;
+      while (!hasFired.get()) {
+        Timer.delay(0.005);
+        count++;
+        assertTrue(count < 1000);
+      }
+      assertEquals(1, counter.get(), "The interrupt did not fire the expected number of times");
     }
+  }
 }
