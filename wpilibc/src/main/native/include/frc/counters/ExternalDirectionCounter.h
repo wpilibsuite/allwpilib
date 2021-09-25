08a98dad0905fe4635e7dc434880712d82c1b896
Thad House <thadhouse1@gmail.com>
2021-09-24 19:20:52 -0700

    Formatting

--- wpilibc/src/main/native/include/frc/counters/ExternalDirectionCounter.h
+++ wpilibc/src/main/native/include/frc/counters/ExternalDirectionCounter.h
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 #pragma once
 
 #include <memory>
@@ -11,31 +15,36 @@
 namespace frc {
 class DigitalSource;
 
-class ExternalDirectionCounter : public wpi::Sendable, public wpi::SendableHelper<ExternalDirectionCounter> {
-  public:
-    ExternalDirectionCounter(DigitalSource& countSource, DigitalSource& directionSource);
-    ExternalDirectionCounter(DigitalSource* countSource, DigitalSource* directionSource);
-    ExternalDirectionCounter(std::shared_ptr<DigitalSource> countSource, std::shared_ptr<DigitalSource> directionSource);
+class ExternalDirectionCounter
+    : public wpi::Sendable,
+      public wpi::SendableHelper<ExternalDirectionCounter> {
+ public:
+  ExternalDirectionCounter(DigitalSource& countSource,
+                           DigitalSource& directionSource);
+  ExternalDirectionCounter(DigitalSource* countSource,
+                           DigitalSource* directionSource);
+  ExternalDirectionCounter(std::shared_ptr<DigitalSource> countSource,
+                           std::shared_ptr<DigitalSource> directionSource);
 
-    ~ExternalDirectionCounter() override;
+  ~ExternalDirectionCounter() override;
 
-    ExternalDirectionCounter(ExternalDirectionCounter&&) = default;
-    ExternalDirectionCounter& operator=(ExternalDirectionCounter&&) = default;
+  ExternalDirectionCounter(ExternalDirectionCounter&&) = default;
+  ExternalDirectionCounter& operator=(ExternalDirectionCounter&&) = default;
 
-    int GetCount() const;
+  int GetCount() const;
 
-    void SetReverseDirection(bool reverseDirection);
-    void Reset();
+  void SetReverseDirection(bool reverseDirection);
+  void Reset();
 
-    void SetEdgeConfiguration(EdgeConfiguration configuration);
+  void SetEdgeConfiguration(EdgeConfiguration configuration);
 
  protected:
   void InitSendable(wpi::SendableBuilder& builder) override;
 
-   private:
+ private:
   void InitExternalDirectionCounter();
   std::shared_ptr<DigitalSource> m_countSource;
   std::shared_ptr<DigitalSource> m_directionSource;
   hal::Handle<HAL_CounterHandle> m_handle;
 };
-}
+}  // namespace frc
