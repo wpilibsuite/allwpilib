wpilibsuite/allwpilib

commit c1e128bd5a835a973c7c0ca2da587deef4081d26

Disable frivolous PMD warnings and enable PMD in ntcore (#3419)
Some valid warnings like throwing NullPointerException or using a for
loop instead of System.arraycopy() were fixed.

Abstract classes marked with PMD.AbstractClassWithoutAbstractMethod were
made concrete because they already had protected constructors.

Fixes #1697.

ntcore/src/main/java/edu/wpi/first/networktables/NetworkTableValue.java
@@ -183,6 +183,7 @@ public final class NetworkTableValue {
    * @return The raw value.
    * @throws ClassCastException if the entry value is not of raw type.
    */
+  @SuppressWarnings("PMD.MethodReturnsInternalArray")
   public byte[] getRaw() {
     if (m_type != NetworkTableType.kRaw) {
       throw new ClassCastException("cannot convert " + m_type + " to raw");
@@ -196,6 +197,7 @@ public final class NetworkTableValue {
    * @return The rpc definition value.
    * @throws ClassCastException if the entry value is not of rpc definition type.
    */
+  @SuppressWarnings("PMD.MethodReturnsInternalArray")
   public byte[] getRpc() {
     if (m_type != NetworkTableType.kRpc) {
       throw new ClassCastException("cannot convert " + m_type + " to rpc");
@@ -209,6 +211,7 @@ public final class NetworkTableValue {
    * @return The boolean array value.
    * @throws ClassCastException if the entry value is not of boolean array type.
    */
+  @SuppressWarnings("PMD.MethodReturnsInternalArray")
   public boolean[] getBooleanArray() {
     if (m_type != NetworkTableType.kBooleanArray) {
       throw new ClassCastException("cannot convert " + m_type + " to boolean array");
@@ -222,6 +225,7 @@ public final class NetworkTableValue {
    * @return The double array value.
    * @throws ClassCastException if the entry value is not of double array type.
    */
+  @SuppressWarnings("PMD.MethodReturnsInternalArray")
   public double[] getDoubleArray() {
     if (m_type != NetworkTableType.kDoubleArray) {
       throw new ClassCastException("cannot convert " + m_type + " to double array");
@@ -235,6 +239,7 @@ public final class NetworkTableValue {
    * @return The string array value.
    * @throws ClassCastException if the entry value is not of string array type.
    */
+  @SuppressWarnings("PMD.MethodReturnsInternalArray")
   public String[] getStringArray() {
     if (m_type != NetworkTableType.kStringArray) {
       throw new ClassCastException("cannot convert " + m_type + " to string array");
@@ -475,33 +480,25 @@ public final class NetworkTableValue {
 
   static boolean[] toNative(Boolean[] arr) {
     boolean[] out = new boolean[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      out[i] = arr[i];
-    }
+    System.arraycopy(arr, 0, out, 0, arr.length);
     return out;
   }
 
   static double[] toNative(Number[] arr) {
     double[] out = new double[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      out[i] = arr[i].doubleValue();
-    }
+    System.arraycopy(arr, 0, out, 0, arr.length);
     return out;
   }
 
   static Boolean[] fromNative(boolean[] arr) {
     Boolean[] out = new Boolean[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      out[i] = arr[i];
-    }
+    System.arraycopy(arr, 0, out, 0, arr.length);
     return out;
   }
 
   static Double[] fromNative(double[] arr) {
     Double[] out = new Double[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      out[i] = arr[i];
-    }
+    System.arraycopy(arr, 0, out, 0, arr.length);
     return out;
   }
 

commit 9027c5501313ec5f538ff7c66ddacdfc40f15dea

[ntcore] Revert to per-element copy for toNative()
arraycopy() does not unbox values, so throws an exception.

This was broken in #3419 due to a false positive in PMD
(see https://sourceforge.net/p/pmd/bugs/804/).

ntcore/src/main/java/edu/wpi/first/networktables/NetworkTableValue.java
@@ -478,27 +478,41 @@ public final class NetworkTableValue {
     return Objects.hash(m_type, m_value);
   }
 
+  // arraycopy() doesn't know how to unwrap boxed values; this is a false positive in PMD
+  // (see https://sourceforge.net/p/pmd/bugs/804/)
+  @SuppressWarnings("PMD.AvoidArrayLoops")
   static boolean[] toNative(Boolean[] arr) {
     boolean[] out = new boolean[arr.length];
-    System.arraycopy(arr, 0, out, 0, arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      out[i] = arr[i];
+    }
     return out;
   }
 
+  @SuppressWarnings("PMD.AvoidArrayLoops")
   static double[] toNative(Number[] arr) {
     double[] out = new double[arr.length];
-    System.arraycopy(arr, 0, out, 0, arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      out[i] = arr[i].doubleValue();
+    }
     return out;
   }
 
+  @SuppressWarnings("PMD.AvoidArrayLoops")
   static Boolean[] fromNative(boolean[] arr) {
     Boolean[] out = new Boolean[arr.length];
-    System.arraycopy(arr, 0, out, 0, arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      out[i] = arr[i];
+    }
     return out;
   }
 
+  @SuppressWarnings("PMD.AvoidArrayLoops")
   static Double[] fromNative(double[] arr) {
     Double[] out = new Double[arr.length];
-    System.arraycopy(arr, 0, out, 0, arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      out[i] = arr[i];
+    }
     return out;
   }
 