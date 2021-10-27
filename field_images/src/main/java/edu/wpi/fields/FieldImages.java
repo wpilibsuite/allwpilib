2774b37ea278034f7dcf1bce57ed4f9f4793a0a0
PJ Reiniger <pj.reiniger@gmail.com>
2021-10-26 23:11:47 -0400

    Attemping CMAKE config

---
+++ field_images/src/main/java/edu/wpi/fields/FieldImages.java
@@ -0,0 +1,14 @@
+
+
+public class FieldImages {
+
+    public static final String k2018PowerUpFieldConfig = "/edu/wpi/first/fields/2018-powerup.json";
+    public static final String k2019DeepSpaceFieldConfig = "/edu/wpi/first/fields/2019-deepspace.json";
+    public static final String k2020InfiniteRechargeFieldConfig = "/edu/wpi/first/fields/2020-infiniterecharge.json";
+    public static final String k2021InfiniteRechargeFieldConfig = "/edu/wpi/first/fields/2021-infiniterecharge.json";
+    public static final String k2021BarrelFieldConfig = "/edu/wpi/first/fields/2021-barrelracingpath.json";
+    public static final String k2021BounceFieldConfig = "/edu/wpi/first/fields/2021-bouncepath.json";
+    public static final String k2021GalacticSearchAFieldConfig = "/edu/wpi/first/fields/2021-galacticsearcha.json";
+    public static final String k2021GalacticSearchBFieldConfig = "/edu/wpi/first/fields/2021-galacticsearchb.json";
+    public static final String k2021SlalomFieldConfig = "/edu/wpi/first/fields/2021-slalompath.json";
+}
