9610d004de483b8063743d4564bc784c10aba853
Oblarg <emichaelbarnett@gmail.com>
2021-11-01 15:35:56 -0400

    remove directional boolean, improve sendable richness

--- wpimath/src/main/java/edu/wpi/first/math/controller/BangBangController.java
+++ wpimath/src/main/java/edu/wpi/first/math/controller/BangBangController.java
@@ -11,160 +11,140 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
 
 /**
- * Implements a bang-bang controller, which outputs either 0 or 1 depending on whether
- * the measurement is less than the setpoint.  This maximally-aggressive control approach
- * works very well for velocity control of high-inertia mechanisms,
- * and poorly on most other things.
+ * Implements a bang-bang controller, which outputs either 0 or 1 depending on whether the
+ * measurement is less than the setpoint. This maximally-aggressive control approach works very well
+ * for velocity control of high-inertia mechanisms, and poorly on most other things.
  *
  * <p>Note that this is an *asymmetric* bang-bang controller - it will not exert any control effort
- * in the reverse direction (e.g. it won't try to slow down an over-speeding shooter wheel).  This
- * asymmetry is *extremely important.*  Bang-bang control is extremely simple, but also
- * potentially hazardous.  Always ensure that your motor controllers are set to "coast" before
- * attempting to control them with a bang-bang controller.
+ * in the reverse direction (e.g. it won't try to slow down an over-speeding shooter wheel). This
+ * asymmetry is *extremely important.* Bang-bang control is extremely simple, but also potentially
+ * hazardous. Always ensure that your motor controllers are set to "coast" before attempting to
+ * control them with a bang-bang controller.
  */
 public class BangBangController implements Sendable {
-    public enum Direction {
-        kForward,
-        kReverse
-    }
+  private static int instances;
 
-    private static int instances;
+  private double m_tolerance;
 
-    private final Direction m_direction;
+  private double m_setpoint;
+  private double m_measurement;
 
-    private double m_tolerance;
+  /**
+   * Creates a new bang-bang controller.
+   *
+   * <p>Always ensure that your motor controllers are set to "coast" before attempting to control
+   * them with a bang-bang controller.
+   *
+   * @param tolerance Tolerance for {@link #atSetpoint() atSetpoint}.
+   */
+  public BangBangController(double tolerance) {
+    instances++;
 
-    private double m_setpoint;
-    private double m_measurement;
+    setTolerance(tolerance);
 
-    /**
-     * Creates a new bang-bang controller.
-     *
-     * <p>Always ensure that your motor controllers are set to "coast" before
-     * attempting to control them with a bang-bang controller.
-     *
-     * @param tolerance The controller's error tolerance, within which the controller
-     *                  will output zero.
-     * @param direction The direction of the bang-bang controller.
-     */
-    public BangBangController(
-        double tolerance,
-        Direction direction
-    ) {
-        instances++;
+    SendableRegistry.addLW(this, "BangBangController", instances);
 
-        m_tolerance = tolerance;
-        m_direction = direction;
+    MathSharedStore.reportUsage(MathUsageId.kController_PIDController2, instances);
+  }
 
-        SendableRegistry.addLW(this, "BangBangController", instances);
+  /**
+   * Sets the setpoint for the bang-bang controller.
+   *
+   * @param setpoint The desired setpoint.
+   */
+  public void setSetpoint(double setpoint) {
+    m_setpoint = setpoint;
+  }
 
-        MathSharedStore.reportUsage(MathUsageId.kController_PIDController2, instances);
-    }
+  /**
+   * Returns the current setpoint of the bang-bang controller.
+   *
+   * @return The current setpoint.
+   */
+  public double getSetpoint() {
+    return m_setpoint;
+  }
 
-    /**
-     * Sets the setpoint for the bang-bang controller.
-     *
-     * @param setpoint The desired setpoint.
-     */
-    public void setSetpoint(double setpoint) {
-        m_setpoint = setpoint;
-    }
+  /**
+   * Sets the error within which the controller will not output.
+   *
+   * @param tolerance Position error which is tolerable.
+   */
+  public void setTolerance(double tolerance) {
+    m_tolerance = tolerance;
+  }
 
-    /**
-     * Returns the current setpoint of the bang-bang controller.
-     *
-     * @return The current setpoint.
-     */
-    public double getSetpoint() {
-        return m_setpoint;
-    }
+  /**
+   * Returns the current tolerance of the controller.
+   *
+   * @return The current tolerance.
+   */
+  public double getTolerance() {
+    return m_tolerance;
+  }
 
-    /**
-     * Sets the error within which the controller will not output.
-     *
-     * @param tolerance Position error which is tolerable.
-     */
-    public void setTolerance(double tolerance) {
-        m_tolerance = tolerance;
-    }
+  /**
+   * Returns the current measurement of the process variable.
+   *
+   * @return The current measurement of the process variable.
+   */
+  public double getMeasurement() {
+    return m_measurement;
+  }
 
-    /**
-     * Returns the current tolerance of the controller.
-     *
-     * @return The current tolerance.
-     */
-    public double getTolerance() {
-        return m_tolerance;
-    }
+  /**
+   * Returns true if the error is within the tolerance of the setpoint.
+   *
+   * <p>This will return false until at least one input value has been computed.
+   *
+   * @return Whether the error is within the acceptable bounds.
+   */
+  public boolean atSetpoint() {
+    return Math.abs(m_setpoint - m_measurement) < m_tolerance;
+  }
 
-    /**
-     * Returns the current measurement of the process variable.
-     *
-     * @return The current measurement of the process variable.
-     */
-    public double getMeasurement() {
-        return m_measurement;
-    }
+  /**
+   * Returns the current error.
+   *
+   * @return The current error.
+   */
+  public double getError() {
+    return m_setpoint - m_measurement;
+  }
 
-    /**
-     * Returns true if the error is within the tolerance of the setpoint.
-     *
-     * <p>This will return false until at least one input value has been computed.
-     *
-     * @return Whether the error is within the acceptable bounds.
-     */
-    public boolean atSetpoint() {
-        return Math.abs(m_setpoint - m_measurement) < m_tolerance;
-    }
+  /**
+   * Returns the calculated control output.
+   *
+   * <p>Always ensure that your motor controllers are set to "coast" before attempting to control
+   * them with a bang-bang controller.
+   *
+   * @param measurement The most recent measurement of the process variable.
+   * @param setpoint The setpoint for the process variable.
+   * @return The calculated motor output (-1, 0, or 1).
+   */
+  public double calculate(double measurement, double setpoint) {
+    m_measurement = measurement;
+    m_setpoint = setpoint;
 
-    /**
-     * Returns the current error.
-     *
-     * @return The current error.
-     */
-    public double getError() {
-        return m_setpoint - m_measurement;
-    }
+    return measurement < setpoint ? 1 : 0;
+  }
 
-    /**
-     * Returns the calculated control output.
-     *
-     * <p>Always ensure that your motor controllers are set to "coast" before
-     * attempting to control them with a bang-bang controller.
-     *
-     * @param measurement The most recent measurement of the process variable.
-     * @param setpoint The setpoint for the process variable.
-     * @return The calculated motor output (-1, 0, or 1).
-     */
-    public double calculate(double measurement, double setpoint) {
-        m_measurement = measurement;
-        m_setpoint = setpoint;
+  /**
+   * Returns the calculated control output.
+   *
+   * @param measurement The most recent measurement of the process variable.
+   * @return The calculated motor output (-1, 0, or 1).
+   */
+  public double calculate(double measurement) {
+    return calculate(measurement, m_setpoint);
+  }
 
-        switch (m_direction) {
-            case kForward:
-                return !atSetpoint() && measurement < setpoint ? 1 : 0;
-            case kReverse:
-                return !atSetpoint() && measurement > setpoint ? -1 : 0;
-            default:
-                return 0;
-        }
-    }
-
-    /**
-     * Returns the calculated control output.
-     *
-     * @param measurement The most recent measurement of the process variable.
-     * @return The calculated motor output (-1, 0, or 1).
-     */
-    public double calculate(double measurement) {
-        return calculate(measurement, m_setpoint);
-    }
-
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        builder.setSmartDashboardType("BangBangController");
-        builder.addDoubleProperty("tolerance", this::getTolerance, this::setTolerance);
-        builder.addDoubleProperty("setpoint", this::getSetpoint, this::setSetpoint);
-        builder.addDoubleProperty("measurement", this::getMeasurement, null);
-    }
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.setSmartDashboardType("BangBangController");
+    builder.addDoubleProperty("tolerance", this::getTolerance, this::setTolerance);
+    builder.addDoubleProperty("setpoint", this::getSetpoint, this::setSetpoint);
+    builder.addBooleanProperty("atSetpoint", this::atSetpoint, null);
+    builder.addDoubleProperty("measurement", this::getMeasurement, null);
+  }
 }
