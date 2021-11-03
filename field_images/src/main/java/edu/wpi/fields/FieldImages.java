af680cd8efc2dd0e699986b1d365acea437c784a
PJ Reiniger <pj.reiniger@gmail.com>
2021-11-03 00:42:12 -0400

    Style fixes

--- field_images/src/main/java/edu/wpi/fields/FieldImages.java
+++ field_images/src/main/java/edu/wpi/fields/FieldImages.java
@@ -1,14 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
+package edu.wpi.fields;
 
 public class FieldImages {
-
-    public static final String k2018PowerUpFieldConfig = "/edu/wpi/first/fields/2018-powerup.json";
-    public static final String k2019DeepSpaceFieldConfig = "/edu/wpi/first/fields/2019-deepspace.json";
-    public static final String k2020InfiniteRechargeFieldConfig = "/edu/wpi/first/fields/2020-infiniterecharge.json";
-    public static final String k2021InfiniteRechargeFieldConfig = "/edu/wpi/first/fields/2021-infiniterecharge.json";
-    public static final String k2021BarrelFieldConfig = "/edu/wpi/first/fields/2021-barrelracingpath.json";
-    public static final String k2021BounceFieldConfig = "/edu/wpi/first/fields/2021-bouncepath.json";
-    public static final String k2021GalacticSearchAFieldConfig = "/edu/wpi/first/fields/2021-galacticsearcha.json";
-    public static final String k2021GalacticSearchBFieldConfig = "/edu/wpi/first/fields/2021-galacticsearchb.json";
-    public static final String k2021SlalomFieldConfig = "/edu/wpi/first/fields/2021-slalompath.json";
+  public static final String k2018PowerUpFieldConfig = "/edu/wpi/first/fields/2018-powerup.json";
+  public static final String k2019DeepSpaceFieldConfig =
+      "/edu/wpi/first/fields/2019-deepspace.json";
+  public static final String k2020InfiniteRechargeFieldConfig =
+      "/edu/wpi/first/fields/2020-infiniterecharge.json";
+  public static final String k2021InfiniteRechargeFieldConfig =
+      "/edu/wpi/first/fields/2021-infiniterecharge.json";
+  public static final String k2021BarrelFieldConfig =
+      "/edu/wpi/first/fields/2021-barrelracingpath.json";
+  public static final String k2021BounceFieldConfig = "/edu/wpi/first/fields/2021-bouncepath.json";
+  public static final String k2021GalacticSearchAFieldConfig =
+      "/edu/wpi/first/fields/2021-galacticsearcha.json";
+  public static final String k2021GalacticSearchBFieldConfig =
+      "/edu/wpi/first/fields/2021-galacticsearchb.json";
+  public static final String k2021SlalomFieldConfig = "/edu/wpi/first/fields/2021-slalompath.json";
 }
