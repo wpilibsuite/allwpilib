680612570892590733652fb1b32958f1e4bd76d8
Thad House <thadhouse1@gmail.com>
2021-12-18 13:09:57 -0800

    Myrobot

--- myRobot/src/main/native/cpp/MyRobot.cpp
+++ myRobot/src/main/native/cpp/MyRobot.cpp
@@ -2,20 +2,15 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <frc/AnalogOutput.h>
-#include <frc/PneumaticHub.h>
 #include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SmartDashboard.h>
 
 class MyRobot : public frc::TimedRobot {
-  frc::AnalogOutput ao{0};
-  frc::PneumaticHub hub{1};
 
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
-  void RobotInit() override { frc::SmartDashboard::PutNumber("AOut", 0); }
+  void RobotInit() override {}
 
   /**
    * This function is run once each time the robot enters autonomous mode
@@ -45,10 +40,7 @@ class MyRobot : public frc::TimedRobot {
   /**
    * This function is called periodically during all modes
    */
-  void RobotPeriodic() override {
-    ao.SetVoltage(frc::SmartDashboard::GetNumber("AOut", 0));
-    frc::SmartDashboard::PutNumber("Pressure", hub.GetAnalogVoltage(0).value());
-  }
+  void RobotPeriodic() override {}
 };
 
 int main() {
