e51250c56c80e2d4db15035d96ccb5727e4e7129
Kevin-OConnor <koconnor@usfirst.org>
2021-12-28 17:03:06 -0500

    Add warning about onboard I2C
    Add warning about onboard I2C on construction

--- wpilibc/src/main/native/cpp/I2C.cpp
+++ wpilibc/src/main/native/cpp/I2C.cpp
@@ -16,6 +16,14 @@ using namespace frc;
 I2C::I2C(Port port, int deviceAddress)
     : m_port(static_cast<HAL_I2CPort>(port)), m_deviceAddress(deviceAddress) {
   int32_t status = 0;
+
+  if(port == I2C::Port::kOnboard) {
+     FRC_ReportError(
+      warn::Warning, "{}",
+      "Onboard I2C port is subject to system lockups. See Known Issues page for"
+      "details");
+  }
+
   HAL_InitializeI2C(m_port, &status);
   FRC_CheckErrorStatus(status, "Port {}", port);
 
