3f77725cd308096388ae3e1c73247c7a44cf590e
Tyler Veness <calcmogul@gmail.com>
2022-02-05 23:00:31 -0800

    Remove uses of iostream (#4004)
    Most of these were unused, the IMU ones were just debug messages.
    
    The only one that wasn't removed is in portable-file-dialogs.cpp since
    the replacement looks less trivial.

--- hal/src/main/native/athena/SerialPort.cpp
+++ hal/src/main/native/athena/SerialPort.cpp
@@ -14,7 +14,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <thread>
--- wpimath/src/test/native/cpp/spline/CubicHermiteSplineTest.cpp
+++ wpimath/src/test/native/cpp/spline/CubicHermiteSplineTest.cpp
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include <chrono>
-#include <iostream>
 #include <vector>
 
 #include "frc/geometry/Pose2d.h"
