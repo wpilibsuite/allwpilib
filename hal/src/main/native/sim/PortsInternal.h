f21f026f0d009abe45aed2a68827127ef4b6f8b1
Dustin Spicuzza <dustin@virtualroadside.com>
2022-02-10 01:15:01 -0500

    [sim] Replace hardcoded sim array sizes with constants

--- hal/src/main/native/sim/PortsInternal.h
+++ hal/src/main/native/sim/PortsInternal.h
@@ -7,6 +7,7 @@
 #include <stdint.h>
 
 namespace hal {
+constexpr int32_t kAccelerometers = 1;
 constexpr int32_t kNumAccumulators = 2;
 constexpr int32_t kNumAnalogTriggers = 8;
 constexpr int32_t kNumAnalogInputs = 8;
@@ -18,6 +19,7 @@ constexpr int32_t kNumDigitalChannels = 31;
 constexpr int32_t kNumPWMChannels = 20;
 constexpr int32_t kNumDigitalPWMOutputs = 6;
 constexpr int32_t kNumEncoders = 8;
+constexpr int32_t kI2CPorts = 2;
 constexpr int32_t kNumInterrupts = 8;
 constexpr int32_t kNumRelayChannels = 8;
 constexpr int32_t kNumRelayHeaders = kNumRelayChannels / 2;
@@ -31,4 +33,6 @@ constexpr int32_t kNumDutyCycles = 8;
 constexpr int32_t kNumAddressableLEDs = 1;
 constexpr int32_t kNumREVPHModules = 63;
 constexpr int32_t kNumREVPHChannels = 16;
+constexpr int32_t kSPIAccelerometers = 5;
+constexpr int32_t kSPIPorts = 5;
 }  // namespace hal
