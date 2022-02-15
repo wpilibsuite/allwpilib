6297a1da5505977f4ccab52bddbcb48d775bdfe6
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2022-02-15 14:03:34 +0200

    add PneumaticsBaseSim

---
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/simulation/PneumaticsBaseSim.java
@@ -0,0 +1,149 @@
+package edu.wpi.first.wpilibj.simulation;
+
+import edu.wpi.first.hal.simulation.NotifyCallback;
+import edu.wpi.first.wpilibj.PneumaticsBase;
+
+public abstract class PneumaticsBaseSim {
+  protected final int m_index;
+
+  protected PneumaticsBaseSim(int index) {
+    m_index = index;
+  }
+
+  protected PneumaticsBaseSim(PneumaticsBase module) {
+    this(module.getModuleNumber());
+  }
+
+  /**
+   * Check whether the PCM/PH has been initialized.
+   *
+   * @return true if initialized
+   */
+  public abstract boolean getInitialized();
+
+  /**
+   * Define whether the PCM/PH has been initialized.
+   *
+   * @param initialized true for initialized
+   */
+  public abstract void setInitialized(boolean initialized);
+
+  /**
+   * Register a callback to be run when the PCM/PH is initialized.
+   *
+   * @param callback the callback
+   * @param initialNotify whether to run the callback with the initial state
+   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
+   *     this object so GC doesn't cancel the callback.
+   */
+  public abstract CallbackStore registerInitializedCallback(NotifyCallback callback,
+                                                     boolean initialNotify);
+
+  /**
+   * Check if the compressor is on.
+   *
+   * @return true if the compressor is active
+   */
+  public abstract boolean getCompressorOn();
+
+  /**
+   * Set whether the compressor is active.
+   *
+   * @param compressorOn the new value
+   */
+  public abstract void setCompressorOn(boolean compressorOn);
+
+  /**
+   * Register a callback to be run when the compressor activates.
+   *
+   * @param callback the callback
+   * @param initialNotify whether to run the callback with the initial state
+   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
+   *     this object so GC doesn't cancel the callback.
+   */
+  public abstract CallbackStore registerCompressorOnCallback(
+          NotifyCallback callback, boolean initialNotify);
+
+  /**
+   * Check the solenoid output on a specific channel.
+   *
+   * @param channel the channel to check
+   * @return the solenoid output
+   */
+  public abstract boolean getSolenoidOutput(int channel);
+
+  /**
+   * Change the solenoid output on a specific channel.
+   *
+   * @param channel the channel to check
+   * @param solenoidOutput the new solenoid output
+   */
+  public abstract void setSolenoidOutput(int channel, boolean solenoidOutput);
+
+  /**
+   * Register a callback to be run when the solenoid output on a channel changes.
+   *
+   * @param channel the channel to monitor
+   * @param callback the callback
+   * @param initialNotify should the callback be run with the initial value
+   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
+   *     this object so GC doesn't cancel the callback.
+   */
+  public abstract CallbackStore registerSolenoidOutputCallback(
+          int channel, NotifyCallback callback, boolean initialNotify);
+
+  /**
+   * Check the value of the pressure switch.
+   *
+   * @return the pressure switch value
+   */
+  public abstract boolean getPressureSwitch();
+
+  /**
+   * Set the value of the pressure switch.
+   *
+   * @param pressureSwitch the new value
+   */
+  public abstract void setPressureSwitch(boolean pressureSwitch);
+
+  /**
+   * Register a callback to be run whenever the pressure switch value changes.
+   *
+   * @param callback the callback
+   * @param initialNotify whether the callback should be called with the initial value
+   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
+   *     this object so GC doesn't cancel the callback.
+   */
+  public abstract CallbackStore registerPressureSwitchCallback(
+          NotifyCallback callback, boolean initialNotify);
+
+  /**
+   * Read the compressor current.
+   *
+   * @return the current of the compressor connected to this module
+   */
+  public abstract double getCompressorCurrent();
+
+  /**
+   * Set the compressor current.
+   *
+   * @param compressorCurrent the new compressor current
+   */
+  public abstract void setCompressorCurrent(double compressorCurrent);
+
+  /**
+   * Register a callback to be run whenever the compressor current changes.
+   *
+   * @param callback the callback
+   * @param initialNotify whether to call the callback with the initial state
+   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
+   *     this object so GC doesn't cancel the callback.
+   */
+  public abstract CallbackStore registerCompressorCurrentCallback(
+          NotifyCallback callback, boolean initialNotify);
+
+  /**
+   * Reset all simulation data for this object.
+   */
+  public abstract void resetData();
+}
--- wpilibj/src/main/java/edu/wpi/first/wpilibj/simulation/REVPHSim.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/simulation/REVPHSim.java
@@ -6,17 +6,16 @@ package edu.wpi.first.wpilibj.simulation;
 
 import edu.wpi.first.hal.simulation.NotifyCallback;
 import edu.wpi.first.hal.simulation.REVPHDataJNI;
-import edu.wpi.first.wpilibj.PneumaticsBase;
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.SensorUtil;
 
 /** Class to control a simulated PneumaticHub (PH). */
 @SuppressWarnings("AbbreviationAsWordInName")
-public class REVPHSim {
-  private final int m_index;
+public class REVPHSim extends PneumaticsBaseSim {
 
   /** Constructs for the default PH. */
   public REVPHSim() {
-    m_index = SensorUtil.getDefaultREVPHModule();
+    super(SensorUtil.getDefaultREVPHModule());
   }
 
   /**
@@ -25,27 +24,19 @@ public class REVPHSim {
    * @param module module number
    */
   public REVPHSim(int module) {
-    m_index = module;
+    super(module);
   }
 
   /**
-   * Constructs from a Compressor object.
+   * Constructs from a PneumaticHum object.
    *
    * @param module PCM module to simulate
    */
-  public REVPHSim(PneumaticsBase module) {
-    m_index = module.getModuleNumber();
+  public REVPHSim(PneumaticHub module) {
+    super(module);
   }
 
-  /**
-   * Register a callback to be run when the solenoid output on a channel changes.
-   *
-   * @param channel the channel to monitor
-   * @param callback the callback
-   * @param initialNotify should the callback be run with the initial value
-   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
-   *     this object so GC doesn't cancel the callback.
-   */
+  @Override
   public CallbackStore registerSolenoidOutputCallback(
       int channel, NotifyCallback callback, boolean initialNotify) {
     int uid =
@@ -53,85 +44,45 @@ public class REVPHSim {
     return new CallbackStore(m_index, channel, uid, REVPHDataJNI::cancelSolenoidOutputCallback);
   }
 
-  /**
-   * Check the solenoid output on a specific channel.
-   *
-   * @param channel the channel to check
-   * @return the solenoid output
-   */
+  @Override
   public boolean getSolenoidOutput(int channel) {
     return REVPHDataJNI.getSolenoidOutput(m_index, channel);
   }
 
-  /**
-   * Change the solenoid output on a specific channel.
-   *
-   * @param channel the channel to check
-   * @param solenoidOutput the new solenoid output
-   */
+  @Override
   public void setSolenoidOutput(int channel, boolean solenoidOutput) {
     REVPHDataJNI.setSolenoidOutput(m_index, channel, solenoidOutput);
   }
 
-  /**
-   * Register a callback to be run when the compressor is initialized.
-   *
-   * @param callback the callback
-   * @param initialNotify whether to run the callback with the initial state
-   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
-   *     this object so GC doesn't cancel the callback.
-   */
+  @Override
   public CallbackStore registerInitializedCallback(NotifyCallback callback, boolean initialNotify) {
     int uid = REVPHDataJNI.registerInitializedCallback(m_index, callback, initialNotify);
     return new CallbackStore(m_index, uid, REVPHDataJNI::cancelInitializedCallback);
   }
 
-  /**
-   * Check whether the compressor has been initialized.
-   *
-   * @return true if initialized
-   */
+  @Override
   public boolean getInitialized() {
     return REVPHDataJNI.getInitialized(m_index);
   }
 
-  /**
-   * Define whether the compressor has been initialized.
-   *
-   * @param initialized whether the compressor is initialized
-   */
+  @Override
   public void setInitialized(boolean initialized) {
     REVPHDataJNI.setInitialized(m_index, initialized);
   }
 
-  /**
-   * Register a callback to be run when the compressor activates.
-   *
-   * @param callback the callback
-   * @param initialNotify whether to run the callback with the initial state
-   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
-   *     this object so GC doesn't cancel the callback.
-   */
+  @Override
   public CallbackStore registerCompressorOnCallback(
       NotifyCallback callback, boolean initialNotify) {
     int uid = REVPHDataJNI.registerCompressorOnCallback(m_index, callback, initialNotify);
     return new CallbackStore(m_index, uid, REVPHDataJNI::cancelCompressorOnCallback);
   }
 
-  /**
-   * Check if the compressor is on.
-   *
-   * @return true if the compressor is active
-   */
+  @Override
   public boolean getCompressorOn() {
     return REVPHDataJNI.getCompressorOn(m_index);
   }
 
-  /**
-   * Set whether the compressor is active.
-   *
-   * @param compressorOn the new value
-   */
+  @Override
   public void setCompressorOn(boolean compressorOn) {
     REVPHDataJNI.setCompressorOn(m_index, compressorOn);
   }
@@ -168,71 +119,41 @@ public class REVPHSim {
     REVPHDataJNI.setCompressorConfigType(m_index, compressorConfigType);
   }
 
-  /**
-   * Register a callback to be run whenever the pressure switch value changes.
-   *
-   * @param callback the callback
-   * @param initialNotify whether the callback should be called with the initial value
-   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
-   *     this object so GC doesn't cancel the callback.
-   */
+  @Override
   public CallbackStore registerPressureSwitchCallback(
       NotifyCallback callback, boolean initialNotify) {
     int uid = REVPHDataJNI.registerPressureSwitchCallback(m_index, callback, initialNotify);
     return new CallbackStore(m_index, uid, REVPHDataJNI::cancelPressureSwitchCallback);
   }
 
-  /**
-   * Check the value of the pressure switch.
-   *
-   * @return the pressure switch value
-   */
+  @Override
   public boolean getPressureSwitch() {
     return REVPHDataJNI.getPressureSwitch(m_index);
   }
 
-  /**
-   * Set the value of the pressure switch.
-   *
-   * @param pressureSwitch the new value
-   */
+  @Override
   public void setPressureSwitch(boolean pressureSwitch) {
     REVPHDataJNI.setPressureSwitch(m_index, pressureSwitch);
   }
 
-  /**
-   * Register a callback to be run whenever the compressor current changes.
-   *
-   * @param callback the callback
-   * @param initialNotify whether to call the callback with the initial state
-   * @return the {@link CallbackStore} object associated with this callback. Save a reference to
-   *     this object so GC doesn't cancel the callback.
-   */
+  @Override
   public CallbackStore registerCompressorCurrentCallback(
       NotifyCallback callback, boolean initialNotify) {
     int uid = REVPHDataJNI.registerCompressorCurrentCallback(m_index, callback, initialNotify);
     return new CallbackStore(m_index, uid, REVPHDataJNI::cancelCompressorCurrentCallback);
   }
 
-  /**
-   * Read the compressor current.
-   *
-   * @return the current of the compressor connected to this module
-   */
+  @Override
   public double getCompressorCurrent() {
     return REVPHDataJNI.getCompressorCurrent(m_index);
   }
 
-  /**
-   * Set the compressor current.
-   *
-   * @param compressorCurrent the new compressor current
-   */
+  @Override
   public void setCompressorCurrent(double compressorCurrent) {
     REVPHDataJNI.setCompressorCurrent(m_index, compressorCurrent);
   }
 
-  /** Reset all simulation data for this object. */
+  @Override
   public void resetData() {
     REVPHDataJNI.resetData(m_index);
   }
