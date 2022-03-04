e783c803d76a1f7f8c7a48e6527bf629907591c4
Ashray._.g <43589795+Ashray-g@users.noreply.github.com>
2022-03-03 23:14:27 -0800

    Update wpimath/src/test/native/cpp/interpolation/InterpolatingMapTest.cpp
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

--- wpimath/src/test/native/cpp/interpolation/InterpolatingMapTest.cpp
+++ wpimath/src/test/native/cpp/interpolation/InterpolatingMapTest.cpp
@@ -1,4 +1,6 @@
-// Copyright (c) FRC Team 3512. All Rights Reserved.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 #include "frc/interpolation/InterpolatingMap.h"  // NOLINT(build/include_order)
 
