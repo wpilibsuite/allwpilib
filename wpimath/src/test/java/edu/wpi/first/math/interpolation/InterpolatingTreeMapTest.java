1567f4c87e3392cf749d1ebe7a3998a707185c84
Ashray._.g <ashray.gupta@gmail.com>
2022-03-05 17:59:36 -0800

    test comments

--- wpimath/src/test/java/edu/wpi/first/math/interpolation/InterpolatingTreeMapTest.java
+++ wpimath/src/test/java/edu/wpi/first/math/interpolation/InterpolatingTreeMapTest.java
@@ -19,11 +19,22 @@ class InterpolatingTreeMapTest {
     interpolatingDoubleTreeMap.put(312.0, 550.0);
     interpolatingDoubleTreeMap.put(326.0, 650.0);
 
+    // Key below minimum gives smallest value
     assertEquals(450.0, interpolatingDoubleTreeMap.getInterpolated(100.0));
+
+    // Minimum key gives exact value
     assertEquals(450.0, interpolatingDoubleTreeMap.getInterpolated(125.0));
+
+    // Key gives interpolated value
     assertEquals(480.0, interpolatingDoubleTreeMap.getInterpolated(162.5));
+
+    // Key at right of interpolation range gives exact value
     assertEquals(510.0, interpolatingDoubleTreeMap.getInterpolated(200.0));
+
+    // Maximum key gives exact value
     assertEquals(650.0, interpolatingDoubleTreeMap.getInterpolated(326.0));
+
+    // Key above maximum gives largest value
     assertEquals(650.0, interpolatingDoubleTreeMap.getInterpolated(400.0));
   }
 }
