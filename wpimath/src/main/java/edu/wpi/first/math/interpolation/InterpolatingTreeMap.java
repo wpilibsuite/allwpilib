0fcff756b65f0e0c45fa42e38cee7b1466983694
Ashray._.g <ashray.gupta@gmail.com>
2022-03-05 20:59:50 -0800

    Interpolated Tree Map takes Objects that extend Number

--- wpimath/src/main/java/edu/wpi/first/math/interpolation/InterpolatingTreeMap.java
+++ wpimath/src/main/java/edu/wpi/first/math/interpolation/InterpolatingTreeMap.java
@@ -10,48 +10,46 @@ import java.util.TreeMap;
  * Interpolating Tree Maps are used to get values at points that are not defined by making a guess
  * from points that are defined. This uses linear interpolation.
  */
-public class InterpolatingDoubleTreeMap extends TreeMap<Double, Double> {
+public class InterpolatingTreeMap<K extends Number, V extends Number> extends TreeMap<K, V> {
   /**
    * @param key Lookup for a value (does not have to exist)
    * @return Double
    */
-  public Double getInterpolated(double key) {
-    Double val = get(key);
+  public Double getInterpolated(K key) {
+    V val = get(key);
     if (val == null) {
-      Double ceilingKey = ceilingKey(key);
-      Double floorKey = floorKey(key);
+      K ceilingKey = ceilingKey(key);
+      K floorKey = floorKey(key);
 
       if (ceilingKey == null && floorKey == null) {
         return null;
       }
       if (ceilingKey == null) {
-        return get(floorKey);
+        return get(floorKey).doubleValue();
       }
       if (floorKey == null) {
-        return get(ceilingKey);
+        return get(ceilingKey).doubleValue();
       }
-      Double floor = get(floorKey);
-      Double ceiling = get(ceilingKey);
+      V floor = get(floorKey);
+      V ceiling = get(ceilingKey);
 
       return interpolate(floor, ceiling, inverseInterpolate(ceilingKey, key, floorKey));
-    } else {
-      return val;
-    }
+    } else return val.doubleValue();
   }
 
-  public Double interpolate(Double val1, Double val2, double d) {
-    double dydx = val2 - val1;
-    return dydx * d + val1;
+  public double interpolate(V val1, V val2, double d) {
+    double dydx = val2.doubleValue() - val1.doubleValue();
+    return dydx * d + val1.doubleValue();
   }
 
-  public double inverseInterpolate(Double up, double q, Double down) {
-    double upperToLower = up - down;
+  public double inverseInterpolate(K up, K q, K down) {
+    double upperToLower = up.doubleValue() - down.doubleValue();
     if (upperToLower <= 0) {
-      return 0;
+      return 0.0;
     }
-    double queryToLower = q - down;
+    double queryToLower = q.doubleValue() - down.doubleValue();
     if (queryToLower <= 0) {
-      return 0;
+      return 0.0;
     }
     return queryToLower / upperToLower;
   }
--- wpimath/src/test/java/edu/wpi/first/math/interpolation/InterpolatingTreeMapTest.java
+++ wpimath/src/test/java/edu/wpi/first/math/interpolation/InterpolatingTreeMapTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 
 class InterpolatingTreeMapTest {
   @Test
-  void testInterpolation() {
-    InterpolatingDoubleTreeMap interpolatingDoubleTreeMap = new InterpolatingDoubleTreeMap();
+  void testInterpolationDouble() {
+    InterpolatingTreeMap<Double, Double> interpolatingDoubleTreeMap = new InterpolatingTreeMap<>();
 
     interpolatingDoubleTreeMap.put(125.0, 450.0);
     interpolatingDoubleTreeMap.put(200.0, 510.0);
@@ -36,5 +36,64 @@ class InterpolatingTreeMapTest {
 
     // Key above maximum gives largest value
     assertEquals(650.0, interpolatingDoubleTreeMap.getInterpolated(400.0));
+  }
+
+  @Test
+  void testInterpolationInteger() {
+    InterpolatingTreeMap<Integer, Integer> interpolatingIntegerTreeMap =
+        new InterpolatingTreeMap<>();
+
+    interpolatingIntegerTreeMap.put(125, 450);
+    interpolatingIntegerTreeMap.put(200, 510);
+    interpolatingIntegerTreeMap.put(268, 525);
+    interpolatingIntegerTreeMap.put(312, 550);
+    interpolatingIntegerTreeMap.put(326, 650);
+
+    // Key below minimum gives smallest value
+    assertEquals(450.0, interpolatingIntegerTreeMap.getInterpolated(100));
+
+    // Minimum key gives exact value
+    assertEquals(450.0, interpolatingIntegerTreeMap.getInterpolated(125));
+
+    // Key gives interpolated value
+    assertEquals(479.6, interpolatingIntegerTreeMap.getInterpolated(162));
+
+    // Key at right of interpolation range gives exact value
+    assertEquals(510.0, interpolatingIntegerTreeMap.getInterpolated(200));
+
+    // Maximum key gives exact value
+    assertEquals(650.0, interpolatingIntegerTreeMap.getInterpolated(326));
+
+    // Key above maximum gives largest value
+    assertEquals(650.0, interpolatingIntegerTreeMap.getInterpolated(400));
+  }
+
+  @Test
+  void testInterpolationLong() {
+    InterpolatingTreeMap<Long, Long> interpolatingIntegerTreeMap = new InterpolatingTreeMap<>();
+
+    interpolatingIntegerTreeMap.put(125L, 450L);
+    interpolatingIntegerTreeMap.put(200L, 510L);
+    interpolatingIntegerTreeMap.put(268L, 525L);
+    interpolatingIntegerTreeMap.put(312L, 550L);
+    interpolatingIntegerTreeMap.put(326L, 650L);
+
+    // Key below minimum gives smallest value
+    assertEquals(450.0, interpolatingIntegerTreeMap.getInterpolated(100L));
+
+    // Minimum key gives exact value
+    assertEquals(450.0, interpolatingIntegerTreeMap.getInterpolated(125L));
+
+    // Key gives interpolated value
+    assertEquals(479.6, interpolatingIntegerTreeMap.getInterpolated(162L));
+
+    // Key at right of interpolation range gives exact value
+    assertEquals(510.0, interpolatingIntegerTreeMap.getInterpolated(200L));
+
+    // Maximum key gives exact value
+    assertEquals(650.0, interpolatingIntegerTreeMap.getInterpolated(326L));
+
+    // Key above maximum gives largest value
+    assertEquals(650.0, interpolatingIntegerTreeMap.getInterpolated(400L));
   }
 }
