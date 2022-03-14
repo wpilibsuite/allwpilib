a2fa5e3ff700e47f6905fed50e0dc908c0b7f93e
Dustin Spicuzza <dustin@virtualroadside.com>
2022-03-14 13:09:55 -0400

    [wpilibc] BatterySim: Provide non-initializer list versions of Calculate (#4076)

--- wpilibc/src/main/native/include/frc/simulation/BatterySim.h
+++ wpilibc/src/main/native/include/frc/simulation/BatterySim.h
@@ -9,6 +9,7 @@
 #include <units/current.h>
 #include <units/impedance.h>
 #include <units/voltage.h>
+#include <wpi/span.h>
 
 namespace frc::sim {
 
@@ -29,11 +30,44 @@ class BatterySim {
    * @param currents       The currents drawn from the battery.
    * @return The battery's voltage under load.
    */
+  static units::volt_t Calculate(units::volt_t nominalVoltage,
+                                 units::ohm_t resistance,
+                                 wpi::span<const units::ampere_t> currents) {
+    return nominalVoltage -
+           std::accumulate(currents.begin(), currents.end(), 0_A) * resistance;
+  }
+
+  /**
+   * Calculate the loaded battery voltage. Use this with
+   * RoboRioSim::SetVInVoltage(double) to set the simulated battery voltage,
+   * which can then be retrieved with the RobotController::GetBatteryVoltage()
+   * method.
+   *
+   * @param nominalVoltage The nominal battery voltage. Usually 12v.
+   * @param resistance     The forward resistance of the battery. Most batteries
+   *                       are at or below 20 milliohms.
+   * @param currents       The currents drawn from the battery.
+   * @return The battery's voltage under load.
+   */
   static units::volt_t Calculate(
       units::volt_t nominalVoltage, units::ohm_t resistance,
       std::initializer_list<units::ampere_t> currents) {
     return nominalVoltage -
            std::accumulate(currents.begin(), currents.end(), 0_A) * resistance;
+  }
+
+  /**
+   * Calculate the loaded battery voltage. Use this with
+   * RoboRioSimSetVInVoltage(double) to set the simulated battery voltage, which
+   * can then be retrieved with the RobotController::GetBatteryVoltage() method.
+   * This function assumes a nominal voltage of 12V and a resistance of 20
+   * milliohms (0.020 ohms).
+   *
+   * @param currents The currents drawn from the battery.
+   * @return The battery's voltage under load.
+   */
+  static units::volt_t Calculate(wpi::span<const units::ampere_t> currents) {
+    return Calculate(12_V, 0.02_Ohm, currents);
   }
 
   /**
