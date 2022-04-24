b3aee28388d8a1e24002807a58361a3223809df3
ohowe <42757516+carelesshippo@users.noreply.github.com>
2022-04-24 08:20:46 -0600

    [commands] Allow BooleanSupplier for Trigger operations (#4103)

--- wpilibNewCommands/src/main/native/include/frc2/command/button/Trigger.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/button/Trigger.h
@@ -35,7 +35,7 @@ class Trigger {
    *
    * @param isActive Whether the trigger is active.
    */
-  explicit Trigger(std::function<bool()> isActive)
+  Trigger(std::function<bool()> isActive)  // NOLINT
       : m_isActive{std::move(isActive)} {}
 
   /**
