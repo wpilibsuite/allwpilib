0a45f10fefc593e2d6bbd989aa62624fcb1b603a
Tyler Veness <calcmogul@gmail.com>
2022-05-04 16:34:21 -0700

    Put Java SimDevice functions back to avoid breaking vendors

--- hal/src/main/java/edu/wpi/first/hal/SimDeviceJNI.java
+++ hal/src/main/java/edu/wpi/first/hal/SimDeviceJNI.java
@@ -43,6 +43,30 @@ public class SimDeviceJNI extends JNIWrapper {
    *
    * @param device simulated device handle
    * @param name value name
+   * @param readonly if the value should not be written from simulation side
+   * @param initialValue initial value
+   * @return simulated value handle
+   * @deprecated Use direction-taking function instead
+   */
+  @Deprecated
+  public static int createSimValue(
+      int device, String name, boolean readonly, HALValue initialValue) {
+    return createSimValueNative(
+        device,
+        name,
+        readonly ? kOutput : kInput,
+        initialValue.getType(),
+        initialValue.getNativeLong(),
+        initialValue.getNativeDouble());
+  }
+
+  /**
+   * Creates a value on a simulated device.
+   *
+   * <p>Returns 0 if not in simulation; this can be used to avoid calls to Set/Get functions.
+   *
+   * @param device simulated device handle
+   * @param name value name
    * @param direction input/output/bidir (from perspective of user code)
    * @param initialValue initial value
    * @return simulated value handle
@@ -94,6 +118,25 @@ public class SimDeviceJNI extends JNIWrapper {
    *
    * @param device simulated device handle
    * @param name value name
+   * @param readonly if the value should not be written from simulation side
+   * @param initialValue initial value
+   * @return simulated value handle
+   * @deprecated Use direction-taking function instead
+   */
+  @Deprecated
+  public static int createSimValueDouble(
+      int device, String name, boolean readonly, double initialValue) {
+    return createSimValueNative(
+        device, name, readonly ? kOutput : kInput, HALValue.kDouble, 0, initialValue);
+  }
+
+  /**
+   * Creates a double value on a simulated device.
+   *
+   * <p>Returns 0 if not in simulation; this can be used to avoid calls to Set/Get functions.
+   *
+   * @param device simulated device handle
+   * @param name value name
    * @param direction input/output/bidir (from perspective of user code)
    * @param initialValue initial value
    * @return simulated value handle
@@ -101,6 +144,27 @@ public class SimDeviceJNI extends JNIWrapper {
   public static int createSimValueDouble(
       int device, String name, int direction, double initialValue) {
     return createSimValueNative(device, name, direction, HALValue.kDouble, 0, initialValue);
+  }
+
+  /**
+   * Creates an enumerated value on a simulated device.
+   *
+   * <p>Enumerated values are always in the range 0 to numOptions-1.
+   *
+   * <p>Returns 0 if not in simulation; this can be used to avoid calls to Set/Get functions.
+   *
+   * @param device simulated device handle
+   * @param name value name
+   * @param readonly if the value should not be written from simulation side
+   * @param options array of option descriptions
+   * @param initialValue initial value (selection)
+   * @return simulated value handle
+   * @deprecated Use direction-taking function instead
+   */
+  @Deprecated
+  public static int createSimValueEnum(
+      int device, String name, boolean readonly, String[] options, int initialValue) {
+    return createSimValueEnum(device, name, readonly ? kOutput : kInput, options, initialValue);
   }
 
   /**
@@ -142,6 +206,25 @@ public class SimDeviceJNI extends JNIWrapper {
       String[] options,
       double[] optionValues,
       int initialValue);
+
+  /**
+   * Creates a boolean value on a simulated device.
+   *
+   * <p>Returns 0 if not in simulation; this can be used to avoid calls to Set/Get functions.
+   *
+   * @param device simulated device handle
+   * @param name value name
+   * @param readonly if the value should not be written from simulation side
+   * @param initialValue initial value
+   * @return simulated value handle
+   * @deprecated Use direction-taking function instead
+   */
+  @Deprecated
+  public static int createSimValueBoolean(
+      int device, String name, boolean readonly, boolean initialValue) {
+    return createSimValueNative(
+        device, name, readonly ? kOutput : kInput, HALValue.kBoolean, initialValue ? 1 : 0, 0.0);
+  }
 
   /**
    * Creates a boolean value on a simulated device.
