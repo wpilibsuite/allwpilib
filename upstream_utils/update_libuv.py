3792a19851890a609ab8690ef5752029023b1ff4
PJ Reiniger <pj.reiniger@gmail.com>
2022-05-07 17:23:40 -0400

    Styleguide

--- upstream_utils/update_libuv.py
+++ upstream_utils/update_libuv.py
@@ -10,21 +10,21 @@ from upstream_utils import setup_upstream_repo, comment_out_invalid_includes, wa
 def main():
     root, repo = setup_upstream_repo("http://github.com/libuv/libuv", "v1.30.1")
     wpinet = os.path.join(root, "wpinet")
-    
+
     # Apply patches to original git repo
     am_patches(repo, [
+        # yapf: disable
         os.path.join(root, "upstream_utils/libuv_patches/0001-Fix-missing-casts.patch"),
         os.path.join(root, "upstream_utils/libuv_patches/0002-Fix-warnings.patch"),
         os.path.join(root, "upstream_utils/libuv_patches/0003-Preprocessor-cleanup.patch"),
         os.path.join(root, "upstream_utils/libuv_patches/0004-Cleanup-problematic-language.patch"),
         os.path.join(root, "upstream_utils/libuv_patches/0005-Use-roborio-time.patch"),
         os.path.join(root, "upstream_utils/libuv_patches/0006-Style-comments-cleanup.patch"),
+        # yapf: enable
     ])
-    
+
     # Delete old install
-    for d in [
-            "src/main/native/thirdparty/libuv"
-    ]:
+    for d in ["src/main/native/thirdparty/libuv"]:
         shutil.rmtree(os.path.join(wpinet, d), ignore_errors=True)
 
     include_blacklist = [
@@ -37,7 +37,7 @@ def main():
     include_files = walk_cwd_and_copy_if(
         lambda dp, f: "include" in dp and f not in include_blacklist,
         os.path.join(wpinet, "src/main/native/thirdparty/libuv"))
-        
+
     src_blacklist = [
         "aix-common.c",
         "aix.c",
@@ -50,9 +50,9 @@ def main():
         "sysinfo-memory.c",
     ]
     src_files = walk_cwd_and_copy_if(
-        lambda dp, f: "src" in dp and "docs" not in dp and f not in src_blacklist,
-        os.path.join(wpinet, "src/main/native/thirdparty/libuv"))
+        lambda dp, f: "src" in dp and "docs" not in dp and f not in
+        src_blacklist, os.path.join(wpinet, "src/main/native/thirdparty/libuv"))
 
-    
+
 if __name__ == "__main__":
     main()
--- upstream_utils/update_stack_walker.py
+++ upstream_utils/update_stack_walker.py
@@ -10,14 +10,17 @@ import subprocess
 
 from upstream_utils import setup_upstream_repo, comment_out_invalid_includes, walk_cwd_and_copy_if, am_patches, walk_if, copy_to
 
+
 def main():
-    root, repo = setup_upstream_repo("https://github.com/JochenKalmbach/StackWalker", "42e7a6e056a9e7aca911a7e9e54e2e4f90bc2652")
+    root, repo = setup_upstream_repo(
+        "https://github.com/JochenKalmbach/StackWalker",
+        "42e7a6e056a9e7aca911a7e9e54e2e4f90bc2652")
     wpiutil = os.path.join(root, "wpiutil")
 
-    
     pr35_url = "http://patch-diff.githubusercontent.com/raw/JochenKalmbach/StackWalker/pull/35.patch"
-    pr35_patch_download_path = os.path.join(tempfile.gettempdir(), "stackwalker.patch")
-    
+    pr35_patch_download_path = os.path.join(tempfile.gettempdir(),
+                                            "stackwalker.patch")
+
     response = urllib.request.urlopen(pr35_url)
 
     with open(pr35_patch_download_path, 'wb') as f:
@@ -25,14 +28,13 @@ def main():
 
     am_patches(repo, [pr35_patch_download_path])
 
-    shutil.copy(
-        os.path.join("Main", "StackWalker", "StackWalker.h"),
-        os.path.join(wpiutil, "src/main/native/windows/StackWalker.h"))
-        
+    shutil.copy(os.path.join("Main", "StackWalker", "StackWalker.h"),
+                os.path.join(wpiutil, "src/main/native/windows/StackWalker.h"))
+
     shutil.copy(
         os.path.join("Main", "StackWalker", "StackWalker.cpp"),
         os.path.join(wpiutil, "src/main/native/windows/StackWalker.cpp"))
 
-    
+
 if __name__ == "__main__":
     main()
