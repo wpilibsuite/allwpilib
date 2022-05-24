35f375a36835934ab723f1ebfc78aebea264a917
PJ Reiniger <pj.reiniger@gmail.com>
2022-05-24 15:26:45 -0400

    Apply CRLF -> LF before doing patches

--- upstream_utils/update_stack_walker.py
+++ upstream_utils/update_stack_walker.py
@@ -2,9 +2,27 @@
 
 import os
 import shutil
+import subprocess
 
 from upstream_utils import setup_upstream_repo, comment_out_invalid_includes, walk_cwd_and_copy_if, am_patches, walk_if, copy_to
 
+def crlf_to_lf(stackwalker_dir):
+    for root, _, files in os.walk(stackwalker_dir):
+        if ".git" in root:
+            continue
+
+        for fname in files:
+            filename = os.path.join(root, fname)
+            print(f"Converting CRLF -> LF for {filename}")
+            with open(filename, 'rb') as f:
+                content = f.read()
+                content = content.replace(b'\r\n', b'\n')
+
+                with open(filename, 'wb') as f:
+                    f.write(content)
+
+    subprocess.check_call(["git", "add", "-A"])
+    subprocess.check_call(["git", "commit", "-m", "Fix line endings"])
 
 def main():
     root, repo = setup_upstream_repo(
@@ -12,6 +30,9 @@ def main():
         "42e7a6e056a9e7aca911a7e9e54e2e4f90bc2652",
         shallow=False)
     wpiutil = os.path.join(root, "wpiutil")
+
+    # Run CRLF -> LF before trying any patches
+    crlf_to_lf(repo)
 
     # Apply patches to original git repo
     patch_dir = os.path.join(root, "upstream_utils/stack_walker_patches")
