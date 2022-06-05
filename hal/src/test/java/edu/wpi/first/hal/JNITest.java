25a0ddd6a5b8684e0222752be30c3277af16a13b
PJ Reiniger <pj.reiniger@gmail.com>
2022-06-05 17:09:43 -0400

    styleguide

--- hal/src/test/java/edu/wpi/first/hal/JNITest.java
+++ hal/src/test/java/edu/wpi/first/hal/JNITest.java
@@ -7,9 +7,9 @@ package edu.wpi.first.hal;
 import org.junit.jupiter.api.Test;
 
 public class JNITest {
-    @Test
-    void jniLinkTest() {
-        // Test to verify that the JNI test link works correctly.
-        HAL.initialize(500, 0);
-    }
+  @Test
+  void jniLinkTest() {
+    // Test to verify that the JNI test link works correctly.
+    HAL.initialize(500, 0);
+  }
 }
