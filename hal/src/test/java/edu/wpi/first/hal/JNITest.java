30f5b682647d33289a0c758595693f9db113c5f8
PJ Reiniger <pj.reiniger@gmail.com>
2022-06-09 01:21:22 -0400

    [wpinet] Fix JNI loading error (#4295)

--- hal/src/test/java/edu/wpi/first/hal/JNITest.java
+++ hal/src/test/java/edu/wpi/first/hal/JNITest.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.net;
+package edu.wpi.first.hal;
 
-import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
-public class WPINetJNITest {
+public class JNITest {
   @Test
-  void jniLinkTest() throws IOException {
-    WPINetJNI.forceLoad();
+  void jniLinkTest() {
+    // Test to verify that the JNI test link works correctly.
+    HAL.initialize(500, 0);
   }
 }
