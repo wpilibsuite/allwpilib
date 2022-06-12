5864f7898c69fe98a9d44d559656789349226e41
Ashray._.g <ashray.gupta@gmail.com>
2022-06-12 05:26:06 -0700

    Sim

--- wpimath/src/main/java/edu/wpi/first/math/optimization/SimulatedAnnealing.java
+++ wpimath/src/main/java/edu/wpi/first/math/optimization/SimulatedAnnealing.java
@@ -1,2 +1,60 @@
-package edu.wpi.first.math.optimization;public class SimulatedAnnealing {
+package edu.wpi.first.math.optimization;
+
+import edu.wpi.first.math.Num;
+import edu.wpi.first.math.Vector;
+import java.util.function.Function;
+
+/**
+ * An implementation of the Simulated Annealing stochastic nonlinear optimization method for finding
+ * global extrema
+ */
+public class SimulatedAnnealing {
+
+  /**
+   * Runs the Simulated Annealing algorithm on a Vector that defines the state.
+   *
+   * @param initialGuess The initial state
+   * @param epochs Number of iterations to run.
+   * @param initialTemperature The initial temperature
+   * @param getNeighbor Function to generate a random neighbor of current state vector.
+   * @param getCost Function to get the double value of a state vector.
+   * @param <T> Natural number defining the number of rows in the state vector.
+   * @return The optimized state vector
+   */
+  public static <T extends Num> Vector<T> minimize(
+      Vector<T> initialGuess,
+      int epochs,
+      double initialTemperature,
+      Function<Vector<T>, Vector<T>> getNeighbor,
+      Function<Vector<T>, Double> getCost) {
+
+    double min = Double.MAX_VALUE;
+    Vector<T> minState = initialGuess;
+
+    int epoch = 0;
+    double temperature;
+
+    Vector<T> state = initialGuess;
+
+    while (epoch++ < epochs) {
+      temperature = initialTemperature / epoch;
+
+      Vector<T> proposedState = getNeighbor.apply(state);
+      double proposedStateCost = getCost.apply(proposedState);
+
+      double deltaCost = proposedStateCost - getCost.apply(state);
+
+      double acceptanceProbability = Math.exp(-deltaCost / temperature);
+
+      if (deltaCost < 0 || Math.random() < acceptanceProbability) {
+        state = proposedState;
+      }
+
+      if (proposedStateCost < min) {
+        min = proposedStateCost;
+        minState = proposedState;
+      }
+    }
+    return minState;
+  }
 }
