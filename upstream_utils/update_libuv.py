5eb44e22a9b351f69944e7e03737e440ab888e96
Tyler Veness <calcmogul@gmail.com>
2022-07-01 06:41:44 -0700

    Format Python scripts with black (NFC) (#4325)

--- upstream_utils/update_libuv.py
+++ upstream_utils/update_libuv.py
@@ -4,27 +4,36 @@ import os
 import shutil
 import re
 
-from upstream_utils import setup_upstream_repo, comment_out_invalid_includes, walk_cwd_and_copy_if, am_patches, walk_if, copy_to
+from upstream_utils import (
+    setup_upstream_repo,
+    comment_out_invalid_includes,
+    walk_cwd_and_copy_if,
+    am_patches,
+    walk_if,
+    copy_to,
+)
 
 
 def main():
-    root, repo = setup_upstream_repo("https://github.com/libuv/libuv",
-                                     "v1.44.1")
+    root, repo = setup_upstream_repo("https://github.com/libuv/libuv", "v1.44.1")
     wpinet = os.path.join(root, "wpinet")
 
     # Apply patches to original git repo
     prefix = os.path.join(root, "upstream_utils/libuv_patches")
-    am_patches(repo, [
-        os.path.join(prefix, "0001-Fix-missing-casts.patch"),
-        os.path.join(prefix, "0002-Fix-warnings.patch"),
-        os.path.join(prefix, "0003-Preprocessor-cleanup.patch"),
-        os.path.join(prefix, "0004-Cleanup-problematic-language.patch"),
-        os.path.join(prefix, "0005-Use-roborio-time.patch"),
-        os.path.join(prefix, "0006-Style-comments-cleanup.patch"),
-        os.path.join(prefix, "0007-Squelch-GCC-12.1-warnings.patch"),
-        os.path.join(prefix, "0008-Fix-Win32-warning-suppression-pragma.patch"),
-        os.path.join(prefix, "0009-Avoid-unused-variable-warning-on-Mac.patch"),
-    ])
+    am_patches(
+        repo,
+        [
+            os.path.join(prefix, "0001-Fix-missing-casts.patch"),
+            os.path.join(prefix, "0002-Fix-warnings.patch"),
+            os.path.join(prefix, "0003-Preprocessor-cleanup.patch"),
+            os.path.join(prefix, "0004-Cleanup-problematic-language.patch"),
+            os.path.join(prefix, "0005-Use-roborio-time.patch"),
+            os.path.join(prefix, "0006-Style-comments-cleanup.patch"),
+            os.path.join(prefix, "0007-Squelch-GCC-12.1-warnings.patch"),
+            os.path.join(prefix, "0008-Fix-Win32-warning-suppression-pragma.patch"),
+            os.path.join(prefix, "0009-Avoid-unused-variable-warning-on-Mac.patch"),
+        ],
+    )
 
     # Delete old install
     for d in ["src/main/native/thirdparty/libuv"]:
@@ -39,7 +48,8 @@ def main():
 
     include_files = walk_cwd_and_copy_if(
         lambda dp, f: "include" in dp and f not in include_ignorelist,
-        os.path.join(wpinet, "src/main/native/thirdparty/libuv"))
+        os.path.join(wpinet, "src/main/native/thirdparty/libuv"),
+    )
 
     src_ignorelist = [
         "aix-common.c",
@@ -58,9 +68,9 @@ def main():
         "sysinfo-memory.c",
     ]
     src_files = walk_cwd_and_copy_if(
-        lambda dp, f: "src" in dp and "docs" not in dp and f
-        not in src_ignorelist,
-        os.path.join(wpinet, "src/main/native/thirdparty/libuv"))
+        lambda dp, f: "src" in dp and "docs" not in dp and f not in src_ignorelist,
+        os.path.join(wpinet, "src/main/native/thirdparty/libuv"),
+    )
 
 
 if __name__ == "__main__":
