63a5ec28a4a384873baafd99ea97426281506437
Tyler Veness <calcmogul@gmail.com>
2022-08-15 09:17:11 -0700

    Suppress enum-enum deprecation warning in OpenCV

--- cmake/modules/CompileWarnings.cmake
+++ cmake/modules/CompileWarnings.cmake
@@ -4,4 +4,12 @@ macro(wpilib_target_warnings target)
     else()
         target_compile_options(${target} PRIVATE /wd4146 /wd4244 /wd4251 /wd4267 /WX /D_CRT_SECURE_NO_WARNINGS ${WPILIB_TARGET_WARNINGS})
     endif()
+
+    # Suppress C++-specific OpenCV warning; C compiler rejects it with an error
+    # https://github.com/opencv/opencv/issues/20269
+    if(UNIX AND NOT APPLE)
+        target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-enum-enum-conversion>)
+    elseif(UNIX AND APPLE)
+        target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-anon-enum-enum-conversion>)
+    endif()
 endmacro()
