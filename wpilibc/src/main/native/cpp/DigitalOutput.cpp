a5df391166b1bed0da5663545752e4bdd1af43a6
Thad House <ThadHouse@users.noreply.github.com>
2022-09-02 16:49:42 -0700

    [hal, wpilib] Fix up DIO pulse API (#4387)
    The FPGA API takes microseconds directly, instead of a scaled value. Also add a new HAL level API to trigger multiple DIOs with the same pulse at once.

--- wpilibc/src/main/native/cpp/DigitalOutput.cpp
+++ wpilibc/src/main/native/cpp/DigitalOutput.cpp
@@ -76,9 +76,9 @@ int DigitalOutput::GetChannel() const {
   return m_channel;
 }
 
-void DigitalOutput::Pulse(double length) {
+void DigitalOutput::Pulse(units::second_t pulseLength) {
   int32_t status = 0;
-  HAL_Pulse(m_handle, length, &status);
+  HAL_Pulse(m_handle, pulseLength.to<double>(), &status);
   FRC_CheckErrorStatus(status, "Channel {}", m_channel);
 }
 
