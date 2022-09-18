1fd8b428dd05098f254693ab84e59815d2ce1293
brennenputh <brennenputh@gmail.com>
2022-09-18 15:57:25 -0400

    Fix link tags in javadocs

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/apriltag/AprilTagFieldLayout.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/apriltag/AprilTagFieldLayout.java
@@ -53,7 +53,7 @@ public class AprilTagFieldLayout {
   }
 
   /**
-   * Set the alliance that your team is on. This changes the {@link #getTag()} method to return the
+   * Set the alliance that your team is on. This changes the {@link #getTag(int)} method to return the
    * correct pose for the alliance you are on.
    *
    * @param alliance the alliance to mirror poses for
@@ -63,7 +63,7 @@ public class AprilTagFieldLayout {
   }
 
   /**
-   * Gets the raw tags. This is not affected by {@link #setAlliance()}.
+   * Gets the raw tags. This is not affected by {@link #setAlliance(DriverStation.Alliance)}.
    *
    * @return map of IDs to poses.
    */
