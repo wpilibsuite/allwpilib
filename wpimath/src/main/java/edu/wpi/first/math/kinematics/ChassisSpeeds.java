5883872f39a6f073cbf3f9317e211f1e02239207
shueja-personal <32416547+shueja-personal@users.noreply.github.com>
2022-10-21 16:09:04 -0700

    are you happy now

--- wpimath/src/main/java/edu/wpi/first/math/kinematics/ChassisSpeeds.java
+++ wpimath/src/main/java/edu/wpi/first/math/kinematics/ChassisSpeeds.java
@@ -72,23 +72,21 @@ public class ChassisSpeeds {
    * Converts a user provided field-relative ChassisSpeeds object into a robot-relative
    * ChassisSpeeds object.
    *
-   * @param fieldRelativeSpeeds The ChassisSpeeds object representing the speeds in the field
-   *     frame of reference. Positive x is away from your alliance wall. Positive y is to your
-   *     left when standing behind the alliance wall.
+   * @param fieldRelativeSpeeds The ChassisSpeeds object representing the speeds in the field frame
+   *     of reference. Positive x is away from your alliance wall. Positive y is to your left when
+   *     standing behind the alliance wall.
    * @param robotAngle The angle of the robot as measured by a gyroscope. The robot's angle is
    *     considered to be zero when it is facing directly away from your alliance station wall.
    *     Remember that this should be CCW positive.
    * @return ChassisSpeeds object representing the speeds in the robot's frame of reference.
    */
   public static ChassisSpeeds fromFieldRelativeSpeeds(
-    ChassisSpeeds fieldRelativeSpeeds,
-    Rotation2d robotAngle) {
+      ChassisSpeeds fieldRelativeSpeeds, Rotation2d robotAngle) {
     return fromFieldRelativeSpeeds(
-      fieldRelativeSpeeds.vxMetersPerSecond,
-      fieldRelativeSpeeds.vyMetersPerSecond,
-      fieldRelativeSpeeds.omegaRadiansPerSecond,
-      robotAngle
-    );
+        fieldRelativeSpeeds.vxMetersPerSecond,
+        fieldRelativeSpeeds.vyMetersPerSecond,
+        fieldRelativeSpeeds.omegaRadiansPerSecond,
+        robotAngle);
   }
 
   @Override
