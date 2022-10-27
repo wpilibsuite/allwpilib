7ad9349d19852003a337c6846c2b8688e86b83c3
Thad House <thadhouse1@gmail.com>
2022-10-26 21:40:00 -0700

    Fix comments

--- wpigui/src/main/native/opengl2/wpigui_opengl2.cpp
+++ wpigui/src/main/native/opengl2/wpigui_opengl2.cpp
@@ -25,21 +25,10 @@ void gui::PlatformDestroyContext() {}
 void gui::PlatformGlfwInitHints() {}
 
 void gui::PlatformGlfwWindowHints() {
-  // Decide GL versions
-#if __APPLE__
-  // GL 3.2 + GLSL 150
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
-  glfwWindowHint(GLFW_COCOA_GRAPHICS_SWITCHING, GLFW_TRUE);
-#else
-  // GL 3.0 + GLSL 130
+
+  // GL 2.1
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+
-  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+
-#endif
 
   // enable 4xMSAA
   glfwWindowHint(GLFW_SAMPLES, 4);
