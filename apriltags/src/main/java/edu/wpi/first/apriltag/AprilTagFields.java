1a7c1ec86e80296d6bf7ffd14b433f0e2bdb274b
PJ Reiniger <pj.reiniger@gmail.com>
2022-11-07 17:32:08 -0500

    make directory format consistent with existing april tag stuff

--- apriltags/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
+++ apriltags/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.apriltags;
+package edu.wpi.first.apriltag;
 
 public enum AprilTagFields {
   k2022RapidReact("2022-rapidreact.json");
 
-  public static final String kBaseResourceDir = "/edu/wpi/first/apriltags/";
+  public static final String kBaseResourceDir = "/edu/wpi/first/apriltag/";
 
   /** Alias to the current game. */
   public static final AprilTagFields kDefaultField = k2022RapidReact;
