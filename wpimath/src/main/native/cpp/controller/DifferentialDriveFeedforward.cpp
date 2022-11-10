5ea0db9a41048fabd463b5e3db0a06dfe192483a
Carlo Woolsey <cdbwoolsey@icloud.com>
2022-11-09 20:05:09 -0800

    Fix function order in DifferentialDriveFeedforward.cpp to match header

--- wpimath/src/main/native/cpp/controller/DifferentialDriveFeedforward.cpp
+++ wpimath/src/main/native/cpp/controller/DifferentialDriveFeedforward.cpp
@@ -4,6 +4,19 @@
 
 #include "frc/controller/DifferentialDriveFeedforward.h"
 
+frc::DifferentialDriveFeedforward::DifferentialDriveFeedforward(
+    decltype(1_V / 1_mps) kVLinear, decltype(1_V / 1_mps_sq) kALinear,
+    decltype(1_V / 1_rad_per_s) kVAngular,
+    decltype(1_V / 1_rad_per_s_sq) kAAngular, units::meter_t trackwidth)
+    : m_plant{frc::LinearSystemId::IdentifyDrivetrainSystem(
+          kVLinear, kALinear, kVAngular, kAAngular, trackwidth)} {}
+
+frc::DifferentialDriveFeedforward::DifferentialDriveFeedforward(
+    decltype(1_V / 1_mps) kVLinear, decltype(1_V / 1_mps_sq) kALinear,
+    decltype(1_V / 1_mps) kVAngular, decltype(1_V / 1_mps_sq) kAAngular)
+    : m_plant{frc::LinearSystemId::IdentifyDrivetrainSystem(
+          kVLinear, kALinear, kVAngular, kAAngular)} {}
+
 frc::DifferentialDriveWheelVoltages
 frc::DifferentialDriveFeedforward::Calculate(
     units::meters_per_second_t currentLeftVelocity,
@@ -17,16 +30,3 @@ frc::DifferentialDriveFeedforward::Calculate(
   auto u = feedforward.Calculate(r, nextR);
   return {units::volt_t{u(0)}, units::volt_t{u(1)}};
 }
-
-frc::DifferentialDriveFeedforward::DifferentialDriveFeedforward(
-    decltype(1_V / 1_mps) kVLinear, decltype(1_V / 1_mps_sq) kALinear,
-    decltype(1_V / 1_rad_per_s) kVAngular,
-    decltype(1_V / 1_rad_per_s_sq) kAAngular, units::meter_t trackwidth)
-    : m_plant{frc::LinearSystemId::IdentifyDrivetrainSystem(
-          kVLinear, kALinear, kVAngular, kAAngular, trackwidth)} {}
-
-frc::DifferentialDriveFeedforward::DifferentialDriveFeedforward(
-    decltype(1_V / 1_mps) kVLinear, decltype(1_V / 1_mps_sq) kALinear,
-    decltype(1_V / 1_mps) kVAngular, decltype(1_V / 1_mps_sq) kAAngular)
-    : m_plant{frc::LinearSystemId::IdentifyDrivetrainSystem(
-          kVLinear, kALinear, kVAngular, kAAngular)} {}
