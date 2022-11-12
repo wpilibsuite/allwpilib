ab21f7e7ea5dd8764b41f84605daac185e876cfb
phlpf <phil.j.p.frey@gmail.com>
2022-11-12 17:59:10 -0500

    [sapphire] Add playback

--- sapphire/src/main/native/cpp/TimeManager.cpp
+++ sapphire/src/main/native/cpp/TimeManager.cpp
@@ -8,10 +8,26 @@ void TimeManager::Display() {
   ImGui::Checkbox("Limit Offset?", &limitOffset);
   ImGui::Text("Manage Overall Time:");
   ImGui::SliderFloat("timestamp", &timestamp ,0, m_logView.GetMaxTimestamp()/1000000.0);
+  ImGui::SameLine();
+  ImGui::Checkbox("Play", &isPlaying);
+  if(isPlaying){
+    double deltaT = (ImGui::GetTime()-lastTime) * selected->m_speed;
+    timestamp += deltaT;
+    if(ImGui::BeginMenu("Speed")){
+      for(auto& option : m_options){
+        option.selected = &option == selected;
+        if(ImGui::MenuItem(option.m_name.c_str(), "", &option.selected)){
+          selected = &option;
+        }
+      }
+      ImGui::EndMenu();
+    }
+  }
   for(auto& log : m_logView.logs){
     ImGui::Text("%s's offset", log->filename.c_str());
     float max =  limitOffset ? std::max(0.0,log->GetMaxTimestamp()/1000000.0 - timestamp) : log->GetMaxTimestamp()/1000000.0;
     ImGui::SliderFloat(log->filename.c_str(), &log->offset ,0, max);
   }
+  lastTime = ImGui::GetTime();
   ImGui::PopID();
 };
--- sapphire/src/main/native/include/TimeManager.h
+++ sapphire/src/main/native/include/TimeManager.h
@@ -8,6 +8,7 @@
 
 namespace sapphire{
 
+
 class TimeManager : public glass::View{
     public:
     TimeManager(DataLogView& logView) : m_logView{logView}, timestamp{logView.GetTimestamp()} {};
@@ -15,6 +16,24 @@ class TimeManager : public glass::View{
     DataLogView& m_logView;
     float& timestamp;
     bool limitOffset = true;
+    bool isPlaying = false;
+    struct TimeOption{
+        TimeOption(std::string name, double speed) : m_name{name}, m_speed{speed} {};
+        std::string m_name;
+        double m_speed;
+        bool selected = false;
+    };
+    TimeOption m_options[7] = {
+        TimeOption{"1/8", 1.0/8.0},
+        TimeOption{"1/4", 1.0/4.0},
+        TimeOption{"1/2", 1.0/2.0},
+        TimeOption{"1", 1.0},
+        TimeOption{"2", 2.0},
+        TimeOption{"4", 4.0},
+        TimeOption{"8", 8.0}
+    };
+    TimeOption* selected = &m_options[3];
+    double lastTime = 0;
 };
 
 }
