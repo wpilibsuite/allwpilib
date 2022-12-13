972eb1327e75a078a263c0ca84ce7606b584bfb5
Michael Leong <43558768+michaelleong25@users.noreply.github.com>
2022-12-12 21:52:17 -0800

    move to 86

--- wpilibcExamples/src/main/cpp/examples/ElevatorSimulation/cpp/Robot.cpp
+++ wpilibcExamples/src/main/cpp/examples/ElevatorSimulation/cpp/Robot.cpp
@@ -83,6 +83,7 @@ class Robot : public frc::TimedRobot {
     m_encoder.SetDistancePerPulse(kArmEncoderDistPerPulse);
 
     // Put Mechanism 2d to SmartDashboard
+    // To view the Elevator Sim in the simulator, select Network Tables -> SmartDashboard -> Elevator Sim
     frc::SmartDashboard::PutData("Elevator Sim", &m_mech2d);
   }
 
@@ -124,7 +125,7 @@ class Robot : public frc::TimedRobot {
     m_motor.Set(0.0);
   }
 };
-// To view the Elevator Sim in the simulator, select Network Tables -> SmartDashboard -> Elevator Sim
+
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
