92c40762a431700d8dd7808ff70a641e51933fd5
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-24 19:18:38 -0500

    fixes in apriltag

--- apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFieldLayout.java
+++ apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFieldLayout.java
@@ -114,7 +114,7 @@ public class AprilTagFieldLayout {
    * Sets the origin based on a predefined enumeration of coordinate frame origins. The origins are
    * calculated from the field dimensions.
    *
-   * <p>This transforms the Pose3ds returned by {@link #getTagPose(int)} to return the correct pose
+   * <p>This transforms the Pose3d objects returned by {@link #getTagPose(int)} to return the correct pose
    * relative to a predefined coordinate frame.
    *
    * @param origin The predefined origin
@@ -139,7 +139,7 @@ public class AprilTagFieldLayout {
   /**
    * Sets the origin for tag pose transformation.
    *
-   * <p>This transforms the Pose3ds returned by {@link #getTagPose(int)} to return the correct pose
+   * <p>This transforms the Pose3d objects returned by {@link #getTagPose(int)} to return the correct pose
    * relative to the provided origin.
    *
    * @param origin The new origin for tag transformations
