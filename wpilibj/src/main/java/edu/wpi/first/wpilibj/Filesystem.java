217a2839aa56852c97f8025ec31cf9301ab5871c
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-25 13:29:10 -0500

    Formatting fixes

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/Filesystem.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/Filesystem.java
@@ -41,9 +41,9 @@ public final class Filesystem {
   }
 
   /**
-   * Obtains the 'deploy' directory of the program, located at src/main/deploy, which is deployed
-   * by default. On the roboRIO, this is /home/lvuser/deploy. In simulation, it is where the
-   * simulation was launched from, in the subdirectory "src/main/deploy" (`pwd`/src/main/deploy).
+   * Obtains the 'deploy' directory of the program, located at src/main/deploy, which is deployed by
+   * default. On the roboRIO, this is /home/lvuser/deploy. In simulation, it is where the simulation
+   * was launched from, in the subdirectory "src/main/deploy" (`pwd`/src/main/deploy).
    *
    * @return The 'deploy' directory
    */
