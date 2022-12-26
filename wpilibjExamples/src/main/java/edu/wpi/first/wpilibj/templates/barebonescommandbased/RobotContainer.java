97e9882ee542eddfb2454eb542954bff7a9d534c
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-26 16:36:57 -0500

    added java template

---
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/templates/barebonescommandbased/RobotContainer.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibj.templates.barebonescommandbased;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
