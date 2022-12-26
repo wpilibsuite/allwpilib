042a994fe9175ca58f8d5a0f0ab248e581ac0f3a
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-26 18:10:02 -0500

    changed getAutoCommand to return nullptr

--- wpilibcExamples/src/main/cpp/templates/barebonescommandbased/cpp/Robot.cpp
+++ wpilibcExamples/src/main/cpp/templates/barebonescommandbased/cpp/Robot.cpp
@@ -21,7 +21,7 @@ void Robot::DisabledExit() {}
 void Robot::AutonomousInit() {
   m_autonomousCommand = m_container.GetAutonomousCommand();
 
-  if (m_autonomousCommand) {
+  if (m_autonomousCommand != nullptr) {
     m_autonomousCommand->Schedule();
   }
 }
@@ -31,8 +31,9 @@ void Robot::AutonomousPeriodic() {}
 void Robot::AutonomousExit() {}
 
 void Robot::TeleopInit() {
-  if (m_autonomousCommand) {
+  if (m_autonomousCommand != nullptr) {
     m_autonomousCommand->Cancel();
+    m_autonomousCommand = nullptr;
   }
 }
 
