babb0c1fcf060df44713c96d552fe33c6f337dd8
Matt <matthew.morley.ca@gmail.com>
2023-01-08 19:30:45 -0500

    [apriltag] Add 2023 field layout JSON (#4912)

--- apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
+++ apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
@@ -5,12 +5,13 @@
 package edu.wpi.first.apriltag;
 
 public enum AprilTagFields {
-  k2022RapidReact("2022-rapidreact.json");
+  k2022RapidReact("2022-rapidreact.json"),
+  k2023ChargedUp("2023-chargedup.json");
 
   public static final String kBaseResourceDir = "/edu/wpi/first/apriltag/";
 
   /** Alias to the current game. */
-  public static final AprilTagFields kDefaultField = k2022RapidReact;
+  public static final AprilTagFields kDefaultField = k2023ChargedUp;
 
   public final String m_resourceFile;
 
