41c70d3b061e363f36de7a1a771639aece945eb4
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-12 13:07:13 -0500

    Update Gyro3D.h

--- wpilibc/src/main/native/include/frc/interfaces/Gyro3D.h
+++ wpilibc/src/main/native/include/frc/interfaces/Gyro3D.h
@@ -13,61 +13,61 @@
 namespace frc {
 
 /** Gyro that supports representation in the 3D space. */
-class Gyro3D: virtual public Gyro {
-    public:
-        /**
-         * Return the Skew angle from the Gyroscope.
-         *
-         * <p>The angle is continuous, that is it will continue from 360 to 361
-         * degrees. This allows algorithms that wouldn't want to see a discontinuity
-         * in the gyro output as it sweeps past from 360 to 0 on the second time
-         * around.
-         *
-         * <p>The angle is expected to increase as the gyro turns clockwise. It needs
-         * to follow the NED axis convention.
-         *
-         * @return skew angle.
-         */
-        virtual double GetRoll() const = 0;
+class Gyro3D : virtual public Gyro {
+ public:
+  /**
+   * Return the Skew angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361
+   * degrees. This allows algorithms that wouldn't want to see a discontinuity
+   * in the gyro output as it sweeps past from 360 to 0 on the second time
+   * around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs
+   * to follow the NED axis convention.
+   *
+   * @return skew angle.
+   */
+  virtual double GetRoll() const = 0;
 
-        /**
-         * Return the Pitch angle from the Gyroscope.
-         *
-         * <p>The angle is continuous, that is it will continue from 360 to 361
-         * degrees. This allows algorithms that wouldn't want to see a discontinuity
-         * in the gyro output as it sweeps past from 360 to 0 on the second time
-         * around.
-         *
-         * <p>The angle is expected to increase as the gyro turns clockwise. It needs
-         * to follow the NED axis convention.
-         *
-         * @return pitch angle.
-         */
-        virtual double GetPitch() const = 0;
+  /**
+   * Return the Pitch angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361
+   * degrees. This allows algorithms that wouldn't want to see a discontinuity
+   * in the gyro output as it sweeps past from 360 to 0 on the second time
+   * around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs
+   * to follow the NED axis convention.
+   *
+   * @return pitch angle.
+   */
+  virtual double GetPitch() const = 0;
 
-        /**
-         * Return the Yaw angle from the Gyroscope.
-         *
-         * <p>The angle is continuous, that is it will continue from 360 to 361
-         * degrees. This allows algorithms that wouldn't want to see a discontinuity
-         * in the gyro output as it sweeps past from 360 to 0 on the second time
-         * around.
-         *
-         * <p>The angle is expected to increase as the gyro turns clockwise. It needs
-         * to follow the NED axis convention.
-         *
-         * @return yaw angle.
-         */
-        virtual double GetYaw() const = 0;
+  /**
+   * Return the Yaw angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361
+   * degrees. This allows algorithms that wouldn't want to see a discontinuity
+   * in the gyro output as it sweeps past from 360 to 0 on the second time
+   * around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs
+   * to follow the NED axis convention.
+   *
+   * @return yaw angle.
+   */
+  virtual double GetYaw() const = 0;
 
-        /**
-         * Return the heading of the robot as a Rotation3d.
-         *
-         * @return the current heading of the robot as a Rotation3d.
-         */
-        virtual Rotation3d GetRotation3d() const {
-            return units::degree_t{GetRoll(), GetPitch(), GetYaw()};
-        }
+  /**
+   * Return the heading of the robot as a Rotation3d.
+   *
+   * @return the current heading of the robot as a Rotation3d.
+   */
+  virtual Rotation3d GetRotation3d() const {
+    return units::degree_t{GetRoll(), GetPitch(), GetYaw()};
+  }
 };
 
 }  // namespace frc
