27b60f567a44029256ed27d12f2aa6b0dae10a4a
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-01-12 11:09:49 -0500

    Fix incorrect package names.

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/apriltagsdetection/Main.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/apriltagsdetection/Main.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj.examples.intermediatevision;
+package edu.wpi.first.wpilibj.examples.apriltagsdetection;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
