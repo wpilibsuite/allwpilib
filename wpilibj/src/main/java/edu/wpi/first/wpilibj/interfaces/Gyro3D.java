25e01e6467654c5df767ff632e6a6ad420d46320
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-12 12:39:30 -0500

    Create Gyro3D.java

---
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/interfaces/Gyro3D.java
@@ -0,0 +1,57 @@
+package edu.wpi.first.wpilibj.interfaces;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+
+/** Gyro that supports representation in the 3D space. */
+public interface Gyro3D extends Gyro {
+  /**
+   * Return the Skew angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361 degrees. This allows
+   * algorithms that wouldn't want to see a discontinuity in the gyro output as it sweeps past from
+   * 360 to 0 on the second time around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs to follow the NED
+   * axis convention.
+   *
+   * @return skew angle.
+   */
+  double getRoll();
+
+  /**
+   * Return the Pitch angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361 degrees. This allows
+   * algorithms that wouldn't want to see a discontinuity in the gyro output as it sweeps past from
+   * 360 to 0 on the second time around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs to follow the NED
+   * axis convention.
+   *
+   * @return pitch angle.
+   */
+  double getPitch();
+
+  /**
+   * Return the Yaw angle from the Gyroscope.
+   *
+   * <p>The angle is continuous, that is it will continue from 360 to 361 degrees. This allows
+   * algorithms that wouldn't want to see a discontinuity in the gyro output as it sweeps past from
+   * 360 to 0 on the second time around.
+   *
+   * <p>The angle is expected to increase as the gyro turns clockwise. It needs to follow the NED
+   * axis convention.
+   *
+   * @return yaw angle.
+   */
+  double getYaw();
+
+  /**
+   * Return the heading of the robot as a {@link edu.wpi.first.math.geometry.Rotation3d}.
+   *
+   * @return the current heading of the robot as a {@link edu.wpi.first.math.geometry.Rotation3d}.
+   */
+  default Rotation3d getRotation3d() {
+    return new Rotation3d(getRoll(), getPitch(), getYaw());
+  }
+}
