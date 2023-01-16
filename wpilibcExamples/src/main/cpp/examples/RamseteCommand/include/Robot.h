86b0ef5981b28df2c62076d51a1cb79a1f98ef07
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2023-01-16 11:50:41 +0200

    cleanup C++ RamseteCommand example

--- wpilibcExamples/src/main/cpp/examples/RamseteCommand/include/Robot.h
+++ wpilibcExamples/src/main/cpp/examples/RamseteCommand/include/Robot.h
@@ -4,8 +4,10 @@
 
 #pragma once
 
+#include <optional>
+
 #include <frc/TimedRobot.h>
-#include <frc2/command/Command.h>
+#include <frc2/command/CommandPtr.h>
 
 #include "RobotContainer.h"
 
@@ -24,7 +26,7 @@ class Robot : public frc::TimedRobot {
  private:
   // Have it null by default so that if testing teleop it
   // doesn't have undefined behavior and potentially crash.
-  frc2::Command* m_autonomousCommand = nullptr;
+  std::optional<frc2::CommandPtr> m_autonomousCommand;
 
   RobotContainer m_container;
 };
