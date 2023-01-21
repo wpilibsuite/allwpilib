b2b25bf09f0dc1b7bb2baea3c42463a100d7412b
Brady Schindler <82062997+Nyxiad@users.noreply.github.com>
2023-01-21 02:47:37 -0500

    [commands] Fix docs inconsistency for toggleOnFalse(Command) (NFC) (#4978)

--- wpilibNewCommands/src/main/native/include/frc2/command/button/Trigger.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/button/Trigger.h
@@ -194,8 +194,7 @@ class Trigger {
   Trigger ToggleOnFalse(Command* command);
 
   /**
-   * Toggles a command when the condition changes from `true` to the low
-   * state.
+   * Toggles a command when the condition changes from `true` to `false`.
    *
    * <p>Takes a raw pointer, and so is non-owning; users are responsible for the
    * lifespan of the command.
