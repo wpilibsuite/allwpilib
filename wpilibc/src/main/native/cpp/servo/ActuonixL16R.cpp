d0e6f4d2db202eec58606d206db2720fc9be8a65
Midnight Veil <midnightveil@fea.st>
2023-01-21 11:59:50 +1100

    Fix review nits

--- wpilibc/src/main/native/cpp/servo/ActuonixL16R.cpp
+++ wpilibc/src/main/native/cpp/servo/ActuonixL16R.cpp
@@ -2,10 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #include "frc/servo/ActuonixL16R.h"
 
 using namespace frc;
