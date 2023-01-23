13df5c3360adaf341ce93976119923790a171aee
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-23 12:42:42 -0500

    Update wpilibc/src/main/native/include/frc/Timer.h
    Co-authored-by: Peter Johnson <johnson.peter@gmail.com>

--- wpilibc/src/main/native/include/frc/Timer.h
+++ wpilibc/src/main/native/include/frc/Timer.h
@@ -78,7 +78,9 @@ class Timer {
 
   /**
    * Restart the timer by stopping the timer, if it is not already stopped,
-   * resetting the accumulated time, then starting the timer again.
+   * resetting the accumulated time, then starting the timer again. If you
+   * want an event to periodically reoccur at some time interval from the
+   * start time, consider using AdvanceIfElapsed() instead.
    */
   void Restart();
 
