13590f799720cba8de9c5b4742e0869b802e81a9
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2023-01-23 14:22:06 +0200

    enhance solenoids example

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/solenoid/Robot.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/solenoid/Robot.java
@@ -4,11 +4,22 @@
 
 package edu.wpi.first.wpilibj.examples.solenoid;
 
+// Using "import static an.enum.or.constants.inner.class.*;" helps reduce verbosity
+// this replaces "DoubleSolenoid.Value.kForward" with just kForward
+// further reading is available at https://www.geeksforgeeks.org/static-import-java/
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.CompressorConfigType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 /**
  * This is a sample program showing the use of the solenoid classes during operator control. Three
@@ -25,34 +36,126 @@ public class Robot extends TimedRobot {
   private final Joystick m_stick = new Joystick(0);
 
   // Solenoid corresponds to a single solenoid.
+  // In this case, it's connected to channel 0 of a PCM with the default CAN ID.
   private final Solenoid m_solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
 
   // DoubleSolenoid corresponds to a double solenoid.
+  // In this case, it's connected to channels 1 and 2 of a PH with the default CAN ID.
   private final DoubleSolenoid m_doubleSolenoid =
-      new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+      new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
 
-  private static final int kSolenoidButton = 1;
-  private static final int kDoubleSolenoidForward = 2;
-  private static final int kDoubleSolenoidReverse = 3;
+  // Compressor connected to a PH with a default CAN ID
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+
+  // External analog pressure sensor
+  // product-specific voltage->pressure conversion, see product manual
+  // in this case, 250(V/5)-25
+  // the scale parameter in the AnalogPotentiometer constructor is scaled from 1 instead of 5,
+  // so if r is the raw AnalogPotentiometer output, the pressure is 250r-25
+  static final double kScale = 250, kOffset = -25;
+  private final AnalogPotentiometer m_pressureTransducer =
+      new AnalogPotentiometer(/* the AnalogIn port*/ 2, kScale, kOffset);
+
+  // A chooser to select the compressor mode from the dashboard
+  private final SendableChooser<CompressorConfigType> m_compressorModeChooser =
+      new SendableChooser<>();
+
+  static final int kSolenoidButton = 1;
+  static final int kDoubleSolenoidForwardButton = 2;
+  static final int kDoubleSolenoidReverseButton = 3;
+  static final int kCompressorButton = 4;
+
+  @Override
+  public void robotInit() {
+    // Publish elements to shuffleboard.
+    ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
+    tab.add("Single Solenoid", m_solenoid);
+    tab.add("Souble Solenoid", m_doubleSolenoid);
+    tab.add("Compressor", m_compressor);
+
+    // Also publish some raw data
+    tab.addDouble(
+        "PH Pressure [PSI]",
+        () -> {
+          // Get the pressure (in PSI) from the analog sensor connected to the PH.
+          // This function is supported only on the PH!
+          // On a PCM, this function will return 0.
+          return m_compressor.getPressure();
+        });
+    tab.addDouble(
+        "External Pressure [PSI]",
+        () -> {
+          // Get the pressure (in PSI) from an analog pressure sensor connected to the RIO.
+          return m_pressureTransducer.get();
+        });
+    tab.addDouble(
+        "Compressor Current",
+        () -> {
+          // Get compressor current draw.
+          return m_compressor.getCurrent();
+        });
+
+    // Add the options and send the chooser to the dashboard.
+    m_compressorModeChooser.setDefaultOption("Digital", CompressorConfigType.Digital);
+    m_compressorModeChooser.addOption("Analog", CompressorConfigType.Analog);
+    m_compressorModeChooser.addOption("Hybrid", CompressorConfigType.Hybrid);
+    tab.add("Chooser", m_compressorModeChooser);
+  }
 
   @Override
   public void teleopPeriodic() {
     /*
      * The output of GetRawButton is true/false depending on whether
      * the button is pressed; Set takes a boolean for whether
-     * to use the default (false) channel or the other (true).
+     * to retract the solenoid (false) or extend it (true).
      */
     m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
 
     /*
-     * In order to set the double solenoid, if just one button
-     * is pressed, set the solenoid to correspond to that button.
-     * If both are pressed, set the solenoid will be set to Forwards.
+     * GetRawButtonPressed will only return true once per press.
+     * If a button is pressed, set the solenoid to the respective channel.
      */
-    if (m_stick.getRawButton(kDoubleSolenoidForward)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-    } else if (m_stick.getRawButton(kDoubleSolenoidReverse)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    if (m_stick.getRawButtonPressed(kDoubleSolenoidForwardButton)) {
+      m_doubleSolenoid.set(kForward);
+    } else if (m_stick.getRawButtonPressed(kDoubleSolenoidReverseButton)) {
+      m_doubleSolenoid.set(kReverse);
+    }
+
+    // On button press, toggle the compressor with the mode selected from the dashboard.
+    if (m_stick.getRawButtonPressed(kCompressorButton)) {
+      // Check whether the compressor is currently enabled.
+      boolean isCompressorEnabled = m_compressor.isEnabled();
+      if (isCompressorEnabled) {
+        // Disable closed-loop mode on the compressor.
+        m_compressor.disable();
+      } else {
+        switch (m_compressorModeChooser.getSelected()) {
+          case Digital:
+            // Enable closed-loop mode based on the digital pressure switch connected to the PCM/PH.
+            m_compressor.enableDigital();
+            break;
+          case Analog:
+            // Enable closed-loop mode based on the analog pressure sensor connected to the PH.
+            // The compressor will run while the pressure reported by the sensor is in the
+            // specified range ([70 PSI, 120 PSI] in this example).
+            // Analog mode exists only on the PH! On the PCM, this enables digital control.
+            m_compressor.enableAnalog(70, 110);
+            break;
+          case Hybrid:
+            // Enable closed-loop mode based on both the digital pressure switch and the analog
+            // pressure sensor connected to the PH.
+            // The compressor will run while the pressure reported by the analog sensor is in the
+            // specified range ([70 PSI, 120 PSI] in this example) AND the digital switch reports
+            // that the system is not full.
+            // Hybrid mode exists only on the PH! On the PCM, this enables digital control.
+            m_compressor.enableHybrid(70, 110);
+            break;
+          case Disabled:
+          default:
+            // This shouldn't happen!
+            break;
+        }
+      }
     }
   }
 }
