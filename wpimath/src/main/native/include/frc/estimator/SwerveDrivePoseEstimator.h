6c964a71e93e69beea897b598426ab1e7d944f2c
Tyler Veness <calcmogul@gmail.com>
2023-02-03 16:44:58 -0800

    Update wpimath/src/main/native/include/frc/estimator/SwerveDrivePoseEstimator.h

--- wpimath/src/main/native/include/frc/estimator/SwerveDrivePoseEstimator.h
+++ wpimath/src/main/native/include/frc/estimator/SwerveDrivePoseEstimator.h
@@ -171,7 +171,7 @@ class SwerveDrivePoseEstimator {
   void AddVisionMeasurement(const Pose2d& visionRobotPose,
                             units::second_t timestamp) {
     // Step 0: If this measurement is old enough to be outside the pose buffer's timespan, skip.
-    if (m_poseBuffer.GetInternalBuffer().front().first - BUFFER_DURATION > timestamp) {
+    if (m_poseBuffer.GetInternalBuffer().front().first - kBufferDuration > timestamp) {
       return;
     }
 
