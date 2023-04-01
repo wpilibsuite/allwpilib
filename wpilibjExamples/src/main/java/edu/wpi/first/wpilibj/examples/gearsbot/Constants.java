71ba330f46eb52cb98645e1503bce554360b4e25
= <918780@dpsk12.net>
2023-04-01 11:49:36 -0600

    create drive constants

---
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/gearsbot/Constants.java
@@ -0,0 +1,29 @@
+package edu.wpi.first.wpilibj.examples.gearsbot;
+
+
+public  final class Constants {
+  
+  public static final class DriveConstants{
+    public static final int leftMotorPort1 = 0;
+    public static final int leftMotorPort2 = 1;
+
+    public static final int rightMotorPort1 = 2;
+    public static final int rightMotorPort2 = 3;
+
+
+    public static final int[] leftEncoderPorts = {0,1};
+    public static final int[] rightEncoderPorts = {2,3};
+    public static final boolean leftEncoderReversed = false;
+    public static final boolean rightEncoderReversed = false;
+
+    public static final int rangeFinderPort = 6;
+    public static final int analogGyroPort = 1;
+
+    public static final int encoderCPR = 1024;
+    public static final double wheelDiameterInches = 6;
+    public static final double encoderDistancePerPulse = 
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (wheelDiameterInches * Math.PI) / (double) encoderCPR;
+
+  }
+}
