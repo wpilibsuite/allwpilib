60d840c7ec53e24a0cadb13cfaf2b022300c2788
bakedPotatoLord <918780@dpsk12.net>
2023-04-05 07:52:32 -0600

    import elevatorConstants directly

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/gearsbot/subsystems/Elevator.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/gearsbot/subsystems/Elevator.java
@@ -6,7 +6,7 @@ package edu.wpi.first.wpilibj.examples.gearsbot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.examples.gearsbot.Constants;
+import edu.wpi.first.wpilibj.examples.gearsbot.Constants.ElevatorConstants;
 import edu.wpi.first.wpilibj.examples.gearsbot.Robot;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,33 +17,34 @@ import edu.wpi.first.wpilibj2.command.PIDSubsystem;
  * values for simulation are different than in the real world do to minor differences.
  */
 public class Elevator extends PIDSubsystem {
-  private final Victor m_motor = new Victor(Constants.ElevatorConstants.kMotorPort);
+  private final Victor m_motor = new Victor(ElevatorConstants.kMotorPort);
   private final AnalogPotentiometer m_pot;
 
   /** Create a new elevator subsystem. */
   public Elevator() {
     super(
         new PIDController(
-            Constants.ElevatorConstants.kP_real,
-            Constants.ElevatorConstants.kI_real,
-            Constants.ElevatorConstants.kD));
+            ElevatorConstants.kP_real,
+            ElevatorConstants.kI_real,
+            ElevatorConstants.kD));
 
     if (Robot.isSimulation()) { // Check for simulation and update PID values
       getController()
           .setPID(
-              Constants.ElevatorConstants.kP_simulation,
-              Constants.ElevatorConstants.kI_simulation,
-              Constants.ElevatorConstants.kD);
+              ElevatorConstants.kP_simulation,
+              ElevatorConstants.kI_simulation,
+              ElevatorConstants.kD);
     }
-    getController().setTolerance(Constants.ElevatorConstants.kTolerance);
+    getController().setTolerance(ElevatorConstants.kTolerance);
 
     // Conversion value of potentiometer varies between the real world and
     // simulation
 
     if (Robot.isReal()) {
-      m_pot = new AnalogPotentiometer(2, -2.0 / 5);
+      m_pot = new AnalogPotentiometer(ElevatorConstants.kPotentiometerPort, -2.0 / 5);
     } else {
-      m_pot = new AnalogPotentiometer(2); // Defaults to meters
+      // Defaults to meters
+      m_pot = new AnalogPotentiometer(ElevatorConstants.kPotentiometerPort); 
     }
 
     // Let's name everything on the LiveWindow
