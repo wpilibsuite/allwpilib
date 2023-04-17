c4b5642bb004e384182c5609bc8588398cb8412e
Autumn <62668093+Autumn-Ou@users.noreply.github.com>
2023-04-16 20:12:54 -0400

    Addes MathUsageID
    note cpp is still just primitive drive class, but java is full function with usage IDs set up

--- wpimath/src/main/java/edu/wpi/first/math/MathUsageId.java
+++ wpimath/src/main/java/edu/wpi/first/math/MathUsageId.java
@@ -7,12 +7,14 @@ package edu.wpi.first.math;
 public enum MathUsageId {
   kKinematics_DifferentialDrive,
   kKinematics_MecanumDrive,
+  kKinematics_HDrive,
   kKinematics_SwerveDrive,
   kTrajectory_TrapezoidProfile,
   kFilter_Linear,
   kOdometry_DifferentialDrive,
   kOdometry_SwerveDrive,
   kOdometry_MecanumDrive,
+  kOdometry_HDrive,
   kController_PIDController2,
   kController_ProfiledPIDController,
 }
