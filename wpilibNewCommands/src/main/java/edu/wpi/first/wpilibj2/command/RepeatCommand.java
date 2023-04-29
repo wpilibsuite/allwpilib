40ca0946861e49e727b93bf6f2893362c9ca5574
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2023-04-28 20:57:33 -0700

    [commands] Fix RepeatCommand calling end() twice (#5261)

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/RepeatCommand.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/RepeatCommand.java
@@ -63,7 +63,12 @@ public class RepeatCommand extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
-    m_command.end(interrupted);
+    // Make sure we didn't already call end() (which would happen if the command finished in the
+    // last call to our execute())
+    if (!m_ended) {
+      m_command.end(interrupted);
+      m_ended = true;
+    }
   }
 
   @Override
