3d89c06682ac6f1514d9654c0f20cf0c7d8f811e
Joey R <joeyruan@zoho.com>
2023-04-30 23:37:34 -0700

    clean and move constants

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/elevatorprofiledpid/Robot.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/elevatorprofiledpid/Robot.java
@@ -15,6 +15,14 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Robot extends TimedRobot {
   private static double kDt = 0.02;
+  private static double kMaxVelocity = 1.75;
+  private static double kMaxAcceleration = 0.75;
+  private static double kP = 1.3;
+  private static double kI = 0.0;
+  private static double kD = 0.7;
+  private static double kS = 1.1;
+  private static double kG = 1.2;
+  private static double kV = 1.3;
 
   private final Joystick m_joystick = new Joystick(1);
   private final Encoder m_encoder = new Encoder(1, 2);
@@ -23,11 +31,11 @@ public class Robot extends TimedRobot {
   // Create a PID controller whose setpoint's change is subject to maximum
   // velocity and acceleration constraints.
   private final TrapezoidProfile.Constraints m_constraints =
-      new TrapezoidProfile.Constraints(1.75, 0.75);
+      new TrapezoidProfile.Constraints(kMaxVelocity, kMaxAcceleration);
   private final ProfiledPIDController m_controller =
-      new ProfiledPIDController(1.3, 0.0, 0.7, m_constraints, kDt);
+      new ProfiledPIDController(kP, kI, kD, m_constraints, kDt);
   private final ElevatorFeedforward m_feedforward = 
-      new ElevatorFeedforward(1, 1.1, 1.2);
+      new ElevatorFeedforward(kS, kG, kV);
 
   @Override
   public void robotInit() {
@@ -43,6 +51,8 @@ public class Robot extends TimedRobot {
     }
 
     // Run controller and update motor output
-    m_motor.setVoltage(m_controller.calculate(m_encoder.getDistance()) + m_feedforward.calculate(m_controller.getSetpoint().velocity));
+    m_motor.setVoltage(
+      m_controller.calculate(m_encoder.getDistance()) + 
+      m_feedforward.calculate(m_controller.getSetpoint().velocity));
   }
 }
