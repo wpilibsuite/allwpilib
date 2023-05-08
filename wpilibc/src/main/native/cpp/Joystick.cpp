b635e0131d1660cb7f4ac4c4522f0725fdaeafb4
Ryan Blue <ryanzblue@gmail.com>
2023-05-07 22:35:43 -0400

    use units in Joystick.GetDirection()

--- wpilibc/src/main/native/cpp/Joystick.cpp
+++ wpilibc/src/main/native/cpp/Joystick.cpp
@@ -8,6 +8,8 @@
 #include <numbers>
 
 #include <hal/FRCUsageReporting.h>
+#include <units/dimensionless.h>
+#include <units/math.h>
 
 #include "frc/event/BooleanEvent.h"
 
@@ -125,4 +127,9 @@ double Joystick::GetDirectionRadians() const {
 
 double Joystick::GetDirectionDegrees() const {
   return (180 / std::numbers::pi) * GetDirectionRadians();
+}
+
+units::radian_t Joystick::GetDirection() const {
+  return units::math::atan2(units::dimensionless::scalar_t{GetX()},
+                            units::dimensionless::scalar_t{-GetY()});
 }
