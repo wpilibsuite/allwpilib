3d83b9caa23bcf4497f109dadc9cff8d87c9a10b
Joseph Eng <s-engjo@bsd405.org>
2023-06-05 20:22:04 -0700

    Add std::copyable (and its dependencies)

--- wpiutil/src/main/native/include/wpi/concepts.h
+++ wpiutil/src/main/native/include/wpi/concepts.h
@@ -14,6 +14,9 @@
 
 namespace std {
 
+template <typename T, typename U>
+concept same_as = is_same_v<T, U> && is_same_v<U, T>;
+
 template <typename T, typename... Args>
 concept constructible_from =
     is_nothrow_destructible_v<T> && is_constructible_v<T, Args...>;
@@ -21,6 +24,24 @@ concept constructible_from =
 template <typename From, typename To>
 concept convertible_to = is_convertible_v<From, To> &&
                          requires { static_cast<To>(declval<From>()); };
+
+template <typename T, typename U>
+concept common_reference_with =
+    same_as<common_reference_t<T, U>, common_reference_t<U, T>> &&
+    convertible_to<T, common_reference_t<T, U>> &&
+    convertible_to<U, common_reference_t<T, U>>;
+
+template <typename LHS, typename RHS>
+concept assignable_from =
+    is_lvalue_reference_v<LHS> &&
+    common_reference_with<const remove_reference_t<LHS>&,
+                          const remove_reference_t<RHS>&> &&
+    requires(LHS lhs, RHS&& rhs) {
+      { lhs = std::forward<RHS>(rhs) } -> same_as<LHS>
+    };
+
+template <typename T>
+concept swappable = requires(T& a, T& b) { ranges::swap(a, b); };
 
 template <typename T>
 concept move_constructible = constructible_from<T, T> && convertible_to<T, T>;
@@ -31,6 +52,15 @@ concept copy_constructible =
     convertible_to<T&, T> && constructible_from<T, const T&> &&
     convertible_to<const T&, T> && constructible_from<T, const T> &&
     convertible_to<const T, T>;
+
+template <typename T>
+concept movable = is_object_v<T> && move_constructible<T> &&
+                  assignable_from<T&, T> && swappable<T>;
+
+template <typename T>
+concept copyable =
+    copy_constructible<T> && movable<T> && assignable_from<T&, T&> &&
+    assignable_from<T&, const T&> && asignable_from<T&, const T>;
 
 template <typename T>
 concept default_initializable =
