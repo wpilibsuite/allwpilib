fa4124d089f3c556a0b0cf2fa00178a8b5e7802c
Ryan Blue <ryanzblue@gmail.com>
2023-06-22 22:04:21 -0400

    Revert "fix Subsystem C++ class docs"

--- wpilibNewCommands/src/main/native/include/frc2/command/Subsystem.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/Subsystem.h
@@ -24,14 +24,18 @@ class CommandPtr;
  * subsystem should generally remain encapsulated and not be shared by other
  * parts of the robot.
  *
- * <p>Subsystems are automatically registered with the scheduler with the
+ * <p>Subsystems must be registered with the scheduler with the
  * CommandScheduler.RegisterSubsystem() method in order for the
- * Periodic() method to be called.
+ * Periodic() method to be called.  It is recommended that this method be called
+ * from the constructor of users' Subsystem implementations.  The
+ * SubsystemBase class offers a simple base for user implementations
+ * that handles this.
  *
  * This class is provided by the NewCommands VendorDep
  *
  * @see Command
  * @see CommandScheduler
+ * @see SubsystemBase
  */
 class Subsystem {
  public:
