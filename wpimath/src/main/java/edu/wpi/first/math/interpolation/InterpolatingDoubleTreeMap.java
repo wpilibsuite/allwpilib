0c0462d4a8157f7ff7e0cbbdfb35464f100f3cb8
Ryan Blue <ryanzblue@gmail.com>
2023-06-24 19:49:57 -0400

    Update to specialize on Double

--- wpimath/src/main/java/edu/wpi/first/math/interpolation/InterpolatingDoubleTreeMap.java
+++ wpimath/src/main/java/edu/wpi/first/math/interpolation/InterpolatingDoubleTreeMap.java
@@ -8,35 +8,8 @@ package edu.wpi.first.math.interpolation;
  * Interpolating Tree Maps are used to get values at points that are not defined by making a guess
  * from points that are defined. This uses linear interpolation.
  */
-public class InterpolatingDoubleTreeMap<K extends Number, V extends Number> {
-  private final InterpolatingTreeMap<Double, Double> m_map =
-      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Interpolator.forDouble());
-
-  /**
-   * Inserts a key-value pair.
-   *
-   * @param key The key.
-   * @param value The value.
-   */
-  public void put(K key, V value) {
-    m_map.put(key.doubleValue(), value.doubleValue());
-  }
-
-  /**
-   * Returns the value associated with a given key.
-   *
-   * <p>If there's no matching key, the value returned will be a linear interpolation between the
-   * keys before and after the provided one.
-   *
-   * @param key The key.
-   * @return The value associated with the given key.
-   */
-  public Double get(K key) {
-    return m_map.get(key.doubleValue());
-  }
-
-  /** Clears the contents. */
-  public void clear() {
-    m_map.clear();
+public class InterpolatingDoubleTreeMap extends InterpolatingTreeMap<Double, Double> {
+  InterpolatingDoubleTreeMap() {
+    super(InverseInterpolator.forDouble(), Interpolator.forDouble());
   }
 }
