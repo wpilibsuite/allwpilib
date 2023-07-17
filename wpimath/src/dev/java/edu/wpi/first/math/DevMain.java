b2b5071c3f456e58450223bc4a5ed5f43f423bcb
Joseph Eng <s-engjo@bsd405.org>
2023-07-17 10:12:55 -0700

    Write toString() of calculation to DataLog

--- wpimath/src/dev/java/edu/wpi/first/math/DevMain.java
+++ wpimath/src/dev/java/edu/wpi/first/math/DevMain.java
@@ -7,29 +7,39 @@ package edu.wpi.first.math;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Twist3d;
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.StringLogEntry;
 
 public final class DevMain {
-  private static double timePose3dExpMicros(Pose3d start, Twist3d twist, int count) {
-    final long startTime = System.nanoTime();
+  private static double timePose3dExpMicros(
+      StringLogEntry discardEntry, Pose3d start, Twist3d twist, int count) {
+    long totalNanos = 0;
     for (int i = 0; i < count; i++) {
-      start.exp(twist);
+      final long startTime = System.nanoTime();
+      final var result = start.exp(twist);
+      final long endTime = System.nanoTime();
+      totalNanos += endTime - startTime;
+      discardEntry.append(result.toString());
     }
-    final long endTime = System.nanoTime();
-    double totalTimeMicros = (endTime - startTime) / 1000.0;
+    double totalTimeMicros = totalNanos / 1000.0;
     return totalTimeMicros / count;
   }
 
-  private static double timePose3dLogMicros(Pose3d start, Pose3d end, int count) {
-    final long startTime = System.nanoTime();
+  private static double timePose3dLogMicros(
+      StringLogEntry discardEntry, Pose3d start, Pose3d end, int count) {
+    long totalNanos = 0;
     for (int i = 0; i < count; i++) {
-      start.log(end);
+      final long startTime = System.nanoTime();
+      final var result = start.log(end);
+      final long endTime = System.nanoTime();
+      totalNanos += endTime - startTime;
+      discardEntry.append(result.toString());
     }
-    final long endTime = System.nanoTime();
-    double totalTimeMicros = (endTime - startTime) / 1000.0;
+    double totalTimeMicros = totalNanos / 1000.0;
     return totalTimeMicros / count;
   }
 
-  private static void timeSuitePose3dExp() {
+  private static void timeSuitePose3dExp(StringLogEntry discardEntry) {
     final var start = new Pose3d(1, 0, 0, new Rotation3d(0, -Math.PI / 2, 0));
     final var twist = new Twist3d(0, 0, 0, Math.PI, 0, 0);
     final int warmupCount = 100_000;
@@ -41,16 +51,20 @@ public final class DevMain {
         "  warm up ("
             + warmupCount
             + " iterations): average "
-            + timePose3dExpMicros(start, twist, warmupCount)
+            + timePose3dExpMicros(discardEntry, start, twist, warmupCount)
             + "µs");
     System.out.println("  iterations per run: " + normalCount);
     for (int i = 1; i <= 5; i++) {
       System.out.println(
-          "  run " + i + ": average " + timePose3dExpMicros(start, twist, normalCount) + "μs");
+          "  run "
+              + i
+              + ": average "
+              + timePose3dExpMicros(discardEntry, start, twist, normalCount)
+              + "μs");
     }
   }
 
-  private static void timeSuitePose3dLog() {
+  private static void timeSuitePose3dLog(StringLogEntry discardEntry) {
     final var start = new Pose3d(1, 0, 0, new Rotation3d(0, -Math.PI / 2, 0));
     final var end = new Pose3d(0, 1, 0, new Rotation3d(Math.PI / 2, 0, 0));
     final int warmupCount = 100_000;
@@ -62,19 +76,25 @@ public final class DevMain {
         "  warm up ("
             + warmupCount
             + " iterations): average "
-            + timePose3dLogMicros(start, end, warmupCount)
+            + timePose3dLogMicros(discardEntry, start, end, warmupCount)
             + "µs");
     System.out.println("  iterations per run: " + normalCount);
     for (int i = 1; i <= 5; i++) {
       System.out.println(
-          "  run " + i + ": average " + timePose3dLogMicros(start, end, normalCount) + "μs");
+          "  run "
+              + i
+              + ": average "
+              + timePose3dLogMicros(discardEntry, start, end, normalCount)
+              + "μs");
     }
   }
 
   /** Main entry point. */
   public static void main(String[] args) {
-    timeSuitePose3dExp();
-    timeSuitePose3dLog();
+    final var datalog = new DataLog();
+    final var discardEntry = new StringLogEntry(datalog, "discard");
+    timeSuitePose3dExp(discardEntry);
+    timeSuitePose3dLog(discardEntry);
   }
 
   private DevMain() {}
