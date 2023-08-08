94f3cb89665ccc854699541c3c632c4dcff7081a
Jason Li <jasonli.0616@outlook.com>
2023-08-08 00:04:28 -0400

    Create Constants.java

---
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/differentialdrivebot/Constants.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibj.examples.differentialdrivebot;
+
+public class Constants {
+  public static final class DriveConstants {
+
+  }
+
+  public static final class OIConstants {
+    
+  }
+}
