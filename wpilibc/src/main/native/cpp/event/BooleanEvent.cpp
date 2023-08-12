0acdd31f7f3114e9666d5dbdf70223771fcb127f
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-08-11 21:47:03 -0400

    Format

--- wpilibc/src/main/native/cpp/event/BooleanEvent.cpp
+++ wpilibc/src/main/native/cpp/event/BooleanEvent.cpp
@@ -10,8 +10,9 @@ BooleanEvent::BooleanEvent(EventLoop* loop, std::function<bool()> condition)
     : m_loop(loop),
       m_condition(std::make_shared<std::function<bool()>>(condition)) {
   m_state = std::make_shared<bool>((*m_condition)());
-  m_loop->Bind(
-      [condition = *m_condition, state = m_state] { *state = condition(); }); // NOLINT
+  m_loop->Bind([condition = *m_condition, state = m_state] {
+    *state = condition();
+  });  // NOLINT
 }
 
 BooleanEvent::operator std::function<bool()>() {
