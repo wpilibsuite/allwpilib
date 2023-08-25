9178123a50fb116fe41e701411a80a8ef7fd766d
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-08-24 23:22:26 -0400

    Improve tests

--- wpilibc/src/test/native/cpp/event/BooleanEventTest.cpp
+++ wpilibc/src/test/native/cpp/event/BooleanEventTest.cpp
@@ -31,6 +31,89 @@ TEST(BooleanEventTest, BinaryCompositions) {
   EXPECT_EQ(1, orCounter);
 }
 
+/**
+ * Tests that composed edge events only execute on edges (two
+ * rising edge events composed with and() should only execute when both signals
+ * are on the rising edge)
+ */
+TEST(BooleanEventTest, BinaryCompositionsWithEdgeDecorators) {
+  EventLoop loop;
+  bool boolean1 = false;
+  bool boolean2 = false;
+  bool boolean3 = false;
+  bool boolean4 = false;
+  int counter = 0;
+
+  auto event1 = BooleanEvent(&loop, [&] { return boolean1; }).Rising();
+  auto event2 = BooleanEvent(&loop, [&] { return boolean2; }).Rising();
+  auto event3 = BooleanEvent(&loop, [&] { return boolean3; }).Rising();
+  auto event4 = BooleanEvent(&loop, [&] { return boolean4; }).Rising();
+  (event1 && event2).IfHigh([&] { ++counter; });
+  (event3 || event4).IfHigh([&] { ++counter; });
+  EXPECT_EQ(0, counter);
+
+  boolean1 = true;
+  boolean2 = true;
+  boolean3 = true;
+  boolean4 = true;
+  loop.Poll();  // Both actions execute
+
+  EXPECT_EQ(2, counter);
+
+  loop.Poll();  // Nothing should happen since nothing is on rising edge
+
+  EXPECT_EQ(2, counter);
+
+  boolean1 = false;
+  boolean2 = false;
+  boolean3 = false;
+  boolean4 = false;
+  loop.Poll();  // Nothing should happen
+
+  EXPECT_EQ(2, counter);
+
+  boolean1 = true;
+  loop.Poll();  // Nothing should happen since only Bool 1 is on rising edge
+
+  EXPECT_EQ(2, counter);
+
+  boolean2 = true;
+  loop.Poll();  // Bool 2 is on rising edge, but Bool 1 isn't, nothing should
+                // happen
+
+  EXPECT_EQ(2, counter);
+
+  boolean1 = false;
+  boolean2 = false;
+  loop.Poll();  // Nothing should happen
+
+  EXPECT_EQ(2, counter);
+
+  boolean1 = true;
+  boolean2 = true;
+  loop.Poll();  // Bool 1 and 2 are on rising edge, increments counter once
+
+  EXPECT_EQ(3, counter);
+
+  boolean3 = true;
+  loop.Poll();  // Bool 3 is on rising edge, increments counter once
+
+  EXPECT_EQ(4, counter);
+
+  loop.Poll();  // Nothing should happen, Bool 3 isn't on rising edge
+
+  EXPECT_EQ(4, counter);
+
+  boolean4 = true;
+  loop.Poll();  // Bool 4 is on rising edge, increments counter once
+
+  EXPECT_EQ(5, counter);
+
+  loop.Poll();  // Nothing should happen, Bool 4 isn't on rising edge
+
+  EXPECT_EQ(5, counter);
+}
+
 TEST(BooleanEventTest, BinaryCompositionLoopSemantics) {
   EventLoop loop1;
   EventLoop loop2;
@@ -49,90 +132,104 @@ TEST(BooleanEventTest, BinaryCompositionLoopSemantics) {
   EXPECT_EQ(0, counter1);
   EXPECT_EQ(0, counter2);
 
-  loop1.Poll();
+  loop1
+      .Poll();  // 1st event executes, Bool 1 and 2 are true, increments counter
 
   EXPECT_EQ(1, counter1);
   EXPECT_EQ(0, counter2);
 
-  loop2.Poll();
+  loop2
+      .Poll();  // 2nd event executes, Bool 1 and 2 are true, increments counter
 
   EXPECT_EQ(1, counter1);
   EXPECT_EQ(1, counter2);
 
   boolean2 = false;
-  loop1.Poll();
+  loop1.Poll();  // 1st event executes, Bool 2 is still true because loop 2
+                 // hasn't updated it, increments counter
 
   EXPECT_EQ(2, counter1);
   EXPECT_EQ(1, counter2);
 
-  loop2.Poll();
+  loop2.Poll();  // 2nd event executes, Bool 2 is now false because loop 1
+                 // updated it, does nothing
 
   EXPECT_EQ(2, counter1);
   EXPECT_EQ(1, counter2);
 
-  loop1.Poll();
+  loop1.Poll();  // All bools are updated at this point, nothing should happen
 
   EXPECT_EQ(2, counter1);
   EXPECT_EQ(1, counter2);
 
   boolean2 = true;
-  loop2.Poll();
+  loop2.Poll();  // 2nd event executes, Bool 2 is still false because loop 1
+                 // hasn't updated it, does nothing
 
   EXPECT_EQ(2, counter1);
   EXPECT_EQ(1, counter2);
 
-  loop1.Poll();
+  loop1.Poll();  // 1st event executes, Bool 2 is true because loop 2 updated
+                 // it, increments counter
 
   EXPECT_EQ(3, counter1);
   EXPECT_EQ(1, counter2);
 
   boolean1 = false;
-  loop2.Poll();
+  loop2.Poll();  // 2nd event executes, Bool 1 is false because this loop
+                 // updated it, does nothing
 
   EXPECT_EQ(3, counter1);
   EXPECT_EQ(1, counter2);
 
-  loop1.Poll();
+  loop1.Poll();  // 1st event executes, Bool 1 is false because this loop
+                 // updated it, does nothing
 
   EXPECT_EQ(3, counter1);
   EXPECT_EQ(1, counter2);
 
-  loop2.Poll();
+  loop2.Poll();  // All bools are updated at this point, nothing should happen
 
   EXPECT_EQ(3, counter1);
   EXPECT_EQ(1, counter2);
 }
 
-/**
- * When a BooleanEvent is constructed, an action is bound to the event loop to
- * update an internal state variable. This state variable is checked during loop
- * polls to determine whether or not to execute an action. If a condition is
- * changed during the loop poll but before the state variable gets updated, the
- * changed condition is used for the state variable.
- */
-TEST(BooleanEventTest, EventConstructionOrdering) {
+/** Tests the order of actions bound to an event loop. */
+TEST(BooleanEventTest, PollOrdering) {
   EventLoop loop;
   bool boolean1 = true;
   bool boolean2 = true;
-  int counter1 = 0;
-  int counter2 = 0;
+  bool enableAssert = false;
+  int counter = 0;
 
-  (BooleanEvent(&loop,
-                [&] {
-                  boolean2 = false;
-                  return boolean1;
-                }) &&
+  (BooleanEvent(  // This event binds an action to the event loop first
+       &loop,
+       [&] {
+         if (enableAssert) {
+           ++counter;
+           EXPECT_EQ(1, counter % 3);
+         }
+         return boolean1;
+       }) &&  // The composed event binds an action to the event loop third
+              // This event binds an action to the event loop second
    BooleanEvent(&loop, [&] {
+     if (enableAssert) {
+       ++counter;
+       EXPECT_EQ(2, counter % 3);
+     }
      return boolean2;
-   })).IfHigh([&] { ++counter1; });
-
-  EXPECT_EQ(0, counter1);
-  EXPECT_EQ(0, counter2);
-
+     // This binds an action to the event loop fourth
+   })).IfHigh([&] {
+    if (enableAssert) {
+      ++counter;
+      EXPECT_EQ(0, counter % 3);
+    }
+  });
+  enableAssert = true;
   loop.Poll();
-
-  EXPECT_EQ(0, counter1);
-  EXPECT_EQ(0, counter2);
+  loop.Poll();
+  loop.Poll();
+  loop.Poll();
 }
 
 TEST(BooleanEventTest, EdgeDecorators) {
@@ -290,9 +387,7 @@ TEST(BooleanEventTest, MidLoopBooleanChange) {
 
 /**
  * Tests that all actions bound to composed events will still execute even if
- * the composed signal changes during the loop poll. Also tests that composed
- * edge events only execute on edges (two rising edge events composed with and()
- * should only execute when both signals are on the rising edge)
+ * the composed signal changes during the loop poll.
  */
 TEST(BooleanEventTest, MidLoopBooleanChangeWithComposedEvents) {
   EventLoop loop;
@@ -302,23 +397,20 @@ TEST(BooleanEventTest, MidLoopBooleanChangeWithComposedEvents) {
   bool boolean4 = false;
   int counter = 0;
 
-  auto event1 = BooleanEvent(&loop, [&] { return boolean1; }).Rising();
-  auto event2 = BooleanEvent(&loop, [&] { return boolean2; }).Rising();
-  auto event3 = BooleanEvent(&loop, [&] { return boolean3; }).Rising();
-  auto event4 = BooleanEvent(&loop, [&] { return boolean4; }).Rising();
+  auto event1 = BooleanEvent(&loop, [&] { return boolean1; });
+  auto event2 = BooleanEvent(&loop, [&] { return boolean2; });
+  auto event3 = BooleanEvent(&loop, [&] { return boolean3; });
+  auto event4 = BooleanEvent(&loop, [&] { return boolean4; });
   event1.IfHigh([&] {
-    // Executes only when Bool 1 is on rising edge
     boolean2 = false;
+    boolean3 = false;
     ++counter;
   });
   (event3 || event4).IfHigh([&] {
-    // Executes only when Bool 3 or 4 are on rising edge
     boolean1 = false;
     ++counter;
   });
   (event1 && event2).IfHigh([&] {
-    // Executes only when Bool 1 and 2 are on rising edge
-    boolean3 = false;
     boolean4 = false;
     ++counter;
   });
@@ -339,48 +431,27 @@ TEST(BooleanEventTest, MidLoopBooleanChangeWithComposedEvents) {
   EXPECT_EQ(3, counter);
 
   boolean1 = true;
-  loop.Poll();  // Bool 1 is on rising edge, increments counter once
-
-  EXPECT_EQ(4, counter);
-
-  loop.Poll();  // Nothing should happen, Bool 1 is true, but not on rising edge
-
-  EXPECT_EQ(4, counter);
-
   boolean2 = true;
-  loop.Poll();  // Nothing should happen, Bool 2 is on rising edge, but Bool 1
-                // isn't
+  loop.Poll();  // Bool 1 and 2 are true, increments counter twice, Bool 2 gets
+                // set to false
 
-  EXPECT_EQ(4, counter);
+  EXPECT_EQ(5, counter);
+
   boolean1 = false;
-  boolean2 = false;
   loop.Poll();  // Nothing should happen
 
-  EXPECT_EQ(4, counter);
+  EXPECT_EQ(5, counter);
 
   boolean1 = true;
-  boolean2 = true;
-  loop.Poll();  // Bool 1 and 2 are on rising edge, so counter is incremented
-                // twice, and Bool 2 is
-  // reset to false
-
-  EXPECT_EQ(6, counter);
-
   boolean3 = true;
-  loop.Poll();  // Bool 3 is on rising edge, increments counter once
+  loop.Poll();  // Bool 1 and 3 are true, increments counter twice, Bool 3 gets
+                // set to false
 
   EXPECT_EQ(7, counter);
 
-  loop.Poll();  // Nothing should happen, Bool 3 isn't on rising edge
-
-  EXPECT_EQ(7, counter);
-
+  boolean1 = false;
   boolean4 = true;
-  loop.Poll();  // Bool 4 is on rising edge, increments counter once
-
-  EXPECT_EQ(8, counter);
-
-  loop.Poll();  // Nothing should happen, Bool 4 isn't on rising edge
+  loop.Poll();  // Bool 4 is true, increments counter once
 
   EXPECT_EQ(8, counter);
 }
--- wpilibj/src/test/java/edu/wpi/first/wpilibj/event/BooleanEventTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/event/BooleanEventTest.java
@@ -30,6 +30,88 @@ class BooleanEventTest {
     assertEquals(1, orCounter.get());
   }
 
+  /**
+   * Tests that composed edge events only execute on edges (two rising edge events composed with
+   * and() should only execute when both signals are on the rising edge).
+   */
+  @Test
+  void testBinaryCompositionsWithEdgeDecorators() {
+    var loop = new EventLoop();
+    var bool1 = new AtomicBoolean(false);
+    var bool2 = new AtomicBoolean(false);
+    var bool3 = new AtomicBoolean(false);
+    var bool4 = new AtomicBoolean(false);
+    var counter = new AtomicInteger(0);
+
+    var event1 = new BooleanEvent(loop, bool1::get).rising();
+    var event2 = new BooleanEvent(loop, bool2::get).rising();
+    var event3 = new BooleanEvent(loop, bool3::get).rising();
+    var event4 = new BooleanEvent(loop, bool4::get).rising();
+    event1.and(event2).ifHigh(counter::incrementAndGet);
+    event3.or(event4).ifHigh(counter::incrementAndGet);
+    assertEquals(0, counter.get());
+
+    bool1.set(true);
+    bool2.set(true);
+    bool3.set(true);
+    bool4.set(true);
+    loop.poll(); // Both actions execute
+
+    assertEquals(2, counter.get());
+
+    loop.poll(); // Nothing should happen since nothing is on rising edge
+
+    assertEquals(2, counter.get());
+
+    bool1.set(false);
+    bool2.set(false);
+    bool3.set(false);
+    bool4.set(false);
+    loop.poll(); // Nothing should happen
+
+    assertEquals(2, counter.get());
+
+    bool1.set(true);
+    loop.poll(); // Nothing should happen since only Bool 1 is on rising edge
+
+    assertEquals(2, counter.get());
+
+    bool2.set(true);
+    loop.poll(); // Bool 2 is on rising edge, but Bool 1 isn't, nothing should happen
+
+    assertEquals(2, counter.get());
+
+    bool1.set(false);
+    bool2.set(false);
+    loop.poll(); // Nothing should happen
+
+    assertEquals(2, counter.get());
+
+    bool1.set(true);
+    bool2.set(true);
+    loop.poll(); // Bool 1 and 2 are on rising edge, increments counter once
+
+    assertEquals(3, counter.get());
+
+    bool3.set(true);
+    loop.poll(); // Bool 3 is on rising edge, increments counter once
+
+    assertEquals(4, counter.get());
+
+    loop.poll(); // Nothing should happen, Bool 3 isn't on rising edge
+
+    assertEquals(4, counter.get());
+
+    bool4.set(true);
+    loop.poll(); // Bool 4 is on rising edge, increments counter once
+
+    assertEquals(5, counter.get());
+
+    loop.poll(); // Nothing should happen, Bool 4 isn't on rising edge
+
+    assertEquals(5, counter.get());
+  }
+
   @Test
   void testBinaryCompositionLoopSemantics() {
     var loop1 = new EventLoop();
@@ -50,90 +132,107 @@ class BooleanEventTest {
     assertEquals(0, counter1.get());
     assertEquals(0, counter2.get());
 
-    loop1.poll();
+    loop1.poll(); // 1st event executes, Bool 1 and 2 are true, increments counter
 
     assertEquals(1, counter1.get());
     assertEquals(0, counter2.get());
 
-    loop2.poll();
+    loop2.poll(); // 2nd event executes, Bool 1 and 2 are true, increments counter
 
     assertEquals(1, counter1.get());
     assertEquals(1, counter2.get());
 
     bool2.set(false);
-    loop1.poll();
+    loop1.poll(); // 1st event executes, Bool 2 is still true because loop 2 hasn't updated it,
+    // increments counter
 
     assertEquals(2, counter1.get());
     assertEquals(1, counter2.get());
 
-    loop2.poll();
+    loop2.poll(); // 2nd event executes, Bool 2 is now false because loop 1 updated it, does nothing
 
     assertEquals(2, counter1.get());
     assertEquals(1, counter2.get());
 
-    loop1.poll();
+    loop1.poll(); // All bools are updated at this point, nothing should happen
 
     assertEquals(2, counter1.get());
     assertEquals(1, counter2.get());
 
     bool2.set(true);
-    loop2.poll();
+    loop2
+        .poll(); // 2nd event executes, Bool 2 is still false because loop 1 hasn't updated it, does
+    // nothing
 
     assertEquals(2, counter1.get());
     assertEquals(1, counter2.get());
 
-    loop1.poll();
+    loop1
+        .poll(); // 1st event executes, Bool 2 is true because loop 2 updated it, increments counter
 
     assertEquals(3, counter1.get());
     assertEquals(1, counter2.get());
 
     bool1.set(false);
-    loop2.poll();
+    loop2.poll(); // 2nd event executes, Bool 1 is false because this loop updated it, does nothing
 
     assertEquals(3, counter1.get());
     assertEquals(1, counter2.get());
 
-    loop1.poll();
+    loop1.poll(); // 1st event executes, Bool 1 is false because this loop updated it, does nothing
 
     assertEquals(3, counter1.get());
     assertEquals(1, counter2.get());
 
-    loop2.poll();
+    loop2.poll(); // All bools are updated at this point, nothing should happen
 
     assertEquals(3, counter1.get());
     assertEquals(1, counter2.get());
   }
 
-  /**
-   * When a BooleanEvent is constructed, an action is bound to the event loop to update an internal
-   * state variable. This state variable is checked during loop polls to determine whether or not to
-   * execute an action. If a condition is changed during the loop poll but before the state variable
-   * gets updated, the changed condition is used for the state variable.
-   */
+  /** Tests the order of actions bound to an event loop. */
   @Test
-  void testEventConstructionOrdering() {
+  void testPollOrdering() {
     var loop = new EventLoop();
     var bool1 = new AtomicBoolean(true);
     var bool2 = new AtomicBoolean(true);
-    var counter1 = new AtomicInteger(0);
-    var counter2 = new AtomicInteger(0);
-
+    var enableAssert = new AtomicBoolean(false);
+    var counter = new AtomicInteger(0);
+    // This event binds an action to the event loop first
     new BooleanEvent(
             loop,
             () -> {
-              bool2.set(false);
+              if (enableAssert.get()) {
+                counter.incrementAndGet();
+                assertEquals(1, counter.get() % 3);
+              }
               return bool1.get();
             })
-        .and(new BooleanEvent(loop, bool2::get))
-        .ifHigh(counter1::incrementAndGet);
-
-    assertEquals(0, counter1.get());
-    assertEquals(0, counter2.get());
-
+        // The composed event binds an action to the event loop third
+        .and(
+            // This event binds an action to the event loop second
+            new BooleanEvent(
+                loop,
+                () -> {
+                  if (enableAssert.get()) {
+                    counter.incrementAndGet();
+                    assertEquals(2, counter.get() % 3);
+                  }
+                  return bool2.get();
+                }))
+        // This binds an action to the event loop fourth
+        .ifHigh(
+            () -> {
+              if (enableAssert.get()) {
+                counter.incrementAndGet();
+                assertEquals(0, counter.get() % 3);
+              }
+            });
+    enableAssert.set(true);
     loop.poll();
-
-    assertEquals(0, counter1.get());
-    assertEquals(0, counter2.get());
+    loop.poll();
+    loop.poll();
+    loop.poll();
   }
 
   @Test
@@ -288,9 +387,7 @@ class BooleanEventTest {
 
   /**
    * Tests that all actions bound to composed events will still execute even if the composed signal
-   * changes during the loop poll. Also tests that composed edge events only execute on edges (two
-   * rising edge events composed with and() should only execute when both signals are on the rising
-   * edge)
+   * changes during the loop poll.
    */
   @Test
   void testMidLoopBooleanChangeWithComposedEvents() {
@@ -301,20 +398,19 @@ class BooleanEventTest {
     var bool4 = new AtomicBoolean(false);
     var counter = new AtomicInteger(0);
 
-    var event1 = new BooleanEvent(loop, bool1::get).rising();
-    var event2 = new BooleanEvent(loop, bool2::get).rising();
-    var event3 = new BooleanEvent(loop, bool3::get).rising();
-    var event4 = new BooleanEvent(loop, bool4::get).rising();
+    var event1 = new BooleanEvent(loop, bool1::get);
+    var event2 = new BooleanEvent(loop, bool2::get);
+    var event3 = new BooleanEvent(loop, bool3::get);
+    var event4 = new BooleanEvent(loop, bool4::get);
     event1.ifHigh(
         () -> {
-          // Executes only when Bool 1 is on rising edge
           bool2.set(false);
+          bool3.set(false);
           counter.incrementAndGet();
         });
     event3
         .or(event4)
         .ifHigh(
-            // Executes only when Bool 3 or 4 are on rising edge
             () -> {
               bool1.set(false);
               counter.incrementAndGet();
@@ -322,12 +418,11 @@ class BooleanEventTest {
     event1
         .and(event2)
         .ifHigh(
-            // Executes only when Bool 1 and 2 are on rising edge
             () -> {
-              bool3.set(false);
               bool4.set(false);
               counter.incrementAndGet();
             });
+
     assertEquals(0, counter.get());
 
     bool1.set(true);
@@ -344,47 +439,25 @@ class BooleanEventTest {
     assertEquals(3, counter.get());
 
     bool1.set(true);
-    loop.poll(); // Bool 1 is on rising edge, increments counter once
-
-    assertEquals(4, counter.get());
-
-    loop.poll(); // Nothing should happen, Bool 1 is true, but not on rising edge
-
-    assertEquals(4, counter.get());
-
     bool2.set(true);
-    loop.poll(); // Nothing should happen, Bool 2 is on rising edge, but Bool 1 isn't
+    loop.poll(); // Bool 1 and 2 are true, increments counter twice, Bool 2 gets set to false
 
-    assertEquals(4, counter.get());
+    assertEquals(5, counter.get());
 
     bool1.set(false);
-    bool2.set(false);
     loop.poll(); // Nothing should happen
 
-    assertEquals(4, counter.get());
+    assertEquals(5, counter.get());
 
     bool1.set(true);
-    bool2.set(true);
-    loop.poll(); // Bool 1 and 2 are on rising edge, so counter is incremented twice, and Bool 2 is
-    // reset to false
-
-    assertEquals(6, counter.get());
-
     bool3.set(true);
-    loop.poll(); // Bool 3 is on rising edge, increments counter once
+    loop.poll(); // Bool 1 and 3 are true, increments counter twice, Bool 3 gets set to false
 
     assertEquals(7, counter.get());
 
-    loop.poll(); // Nothing should happen, Bool 3 isn't on rising edge
-
-    assertEquals(7, counter.get());
-
+    bool1.set(false);
     bool4.set(true);
-    loop.poll(); // Bool 4 is on rising edge, increments counter once
-
-    assertEquals(8, counter.get());
-
-    loop.poll(); // Nothing should happen, Bool 4 isn't on rising edge
+    loop.poll(); // Bool 4 is true, increments counter once
 
     assertEquals(8, counter.get());
   }
