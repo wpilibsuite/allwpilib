c08f141934a7dd30f99326dfbc634ce8f9359350
Tyler Veness <calcmogul@gmail.com>
2023-09-17 16:09:49 -0700

    Mark format_to_n_c_str() as inline

--- wpiutil/src/main/native/thirdparty/llvm/include/wpi/StringExtras.h
+++ wpiutil/src/main/native/thirdparty/llvm/include/wpi/StringExtras.h
@@ -736,8 +736,8 @@ std::pair<std::string_view, std::string_view> UnescapeCString(
  * @param args The format string arguments.
  */
 template <class OutputIt, class... Args>
-void format_to_n_c_str(OutputIt out, std::iter_difference_t<OutputIt> n,
-                       fmt::format_string<Args...> fmt, Args&&... args) {
+inline void format_to_n_c_str(OutputIt out, std::iter_difference_t<OutputIt> n,
+                              fmt::format_string<Args...> fmt, Args&&... args) {
   const auto result =
       fmt::format_to_n(out, n - 1, fmt, std::forward<Args>(args)...);
   *result.out = '\0';
