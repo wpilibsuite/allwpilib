2b89c6cbb5e4247fa8f7c79609ba523ccef6c978
Colin <cfinn68916@gmail.com>
2023-09-18 17:31:50 -0500

    Switch to std::numbers::pi
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- wpimath/src/test/native/cpp/controller/ArmFeedforwardTest.cpp
+++ wpimath/src/test/native/cpp/controller/ArmFeedforwardTest.cpp
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include <cmath>
+#include <numbers>
 
 #include <gtest/gtest.h>
 
@@ -20,14 +21,14 @@ TEST(ArmFeedforwardTest, Calculate) {
   auto Kg = 1_V;
 
   frc::ArmFeedforward armFF{Ks, Kg, Kv, Ka};
-  EXPECT_NEAR(armFF.Calculate(M_PI * 1_rad / 3, 0_rad / 1_s).value(), 0.5,
+  EXPECT_NEAR(armFF.Calculate(std::numbers::pi * 1_rad / 3, 0_rad / 1_s).value(), 0.5,
               0.002);  // 0.5
-  EXPECT_NEAR(armFF.Calculate(M_PI * 1_rad / 3, 1_rad / 1_s).value(), 2.5,
+  EXPECT_NEAR(armFF.Calculate(std::numbers::pi * 1_rad / 3, 1_rad / 1_s).value(), 2.5,
               0.002);
   EXPECT_NEAR(
-      armFF.Calculate(M_PI * 1_rad / 3, 1_rad / 1_s, 2_rad / 1_s / 1_s).value(),
+      armFF.Calculate(std::numbers::pi * 1_rad / 3, 1_rad / 1_s, 2_rad / 1_s / 1_s).value(),
       6.5, 0.002);
-  EXPECT_NEAR(armFF.Calculate(M_PI * 1_rad / 3, -1_rad / 1_s, 2_rad / 1_s / 1_s)
+  EXPECT_NEAR(armFF.Calculate(std::numbers::pi * 1_rad / 3, -1_rad / 1_s, 2_rad / 1_s / 1_s)
                   .value(),
               2.5, 0.002);
 }
@@ -40,11 +41,11 @@ TEST(ArmFeedforwardTest, AchievableVelocity) {
 
   frc::ArmFeedforward armFF{Ks, Kg, Kv, Ka};
   EXPECT_NEAR(
-      armFF.MaxAchievableVelocity(12_V, M_PI * 1_rad / 3, 1_rad / 1_s / 1_s)
+      armFF.MaxAchievableVelocity(12_V, std::numbers::pi * 1_rad / 3, 1_rad / 1_s / 1_s)
           .value(),
       6, 0.002);
   EXPECT_NEAR(
-      armFF.MinAchievableVelocity(11.5_V, M_PI * 1_rad / 3, 1_rad / 1_s / 1_s)
+      armFF.MinAchievableVelocity(11.5_V, std::numbers::pi * 1_rad / 3, 1_rad / 1_s / 1_s)
           .value(),
       -9, 0.002);
 }
@@ -57,19 +58,19 @@ TEST(ArmFeedforwardTest, AchievableAcceleration) {
 
   frc::ArmFeedforward armFF{Ks, Kg, Kv, Ka};  //
   EXPECT_NEAR(
-      armFF.MaxAchievableAcceleration(12_V, M_PI * 1_rad / 3, 1_rad / 1_s)
+      armFF.MaxAchievableAcceleration(12_V, std::numbers::pi * 1_rad / 3, 1_rad / 1_s)
           .value(),
       4.75, 0.002);
   EXPECT_NEAR(
-      armFF.MaxAchievableAcceleration(12_V, M_PI * 1_rad / 3, -1_rad / 1_s)
+      armFF.MaxAchievableAcceleration(12_V, std::numbers::pi * 1_rad / 3, -1_rad / 1_s)
           .value(),
       6.75, 0.002);  //
   EXPECT_NEAR(
-      armFF.MinAchievableAcceleration(12_V, M_PI * 1_rad / 3, 1_rad / 1_s)
+      armFF.MinAchievableAcceleration(12_V, std::numbers::pi * 1_rad / 3, 1_rad / 1_s)
           .value(),
       -7.25, 0.002);  //
   EXPECT_NEAR(
-      armFF.MinAchievableAcceleration(12_V, M_PI * 1_rad / 3, -1_rad / 1_s)
+      armFF.MinAchievableAcceleration(12_V, std::numbers::pi * 1_rad / 3, -1_rad / 1_s)
           .value(),
       -5.25, 0.002);  //
 }
