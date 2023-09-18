5383589f991c0b79116bb8dd59c64f435eb66d95
Peter Johnson <johnson.peter@gmail.com>
2023-09-17 20:01:43 -0700

    [wpinet] uv::Request: Return shared_ptr from Release() (#5656)
    Use this in Stream to allow the finish() callback to reuse the request.

--- wpinet/src/main/native/include/wpinet/uv/Request.h
+++ wpinet/src/main/native/include/wpinet/uv/Request.h
@@ -9,6 +9,7 @@
 
 #include <functional>
 #include <memory>
+#include <utility>
 
 #include "wpinet/uv/Error.h"
 
@@ -92,8 +93,12 @@ class Request : public std::enable_shared_from_this<Request> {
    *
    * Derived classes can override this method for different memory management
    * approaches (e.g. pooled storage of requests).
+   *
+   * @return Previous shared pointer
    */
-  virtual void Release() noexcept { m_self.reset(); }
+  virtual std::shared_ptr<Request> Release() noexcept {
+    return std::move(m_self);
+  }
 
   /**
    * Error callback.  By default, this is set up to report errors to the handle
