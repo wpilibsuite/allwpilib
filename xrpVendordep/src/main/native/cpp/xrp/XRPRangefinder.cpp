2b58bbde0b94f7d41ae2e7e1370a1df0f2ad27c3
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2023-09-22 16:03:43 -0400

    [xrp] Add Reflectance sensor and rangefinder classes (#5673)
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

---
+++ xrpVendordep/src/main/native/cpp/xrp/XRPRangefinder.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "frc/xrp/XRPRangefinder.h"
+
+using namespace frc;
+
+units::meter_t XRPRangefinder::GetDistance() const {
+  return units::meter_t{m_rangefinder.GetVoltage() / 5.0 * 4.0};
+}
