98546063ec353aceeefa34ae20cabb4f89988485
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2023-10-02 17:03:00 +0300

    use string

--- wpilibcExamples/src/test/cpp/examples/RapidReactCommandBot/cpp/subsystems/StorageTest.cpp
+++ wpilibcExamples/src/test/cpp/examples/RapidReactCommandBot/cpp/subsystems/StorageTest.cpp
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <string>
 #include <frc/simulation/DriverStationSim.h>
 #include <frc2/command/CommandScheduler.h>
 #include <gtest/gtest.h>
@@ -48,5 +49,5 @@ TEST_F(StorageTest, IdleCommand) {
 
   EXPECT_NEAR(0.0, sim.GetMotor(), 1e-6);
   auto idleCommand = storage.GetCurrentCommand();
-  EXPECT_STREQ("Idle", idleCommand->GetName());
+  EXPECT_STREQ(std::string{"Idle"}, idleCommand->GetName());
 }
