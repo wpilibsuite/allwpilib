d12a0105d682eafb5cbf57d1af93956d188c2d03
Ryan Blue <ryanzblue@gmail.com>
2023-10-10 02:02:34 -0400

    Use parameterized test

--- wpilibNewCommands/src/test/native/cpp/frc2/command/DeferredCommandTest.cpp
+++ wpilibNewCommands/src/test/native/cpp/frc2/command/DeferredCommandTest.cpp
@@ -9,51 +9,48 @@
 
 using namespace frc2;
 
-class DeferredCommandTest : public CommandTestBase {
- public:
-  void DeferredFunctionsTest(bool testInterrupted) {
-    int initializeCount = 0;
-    int executeCount = 0;
-    int isFinishedCount = 0;
-    int endCount = 0;
-    bool finished = false;
+class DeferredFunctionsTest : public CommandTestBaseWithParam<bool> {};
 
-    DeferredCommand deferred{[&] {
-                               return FunctionalCommand{
-                                   [&] { initializeCount++; },
-                                   [&] { executeCount++; },
-                                   [&](bool interrupted) {
-                                     EXPECT_EQ(interrupted, testInterrupted);
-                                     endCount++;
-                                   },
-                                   [&] {
-                                     isFinishedCount++;
-                                     return finished;
-                                   }}
-                                   .ToPtr();
-                             },
-                             {}};
+TEST_P(DeferredFunctionsTest, DeferredFunctions) {
+  int initializeCount = 0;
+  int executeCount = 0;
+  int isFinishedCount = 0;
+  int endCount = 0;
+  bool finished = false;
 
-    deferred.Initialize();
-    EXPECT_EQ(1, initializeCount);
-    deferred.Execute();
-    EXPECT_EQ(1, executeCount);
-    EXPECT_FALSE(deferred.IsFinished());
-    EXPECT_EQ(1, isFinishedCount);
-    finished = true;
-    EXPECT_TRUE(deferred.IsFinished());
-    EXPECT_EQ(2, isFinishedCount);
-    deferred.End(testInterrupted);
-    EXPECT_EQ(1, endCount);
-  }
-};
+  DeferredCommand deferred{[&] {
+                             return FunctionalCommand{
+                                 [&] { initializeCount++; },
+                                 [&] { executeCount++; },
+                                 [&](bool interrupted) {
+                                   EXPECT_EQ(interrupted, GetParam());
+                                   endCount++;
+                                 },
+                                 [&] {
+                                   isFinishedCount++;
+                                   return finished;
+                                 }}
+                                 .ToPtr();
+                           },
+                           {}};
 
-TEST_F(DeferredCommandTest, DeferredFunctions) {
-  DeferredFunctionsTest(false);
-  DeferredFunctionsTest(true);
+  deferred.Initialize();
+  EXPECT_EQ(1, initializeCount);
+  deferred.Execute();
+  EXPECT_EQ(1, executeCount);
+  EXPECT_FALSE(deferred.IsFinished());
+  EXPECT_EQ(1, isFinishedCount);
+  finished = true;
+  EXPECT_TRUE(deferred.IsFinished());
+  EXPECT_EQ(2, isFinishedCount);
+  deferred.End(GetParam());
+  EXPECT_EQ(1, endCount);
 }
 
-TEST_F(DeferredCommandTest, DeferredSupplierOnlyCalledDuringInit) {
+INSTANTIATE_TEST_SUITE_P(DeferredCommandTests, DeferredFunctionsTest,
+                         testing::Values(true, false));
+
+TEST(DeferredCommandTest, DeferredSupplierOnlyCalledDuringInit) {
   int count = 0;
   DeferredCommand command{[&count] {
                             count++;
@@ -70,7 +67,7 @@ TEST_F(DeferredCommandTest, DeferredSupplierOnlyCalledDuringInit) {
   EXPECT_EQ(1, count);
 }
 
-TEST_F(DeferredCommandTest, DeferredRequirements) {
+TEST(DeferredCommandTest, DeferredRequirements) {
   TestSubsystem subsystem;
   DeferredCommand command{cmd::None, {&subsystem}};
 
