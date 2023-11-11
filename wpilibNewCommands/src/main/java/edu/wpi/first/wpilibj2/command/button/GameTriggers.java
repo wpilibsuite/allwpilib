20c31a6e0a486577e92ce8bec30b8d3ffe2850a4
DeltaDizzy <EJBraswell@gmail.com>
2023-11-10 19:26:48 -0500

    add test mode trigger

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/button/GameTriggers.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/button/GameTriggers.java
@@ -3,19 +3,23 @@ package edu.wpi.first.wpilibj2.command.button;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class GameTriggers {
-    // TODO: Add documentation
-    // Utility class
-    private GameTriggers() { }
+  // TODO: Add documentation
+  // Utility class
+  private GameTriggers() {}
 
-    public static Trigger autonomous() {
-        return new Trigger(DriverStation::isAutonomousEnabled);
-    }
+  public static Trigger autonomous() {
+    return new Trigger(DriverStation::isAutonomousEnabled);
+  }
 
-    public static Trigger teleop() {
-        return new Trigger(DriverStation::isTeleopEnabled);
-    }
+  public static Trigger teleop() {
+    return new Trigger(DriverStation::isTeleopEnabled);
+  }
 
-    public static Trigger disabled() {
-        return new Trigger(DriverStation::isDisabled);
-    }
+  public static Trigger disabled() {
+    return new Trigger(DriverStation::isDisabled);
+  }
+
+  public static Trigger test() {
+    return new Trigger(DriverStation::isTestEnabled);
+  }
 }
