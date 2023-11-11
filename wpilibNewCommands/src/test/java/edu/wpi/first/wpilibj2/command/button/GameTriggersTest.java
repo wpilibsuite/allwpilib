ae299642c48378f7060fa71a76bed1e994010db4
DeltaDizzy <ejbraswell@gmail.com>
2023-11-11 17:27:27 -0500

    Add java tests

--- wpilibNewCommands/src/test/java/edu/wpi/first/wpilibj2/command/button/GameTriggersTest.java
+++ wpilibNewCommands/src/test/java/edu/wpi/first/wpilibj2/command/button/GameTriggersTest.java
@@ -6,19 +6,53 @@ package edu.wpi.first.wpilibj2.command.button;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.CommandTestBase;
+import org.junit.jupiter.api.Test;
 
 class GameTriggersTest extends CommandTestBase {
-    
-    @Test
-    void autonomousTest() {
-        Trigger auto = GameTriggers.autonomous();
-        DriverStationSim.setAutonomous(true);
-        DriverStation.refreshData();
-        assertTrue(auto.getAsBoolean());
-    }
+  @Test
+  void autonomousTest() {
+    DriverStationSim.resetData();
+    DriverStationSim.setAutonomous(true);
+    DriverStationSim.setTest(false);
+    DriverStationSim.setEnabled(true);
+    DriverStation.refreshData();
+    Trigger auto = GameTriggers.autonomous();
+    assertTrue(auto.getAsBoolean());
+  }
+
+  @Test
+  void teleopTest() {
+    DriverStationSim.resetData();
+    DriverStationSim.setAutonomous(false);
+    DriverStationSim.setTest(false);
+    DriverStationSim.setEnabled(true);
+    DriverStation.refreshData();
+    Trigger teleop = GameTriggers.teleop();
+    assertTrue(teleop.getAsBoolean());
+  }
+
+  @Test
+  void testModeTest() {
+    DriverStationSim.resetData();
+    DriverStationSim.setAutonomous(false);
+    DriverStationSim.setTest(true);
+    DriverStationSim.setEnabled(true);
+    DriverStation.refreshData();
+    Trigger test = GameTriggers.test();
+    assertTrue(test.getAsBoolean());
+  }
+
+  @Test
+  void disabledTest() {
+    DriverStationSim.resetData();
+    DriverStationSim.setAutonomous(false);
+    DriverStationSim.setTest(false);
+    DriverStationSim.setEnabled(false);
+    DriverStation.refreshData();
+    Trigger disabled = GameTriggers.disabled();
+    assertTrue(disabled.getAsBoolean());
+  }
 }
