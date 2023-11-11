618f71828431502821700cfbd0a9b91de5ab3835
DeltaDizzy <EJBraswell@gmail.com>
2023-11-11 13:13:34 -0500

    start autonomous test

---
+++ wpilibNewCommands/src/test/java/edu/wpi/first/wpilibj2/command/button/GameTriggersTest.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibj2.command.button;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj2.command.CommandTestBase;
+
+class GameTriggersTest extends CommandTestBase {
+    
+    @Test
+    void autonomousTest() {
+        Trigger auto = GameTriggers.autonomous();
+        DriverStationSim.setAutonomous(true);
+        DriverStation.refreshData();
+        assertTrue(auto.getAsBoolean());
+    }
+}
