35fceb9af0c4b10c88fd4d44a753bd64b5edfc95
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-11-13 22:38:47 -0500

    Formatting fixes

--- apriltag/src/test/java/edu/wpi/first/apriltag/AprilTagFieldLayoutTest.java
+++ apriltag/src/test/java/edu/wpi/first/apriltag/AprilTagFieldLayoutTest.java
@@ -1,14 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package edu.wpi.first.apriltag;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class AprilTagFieldLayoutTest {
   @Test
   void protobufTest() {
-    var layout = new AprilTagFieldLayout(List.of(), 12.3, 4.2);
+    var layout =
+        new AprilTagFieldLayout(
+            List.of(
+                new AprilTag(0, new Pose3d(0, 1, 1, new Rotation3d())),
+                new AprilTag(1, new Pose3d(2, 3, 1, new Rotation3d()))),
+            12.3,
+            4.2);
     var packedLayout = AprilTagFieldLayout.proto.createMessage();
     AprilTagFieldLayout.proto.pack(packedLayout, layout);
     var unpackedLayout = AprilTagFieldLayout.proto.unpack(packedLayout);
