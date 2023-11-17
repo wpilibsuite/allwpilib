bf9e02fe738ce2c575ceae9933480b7ab164f335
PJ Reiniger <pj.reiniger@gmail.com>
2023-11-17 16:46:59 -0500

    lint

--- wpimath/src/main/java/edu/wpi/first/math/system/plant/struct/DCMotorStruct.java
+++ wpimath/src/main/java/edu/wpi/first/math/system/plant/struct/DCMotorStruct.java
@@ -26,7 +26,8 @@ public class DCMotorStruct implements Struct<DCMotor> {
 
   @Override
   public String getSchema() {
-    return "double nominal_voltage;double stall_torque;double stall_current;double free_current;double free_speed";
+    return "double nominal_voltage;double stall_torque;double stall_current;"
+        + "double free_current;double free_speed";
   }
 
   @Override
