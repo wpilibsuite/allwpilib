36a132653bdd68d23607f28adc4dc38711a333ec
PJ Reiniger <pj.reiniger@gmail.com>
2023-11-17 13:36:01 -0500

    Revert "Delete C++ tests due to #5920"
    This reverts commit 0155823bd7b7a525698153083065de397fc8a4c1.

---
+++ wpimath/src/test/native/cpp/geometry/proto/Rotation2dProtoTest.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <gtest/gtest.h>
+
+#include "frc/geometry/Rotation2d.h"
+#include "geometry2d.pb.h"
+
+using namespace frc;
+
+namespace {
+
+using ProtoType = wpi::Protobuf<frc::Rotation2d>;
+
+const Rotation2d kExpectedData = Rotation2d{1.91_rad};
+}  // namespace
+
+TEST(Rotation2dProtoTest, Roundtrip) {
+  wpi::proto::ProtobufRotation2d proto;
+  ProtoType::Pack(&proto, kExpectedData);
+
+  Rotation2d unpacked_data = ProtoType::Unpack(proto);
+  EXPECT_EQ(kExpectedData.Radians().value(), unpacked_data.Radians().value());
+}
