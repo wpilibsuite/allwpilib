wpilibsuite/allwpilib

commit ca81ced4097ebd6c2023e340ce914a482f96a573

[wpiutil] Move RawFrame to wpiutil; add generation of RawFrame for AprilTags (#5923)

cscore/src/main/native/include/cscore_c.h
@@ -7,6 +7,8 @@
 
 #include <stdint.h>
 
+#include <wpi/RawFrame.h>
+
 #ifdef __cplusplus
 #include <cstddef>
 #else
@@ -82,20 +84,6 @@ enum CS_LogLevel {
   CS_LOG_DEBUG2 = 8,
   CS_LOG_DEBUG3 = 7,
   CS_LOG_DEBUG4 = 6
-};
-
-/**
- * Pixel formats
- */
-enum CS_PixelFormat {
-  CS_PIXFMT_UNKNOWN = 0,
-  CS_PIXFMT_MJPEG,
-  CS_PIXFMT_YUYV,
-  CS_PIXFMT_RGB565,
-  CS_PIXFMT_BGR,
-  CS_PIXFMT_GRAY,
-  CS_PIXFMT_Y16,
-  CS_PIXFMT_UYVY
 };
 
 /**
@@ -302,11 +290,11 @@ void CS_GetSourceVideoMode(CS_Source source, CS_VideoMode* mode,
 CS_Bool CS_SetSourceVideoMode(CS_Source source, const CS_VideoMode* mode,
                               CS_Status* status);
 CS_Bool CS_SetSourceVideoModeDiscrete(CS_Source source,
-                                      enum CS_PixelFormat pixelFormat,
+                                      enum WPI_PixelFormat pixelFormat,
                                       int width, int height, int fps,
                                       CS_Status* status);
 CS_Bool CS_SetSourcePixelFormat(CS_Source source,
-                                enum CS_PixelFormat pixelFormat,
+                                enum WPI_PixelFormat pixelFormat,
                                 CS_Status* status);
 CS_Bool CS_SetSourceResolution(CS_Source source, int width, int height,
                                CS_Status* status);

apriltag/src/main/java/edu/wpi/first/apriltag/AprilTag.java
@@ -6,7 +6,9 @@ package edu.wpi.first.apriltag;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import edu.wpi.first.apriltag.jni.AprilTagJNI;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.util.RawFrame;
 import java.util.Objects;
 
 @SuppressWarnings("MemberName")
@@ -43,5 +45,29 @@ public class AprilTag {
   @Override
   public String toString() {
     return "AprilTag(ID: " + ID + ", pose: " + pose + ")";
+  }
+
+  /**
+   * Generates a RawFrame containing the apriltag with the id with family 16h5 passed in.
+   *
+   * @param id id
+   * @return A RawFrame containing the AprilTag image
+   */
+  public static RawFrame generate16h5AprilTagImage(int id) {
+    RawFrame generatedImage = new RawFrame();
+    AprilTagJNI.generate16h5AprilTagImage(id, generatedImage.getDataPtr());
+    return generatedImage;
+  }
+
+  /**
+   * Generates a RawFrame containing the apriltag with the id with family 36h11 passed in.
+   *
+   * @param id id
+   * @return A RawFrame containing the AprilTag image
+   */
+  public static RawFrame generate36h11AprilTagImage(int id) {
+    RawFrame generatedImage = new RawFrame();
+    AprilTagJNI.generate36h11AprilTagImage(id, generatedImage.getDataPtr());
+    return generatedImage;
   }
 }

apriltag/src/main/java/edu/wpi/first/apriltag/jni/AprilTagJNI.java
@@ -189,4 +189,8 @@ public class AprilTagJNI {
       double fy,
       double cx,
       double cy);
+
+  public static native void generate16h5AprilTagImage(int id, long nativeAddr);
+
+  public static native void generate36h11AprilTagImage(int id, long nativeAddr);
 }

apriltag/src/main/native/cpp/AprilTag.cpp
@@ -6,7 +6,55 @@
 
 #include <wpi/json.h>
 
+#ifdef _WIN32
+#pragma warning(disable : 4200)
+#elif defined(__clang__)
+#pragma clang diagnostic ignored "-Wc99-extensions"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
+#include "apriltag.h"
+#include "tag16h5.h"
+#include "tag36h11.h"
+
 using namespace frc;
+
+wpi::RawFrame AprilTag::Generate36h11AprilTagImage(int id) {
+  apriltag_family_t* tagFamily = tag36h11_create();
+  image_u8_t* image = apriltag_to_image(tagFamily, id);
+  wpi::RawFrame markerFrame{};
+  size_t totalDataSize = image->height * image->stride * sizeof(char);
+  markerFrame.data = static_cast<char*>(
+      std::calloc(image->height * image->stride, sizeof(char)));
+  std::memcpy(markerFrame.data, image->buf, totalDataSize);
+  markerFrame.dataLength = image->width;
+  markerFrame.height = image->height;
+  markerFrame.pixelFormat = WPI_PIXFMT_GRAY;
+  markerFrame.width = image->stride;
+  markerFrame.totalData = totalDataSize;
+  image_u8_destroy(image);
+  tag36h11_destroy(tagFamily);
+  return markerFrame;
+}
+
+wpi::RawFrame AprilTag::Generate16h5AprilTagImage(int id) {
+  apriltag_family_t* tagFamily = tag16h5_create();
+  image_u8_t* image = apriltag_to_image(tagFamily, id);
+  wpi::RawFrame markerFrame{};
+  size_t totalDataSize = image->height * image->stride * sizeof(char);
+  markerFrame.data = static_cast<char*>(
+      std::calloc(image->height * image->stride, sizeof(char)));
+  std::memcpy(markerFrame.data, image->buf, totalDataSize);
+  markerFrame.dataLength = image->width;
+  markerFrame.height = image->height;
+  markerFrame.pixelFormat = WPI_PIXFMT_GRAY;
+  markerFrame.width = image->stride;
+  markerFrame.totalData = totalDataSize;
+  image_u8_destroy(image);
+  tag16h5_destroy(tagFamily);
+  return markerFrame;
+}
 
 void frc::to_json(wpi::json& json, const AprilTag& apriltag) {
   json = wpi::json{{"ID", apriltag.ID}, {"pose", apriltag.pose}};

apriltag/src/main/native/cpp/jni/AprilTagJNI.cpp
@@ -8,6 +8,7 @@
 #include <wpi/jni_util.h>
 
 #include "edu_wpi_first_apriltag_jni_AprilTagJNI.h"
+#include "frc/apriltag/AprilTag.h"
 #include "frc/apriltag/AprilTagDetector.h"
 #include "frc/apriltag/AprilTagPoseEstimator.h"
 
@@ -585,6 +586,32 @@ Java_edu_wpi_first_apriltag_jni_AprilTagJNI_estimatePose
 
   AprilTagPoseEstimator estimator({units::meter_t{tagSize}, fx, fy, cx, cy});
   return MakeJObject(env, estimator.Estimate(harr, carr));
+}
+
+/*
+ * Class:     edu_wpi_first_apriltag_jni_AprilTagJNI
+ * Method:    generate16h5AprilTagImage
+ * Signature: (IJ)V
+ */
+JNIEXPORT void JNICALL
+Java_edu_wpi_first_apriltag_jni_AprilTagJNI_generate16h5AprilTagImage
+  (JNIEnv* env, jclass, jint id, jlong framePtr)
+{
+  wpi::RawFrame* javaRawFrame = (wpi::RawFrame*)framePtr;
+  *javaRawFrame = AprilTag::Generate16h5AprilTagImage(id);
+}
+
+/*
+ * Class:     edu_wpi_first_apriltag_jni_AprilTagJNI
+ * Method:    generate36h11AprilTagImage
+ * Signature: (IJ)V
+ */
+JNIEXPORT void JNICALL
+Java_edu_wpi_first_apriltag_jni_AprilTagJNI_generate36h11AprilTagImage
+  (JNIEnv* env, jclass, jint id, jlong framePtr)
+{
+  wpi::RawFrame* javaRawFrame = (wpi::RawFrame*)framePtr;
+  *javaRawFrame = AprilTag::Generate36h11AprilTagImage(id);
 }
 
 }  // extern "C"

apriltag/src/main/native/include/frc/apriltag/AprilTag.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <wpi/RawFrame.h>
 #include <wpi/SymbolExports.h>
 #include <wpi/json_fwd.h>
 
@@ -20,6 +21,9 @@ struct WPILIB_DLLEXPORT AprilTag {
    * Checks equality between this AprilTag and another object.
    */
   bool operator==(const AprilTag&) const = default;
+
+  static wpi::RawFrame Generate36h11AprilTagImage(int id);
+  static wpi::RawFrame Generate16h5AprilTagImage(int id);
 };
 
 WPILIB_DLLEXPORT

cscore/src/main/java/edu/wpi/first/cscore/CameraServerJNI.java
@@ -4,7 +4,7 @@
 
 package edu.wpi.first.cscore;
 
-import edu.wpi.first.cscore.raw.RawFrame;
+import edu.wpi.first.util.RawFrame;
 import edu.wpi.first.util.RuntimeLoader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -386,10 +386,6 @@ public class CameraServerJNI {
   public static native String getHostname();
 
   public static native String[] getNetworkInterfaces();
-
-  public static native long allocateRawFrame();
-
-  public static native void freeRawFrame(long frame);
 
   public static native void runMainRunLoop();
 

cscore/src/main/java/edu/wpi/first/cscore/raw/RawSink.java
@@ -6,6 +6,7 @@ package edu.wpi.first.cscore.raw;
 
 import edu.wpi.first.cscore.CameraServerJNI;
 import edu.wpi.first.cscore.ImageSink;
+import edu.wpi.first.util.RawFrame;
 
 /**
  * A sink for user code to accept video frames as raw bytes.

commit 4bb7c6960a092dd25a9d51d70ac40a55c82e4b1b

[cscore] Replace CS_PixelFormat with WPI_PixelFormat
This fixes a compilation error introduced by #5923.

cscore/src/main/native/cpp/CvSinkImpl.cpp
@@ -211,14 +211,14 @@ void SetSinkEnabled(CS_Sink sink, bool enabled, CS_Status* status) {
 
 extern "C" {
 
-CS_Sink CS_CreateCvSink(const char* name, enum CS_PixelFormat pixelFormat,
+CS_Sink CS_CreateCvSink(const char* name, enum WPI_PixelFormat pixelFormat,
                         CS_Status* status) {
   return cs::CreateCvSink(
       name, static_cast<VideoMode::PixelFormat>(pixelFormat), status);
 }
 
 CS_Sink CS_CreateCvSinkCallback(const char* name,
-                                enum CS_PixelFormat pixelFormat, void* data,
+                                enum WPI_PixelFormat pixelFormat, void* data,
                                 void (*processFrame)(void* data, uint64_t time),
                                 CS_Status* status) {
   return cs::CreateCvSinkCallback(

cscore/src/main/native/include/cscore_c.h
@@ -370,10 +370,10 @@ void CS_SetSourceEnumPropertyChoices(CS_Source source, CS_Property property,
  */
 CS_Sink CS_CreateMjpegServer(const char* name, const char* listenAddress,
                              int port, CS_Status* status);
-CS_Sink CS_CreateCvSink(const char* name, enum CS_PixelFormat pixelFormat,
+CS_Sink CS_CreateCvSink(const char* name, enum WPI_PixelFormat pixelFormat,
                         CS_Status* status);
 CS_Sink CS_CreateCvSinkCallback(const char* name,
-                                enum CS_PixelFormat pixelFormat, void* data,
+                                enum WPI_PixelFormat pixelFormat, void* data,
                                 void (*processFrame)(void* data, uint64_t time),
                                 CS_Status* status);
 /** @} */