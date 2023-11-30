58df1bb9aa071be979258668aeb994ce65e292c1
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-11-30 15:02:05 -0500

    Format

--- cmake/modules/CompileWarnings.cmake
+++ cmake/modules/CompileWarnings.cmake
@@ -1,8 +1,14 @@
 macro(wpilib_target_warnings target)
     if(NOT MSVC)
-        target_compile_options(${target} PRIVATE -Wall -pedantic -Wextra -Werror -Wno-unused-parameter ${WPILIB_TARGET_WARNINGS})
+        target_compile_options(
+            ${target}
+            PRIVATE -Wall -pedantic -Wextra -Werror -Wno-unused-parameter ${WPILIB_TARGET_WARNINGS}
+        )
     else()
-        target_compile_options(${target} PRIVATE /wd4146 /wd4244 /wd4251 /wd4267 /WX /D_CRT_SECURE_NO_WARNINGS ${WPILIB_TARGET_WARNINGS})
+        target_compile_options(
+            ${target}
+            PRIVATE /wd4146 /wd4244 /wd4251 /wd4267 /WX /D_CRT_SECURE_NO_WARNINGS ${WPILIB_TARGET_WARNINGS}
+        )
     endif()
 
     # Suppress C++-specific OpenCV warning; C compiler rejects it with an error
@@ -16,13 +22,11 @@ macro(wpilib_target_warnings target)
     # Suppress warning "enumeration types with a fixed underlying type are a
     # Clang extension"
     if(APPLE)
-      target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:C>:-Wno-fixed-enum-extension>)
+        target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:C>:-Wno-fixed-enum-extension>)
     endif()
 
     # Compress debug info with GCC
-    if ((${CMAKE_BUILD_TYPE} STREQUAL "Debug" OR
-         ${CMAKE_BUILD_TYPE} STREQUAL "RelWithDebInfo") AND
-        ${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-      target_compile_options(${target} PRIVATE -gz=zlib)
+    if((${CMAKE_BUILD_TYPE} STREQUAL "Debug" OR ${CMAKE_BUILD_TYPE} STREQUAL "RelWithDebInfo") AND ${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+        target_compile_options(${target} PRIVATE -gz=zlib)
     endif()
 endmacro()
