4fcf0b25a1eea4bc7707f9d3f60bd805da482c23
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-11-30 19:52:21 -0500

    [build] Apply a formatter for CMake files (#5973)

--- cmake/modules/GenResources.cmake
+++ cmake/modules/GenResources.cmake
@@ -1,26 +1,30 @@
 set(scripts_dir "${CMAKE_CURRENT_LIST_DIR}/../scripts")
-macro(generate_resources inputDir outputDir prefix namespace outputFiles)
-  file(GLOB inputFiles ${inputDir}/*)
-  set(${outputFiles})
-  foreach(input ${inputFiles})
-    get_filename_component(inputBase ${input} NAME)
-    if("${inputBase}" MATCHES "^\\.")
-      continue()
-    endif()
-    set(output "${outputDir}/${inputBase}.cpp")
-    list(APPEND ${outputFiles} "${output}")
+macro(
+    generate_resources
+    inputDir
+    outputDir
+    prefix
+    namespace
+    outputFiles
+)
+    file(GLOB inputFiles ${inputDir}/*)
+    set(${outputFiles})
+    foreach(input ${inputFiles})
+        get_filename_component(inputBase ${input} NAME)
+        if("${inputBase}" MATCHES "^\\.")
+            continue()
+        endif()
+        set(output "${outputDir}/${inputBase}.cpp")
+        list(APPEND ${outputFiles} "${output}")
 
-    add_custom_command(
-      OUTPUT ${output}
-      COMMAND ${CMAKE_COMMAND}
-        "-Dinput=${input}"
-        "-Doutput=${output}"
-        "-Dprefix=${prefix}"
-        "-Dnamespace=${namespace}"
-        -P "${scripts_dir}/GenResource.cmake"
-      MAIN_DEPENDENCY ${input}
-      DEPENDS ${scripts_dir}/GenResource.cmake
-      VERBATIM
-    )
-  endforeach()
+        add_custom_command(
+            OUTPUT ${output}
+            COMMAND
+                ${CMAKE_COMMAND} "-Dinput=${input}" "-Doutput=${output}" "-Dprefix=${prefix}"
+                "-Dnamespace=${namespace}" -P "${scripts_dir}/GenResource.cmake"
+            MAIN_DEPENDENCY ${input}
+            DEPENDS ${scripts_dir}/GenResource.cmake
+            VERBATIM
+        )
+    endforeach()
 endmacro()
