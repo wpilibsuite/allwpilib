0ba4d31e8e586e30528273b2f939c4124fdee574
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-12-02 18:18:23 -0500

    Revert "Generate protobuf files"
    This reverts commit 2066b1970257c80d944f79036640c86a4747487d.

--- wpimath/generate_quickbuf.py
+++ wpimath/generate_quickbuf.py
@@ -17,18 +17,6 @@ if __name__ == "__main__":
         subprocess.run(
             f"{sys.argv[1]} --plugin=protoc-gen-quickbuf={sys.argv[2]} --quickbuf_out=gen_descriptors=true:{os.path.abspath('./wpimath/src/generated/main/java')} -I{absolute_dir} {absolute_filename}"
         )
-        subprocess.run(
-            f"{sys.argv[1]} --cpp_out={os.path.abspath('./wpimath/src/generated/main/native/protobuf')} -I{absolute_dir} {absolute_filename}"
-        )
-    cpp_files = glob("wpimath/src/generated/main/native/protobuf/*.*")
-    for cpp_file in cpp_files:
-        with open(cpp_file) as file:
-            content = file.read()
-        with open(cpp_file, "tw") as file:
-            file.write(
-                "// Copyright (c) FIRST and other WPILib contributors.\n// Open Source Software; you can modify and/or share it under the terms of\n// the WPILib BSD license file in the root directory of this project.\n\n"
-                + content
-            )
     java_files = glob("wpimath/src/generated/main/java/edu/wpi/first/math/proto/*.java")
     for java_file in java_files:
         with open(java_file) as file:
