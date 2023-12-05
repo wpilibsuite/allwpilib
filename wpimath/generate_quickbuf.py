4b888c07802b74a5213f2b1f91c213c7eba97d95
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-12-05 14:12:41 -0500

    Remove unused code

--- wpimath/generate_quickbuf.py
+++ wpimath/generate_quickbuf.py
@@ -9,11 +9,6 @@ if __name__ == "__main__":
     for path in proto_files:
         absolute_filename = os.path.abspath(path)
         absolute_dir, filename = os.path.split(absolute_filename)
-        file, _ = os.path.splitext(filename)
-        path.split()
-        pattern = re.compile("[A-Za-z_]+|[0-9]+")
-        filename_parts: list[str] = pattern.findall(file)
-        filename_parts = [part.capitalize() for part in filename_parts]
         subprocess.run(
             f"{sys.argv[1]} --plugin=protoc-gen-quickbuf={sys.argv[2]} --quickbuf_out=gen_descriptors=true:{os.path.abspath('./wpimath/src/generated/main/java')} -I{absolute_dir} {absolute_filename}"
         )
@@ -23,6 +18,6 @@ if __name__ == "__main__":
             content = file.read()
         with open(java_file, "tw") as file:
             file.write(
-                "// Copyright (c) FIRST and other WPILib contributors.\n// Open Source Software; you can modify and/or share it under the terms of\n// the WPILib BSD license file in the root directory of this project.\n\n// CHECKSTYLE.OFF\n"
+                "// Copyright (c) FIRST and other WPILib contributors.\n// Open Source Software; you can modify and/or share it under the terms of\n// the WPILib BSD license file in the root directory of this project.\n"
                 + content
             )
