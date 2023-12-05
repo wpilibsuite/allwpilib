f422371001c7c4a2f3946459d7da5d1d7dbdb1d6
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-12-05 17:47:55 -0500

    Run script in pregenerate

--- wpimath/generate_quickbuf.py
+++ wpimath/generate_quickbuf.py
@@ -1,8 +1,12 @@
-from glob import glob
+#!/usr/bin/env python3
+
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
 import os.path
-import re
 import subprocess
 import sys
+from glob import glob
 
 if __name__ == "__main__":
     proto_files = glob("wpimath/src/main/proto/*.proto")
@@ -10,7 +14,13 @@ if __name__ == "__main__":
         absolute_filename = os.path.abspath(path)
         absolute_dir, filename = os.path.split(absolute_filename)
         subprocess.run(
-            f"{sys.argv[1]} --plugin=protoc-gen-quickbuf={sys.argv[2]} --quickbuf_out=gen_descriptors=true:{os.path.abspath('./wpimath/src/generated/main/java')} -I{absolute_dir} {absolute_filename}"
+            [
+                sys.argv[1],
+                f"--plugin=protoc-gen-quickbuf={sys.argv[2]}",
+                f"--quickbuf_out=gen_descriptors=true:{os.path.abspath('./wpimath/src/generated/main/java')}",
+                f"-I{absolute_dir}",
+                absolute_filename,
+            ]
         )
     java_files = glob("wpimath/src/generated/main/java/edu/wpi/first/math/proto/*.java")
     for java_file in java_files:
