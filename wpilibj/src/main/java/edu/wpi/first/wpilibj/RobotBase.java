64233532964eb36e0810f9718dabe8cb69146a11
Sam Carlberg <sam@slfc.dev>
2023-12-11 18:04:29 -0500

    JDK 21 (graal)

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/RobotBase.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/RobotBase.java
@@ -148,7 +148,7 @@ public abstract class RobotBase implements AutoCloseable {
    */
   protected RobotBase() {
     final NetworkTableInstance inst = NetworkTableInstance.getDefault();
-    m_threadId = Thread.currentThread().getId();
+    m_threadId = Thread.currentThread().threadId();
     setupCameraServerShared();
     setupMathShared();
     // subscribe to "" to force persistent values to propagate to local
