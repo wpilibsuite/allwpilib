5d165e14115654808caa9d823ba33a3d4efe7a25
Nathan Correa <correanathanh@gmail.com>
2023-12-22 14:06:23 -0500

    Feedforward throws for kv and ka less than zero.

--- wpimath/src/main/java/edu/wpi/first/math/controller/ArmFeedforward.java
+++ wpimath/src/main/java/edu/wpi/first/math/controller/ArmFeedforward.java
@@ -28,12 +28,20 @@ public class ArmFeedforward {
    * @param kg The gravity gain.
    * @param kv The velocity gain.
    * @param ka The acceleration gain.
+   * @throws IllegalArgumentException for kv &lt; zero.
+   * @throws IllegalArgumentException for ka &lt; zero. 
    */
   public ArmFeedforward(double ks, double kg, double kv, double ka) {
     this.ks = ks;
     this.kg = kg;
     this.kv = kv;
     this.ka = ka;
+    if (kv < 0.0) {
+      throw new IllegalArgumentException("kv must be a non-negative number, got " + kv + "!");
+    }
+    if (ka < 0.0) {
+      throw new IllegalArgumentException("ka must be a non-negative number, got " + kv + "!");
+    }
   }
 
   /**
