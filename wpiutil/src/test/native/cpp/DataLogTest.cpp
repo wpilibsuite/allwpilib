45e3f623c34dae2b16edf36078d14d78dc4b6c91
Peter Johnson <johnson.peter@gmail.com>
2023-12-23 12:33:43 -0800

    Formatting

--- wpiutil/src/test/native/cpp/DataLogTest.cpp
+++ wpiutil/src/test/native/cpp/DataLogTest.cpp
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "wpi/DataLog.h"
-
 #include <gtest/gtest.h>
 
-TEST(DataLog, SimpleInt) {
+#include "wpi/DataLog.h"
+
+TEST(DataLogTest, SimpleInt) {
   std::vector<uint8_t> data;
   {
     wpi::log::DataLog log{
