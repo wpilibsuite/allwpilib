07e9ead79ef8bd7682ea91f196bcac67a629ba9d
Peter Johnson <johnson.peter@gmail.com>
2023-12-26 18:17:21 -0800

    Don't check stride exact value in test

--- apriltag/src/test/java/edu/wpi/first/apriltag/AprilTagGenerationTest.java
+++ apriltag/src/test/java/edu/wpi/first/apriltag/AprilTagGenerationTest.java
@@ -16,19 +16,19 @@ class AprilTagGenerationTest {
     assertEquals(PixelFormat.kGray, frame.getPixelFormat());
     assertEquals(10, frame.getWidth());
     assertEquals(10, frame.getHeight());
-    assertEquals(96, frame.getStride()); // default stride in apriltag library
-    assertEquals(960, frame.getSize());
+    int stride = frame.getStride();
+    assertEquals(stride * 10, frame.getSize());
     // check the diagonal values
     var data = frame.getData();
-    assertEquals(-1, data.get(96 * 0 + 0)); // outer border is white
-    assertEquals(0, data.get(96 * 1 + 1)); // inner border is black
-    assertEquals(-1, data.get(96 * 2 + 2));
-    assertEquals(-1, data.get(96 * 3 + 3));
-    assertEquals(-1, data.get(96 * 4 + 4));
-    assertEquals(0, data.get(96 * 5 + 5));
-    assertEquals(0, data.get(96 * 6 + 6));
-    assertEquals(-1, data.get(96 * 7 + 7));
-    assertEquals(0, data.get(96 * 8 + 8)); // inner border
-    assertEquals(-1, data.get(96 * 9 + 9)); // outer border
+    assertEquals(-1, data.get(stride * 0 + 0)); // outer border is white
+    assertEquals(0, data.get(stride * 1 + 1)); // inner border is black
+    assertEquals(-1, data.get(stride * 2 + 2));
+    assertEquals(-1, data.get(stride * 3 + 3));
+    assertEquals(-1, data.get(stride * 4 + 4));
+    assertEquals(0, data.get(stride * 5 + 5));
+    assertEquals(0, data.get(stride * 6 + 6));
+    assertEquals(-1, data.get(stride * 7 + 7));
+    assertEquals(0, data.get(stride * 8 + 8)); // inner border
+    assertEquals(-1, data.get(stride * 9 + 9)); // outer border
   }
 }
