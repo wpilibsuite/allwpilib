f65351116919897475d1015cffe43bff07b90c90
Tyler Veness <calcmogul@gmail.com>
2023-12-26 21:20:09 -0800

    Mark setDeadline() as final

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/ParallelDeadlineGroup.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/ParallelDeadlineGroup.java
@@ -38,7 +38,6 @@ public class ParallelDeadlineGroup extends Command {
    * @param otherCommands the other commands to be executed
    * @throws IllegalArgumentException if the deadline command is also in the otherCommands argument
    */
-  @SuppressWarnings("this-escape")
   public ParallelDeadlineGroup(Command deadline, Command... otherCommands) {
     addCommands(otherCommands);
     setDeadline(deadline);
@@ -51,7 +50,7 @@ public class ParallelDeadlineGroup extends Command {
    * @param deadline the command that determines when the group ends
    * @throws IllegalArgumentException if the deadline command is already in the composition
    */
-  public void setDeadline(Command deadline) {
+  public final void setDeadline(Command deadline) {
     @SuppressWarnings("PMD.CompareObjectsWithEquals")
     boolean isAlreadyDeadline = deadline == m_deadline;
     if (isAlreadyDeadline) {
