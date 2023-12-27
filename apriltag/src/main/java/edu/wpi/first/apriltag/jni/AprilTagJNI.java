4f393c7db622c9c0c60dab8e9f92dbcbbeaec827
Peter Johnson <johnson.peter@gmail.com>
2023-12-26 17:34:40 -0800

    Fix typo

--- apriltag/src/main/java/edu/wpi/first/apriltag/jni/AprilTagJNI.java
+++ apriltag/src/main/java/edu/wpi/first/apriltag/jni/AprilTagJNI.java
@@ -191,7 +191,7 @@ public class AprilTagJNI {
       double cx,
       double cy);
 
-  public static native void generate16h5AprilTagImage(RawFrame frameObj, jlong frame, int id);
+  public static native void generate16h5AprilTagImage(RawFrame frameObj, long frame, int id);
 
-  public static native void generate36h11AprilTagImage(RawFrame frameObj, jlong frame, int id);
+  public static native void generate36h11AprilTagImage(RawFrame frameObj, long frame, int id);
 }
