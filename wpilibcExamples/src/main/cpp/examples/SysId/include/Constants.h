1c0a99b201b165229aa314b3418149a612446aa3
Drew Williams <williams.r.drew@gmail.com>
2023-12-31 19:15:48 -0500

    Change static consts to inline to fix ODR

--- wpilibcExamples/src/main/cpp/examples/SysId/include/Constants.h
+++ wpilibcExamples/src/main/cpp/examples/SysId/include/Constants.h
@@ -15,19 +15,19 @@
 
 namespace constants {
 namespace drive {
-static constexpr int kLeftMotor1Port = 0;
-static constexpr int kLeftMotor2Port = 1;
-static constexpr int kRightMotor1Port = 2;
-static constexpr int kRightMotor2Port = 3;
+inline constexpr int kLeftMotor1Port = 0;
+inline constexpr int kLeftMotor2Port = 1;
+inline constexpr int kRightMotor1Port = 2;
+inline constexpr int kRightMotor2Port = 3;
 
-static constexpr std::array<int, 2> kLeftEncoderPorts = {0, 1};
-static constexpr std::array<int, 2> kRightEncoderPorts = {2, 3};
-static constexpr bool kLeftEncoderReversed = false;
-static constexpr bool kRightEncoderReversed = true;
+inline constexpr std::array<int, 2> kLeftEncoderPorts = {0, 1};
+inline constexpr std::array<int, 2> kRightEncoderPorts = {2, 3};
+inline constexpr bool kLeftEncoderReversed = false;
+inline constexpr bool kRightEncoderReversed = true;
 
-static constexpr int kEncoderCpr = 1024;
-static constexpr units::meter_t kWheelDiameter = 6_in;
-static constexpr units::meter_t kEncoderDistancePerPulse =
+inline constexpr int kEncoderCpr = 1024;
+inline constexpr units::meter_t kWheelDiameter = 6_in;
+inline constexpr units::meter_t kEncoderDistancePerPulse =
     (kWheelDiameter * std::numbers::pi) / static_cast<double>(kEncoderCpr);
 }  // namespace drive
 
@@ -38,44 +38,44 @@ using kv_unit =
                          units::inverse<units::turns>>;
 using kv_unit_t = units::unit_t<kv_unit>;
 
-static constexpr std::array<int, 2> kEncoderPorts = {4, 5};
-static constexpr bool kLeftEncoderReversed = false;
-static constexpr int kEncoderCpr = 1024;
-static constexpr units::turn_t kEncoderDistancePerPulse =
+inline constexpr std::array<int, 2> kEncoderPorts = {4, 5};
+inline constexpr bool kLeftEncoderReversed = false;
+inline constexpr int kEncoderCpr = 1024;
+inline constexpr units::turn_t kEncoderDistancePerPulse =
     units::turn_t{1.0} / static_cast<double>(kEncoderCpr);
 
-static constexpr int kShooterMotorPort = 4;
-static constexpr int kFeederMotorPort = 5;
+inline constexpr int kShooterMotorPort = 4;
+inline constexpr int kFeederMotorPort = 5;
 
-static constexpr units::turns_per_second_t kShooterFreeSpeed = 5300_tps;
-static constexpr units::turns_per_second_t kShooterTargetSpeed = 4000_tps;
-static constexpr units::turns_per_second_t kShooterTolerance = 50_tps;
+inline constexpr units::turns_per_second_t kShooterFreeSpeed = 5300_tps;
+inline constexpr units::turns_per_second_t kShooterTargetSpeed = 4000_tps;
+inline constexpr units::turns_per_second_t kShooterTolerance = 50_tps;
 
-static constexpr double kP = 1.0;
+inline constexpr double kP = 1.0;
 
-static constexpr units::volt_t kS = 0.05_V;
-static constexpr kv_unit_t kV = (12_V) / kShooterFreeSpeed;
+inline constexpr units::volt_t kS = 0.05_V;
+inline constexpr kv_unit_t kV = (12_V) / kShooterFreeSpeed;
 
-static constexpr double kFeederSpeed = 0.5;
+inline constexpr double kFeederSpeed = 0.5;
 }  // namespace shooter
 
 namespace intake {
-static constexpr int kMotorPort = 6;
-static constexpr std::array<int, 2> kSolenoidPorts = {2, 3};
+inline constexpr int kMotorPort = 6;
+inline constexpr std::array<int, 2> kSolenoidPorts = {2, 3};
 }  // namespace intake
 
 namespace storage {
-static constexpr int kMotorPort = 7;
-static constexpr int kBallSensorPort = 6;
+inline constexpr int kMotorPort = 7;
+inline constexpr int kBallSensorPort = 6;
 }  // namespace storage
 
 namespace autonomous {
-static constexpr units::second_t kTimeout = 3_s;
-static constexpr units::meter_t kDriveDistance = 2_m;
-static constexpr double kDriveSpeed = 0.5;
+inline constexpr units::second_t kTimeout = 3_s;
+inline constexpr units::meter_t kDriveDistance = 2_m;
+inline constexpr double kDriveSpeed = 0.5;
 }  // namespace autonomous
 
 namespace oi {
-static constexpr int kDriverControllerPort = 0;
+inline constexpr int kDriverControllerPort = 0;
 }  // namespace oi
 }  // namespace constants
