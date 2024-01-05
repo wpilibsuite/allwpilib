bd530e23fe6b9bde019d7f1467e637f9244a5cc8
Theinatorinator <Logan.mamanakis@gmail.com>
2024-01-05 11:29:21 -0700

    fix formating

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/simulation/ADIS16448SimTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/simulation/ADIS16448SimTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class ADIS16448SimTest {
-
   @ParameterizedTest
   @MethodSource("provideEnumCombinations")
   void testYawAxis(ADIS16448_IMU.IMUAxis yawAxis, ADIS16448_IMU.CalibrationTime calibrationTime) {
