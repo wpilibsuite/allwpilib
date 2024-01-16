a2d45dbca406ee7bdc17d1da010c509006606dd2
Thad House <ThadHouse@users.noreply.github.com>
2024-01-15 23:34:18 -0800

    [wpiutil] DataLog: Add AddSchema functions to C API (#6232)

--- wpiutil/src/main/native/cpp/DataLog.cpp
+++ wpiutil/src/main/native/cpp/DataLog.cpp
@@ -1205,4 +1205,18 @@ void WPI_DataLog_AppendStringArray(struct WPI_DataLog* datalog, int entry,
                                                          timestamp);
 }
 
+void WPI_DataLog_AddSchemaString(struct WPI_DataLog* datalog, int entry,
+                                 const char* name, const char* type,
+                                 const char* schema, int64_t timestamp) {
+  reinterpret_cast<DataLog*>(datalog)->AddSchema(name, type, schema, timestamp);
+}
+
+void WPI_DataLog_AddSchema(struct WPI_DataLog* datalog, int entry,
+                           const char* name, const char* type,
+                           const uint8_t* schema, size_t schema_len,
+                           int64_t timestamp) {
+  reinterpret_cast<DataLog*>(datalog)->AddSchema(
+      name, type, std::span<const uint8_t>{schema, schema_len}, timestamp);
+}
+
 }  // extern "C"
