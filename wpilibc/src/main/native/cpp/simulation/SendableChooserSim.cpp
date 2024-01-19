102d39bcd8cf5a6643c5072cc2a9e416337db184
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-01-18 22:40:37 -0500

    Add SendableChooserSim

---
+++ wpilibc/src/main/native/cpp/simulation/SendableChooserSim.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "frc/simulation/SendableChooserSim.h"
+
+using namespace frc::sim;
+
+SendableChooserSim::SendableChooserSim(std::string_view path) {
+  if (RobotBase::IsSimulation()) {
+    m_publisher = nt::NetworkTableInstance::GetDefault()
+                      .GetStringTopic(fmt::format("{}{}", path, "selected"))
+                      .Publish();
+  }
+}
+SendableChooserSim::SendableChooserSim(nt::NetworkTableInstance inst,
+                                       std::string_view path) {
+  if (RobotBase::IsSimulation()) {
+    m_publisher =
+        inst.GetStringTopic(fmt::format("{}{}", path, "selected")).Publish();
+  }
+}
+
+void SendableChooserSim::SetSelected(std::string_view option) {
+  m_publisher.Set(option);
+}
