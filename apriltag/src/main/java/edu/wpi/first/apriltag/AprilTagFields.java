0ad6b3acb3ba18477654d6f570c99bb6b17bcbb9
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-02-17 21:12:59 -0800

    [apriltag] Add AprilTagFieldLayout.loadField() (#6377)

--- apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
+++ apriltag/src/main/java/edu/wpi/first/apriltag/AprilTagFields.java
@@ -4,7 +4,6 @@
 
 package edu.wpi.first.apriltag;
 
-import java.io.IOException;
 import java.io.UncheckedIOException;
 
 /** Loadable AprilTag field layouts. */
@@ -36,11 +35,6 @@ public enum AprilTagFields {
    * @throws UncheckedIOException If the layout does not exist
    */
   public AprilTagFieldLayout loadAprilTagLayoutField() {
-    try {
-      return AprilTagFieldLayout.loadFromResource(m_resourceFile);
-    } catch (IOException e) {
-      throw new UncheckedIOException(
-          "Could not load AprilTagFieldLayout from " + m_resourceFile, e);
-    }
+    return AprilTagFieldLayout.loadField(this);
   }
 }
