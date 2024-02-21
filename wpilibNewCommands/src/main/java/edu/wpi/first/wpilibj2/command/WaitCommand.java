56f310f9d9840c05c834619d4f431d22e9fa3b73
Isaac Turner <spacey_sooty@outlook.com>
2024-02-21 22:07:07 +0800

    [command based] add Measure<Time> overload of WaitCommand

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/WaitCommand.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/WaitCommand.java
@@ -4,6 +4,10 @@
 
 package edu.wpi.first.wpilibj2.command;
 
+import edu.wpi.first.units.Measure;
+import static edu.wpi.first.units.Units.Second;
+import edu.wpi.first.units.Time;
+import edu.wpi.first.units.Unit;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Timer;
@@ -28,6 +32,16 @@ public class WaitCommand extends Command {
   public WaitCommand(double seconds) {
     m_duration = seconds;
     SendableRegistry.setName(this, getName() + ": " + seconds + " seconds");
+  }
+
+  /**
+   * Creates a new WaitCommand. This command will do nothing, and end after the specified duration.
+   *
+   * @param time the time to wait
+   */
+  public WaitCommand(Measure<Time> time) {
+    m_duration = time.in(Second);
+    SendableRegistry.setName(this, getName() + ": " + m_duration + " seconds");
   }
 
   @Override
