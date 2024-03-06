a15a64d8c035024fb19a0a765a527761da902cc6
DeltaDizzy <ejbraswell@gmail.com>
2024-03-05 20:48:34 -0500

    add top comment

--- wpimath/src/main/java/edu/wpi/first/math/kinematics/struct/DifferentialDriveWheelPositionsStruct.java
+++ wpimath/src/main/java/edu/wpi/first/math/kinematics/struct/DifferentialDriveWheelPositionsStruct.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package edu.wpi.first.math.kinematics.struct;
 
 import java.nio.ByteBuffer;
@@ -5,7 +9,7 @@ import java.nio.ByteBuffer;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
 import edu.wpi.first.util.struct.Struct;
 
-public class DifferentialDriveWheelPositionsStruct implements Struct<DifferentialDriveWheelPositions> {    
+public class DifferentialDriveWheelPositionsStruct implements Struct<DifferentialDriveWheelPositions> {
     @Override
     public Class<DifferentialDriveWheelPositions> getTypeClass() {
         return DifferentialDriveWheelPositions.class;
