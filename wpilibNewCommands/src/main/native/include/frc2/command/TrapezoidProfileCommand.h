2f93de71a20d63548c6c0b8f021118323e4c1bcd
Drew Williams <williams.r.drew@gmail.com>
2024-04-22 12:18:53 -0400

    reverted formatting change to unneeded file

--- wpilibNewCommands/src/main/native/include/frc2/command/TrapezoidProfileCommand.h
+++ wpilibNewCommands/src/main/native/include/frc2/command/TrapezoidProfileCommand.h
@@ -67,8 +67,7 @@ class TrapezoidProfileCommand
    */
   [[deprecated(
       "The new constructor allows you to pass in a supplier for desired and "
-      "current state. This allows you to change goals at "
-      "runtime.")]]
+      "current state. This allows you to change goals at runtime.")]]
   TrapezoidProfileCommand(frc::TrapezoidProfile<Distance> profile,
                           std::function<void(State)> output,
                           Requirements requirements = {})
