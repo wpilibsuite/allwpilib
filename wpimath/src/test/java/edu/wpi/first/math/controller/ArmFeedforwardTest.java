e552906046282c43fe3ea982475352238d72c5df
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-04-27 14:03:44 -0400

    added Java version of test

--- wpimath/src/test/java/edu/wpi/first/math/controller/ArmFeedforwardTest.java
+++ wpimath/src/test/java/edu/wpi/first/math/controller/ArmFeedforwardTest.java
@@ -6,7 +6,16 @@ package edu.wpi.first.math.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.function.BiFunction;
+
 import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.NumericalIntegration;
 
 class ArmFeedforwardTest {
   private static final double ks = 0.5;
@@ -21,6 +30,24 @@ class ArmFeedforwardTest {
     assertEquals(2.5, m_armFF.calculate(Math.PI / 3, 1), 0.002);
     assertEquals(6.5, m_armFF.calculate(Math.PI / 3, 1, 2), 0.002);
     assertEquals(2.5, m_armFF.calculate(Math.PI / 3, -1, 2), 0.002);
+    final double currentAngleRadians = Math.PI / 3;
+    final double currentVelocityRadPerSec = 1.0;
+    final double nextVelocityRadPerSec = 1.05;
+    final double dtSeconds = 0.020;
+    final double u = m_armFF.calculate(currentAngleRadians, currentVelocityRadPerSec, nextVelocityRadPerSec, dtSeconds);
+    final Matrix<N2, N2> A = new Matrix<>(Nat.N2(), Nat.N2(), new double[] { 0.0, 1.0, 0.0, -kv / ka });
+    final Matrix<N2, N1> B = new Matrix<>(Nat.N2(), Nat.N1(), new double[] { 0.0, 1.0 / ka });
+    final BiFunction<Matrix<N2, N1>, Matrix<N1, N1>, Matrix<N2, N1>> f = (x_, u_) -> {
+      Matrix<N2, N1> c = MatBuilder.fill(Nat.N2(), Nat.N1(), 0.0,
+          Math.signum(x_.get(1, 0)) * (-ks / ka) - (kg / ka) * Math.cos(x_.get(0, 0)));
+      return A.times(x_).plus(B.times(u_)).plus(c);
+    };
+    final Matrix<N2, N1> actual_x_k1 = NumericalIntegration.rk4(
+        f,
+        MatBuilder.fill(Nat.N2(), Nat.N1(), currentAngleRadians, currentVelocityRadPerSec),
+        MatBuilder.fill(Nat.N1(), Nat.N1(), u),
+        dtSeconds);
+    assertEquals(nextVelocityRadPerSec, actual_x_k1.get(1, 0), 1e-12);
   }
 
   @Test
