a53f56c03705de9ae9ded3b454bcec76648cefc2
Tyler Veness <calcmogul@gmail.com>
2024-04-27 17:20:24 -0700

    Add comments to Java tests

--- wpimath/src/test/java/edu/wpi/first/math/controller/ArmFeedforwardTest.java
+++ wpimath/src/test/java/edu/wpi/first/math/controller/ArmFeedforwardTest.java
@@ -24,43 +24,53 @@ class ArmFeedforwardTest {
 
   @Test
   void testCalculate() {
-    assertEquals(0.5, m_armFF.calculate(Math.PI / 3, 0), 0.002);
-    assertEquals(2.5, m_armFF.calculate(Math.PI / 3, 1), 0.002);
-    assertEquals(6.5, m_armFF.calculate(Math.PI / 3, 1, 2), 0.002);
-    assertEquals(2.5, m_armFF.calculate(Math.PI / 3, -1, 2), 0.002);
+    // calculate(angle, angular velocity, dt)
+    {
+      assertEquals(0.5, m_armFF.calculate(Math.PI / 3, 0), 0.002);
+      assertEquals(2.5, m_armFF.calculate(Math.PI / 3, 1), 0.002);
+    }
 
-    final double currentAngleRadians = Math.PI / 3;
-    final double currentVelocityRadPerSec = 1.0;
-    final double nextVelocityRadPerSec = 1.05;
-    final double dtSeconds = 0.020;
+    // calculate(angle, angular velocity, angular acceleration, dt)
+    {
+      assertEquals(6.5, m_armFF.calculate(Math.PI / 3, 1, 2), 0.002);
+      assertEquals(2.5, m_armFF.calculate(Math.PI / 3, -1, 2), 0.002);
+    }
 
-    final double u =
-        m_armFF.calculate(
-            currentAngleRadians, currentVelocityRadPerSec, nextVelocityRadPerSec, dtSeconds);
+    // calculate(currentAngle, currentVelocity, nextAngle, dt)
+    {
+      final double currentAngleRadians = Math.PI / 3;
+      final double currentVelocityRadPerSec = 1.0;
+      final double nextVelocityRadPerSec = 1.05;
+      final double dtSeconds = 0.020;
 
-    final Matrix<N2, N2> A =
-        new Matrix<>(Nat.N2(), Nat.N2(), new double[] {0.0, 1.0, 0.0, -kv / ka});
-    final Matrix<N2, N1> B = new Matrix<>(Nat.N2(), Nat.N1(), new double[] {0.0, 1.0 / ka});
+      final double u =
+          m_armFF.calculate(
+              currentAngleRadians, currentVelocityRadPerSec, nextVelocityRadPerSec, dtSeconds);
 
-    final BiFunction<Matrix<N2, N1>, Matrix<N1, N1>, Matrix<N2, N1>> f =
-        (x_, u_) -> {
-          Matrix<N2, N1> c =
-              MatBuilder.fill(
-                  Nat.N2(),
-                  Nat.N1(),
-                  0.0,
-                  Math.signum(x_.get(1, 0)) * (-ks / ka) - (kg / ka) * Math.cos(x_.get(0, 0)));
-          return A.times(x_).plus(B.times(u_)).plus(c);
-        };
+      final Matrix<N2, N2> A =
+          new Matrix<>(Nat.N2(), Nat.N2(), new double[] {0.0, 1.0, 0.0, -kv / ka});
+      final Matrix<N2, N1> B = new Matrix<>(Nat.N2(), Nat.N1(), new double[] {0.0, 1.0 / ka});
 
-    final Matrix<N2, N1> actual_x_k1 =
-        NumericalIntegration.rk4(
-            f,
-            MatBuilder.fill(Nat.N2(), Nat.N1(), currentAngleRadians, currentVelocityRadPerSec),
-            MatBuilder.fill(Nat.N1(), Nat.N1(), u),
-            dtSeconds);
+      final BiFunction<Matrix<N2, N1>, Matrix<N1, N1>, Matrix<N2, N1>> f =
+          (x_, u_) -> {
+            Matrix<N2, N1> c =
+                MatBuilder.fill(
+                    Nat.N2(),
+                    Nat.N1(),
+                    0.0,
+                    Math.signum(x_.get(1, 0)) * (-ks / ka) - (kg / ka) * Math.cos(x_.get(0, 0)));
+            return A.times(x_).plus(B.times(u_)).plus(c);
+          };
 
-    assertEquals(nextVelocityRadPerSec, actual_x_k1.get(1, 0), 1e-12);
+      final Matrix<N2, N1> actual_x_k1 =
+          NumericalIntegration.rk4(
+              f,
+              MatBuilder.fill(Nat.N2(), Nat.N1(), currentAngleRadians, currentVelocityRadPerSec),
+              MatBuilder.fill(Nat.N1(), Nat.N1(), u),
+              dtSeconds);
+
+      assertEquals(nextVelocityRadPerSec, actual_x_k1.get(1, 0), 1e-12);
+    }
   }
 
   @Test
