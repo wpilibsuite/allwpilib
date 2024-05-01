177a3bd4197f93413eeeb24c0f06103a6bd84139
Tyler Veness <calcmogul@gmail.com>
2024-05-01 13:50:13 -0700

    Don't use negative angles in comments

--- wpimath/src/main/java/edu/wpi/first/math/geometry/Rotation2d.java
+++ wpimath/src/main/java/edu/wpi/first/math/geometry/Rotation2d.java
@@ -40,14 +40,14 @@ public class Rotation2d
   public static final Rotation2d kZero = new Rotation2d();
 
   /**
-   * A preallocated Rotation2d representing a counterclockwise rotation by −π/2 rad (−90°).
+   * A preallocated Rotation2d representing a clockwise rotation by π/2 rad (90°).
    *
    * <p>This exists to avoid allocations for common rotations.
    */
   public static final Rotation2d kCW_Pi_2 = new Rotation2d(-Math.PI / 2);
 
   /**
-   * A preallocated Rotation2d representing a counterclockwise rotation by −90° (−π/2 rad).
+   * A preallocated Rotation2d representing a clockwise rotation by 90° (π/2 rad).
    *
    * <p>This exists to avoid allocations for common rotations.
    */
