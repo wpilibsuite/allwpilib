e7fd726c6125cf4543fb8689f8aa769f604d224f
Tyler Veness <calcmogul@gmail.com>
2024-05-12 20:21:34 -0700

    [upstream_utils] Suppress deprecation warnings for Eigen's has_denorm
    std::has_denorm and std::has_denorm_loss were deprecated in C++23.
    This avoids deprecation warnings with Clang 18 set to C++23.

--- upstream_utils/update_eigen.py
+++ upstream_utils/update_eigen.py
@@ -109,6 +109,7 @@ def main():
     for f in [
         "0001-Disable-warnings.patch",
         "0002-Intellisense-fix.patch",
+        "0003-Suppress-C-23-deprecation-warnings-for-std-has_denor.patch",
     ]:
         git_am(os.path.join(wpilib_root, "upstream_utils/eigen_patches", f))
 
