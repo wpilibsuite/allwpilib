a7ef8a23b8708b634b977d85761ff384a2a8c22c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-05-15 10:25:37 -0400

    arguments fix for ElevatorExponentialSimulation example

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/elevatorexponentialsimulation/subsystems/Elevator.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/elevatorexponentialsimulation/subsystems/Elevator.java
@@ -59,7 +59,7 @@ public class Elevator implements AutoCloseable {
           Constants.kMaxElevatorHeightMeters,
           true,
           0,
-          0.005);
+          0.005, 0.0);
   private final EncoderSim m_encoderSim = new EncoderSim(m_encoder);
   private final PWMSim m_motorSim = new PWMSim(m_motor);
 
