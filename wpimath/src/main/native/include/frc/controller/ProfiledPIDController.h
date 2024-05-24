294c9946aee029a3c1a1021796a875b8b73bb130
Carl Hauser <chauser@users.noreply.github.com>
2024-05-24 11:49:00 -0700

    [wpimath] PIDController.setIntegratorRange documentation fix (#6489)

--- wpimath/src/main/native/include/frc/controller/ProfiledPIDController.h
+++ wpimath/src/main/native/include/frc/controller/ProfiledPIDController.h
@@ -257,13 +257,14 @@ class ProfiledPIDController
   void DisableContinuousInput() { m_controller.DisableContinuousInput(); }
 
   /**
-   * Sets the minimum and maximum values for the integrator.
+   * Sets the minimum and maximum contributions of the integral term.
    *
-   * When the cap is reached, the integrator value is added to the controller
-   * output rather than the integrator value times the integral gain.
+   * The internal integrator is clamped so that the integral term's contribution
+   * to the output stays between minimumIntegral and maximumIntegral. This
+   * prevents integral windup.
    *
-   * @param minimumIntegral The minimum value of the integrator.
-   * @param maximumIntegral The maximum value of the integrator.
+   * @param minimumIntegral The minimum contribution of the integral term.
+   * @param maximumIntegral The maximum contribution of the integral term.
    */
   void SetIntegratorRange(double minimumIntegral, double maximumIntegral) {
     m_controller.SetIntegratorRange(minimumIntegral, maximumIntegral);
