62f73c64330e4f90cd70bcd45ecf752ec1879f39
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-06-22 16:38:16 -0400

    Apply suggestions from code review
    Co-authored-by: Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>

--- wpilibc/src/main/native/cpp/simulation/SendableChooserSim.cpp
+++ wpilibc/src/main/native/cpp/simulation/SendableChooserSim.cpp
@@ -6,13 +6,9 @@
 
 using namespace frc::sim;
 
-SendableChooserSim::SendableChooserSim(std::string_view path) {
-  if constexpr (RobotBase::IsSimulation()) {
-    m_publisher = nt::NetworkTableInstance::GetDefault()
-                      .GetStringTopic(fmt::format("{}{}", path, "selected"))
-                      .Publish();
-  }
-}
+SendableChooserSim::SendableChooserSim(std::string_view path)
+    : SendableChooserSim(nt::NetworkTableInstance::GetDefault(), path) {}
+
 SendableChooserSim::SendableChooserSim(nt::NetworkTableInstance inst,
                                        std::string_view path) {
   if constexpr (RobotBase::IsSimulation()) {
