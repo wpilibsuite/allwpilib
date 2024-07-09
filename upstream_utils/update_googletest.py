651ddf77096136ce06aad73287df749a9421886c
PJ Reiniger <pj.reiniger@gmail.com>
2024-07-09 18:29:39 -0400

    [upstream utils] Add gtest

---
+++ upstream_utils/update_googletest.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import shutil
+
+from upstream_utils import (
+    get_repo_root,
+    clone_repo,
+    comment_out_invalid_includes,
+    walk_cwd_and_copy_if,
+    git_am,
+    walk_if,
+)
+
+EXCLUDED_FILES = [
+    "gtest_main.cc",
+    "gtest-all.cc",
+    "gmock_main.cc",
+    "gmock-all.cc",
+    "README.md",
+]
+
+
+def walk_and_remap_copy(third_party_root, include_prefix):
+    gmock_files = walk_if(
+        ".", lambda dp, f: include_prefix in dp and f not in EXCLUDED_FILES
+    )
+
+    for f in gmock_files:
+        dst_file = os.path.join(
+            third_party_root, "include", f[len(include_prefix) + 1 :]
+        )
+        dest_dir = os.path.dirname(dst_file)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        shutil.copyfile(f, dst_file)
+
+
+def main():
+    upstream_root = clone_repo(
+        "https://github.com/google/googletest.git",
+        "f8d7d77c06936315286eb55f8de22cd23c188571",
+        shallow=False,
+    )
+    wpilib_root = get_repo_root()
+    third_party_root = os.path.join(wpilib_root, "third_party/googletest")
+
+    # Delete old install
+    for d in [
+        "include",
+        "src",
+    ]:
+        shutil.rmtree(os.path.join(third_party_root, d), ignore_errors=True)
+
+    # Apply patches to upstream Git repo
+    os.chdir(upstream_root)
+
+    walk_cwd_and_copy_if(
+        lambda dp, f: "googlemock/src" in dp and f not in EXCLUDED_FILES,
+        os.path.join(third_party_root, "src"),
+    )
+
+    walk_cwd_and_copy_if(
+        lambda dp, f: "googletest/src" in dp and f not in EXCLUDED_FILES,
+        os.path.join(third_party_root, "src"),
+    )
+
+    walk_and_remap_copy(
+        third_party_root,
+        "./googlemock/include",
+    )
+
+    walk_and_remap_copy(
+        third_party_root,
+        "./googletest/include",
+    )
+
+
+if __name__ == "__main__":
+    main()
