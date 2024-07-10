fd64380a0954b656cd26db5884ac9f3646686702
PJ Reiniger <pj.reiniger@gmail.com>
2024-07-10 02:01:13 -0400

    Run generation

---
+++ thirdparty/imgui_suite/stb/cpp/stb_image.cpp
@@ -0,0 +1,8 @@
+#define STBI_WINDOWS_UTF8
+#define STB_IMAGE_IMPLEMENTATION
+
+
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
+#include "stb_image.h"
