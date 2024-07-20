0e41a6e4462091979f4b57f96e3cede2b853f57b
Jade Turner <spacey-sooty@proton.me>
2024-07-20 22:05:44 +0800

    [commands, documentation] Update templates to use CommandRobot

--- wpilibcExamples/src/main/cpp/templates/commandbased/cpp/Robot.cpp
+++ wpilibcExamples/src/main/cpp/templates/commandbased/cpp/Robot.cpp
@@ -4,74 +4,22 @@
 
 #include "Robot.h"
 
-#include <frc2/command/CommandScheduler.h>
+#include "commands/ExampleCommand.h"
 
-Robot::Robot() {}
+Robot::Robot() {
+  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+  frc2::Trigger([this] {
+    return m_subsystem.ExampleCondition();
+  }).OnTrue(ExampleCommand(&m_subsystem).ToPtr());
 
-/**
- * This function is called every 20 ms, no matter the mode. Use
- * this for items like diagnostics that you want to run during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {
-  frc2::CommandScheduler::GetInstance().Run();
+  // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
+  // pressed, cancelling on release.
+  m_driverController.B().WhileTrue(m_subsystem.ExampleMethodCommand());
+
+  m_autoChooser.SetDefaultOption(
+      "Example Auto",
+      m_exampleAuto.get());  // An example Command that will be run in auto.
 }
-
-/**
- * This function is called once each time the robot enters Disabled mode. You
- * can use it to reset any subsystem information you want to clear when the
- * robot is disabled.
- */
-void Robot::DisabledInit() {}
-
-void Robot::DisabledPeriodic() {}
-
-/**
- * This autonomous runs the autonomous command selected by your {@link
- * RobotContainer} class.
- */
-void Robot::AutonomousInit() {
-  m_autonomousCommand = m_container.GetAutonomousCommand();
-
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Schedule();
-  }
-}
-
-void Robot::AutonomousPeriodic() {}
-
-void Robot::TeleopInit() {
-  // This makes sure that the autonomous stops running when
-  // teleop starts running. If you want the autonomous to
-  // continue until interrupted by another command, remove
-  // this line or comment it out.
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Cancel();
-  }
-}
-
-/**
- * This function is called periodically during operator control.
- */
-void Robot::TeleopPeriodic() {}
-
-/**
- * This function is called periodically during test mode.
- */
-void Robot::TestPeriodic() {}
-
-/**
- * This function is called once when the robot is first started up.
- */
-void Robot::SimulationInit() {}
-
-/**
- * This function is called periodically whilst in simulation.
- */
-void Robot::SimulationPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
--- wpilibcExamples/src/main/cpp/templates/commandbasedskeleton/cpp/Robot.cpp
+++ wpilibcExamples/src/main/cpp/templates/commandbasedskeleton/cpp/Robot.cpp
@@ -4,49 +4,7 @@
 
 #include "Robot.h"
 
-#include <frc2/command/CommandScheduler.h>
-
 Robot::Robot() {}
-
-void Robot::RobotPeriodic() {
-  frc2::CommandScheduler::GetInstance().Run();
-}
-
-void Robot::DisabledInit() {}
-
-void Robot::DisabledPeriodic() {}
-
-void Robot::DisabledExit() {}
-
-void Robot::AutonomousInit() {
-  m_autonomousCommand = m_container.GetAutonomousCommand();
-
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Schedule();
-  }
-}
-
-void Robot::AutonomousPeriodic() {}
-
-void Robot::AutonomousExit() {}
-
-void Robot::TeleopInit() {
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Cancel();
-  }
-}
-
-void Robot::TeleopPeriodic() {}
-
-void Robot::TeleopExit() {}
-
-void Robot::TestInit() {
-  frc2::CommandScheduler::GetInstance().CancelAll();
-}
-
-void Robot::TestPeriodic() {}
-
-void Robot::TestExit() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
