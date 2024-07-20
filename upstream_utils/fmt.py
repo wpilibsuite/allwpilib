d827c84c5efce62afbd2b3f3708715ec3b7719f3
Tyler Veness <calcmogul@gmail.com>
2024-07-20 07:01:06 -0700

    [upstream_utils] Clean up package imports (#6857)

--- upstream_utils/fmt.py
+++ upstream_utils/fmt.py
@@ -4,11 +4,7 @@ import os
 import shutil
 
 from upstream_utils import (
-    get_repo_root,
-    clone_repo,
-    comment_out_invalid_includes,
     walk_cwd_and_copy_if,
-    git_am,
     Lib,
 )
 
@@ -24,25 +20,16 @@ def copy_upstream_src(wpilib_root):
         shutil.rmtree(os.path.join(wpiutil, d), ignore_errors=True)
 
     # Copy fmt source files into allwpilib
-    src_files = walk_cwd_and_copy_if(
+    walk_cwd_and_copy_if(
         lambda dp, f: dp.startswith("./src") and f.endswith(".cc") and f != "fmt.cc",
         os.path.join(wpiutil, "src/main/native/thirdparty/fmtlib"),
     )
 
     # Copy fmt header files into allwpilib
-    include_files = walk_cwd_and_copy_if(
+    walk_cwd_and_copy_if(
         lambda dp, f: dp.startswith("./include/fmt"),
         os.path.join(wpiutil, "src/main/native/thirdparty/fmtlib"),
     )
-
-    for f in src_files:
-        comment_out_invalid_includes(
-            f, [os.path.join(wpiutil, "src/main/native/thirdparty/fmtlib/include")]
-        )
-    for f in include_files:
-        comment_out_invalid_includes(
-            f, [os.path.join(wpiutil, "src/main/native/thirdparty/fmtlib/include")]
-        )
 
 
 def main():
