c27f621cf2ca90432e3657819e25e44871470939
Jade Turner <spacey-sooty@proton.me>
2024-07-21 14:01:59 +0800

    [commands, documentation] Update RapidReactCommandBot example

--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/rapidreactcommandbot/Robot.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/rapidreactcommandbot/Robot.java
@@ -7,92 +7,80 @@ package edu.wpi.first.wpilibj.examples.rapidreactcommandbot;
 import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.Constants.AutoConstants;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.Constants.OIConstants;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.Constants.ShooterConstants;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.subsystems.Drive;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.subsystems.Intake;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.subsystems.Pneumatics;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.subsystems.Storage;
+import edu.wpi.first.wpilibj2.command.CommandRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
+ * This class automatically runs the {@link CommandScheduler} for you. If you change the name of this class or the package after creating this project, you must also update the Main.java file in the project.
  */
 @Logged(name = "Rapid React Command Robot")
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public class Robot extends CommandRobot {
+  // The robot's subsystems
+  private final Drive m_drive = new Drive();
+  private final Intake m_intake = new Intake();
+  private final Storage m_storage = new Storage();
+  private final Shooter m_shooter = new Shooter();
+  private final Pneumatics m_pneumatics = new Pneumatics();
 
-  private final RapidReactCommandBot m_robot = new RapidReactCommandBot();
+  // The driver's controller
+  CommandXboxController m_driverController =
+      new CommandXboxController(OIConstants.kDriverControllerPort);
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   public Robot() {
-    // Configure default commands and condition bindings on robot startup
-    m_robot.configureBindings();
+    // Automatically run the storage motor whenever the ball storage is not full,
+    // and turn it off whenever it fills. Uses subsystem-hosted trigger to
+    // improve readability and make inter-subsystem communication easier.
+    m_storage.hasCargo.whileFalse(m_storage.runCommand());
+
+    // Automatically disable and retract the intake whenever the ball storage is
+    // full.
+    m_storage.hasCargo.onTrue(m_intake.retractCommand());
+
+    // Control the drive with split-stick arcade controls
+    m_drive.setDefaultCommand(
+        m_drive.arcadeDriveCommand(
+            () -> -m_driverController.getLeftY(), () -> -m_driverController.getRightX()));
+
+    // Deploy the intake with the X button
+    m_driverController.x().onTrue(m_intake.intakeCommand());
+    // Retract the intake with the Y button
+    m_driverController.y().onTrue(m_intake.retractCommand());
+
+    // Fire the shooter with the A button
+    m_driverController
+        .a()
+        .onTrue(
+            Commands.parallel(
+                    m_shooter.shootCommand(ShooterConstants.kShooterTargetRPS),
+                    m_storage.runCommand())
+                // Since we composed this inline we should give it a name
+                .withName("Shoot"));
+
+    // Toggle compressor with the Start button
+    m_driverController.start().toggleOnTrue(m_pneumatics.disableCompressorCommand());
 
     // Initialize data logging.
     DataLogManager.start();
     Epilogue.bind(this);
+
+    m_autoChooser.setDefaultOption(
+        "Simple Auto",
+        m_drive
+            .driveDistanceCommand(AutoConstants.kDriveDistanceMeters, AutoConstants.kDriveSpeed)
+            .withTimeout(AutoConstants.kTimeoutSeconds));
   }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robot.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
 }
--- wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/templates/commandbased/Robot.java
+++ wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/templates/commandbased/Robot.java
@@ -15,18 +15,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class automatically runs the {@link CommandScheduler} for you. If you change the name of this class or the package after creating this project, you must also update the Main.java file in the project.
-*/
+ */
 public class Robot extends CommandRobot {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(
-      OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /**
-   * This function is run when the robot is first started up and should be used
-   * for any
+   * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   public Robot() {
@@ -39,7 +38,8 @@ public class Robot extends CommandRobot {
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
-    m_autoChooser.setDefaultOption("Example Auto", Autos.exampleAuto(m_exampleSubsystem)); // An example command will be
-                                                                                           // run in autonomous
+    m_autoChooser.setDefaultOption(
+        "Example Auto", Autos.exampleAuto(m_exampleSubsystem)); // An example command will be
+    // run in autonomous
   }
 }
