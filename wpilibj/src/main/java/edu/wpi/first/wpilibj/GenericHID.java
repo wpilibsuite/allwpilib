319d6c0761ef8542cd1985775d7c97700b85cf67
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-28 03:44:32 -0400

    Add mutex

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/GenericHID.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/GenericHID.java
@@ -166,8 +166,10 @@ public class GenericHID {
    * @return an event instance representing the button's digital signal attached to the given loop.
    */
   public BooleanEvent button(int button, EventLoop loop) {
-    var cache = m_buttonCache.computeIfAbsent(loop, k -> new HashMap<>());
-    return cache.computeIfAbsent(button, k -> new BooleanEvent(loop, () -> getRawButton(k)));
+    synchronized (m_buttonCache) {
+      var cache = m_buttonCache.computeIfAbsent(loop, k -> new HashMap<>());
+      return cache.computeIfAbsent(button, k -> new BooleanEvent(loop, () -> getRawButton(k)));
+    }
   }
 
   /**
@@ -231,10 +233,12 @@ public class GenericHID {
    * @return a BooleanEvent instance based around this angle of a POV on the HID.
    */
   public BooleanEvent pov(int pov, int angle, EventLoop loop) {
-    var cache = m_povCache.computeIfAbsent(loop, k -> new HashMap<>());
-    // angle can be -1, so use 3600 instead of 360
-    return cache.computeIfAbsent(
-        pov * 3600 + angle, k -> new BooleanEvent(loop, () -> getPOV(pov) == angle));
+    synchronized (m_povCache) {
+      var cache = m_povCache.computeIfAbsent(loop, k -> new HashMap<>());
+      // angle can be -1, so use 3600 instead of 360
+      return cache.computeIfAbsent(
+          pov * 3600 + angle, k -> new BooleanEvent(loop, () -> getPOV(pov) == angle));
+    }
   }
 
   /**
@@ -346,9 +350,12 @@ public class GenericHID {
    * @return an event instance that is true when the axis value is less than the provided threshold.
    */
   public BooleanEvent axisLessThan(int axis, double threshold, EventLoop loop) {
-    var cache = m_axisLessThanCache.computeIfAbsent(loop, k -> new HashMap<>());
-    return cache.computeIfAbsent(
-        Pair.of(axis, threshold), k -> new BooleanEvent(loop, () -> getRawAxis(axis) < threshold));
+    synchronized (m_axisLessThanCache) {
+      var cache = m_axisLessThanCache.computeIfAbsent(loop, k -> new HashMap<>());
+      return cache.computeIfAbsent(
+          Pair.of(axis, threshold),
+          k -> new BooleanEvent(loop, () -> getRawAxis(axis) < threshold));
+    }
   }
 
   /**
@@ -362,9 +369,12 @@ public class GenericHID {
    *     threshold.
    */
   public BooleanEvent axisGreaterThan(int axis, double threshold, EventLoop loop) {
-    var cache = m_axisGreaterThanCache.computeIfAbsent(loop, k -> new HashMap<>());
-    return cache.computeIfAbsent(
-        Pair.of(axis, threshold), k -> new BooleanEvent(loop, () -> getRawAxis(axis) > threshold));
+    synchronized (m_axisGreaterThanCache) {
+      var cache = m_axisGreaterThanCache.computeIfAbsent(loop, k -> new HashMap<>());
+      return cache.computeIfAbsent(
+          Pair.of(axis, threshold),
+          k -> new BooleanEvent(loop, () -> getRawAxis(axis) > threshold));
+    }
   }
 
   /**
