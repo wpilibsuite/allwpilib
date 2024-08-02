017952f42eb16cd4f44f33a434cc4e5d342eb7d3
Ryan Blue <ryanzblue@gmail.com>
2024-08-02 04:05:05 -0400

    Set power throttling options correctly

--- hal/src/main/native/sim/HAL.cpp
+++ hal/src/main/native/sim/HAL.cpp
@@ -399,31 +399,33 @@ HAL_Bool HAL_Initialize(int32_t timeout, int32_t mode) {
 
   // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setprocessinformation
   // Enable HighQoS to achieve maximum performance, and turn off power saving.
-  {
-    PROCESS_POWER_THROTTLING_STATE PowerThrottling{};
-    PowerThrottling.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
-    PowerThrottling.ControlMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED;
-    PowerThrottling.StateMask = 0;
-
-    SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling,
-                          &PowerThrottling, sizeof(PowerThrottling));
-  }
 
   // https://forums.oculusvr.com/t5/General/SteamVR-has-fixed-the-problems-with-Windows-11/td-p/956413
   // Always honor Timer Resolution Requests. This is to ensure that the timer
   // resolution set-up above sticks through transitions of the main window (eg:
   // minimization).
-  {
-    // This setting was introduced in Windows 11 and the definition is not
-    // available in older headers.
+  // This setting was introduced in Windows 11 and the definition is not
+  // available in older headers.
 #ifndef PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION
-    const auto PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION = 0x4U;
+  const auto PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION = 0x4U;
 #endif
 
-    PROCESS_POWER_THROTTLING_STATE PowerThrottling{};
+  // Try both at once, and if that doesn't succeed, only set HighQoS
+  PROCESS_POWER_THROTTLING_STATE PowerThrottling{};
+  PowerThrottling.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
+  PowerThrottling.ControlMask =
+      PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION |
+      PROCESS_POWER_THROTTLING_EXECUTION_SPEED;
+  PowerThrottling.StateMask = 0;
+
+  auto status =
+      SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling,
+                            &PowerThrottling, sizeof(PowerThrottling));
+
+  // setting both failed, fall back to HighQoS only
+  if (status == 0) {
     PowerThrottling.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
-    PowerThrottling.ControlMask =
-        PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION;
+    PowerThrottling.ControlMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED;
     PowerThrottling.StateMask = 0;
 
     SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling,
