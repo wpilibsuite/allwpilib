f0c36104653f1708670dcffa215266b539a8c840
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-08-19 16:26:36 -0700

    Fix various typos (#6980)
    Found with misspell

--- hal/src/main/java/edu/wpi/first/hal/REVPHStickyFaults.java
+++ hal/src/main/java/edu/wpi/first/hal/REVPHStickyFaults.java
@@ -13,7 +13,7 @@ public class REVPHStickyFaults {
   /** The compressor output has an open circuit. */
   public final boolean CompressorOpen;
 
-  /** An overcurrent event occurred on a solenoid ouput. */
+  /** An overcurrent event occurred on a solenoid output. */
   public final boolean SolenoidOverCurrent;
 
   /** The input voltage is below the minimum voltage. */
