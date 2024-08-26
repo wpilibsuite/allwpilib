1b797ef5af5d95c713ccb3ed3b970e51c006f414
Matt <matthew.morley.ca@gmail.com>
2024-08-26 07:50:20 -0700

    Run wpiformat

--- TimeSyncClient.cpp
+++ TimeSyncClient.cpp
@@ -1,73 +1,96 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <chrono>
-#include <thread>
-#include <sys/time.h>
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-#define SERVER_IP "127.0.0.1"  // Replace with the server's IP address if different
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <thread>
+
+#define SERVER_IP \
+  "127.0.0.1"  // Replace with the server's IP address if different
 #define PORT 5810
 
-uint64_t getCurrentTimeInMicroseconds() {
-    struct timeval tv;
-    gettimeofday(&tv, nullptr);
-    return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+inline uint64_t getCurrentTimeInMicroseconds() {
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
 }
 
 using std::cout, std::endl;
 
 int main() {
-    int sockfd;
-    struct sockaddr_in server_addr;
+  int sockfd;
+  struct sockaddr_in server_addr;
 
-    // Create a UDP socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+  // Create a UDP socket
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    std::perror("Socket creation failed");
+    std::exit(EXIT_FAILURE);
+  }
 
-    // Enable the SO_TIMESTAMP option
-    int enable = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_TIMESTAMP, &enable, sizeof(enable)) < 0) {
-        perror("Setting SO_TIMESTAMP failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up the server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/Address not supported");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    while (true) {
-        // Get the current system time as a uint64_t
-        uint64_t current_time = getCurrentTimeInMicroseconds();
-
-        // Send the current time to the server
-        sendto(sockfd, &current_time, sizeof(current_time), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-        // std::cout << "Sent current time (microseconds since epoch): " << current_time << std::endl;
-        uint64_t server_time {0};
-        int len = recvmsg(sockfd, &server_time, sizeof(server_time), 0);
-        if (len != sizeof(server_time)) {
-            cout << "Got only x bytes on recv: " << len << endl;
-        }
-        cout << "Local time: " << current_time << " server time " << server_time << endl;
-
-        // Sleep for 1 second before sending the next time
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-
-    // Close the socket
+  // Enable the SO_TIMESTAMP option
+  int enable = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_TIMESTAMP, &enable, sizeof(enable)) <
+      0) {
+    std::perror("Setting SO_TIMESTAMP failed");
     close(sockfd);
-    return 0;
+    std::exit(EXIT_FAILURE);
+  }
+
+  // Set up the server address
+  std::memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+    std::perror("Invalid address/Address not supported");
+    close(sockfd);
+    std::exit(EXIT_FAILURE);
+  }
+
+  while (true) {
+    // Get the current system time as a uint64_t
+    uint64_t send_time = getCurrentTimeInMicroseconds();
+
+    // Send the current time to the server
+    sendto(sockfd, &send_time, sizeof(send_time), 0,
+           (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    // std::cout << "Sent current time (microseconds since epoch): " <<
+    // send_time << std::endl;
+    uint64_t server_time{0};
+    int len = recv(sockfd, &server_time, sizeof(server_time), 0);
+    uint64_t rx_time = getCurrentTimeInMicroseconds();
+
+    if (len != sizeof(server_time)) {
+      cout << "Got only x bytes on recv: " << len << endl;
+    }
+
+    // when time = send_time+rtt2/2, server time = server time
+    // server time = local time + offset
+    // offset = (server time - local time) = (server time) - (send_time +
+    // rtt2/2)
+    auto rtt2 = rx_time - send_time;
+    int64_t serverTimeOffsetUs = server_time - rtt2 / 2 - send_time;
+
+    cout << "Ping-ponged! RTT " << (rx_time - send_time) << " uS, offset "
+         << serverTimeOffsetUs << " uS" << endl;
+    cout << "Estimated server time "
+         << (getCurrentTimeInMicroseconds() + serverTimeOffsetUs) / 1000000.0
+         << " s" << endl;
+
+    // Sleep for 1 second before sending the next time
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  // Close the socket
+  close(sockfd);
+  return 0;
 }
--- TimeSyncServer.cpp
+++ TimeSyncServer.cpp
@@ -1,100 +1,76 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <sys/time.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <cstring>
 #include <ctime>
+#include <iostream>
 
 #define PORT 5810
 #define BUFFER_SIZE 8
 
 uint64_t getCurrentTimeInMicroseconds() {
-    struct timeval tv;
-    gettimeofday(&tv, nullptr);
-    return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
 }
 
+uint64_t startTime = getCurrentTimeInMicroseconds();
+
 int main() {
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[BUFFER_SIZE];
-    socklen_t client_len = sizeof(client_addr);
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[BUFFER_SIZE];
+  socklen_t client_len = sizeof(client_addr);
 
-    // Create a UDP socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+  // Create a UDP socket
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    std::perror("Socket creation failed");
+    std::exit(EXIT_FAILURE);
+  }
 
-    // Enable the SO_TIMESTAMP option
-    int enable = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_TIMESTAMP, &enable, sizeof(enable)) < 0) {
-        perror("Setting SO_TIMESTAMP failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
+  // Set up the server address and bind it to the socket
+  std::memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(PORT);
 
-    // Set up the server address and bind it to the socket
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    while (true) {
-        // Prepare structures for ancillary data
-        struct iovec iov[1];
-        struct msghdr msg;
-        char control_buf[CMSG_SPACE(sizeof(struct timeval))];
-        struct timeval *recv_time;
-
-        // Clear buffer and receive message
-        memset(buffer, 0, BUFFER_SIZE);
-        memset(&msg, 0, sizeof(msg));
-        memset(control_buf, 0, sizeof(control_buf));
-
-        // Set up the message header
-        iov[0].iov_base = buffer;
-        iov[0].iov_len = BUFFER_SIZE;
-        msg.msg_name = &client_addr;
-        msg.msg_namelen = sizeof(client_addr);
-        msg.msg_iov = iov;
-        msg.msg_iovlen = 1;
-        msg.msg_control = control_buf;
-        msg.msg_controllen = sizeof(control_buf);
-
-        int n = recvmsg(sockfd, &msg, 0);
-        if (n < 0) {
-            perror("Failed to receive message");
-            continue;
-        }
-
-        // Retrieve the timestamp
-        struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
-        if (cmsg && cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SO_TIMESTAMP) {
-            recv_time = (struct timeval *)CMSG_DATA(cmsg);
-            std::cout << "Packet received at (seconds): " << recv_time->tv_sec 
-                      << " (microseconds): " << recv_time->tv_usec << std::endl;
-        }
-
-        // Get the current time as a 64-bit integer
-        uint64_t current_time = getCurrentTimeInMicroseconds();
-
-        // Send the current time back to the client as a uint64_t
-        sendto(sockfd, &current_time, sizeof(current_time), 0, (struct sockaddr *)&client_addr, client_len);
-
-        std::cout << "Sent current time (microseconds since epoch) to client: " << current_time << std::endl;
-    }
-
-    // Close the socket
+  if (bind(sockfd, (const struct sockaddr*)&server_addr, sizeof(server_addr)) <
+      0) {
+    std::perror("Bind failed");
     close(sockfd);
-    return 0;
+    std::exit(EXIT_FAILURE);
+  }
+
+  while (true) {
+    std::memset(buffer, 0, BUFFER_SIZE);
+    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                     (struct sockaddr*)&client_addr, &client_len);
+
+    if (n < 0) {
+      std::perror("Failed to receive message");
+      continue;
+    }
+
+    // Get the current time as a 64-bit integer
+    uint64_t current_time = getCurrentTimeInMicroseconds() - startTime;
+
+    // Send the current time back to the client as a uint64_t
+    sendto(sockfd, &current_time, sizeof(current_time), 0,
+           (struct sockaddr*)&client_addr, client_len);
+
+    std::cout << "Sent current time (microseconds since epoch) to client: "
+              << current_time << std::endl;
+  }
+
+  // Close the socket
+  close(sockfd);
+  return 0;
 }
