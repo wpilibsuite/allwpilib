6966028984e746d4f55f63585c3907cc072ba59c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-09-04 06:46:10 -0400

    fixed recursive mistake

--- wpimath/src/main/native/cpp/controller/ArmFeedforward.cpp
+++ wpimath/src/main/native/cpp/controller/ArmFeedforward.cpp
@@ -99,7 +99,7 @@ units::volt_t ArmFeedforward::Calculate(units::unit_t<Angle> currentAngle,
 
 units::volt_t ArmFeedforward::Calculate(
     units::unit_t<Angle> currentAngle, units::unit_t<Velocity> setpoint) const {
-  return Calculate(currentAngle, setpoint);
+  return Calculate(currentAngle, setpoint, setpoint);
 }
 
 units::volt_t ArmFeedforward::Calculate(
