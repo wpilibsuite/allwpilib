1b172439c34d873baabbeb4ac8b3d083e30ed7c7
DeltaDizzy <ejbraswell@gmail.com>
2024-09-10 23:38:38 -0400

    add cli commands

--- wpilogcli/src/main/native/cpp/main.cpp
+++ wpilogcli/src/main/native/cpp/main.cpp
@@ -1,29 +1,83 @@
 #include <cstring>
+#include <string>
 #include <string_view>
-#include <fmt/format.h>
+#include "argparse/argparse.hpp"
 
-void show(std::string_view message_type, bool raw) {
-
-}
-
-void list() {
+void export_json(std::string_view output_path) {
 
 }
 
-int main(int argc, char *argv[]) {
-  if (argc > 1) { // 
-    std::string_view command{argv[1]};
-    std::string_view message_type{};
+void export_csv(std::string_view output_path) {
 
-    if (command.compare("json")) { // 
-      
-    } else if (command.compare("csv")) {
+}
 
-    } else if (command.compare("extract")) {
-      
-    }
-  } else {
-    // no args given
-    // report error
+void extract_field(std::string_view field_name, std::string_view output_path, bool use_json) {
+
+}
+
+void open_log(std::string_view log_path) {
+  // TODO: Add Log reader (base it on SysId?)
+}
+
+int main(int argc, char* argv[]) {
+  argparse::ArgumentParser cli{"wpilog-cli"};
+
+  argparse::ArgumentParser export_json_command{"json"};
+  export_json_command.add_description(
+      "Export a JSON representation of a WPILOG file");
+  export_json_command.add_argument("log_file")
+      .help("Path to the WPILOG file to export");
+  export_json_command.add_argument("json_file")
+      .help(
+          "Path of the JSON file to create with the exported data. If it "
+          "exists, it will be overwritten.");
+
+  argparse::ArgumentParser export_csv_command{"csv"};
+  export_csv_command.add_description(
+      "Export a CSV representation of a WPILOG file");
+  export_csv_command.add_argument("-l", "--log-file").help("The WPILOG file to export");
+  export_csv_command.add_argument("-o", "--output-file")
+      .required()
+      .help(
+          "The CSV file to create with the exported data. If it "
+          "exists, it will be overwritten.");
+
+  argparse::ArgumentParser extract_field_command{"extract"};
+  extract_field_command.add_description(
+      "Extract the histoy of one field from a WPILOG file and store it in a "
+      "JSON or CSV file");
+  extract_field_command.add_argument("-f", "--field")
+      .required()
+      .help("The field to extract from the WPILOG");
+  extract_field_command.add_argument("-l", "--log")
+      .required()
+      .help("The WPILOG file to extract from");
+  extract_field_command.add_argument("--time-start")
+    .help("The timestamp to start extracting at");
+  extract_field_command.add_argument("-o", "--output")
+      .required()
+      .help(
+          "The file to export the field and data to. It will be created or "
+          "overwritten if it already exists");
+  
+  cli.add_subparser(export_json_command);
+  cli.add_subparser(export_csv_command);
+  cli.add_subparser(extract_field_command);
+
+  try {
+    cli.parse_args(argc, argv);
+  } catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << cli;
+    return 1;
+  }
+
+  // see which one was called
+  if (export_json_command) {
+    //export_json();
+  } else if (export_csv_command) {
+    export_csv(export_csv_command.get("--log-file"));
+  } else if (extract_field_command) {
+    //extract_field();
   }
 }
