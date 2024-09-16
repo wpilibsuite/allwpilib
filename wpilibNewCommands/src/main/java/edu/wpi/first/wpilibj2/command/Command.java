dd3f9d4754d574452dec747a51c9ef484ce37f7e
Tyler Veness <calcmogul@gmail.com>
2024-09-15 19:47:45 -0700

    Run wpiformat

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/Command.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/Command.java
@@ -220,13 +220,13 @@ public abstract class Command implements Sendable {
 
   /**
    * Decorates this command to run once a condition becomes true.
-   * 
+   *
    * <p>Note: This decorator works by adding this command to a composition. The command the
    * decorator was called on cannot be scheduled independently or be added to a different
    * composition (namely, decorators), unless it is manually cleared from the list of composed
    * commands with {@link CommandScheduler#removeComposedCommand(Command)}. The command composition
    * returned from this method can be further decorated without issue.
-   * 
+   *
    * @param condition the condition to wait for
    * @return the decorated command
    */
@@ -238,13 +238,13 @@ public abstract class Command implements Sendable {
 
   /**
    * Decorates this command to run after a time delay.
-   * 
+   *
    * <p>Note: This decorator works by adding this command to a composition. The command the
    * decorator was called on cannot be scheduled independently or be added to a different
    * composition (namely, decorators), unless it is manually cleared from the list of composed
    * commands with {@link CommandScheduler#removeComposedCommand(Command)}. The command composition
    * returned from this method can be further decorated without issue.
-   * 
+   *
    * @param seconds the seconds to wait
    * @return the decorated command
    */
@@ -254,13 +254,13 @@ public abstract class Command implements Sendable {
 
   /**
    * Decorates this command to run after a time delay.
-   * 
+   *
    * <p>Note: This decorator works by adding this command to a composition. The command the
    * decorator was called on cannot be scheduled independently or be added to a different
    * composition (namely, decorators), unless it is manually cleared from the list of composed
    * commands with {@link CommandScheduler#removeComposedCommand(Command)}. The command composition
    * returned from this method can be further decorated without issue.
-   * 
+   *
    * @param time the time to wait
    * @return the decorated command
    */
