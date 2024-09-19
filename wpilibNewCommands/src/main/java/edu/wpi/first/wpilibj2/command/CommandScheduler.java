7035c612fdc83e0e6387a198cc31c5b24a83e475
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-09-19 14:09:28 -0400

    Revert "fixed CommandScheduler for now"
    This reverts commit 2294b0b8b63e4770a34ef5f2418ebf1ef0ff57d5.

--- wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/CommandScheduler.java
+++ wpilibNewCommands/src/main/java/edu/wpi/first/wpilibj2/command/CommandScheduler.java
@@ -170,6 +170,8 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
     for (Consumer<Command> action : m_initActions) {
       action.accept(command);
     }
+
+    m_watchdog.addEpoch(command.getName() + ".initialize()");
   }
 
   /**
@@ -262,6 +264,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
       if (RobotBase.isSimulation()) {
         subsystem.simulationPeriodic();
       }
+      m_watchdog.addEpoch(subsystem.getName() + ".periodic()");
     }
 
     // Cache the active instance to avoid concurrency problems if setActiveLoop() is called from
@@ -269,6 +272,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
     EventLoop loopCache = m_activeButtonLoop;
     // Poll buttons for new commands to add.
     loopCache.poll();
+    m_watchdog.addEpoch("buttons.run()");
 
     m_inRunLoop = true;
     boolean isDisabled = RobotState.isDisabled();
@@ -285,6 +289,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
       for (Consumer<Command> action : m_executeActions) {
         action.accept(command);
       }
+      m_watchdog.addEpoch(command.getName() + ".execute()");
       if (command.isFinished()) {
         m_endingCommands.add(command);
         command.end(false);
@@ -295,6 +300,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
         iterator.remove();
 
         m_requirements.keySet().removeAll(command.getRequirements());
+        m_watchdog.addEpoch(command.getName() + ".end(false)");
       }
     }
     m_inRunLoop = false;
@@ -323,6 +329,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
     m_watchdog.disable();
     if (m_watchdog.isExpired()) {
       System.out.println("CommandScheduler loop overrun");
+      m_watchdog.printEpochs();
     }
   }
 
@@ -480,6 +487,7 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
     m_endingCommands.remove(command);
     m_scheduledCommands.remove(command);
     m_requirements.keySet().removeAll(command.getRequirements());
+    m_watchdog.addEpoch(command.getName() + ".end(true)");
   }
 
   /** Cancels all commands that are currently scheduled. */
@@ -520,6 +528,11 @@ public final class CommandScheduler implements Sendable, AutoCloseable {
   /** Enables the command scheduler. */
   public void enable() {
     m_disabled = false;
+  }
+
+  /** Prints list of epochs added so far and their times. */
+  public void printWatchdogEpochs() {
+    m_watchdog.printEpochs();
   }
 
   /**
