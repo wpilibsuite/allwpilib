d41a464f8f95b31c31dcfc178ca18d71b8732591
Ryan Blue <ryanzblue@gmail.com>
2024-09-19 15:04:47 -0400

    more docs, rename some stuff for consistency

--- hal/src/main/native/athena/AddressableLEDSimd.h
+++ hal/src/main/native/athena/AddressableLEDSimd.h
@@ -8,14 +8,22 @@
 #include "hal/AddressableLEDTypes.h"
 #include "simd/simd.h"
 
+// Timing info
 // https://developer.arm.com/documentation/ddi0409/i/instruction-timing/instruction-specific-scheduling/advanced-simd-load-store-instructions?lang=en
-// https://developer.arm.com/documentation/ddi0406/c/Application-Level-Architecture/Instruction-Details/Alphabetical-list-of-instructions/VLD4--single-4-element-structure-to-one-lane-
 
 namespace {
 using namespace Simd::Neon;
 
 template <typename T>
 using ConvertFunc = void (*)(T);
+
+/*
+ * Conversion funtions perform in-place conversion by swapping elements.
+ */
+
+// TODO: functions have been shimmed to convert with the input in BGR order...
+// need to figure out what the fpga actually outputs and maybe rename
+// accordingly
 
 template <typename T>
 void RGBToRBG(T val) {
@@ -44,87 +52,132 @@ void RGBToGBR(T val) {
   std::swap(val[1], val[2]);  // swap B and G
 }
 
-template <bool inAlign, bool outAlign, ConvertFunc<uint8x16_t*> Convert>
-void ConvertNEON_16(const uint8_t* in, uint8_t* out) {
-  uint8x16x4_t rgb = Load4<inAlign>(in);
+/**
+ * Copies 16 pixels from src to dst using NEON instructions, converting using
+ * the provided conversion function. Optimizes based on alignment of input and
+ * output arrays specified by srcAlign and dstAlign
+ * @tparam srcAlign whether src is aligned to the size of a NEON register (16
+ * bytes)
+ * @tparam dstAlign whether dst is aligned to the size of a NEON register (16
+ * bytes)
+ * @tparam the conversion function
+ * @param[in] src The source array
+ * @param[out] dst the destination array
+ * @pre src and dst must contain at least 16 pixels (64 bytes)
+ * @pre if srcAlign is true, src must be 16 byte aligned
+ * @pre if dstAlign is true, src muts be 16 byte aligned
+ */
+template <bool srcAlign, bool dstAlign, ConvertFunc<uint8x16_t*> Convert>
+void ConvertNEON_16(const uint8_t* src, uint8_t* dst) {
+  uint8x16x4_t rgb = Load4<srcAlign>(src);
   Convert(rgb.val);
-  Store4<outAlign>(out, rgb);
+  Store4<dstAlign>(dst, rgb);
 }
 
-template <bool inAlign, bool outAlign, ConvertFunc<uint8x8_t*> Convert>
-void ConvertNEON_8(const uint8_t* in, uint8_t* out) {
-  uint8x8x4_t rgb = LoadHalf4<inAlign>(in);
+/**
+ * Copies 16 pixels from src to dst using NEON instructions, converting using
+ * the provided conversion function. Optimizes based on alignment of input and
+ * output arrays specified by srcAlign and dstAlign
+ * @tparam srcAlign whether src is aligned to the size of a NEON register (16
+ * bytes)
+ * @tparam dstAlign whether dst is aligned to the size of a NEON register (16
+ * bytes)
+ * @tparam the conversion function
+ * @param[in] src The source array
+ * @param[out] dst the destination array
+ * @pre src and dst must contain at least 8 pixels (32 bytes)
+ * @pre if srcAlign is true, src must be 16 byte aligned
+ * @pre if dstAlign is true, src muts be 16 byte aligned
+ */
+template <bool srcAlign, bool dstAlign, ConvertFunc<uint8x8_t*> Convert>
+void ConvertNEON_8(const uint8_t* src, uint8_t* dst) {
+  uint8x8x4_t rgb = LoadHalf4<srcAlign>(src);
   Convert(rgb.val);
-  Store4<outAlign>(out, rgb);
+  Store4<dstAlign>(dst, rgb);
 }
 
-template <HAL_AddressableLEDColorOrder order, bool inAlign, bool outAlign>
+/**
+ * Copies 16 pixels from src to dst, converting from RGB(?) to order. Optimizes
+ * based on alignment of input and output arrays specified by srcAlign and
+ * dstAlign
+ * @tparam order the color order to convert to
+ * @tparam srcAlign whether src is aligned to the size of a NEON register (16
+ * bytes)
+ * @tparam dstAlign whether dst is aligned to the size of a NEON register (16
+ * bytes)
+ * @param[in] src The source array
+ * @param[out] dst the destination array
+ * @pre src and dst must contain at least 16 pixels (64 bytes)
+ * @pre if srcAlign is true, src must be 16 byte aligned
+ * @pre if dstAlign is true, src muts be 16 byte aligned
+ */
+template <HAL_AddressableLEDColorOrder order, bool srcAlign, bool dstAlign>
 void RGBConvert_16(const uint8_t* src, uint8_t* dst) {
   switch (order) {
     case HAL_ALED_RBG:
-      ConvertNEON_16<inAlign, outAlign, RGBToRBG>(src, dst);
+      ConvertNEON_16<srcAlign, dstAlign, RGBToRBG>(src, dst);
       break;
     case HAL_ALED_BGR:
-      ConvertNEON_16<inAlign, outAlign, RGBToBGR>(src, dst);
+      ConvertNEON_16<srcAlign, dstAlign, RGBToBGR>(src, dst);
       break;
     case HAL_ALED_BRG:
-      ConvertNEON_16<inAlign, outAlign, RGBToBRG>(src, dst);
+      ConvertNEON_16<srcAlign, dstAlign, RGBToBRG>(src, dst);
       break;
     case HAL_ALED_GRB:
-      ConvertNEON_16<inAlign, outAlign, RGBToGRB>(src, dst);
+      ConvertNEON_16<srcAlign, dstAlign, RGBToGRB>(src, dst);
       break;
     case HAL_ALED_GBR:
-      ConvertNEON_16<inAlign, outAlign, RGBToGBR>(src, dst);
+      ConvertNEON_16<srcAlign, dstAlign, RGBToGBR>(src, dst);
       break;
   }
 }
 
 /**
  * Copies 8 pixels from src to dst, converting from RGB(?) to order. Optimizes
- * based on alignment of input and output arrays specified by inAlign and
- * outAlign
+ * based on alignment of input and output arrays specified by srcAlign and
+ * dstAlign
  * @tparam order the color order to convert to
- * @tparam inAlign whether src is aligned to the size of a NEON register (16
+ * @tparam srcAlign whether src is aligned to the size of a NEON register (16
  * bytes)
- * @tparam outAlign whether dst is aligned to the size of a NEON register (16
+ * @tparam dstAlign whether dst is aligned to the size of a NEON register (16
  * bytes)
  * @param[in] src The source array
  * @param[out] dst the destination array
  * @pre src and dst must contain at least 32 bytes (8 pixels)
- * @pre if inAlign is true, src must be 16 byte aligned
- * @pre if outAlign is true, src muts be 16 byte aligned
+ * @pre if srcAlign is true, src must be 16 byte aligned
+ * @pre if dstAlign is true, src muts be 16 byte aligned
  */
-template <HAL_AddressableLEDColorOrder order, bool inAlign, bool outAlign>
+template <HAL_AddressableLEDColorOrder order, bool srcAlign, bool dstAlign>
 void RGBConvert_8(const uint8_t* src, uint8_t* dst) {
   switch (order) {
     case HAL_ALED_RBG:
-      ConvertNEON_8<inAlign, outAlign, RGBToRBG>(src, dst);
+      ConvertNEON_8<srcAlign, dstAlign, RGBToRBG>(src, dst);
       break;
     case HAL_ALED_BGR:
-      ConvertNEON_8<inAlign, outAlign, RGBToBGR>(src, dst);
+      ConvertNEON_8<srcAlign, dstAlign, RGBToBGR>(src, dst);
       break;
     case HAL_ALED_BRG:
-      ConvertNEON_8<inAlign, outAlign, RGBToBRG>(src, dst);
+      ConvertNEON_8<srcAlign, dstAlign, RGBToBRG>(src, dst);
       break;
     case HAL_ALED_GRB:
-      ConvertNEON_8<inAlign, outAlign, RGBToGRB>(src, dst);
+      ConvertNEON_8<srcAlign, dstAlign, RGBToGRB>(src, dst);
       break;
     case HAL_ALED_GBR:
-      ConvertNEON_8<inAlign, outAlign, RGBToGBR>(src, dst);
+      ConvertNEON_8<srcAlign, dstAlign, RGBToGBR>(src, dst);
       break;
   }
 }
 /**
- * Copies 1 pixel from in to out, converting from RGB to the specified order.
+ * Copies 1 pixel from src to dst, converting from RGB to the specified order.
  * @param[in] order the color order to convert to
  * @param[in] in the source array
  * @param[out] the destination array
- * @pre in and out must contain at least 1 pixel.
+ * @pre in and out must contain at least 1 pixel (4 bytes).
  */
-void RGBConvert_1(HAL_AddressableLEDColorOrder order, const uint8_t* in,
-                  uint8_t* out) {
+void RGBConvert_1(HAL_AddressableLEDColorOrder order, const uint8_t* src,
+                  uint8_t* dst) {
   uint8_t tmp[4];
-  std::memcpy(tmp, in, 4);
+  std::memcpy(tmp, src, 4);
   switch (order) {
     case HAL_ALED_RBG:
       RGBToRBG(tmp);
@@ -142,26 +195,26 @@ void RGBConvert_1(HAL_AddressableLEDColorOrder order, const uint8_t* in,
       RGBToGBR(tmp);
       break;
     case HAL_ALED_RGB:
-      std::memcpy(out, in, 4);
+      std::memcpy(dst, src, 4);
   }
 }
 /**
  * Copies len pixels from src to dst, converting from RGB(?) to order. Optimizes
- * based on alignment of input and output arrays specified by inAlign and
- * outAlign
+ * based on alignment of input and output arrays specified by srcAlign and
+ * dstAlign
  * @tparam order the color order to convert to
- * @tparam inAlign whether src is aligned to the size of a NEON register (16
+ * @tparam srcAlign whether src is aligned to the size of a NEON register (16
  * bytes)
- * @tparam outAlign whether dst is aligned to the size of a NEON register (16
+ * @tparam dstAlign whether dst is aligned to the size of a NEON register (16
  * bytes)
  * @param[in] src The source array
  * @param[out] dst the destination array
  * @param[in] len the size (in pixels, len = (size in bytes) / 4)
  * @pre src and dst must have at least len*4 capacity in bytes
- * @pre if inAlign is true, src must be 16 byte aligned
- * @pre if outAlign is true, src muts be 16 byte aligned
+ * @pre if srcAlign is true, src must be 16 byte aligned
+ * @pre if dstAlign is true, src muts be 16 byte aligned
  */
-template <HAL_AddressableLEDColorOrder order, bool inAlign, bool outAlign>
+template <HAL_AddressableLEDColorOrder order, bool srcAlign, bool dstAlign>
 void RGBConvert(const uint8_t* src, uint8_t* dst, size_t len) {
   if (len >= 16) {
     constexpr size_t A4 =
@@ -169,7 +222,7 @@ void RGBConvert(const uint8_t* src, uint8_t* dst, size_t len) {
     size_t size = len * 4;
     size_t aligned = Simd::AlignLo(size, A4);
     for (size_t i = 0; i < aligned; i += A4) {
-      RGBConvert_16<order, inAlign, outAlign>(src + i, dst + i);
+      RGBConvert_16<order, srcAlign, dstAlign>(src + i, dst + i);
     }
     if (aligned < size) {
       RGBConvert_16<order, false, false>(
@@ -177,7 +230,7 @@ void RGBConvert(const uint8_t* src, uint8_t* dst, size_t len) {
           dst + size - A4);  // copy last 16 pixels, possibly recopying.
     }
   } else if (len >= 8) {
-    RGBConvert_8<order, inAlign, outAlign>(src, dst);
+    RGBConvert_8<order, srcAlign, dstAlign>(src, dst);
     if (len > 8) {
       size_t recopyOffset = (len * 4) - (HA * 4);
       RGBConvert_8<order, false, false>(
@@ -189,8 +242,7 @@ void RGBConvert(const uint8_t* src, uint8_t* dst, size_t len) {
       RGBConvert_1(order, src + i, dst + i);
     }
     // we could also use neon single lane instructions
-    // https://developer.arm.com/documentation/ddi0406/c/Application-Level-Architecture/Instruction-Details/Alphabetical-list-of-instructions/VLD4--single-4-element-structure-to-all-lanes-
-    // https://developer.arm.com/documentation/102474/0100/Fundamentals-of-Armv8-Neon-technology/Registers--vectors--lanes-and-elements
+    // https://developer.arm.com/documentation/ddi0406/c/Application-Level-Architecture/Instruction-Details/Alphabetical-list-of-instructions/VLD4--single-4-element-structure-to-one-lane-
     // vld4_lane_u8
   }
 }
@@ -198,7 +250,10 @@ void RGBConvert(const uint8_t* src, uint8_t* dst, size_t len) {
 /**
  * Copies pixelCount pixels from src to dst, converting from RGB to the
  * specified order
- *
+ * @tparam order the color order to convert to
+ * @param src the source array
+ * @param dst the destination array
+ * @param pixelCount the number of pixels to convert and copy
  */
 template <HAL_AddressableLEDColorOrder order>
 void RGBConvert(const uint8_t* src, uint8_t* dst, size_t pixelCount) {
