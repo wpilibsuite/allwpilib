9278dcb12ebf44eed97912d70e9b98e0111334d8
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-09-21 09:55:20 -0400

    fixed more tests.

--- wpimath/src/test/native/cpp/kinematics/SwerveModuleStateTest.cpp
+++ wpimath/src/test/native/cpp/kinematics/SwerveModuleStateTest.cpp
@@ -12,17 +12,17 @@ static constexpr double kEpsilon = 1E-9;
 TEST(SwerveModuleStateTest, Optimize) {
   frc::Rotation2d angleA{45_deg};
   frc::SwerveModuleState refA{-2_mps, 180_deg};
-  auto optimizedA = frc::SwerveModuleState::Optimize(refA, angleA);
+  refA.Optimize(angleA);
 
-  EXPECT_NEAR(optimizedA.speed.value(), 2.0, kEpsilon);
-  EXPECT_NEAR(optimizedA.angle.Degrees().value(), 0.0, kEpsilon);
+  EXPECT_NEAR(refA.speed.value(), 2.0, kEpsilon);
+  EXPECT_NEAR(refA.angle.Degrees().value(), 0.0, kEpsilon);
 
   frc::Rotation2d angleB{-50_deg};
   frc::SwerveModuleState refB{4.7_mps, 41_deg};
-  auto optimizedB = frc::SwerveModuleState::Optimize(refB, angleB);
+  refB.Optimize(angleB);
 
-  EXPECT_NEAR(optimizedB.speed.value(), -4.7, kEpsilon);
-  EXPECT_NEAR(optimizedB.angle.Degrees().value(), -139.0, kEpsilon);
+  EXPECT_NEAR(refB.speed.value(), -4.7, kEpsilon);
+  EXPECT_NEAR(refB.angle.Degrees().value(), -139.0, kEpsilon);
 }
 
 TEST(SwerveModuleStateTest, NoOptimize) {
