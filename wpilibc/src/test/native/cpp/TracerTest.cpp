c93e2afecce275b09990f5e0637889bcbd17c44d
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-09-25 12:06:42 -0400

    added scoped tracer unit test

--- wpilibc/src/test/native/cpp/TracerTest.cpp
+++ wpilibc/src/test/native/cpp/TracerTest.cpp
@@ -5,12 +5,13 @@
 #include <string_view>
 
 #include <gtest/gtest.h>
+#include <networktables/DoubleTopic.h>
 #include <networktables/NetworkTableInstance.h>
 
 #include "frc/Tracer.h"
 #include "frc/simulation/SimHooks.h"
 
-TEST(TracerTest, Timing) {
+TEST(TracerTest, TraceFunc) {
   frc::Tracer::SetThreadName("TracerTest");
 
   frc::sim::PauseTiming();
@@ -25,8 +26,33 @@ TEST(TracerTest, Timing) {
   auto test2Entry = nt::NetworkTableInstance::GetDefault().GetEntry(
       "/Tracer/TracerTest/Test1/Test2");
 
-  EXPECT_EQ(test1Entry.GetDouble(0.0), 500.0);
-  EXPECT_EQ(test2Entry.GetDouble(0.0), 400.0);
+  EXPECT_TRUE(test1Entry.GetDouble(0.0) - 500.0 < 1.0);
+  EXPECT_TRUE(test2Entry.GetDouble(0.0) - 400.0 < 1.0);
+
+  frc::sim::ResumeTiming();
+}
+
+TEST(TracerTest, ScopedTrace) {
+  frc::Tracer::SetThreadName("TracerTest");
+
+  frc::sim::PauseTiming();
+
+
+  frc::Tracer::StartTrace("Test3");
+  {
+    frc::Tracer::StartScopedTrace("Test4");
+    frc::sim::StepTiming(400_ms);
+  }
+  frc::sim::StepTiming(100_ms);
+  frc::Tracer::EndTrace();
+
+  auto test3Entry = nt::NetworkTableInstance::GetDefault().GetEntry(
+      "/Tracer/TracerTest/Test3");
+  auto test4Entry = nt::NetworkTableInstance::GetDefault().GetEntry(
+      "/Tracer/TracerTest/Test3/Test4");
+
+  EXPECT_TRUE(test3Entry.GetDouble(0.0) - 500.0 < 1.0);
+  EXPECT_TRUE(test4Entry.GetDouble(0.0) - 400.0 < 1.0);
 
   frc::sim::ResumeTiming();
 }
