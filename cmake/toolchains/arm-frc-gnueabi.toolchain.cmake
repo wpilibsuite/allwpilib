46356c9433c8c49d202e7639eeae4b3a423a8a95
Jade <spacey-sooty@proton.me>
2024-10-22 14:01:17 +0800

    Update roboRIO toolchain file

--- cmake/toolchains/arm-frc-gnueabi.toolchain.cmake
+++ cmake/toolchains/arm-frc-gnueabi.toolchain.cmake
@@ -1,4 +1,5 @@
 set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
-set(GNU_MACHINE "arm-frc2022-linux-gnueabi" CACHE STRING "GNU compiler triple")
+set(GNU_MACHINE "arm-frc2024-linux-gnueabi" CACHE STRING "GNU compiler triple")
 set(SOFTFP yes)
+set(ARM_LINUX_SYSROOT /usr/local/arm-nilrt-linux-gnueabi/sysroot)
 include("${CMAKE_CURRENT_LIST_DIR}/arm.toolchain.cmake")
