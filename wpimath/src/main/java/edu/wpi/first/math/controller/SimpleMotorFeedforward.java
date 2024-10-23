2a45b5f51c5e82cf675ef1a060499d6eaeaa5274
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-10-23 10:41:45 -0400

    current branch

--- wpimath/src/main/java/edu/wpi/first/math/controller/SimpleMotorFeedforward.java
+++ wpimath/src/main/java/edu/wpi/first/math/controller/SimpleMotorFeedforward.java
@@ -8,8 +8,6 @@ import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.math.controller.proto.SimpleMotorFeedforwardProto;
 import edu.wpi.first.math.controller.struct.SimpleMotorFeedforwardStruct;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.PerUnit;
 import edu.wpi.first.units.TimeUnit;
