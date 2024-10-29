fb2c8b120f71305c184b63699f63519f3455df3c
ElliotScher <ecscher84@gmail.com>
2024-10-28 23:31:19 -0400

    add deprecated declarations suppression.

--- wpical/src/main/native/include/fieldcalibration.h
+++ wpical/src/main/native/include/fieldcalibration.h
@@ -48,11 +48,11 @@ namespace fieldcalibration {
 std::tuple<Eigen::Matrix<double, 3, 3>, Eigen::Matrix<double, 8, 1>>
 load_camera_model(std::string path);
 std::tuple<Eigen::Matrix<double, 3, 3>, Eigen::Matrix<double, 8, 1>>
-load_camera_model(nlohmann::json json_data);
-std::map<int, nlohmann::json> load_ideal_map(std::string path);
+load_camera_model(wpi::json json_data);
+std::map<int, wpi::json> load_ideal_map(std::string path);
 std::vector<Eigen::Vector3d> get_model_corners(double tag_size);
 Eigen::Matrix<double, 4, 4> get_tag_transform(
-    std::map<int, nlohmann::json>& ideal_map, int tag_id);
+    std::map<int, wpi::json>& ideal_map, int tag_id);
 Eigen::Matrix<double, 4, 4> estimate_tag_pose(
     apriltag_detection_t* tag_detection,
     const Eigen::Matrix<double, 3, 3>& camera_matrix,
