98b2b512550f2adaa35e06e9cc7d04d0d037108c
Jade Turner <spacey-sooty@proton.me>
2024-11-04 11:23:42 +0800

    [wpiutil] Rename wpi::CreateSemaphore to wpi::MakeSemaphore
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- wpiutil/src/main/native/cpp/Synchronization.cpp
+++ wpiutil/src/main/native/cpp/Synchronization.cpp
@@ -92,7 +92,7 @@ void wpi::ResetEvent(WPI_EventHandle handle) {
   ResetSignalObject(handle);
 }
 
-WPI_SemaphoreHandle wpi::CreateSemaphore(int initialCount, int maximumCount) {
+WPI_SemaphoreHandle wpi::MakeSemaphore(int initialCount, int maximumCount) {
   auto& manager = GetManager();
   if (gShutdown) {
     return {};
@@ -341,8 +341,8 @@ void WPI_ResetEvent(WPI_EventHandle handle) {
   wpi::ResetEvent(handle);
 }
 
-WPI_SemaphoreHandle WPI_CreateSemaphore(int initial_count, int maximum_count) {
-  return wpi::CreateSemaphore(initial_count, maximum_count);
+WPI_SemaphoreHandle WPI_MakeSemaphore(int initial_count, int maximum_count) {
+  return wpi::MakeSemaphore(initial_count, maximum_count);
 }
 
 void WPI_DestroySemaphore(WPI_SemaphoreHandle handle) {
