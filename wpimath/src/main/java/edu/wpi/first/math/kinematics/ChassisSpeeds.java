7753b30044cc9e3534fc5717d9a560a9d2b8d352
Michael Jansen <mjansen4857@gmail.com>
2024-11-29 11:19:35 -0500

    Revert "Return self reference from ChassisSpeeds modifiers"
    This reverts commit 3b546c4379a297e6e5063fa2ab4cffc45fff6dbb.

--- wpimath/src/main/java/edu/wpi/first/math/kinematics/ChassisSpeeds.java
+++ wpimath/src/main/java/edu/wpi/first/math/kinematics/ChassisSpeeds.java
@@ -189,9 +189,8 @@ public class ChassisSpeeds implements ProtobufSerializable, StructSerializable {
    * swerve drivetrain.
    *
    * @param dtSeconds The duration of the timestep the speeds should be applied for.
-   * @return Reference to itself
    */
-  public ChassisSpeeds discretize(double dtSeconds) {
+  public void discretize(double dtSeconds) {
     var desiredDeltaPose =
         new Pose2d(
             vxMetersPerSecond * dtSeconds,
@@ -206,8 +205,6 @@ public class ChassisSpeeds implements ProtobufSerializable, StructSerializable {
     vxMetersPerSecond = twist.dx / dtSeconds;
     vyMetersPerSecond = twist.dy / dtSeconds;
     omegaRadiansPerSecond = twist.dtheta / dtSeconds;
-
-    return this;
   }
 
   /**
@@ -288,16 +285,13 @@ public class ChassisSpeeds implements ProtobufSerializable, StructSerializable {
    * @param robotAngle The angle of the robot as measured by a gyroscope. The robot's angle is
    *     considered to be zero when it is facing directly away from your alliance station wall.
    *     Remember that this should be CCW positive.
-   * @return Reference to itself
    */
-  public ChassisSpeeds toRobotRelativeSpeeds(Rotation2d robotAngle) {
+  public void toRobotRelativeSpeeds(Rotation2d robotAngle) {
     // CW rotation into chassis frame
     var rotated =
         new Translation2d(vxMetersPerSecond, vyMetersPerSecond).rotateBy(robotAngle.unaryMinus());
     vxMetersPerSecond = rotated.getX();
     vyMetersPerSecond = rotated.getY();
-
-    return this;
   }
 
   /**
@@ -377,15 +371,12 @@ public class ChassisSpeeds implements ProtobufSerializable, StructSerializable {
    * @param robotAngle The angle of the robot as measured by a gyroscope. The robot's angle is
    *     considered to be zero when it is facing directly away from your alliance station wall.
    *     Remember that this should be CCW positive.
-   * @return Reference to itself
    */
-  public ChassisSpeeds toFieldRelativeSpeeds(Rotation2d robotAngle) {
+  public void toFieldRelativeSpeeds(Rotation2d robotAngle) {
     // CCW rotation out of chassis frame
     var rotated = new Translation2d(vxMetersPerSecond, vyMetersPerSecond).rotateBy(robotAngle);
     vxMetersPerSecond = rotated.getX();
     vyMetersPerSecond = rotated.getY();
-
-    return this;
   }
 
   /**
