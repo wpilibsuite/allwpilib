281169f32f23c5ff5fa4923a881352c9882ec1f7
Sam Carlberg <sam@slfc.dev>
2024-12-02 19:53:26 -0500

    Add missing module info for fieldimages

---
+++ fieldImages/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+module wpilib.fieldimages {
+  requires com.fasterxml.jackson.databind;
+
+  exports edu.wpi.first.fields;
+}
