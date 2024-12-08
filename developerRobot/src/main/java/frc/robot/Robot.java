33d06384949111e1a5df91bb1da5577979b7bfab
Thad House <thadhouse1@gmail.com>
2024-12-08 09:04:56 -0800

    Rever developer robot

--- developerRobot/src/main/java/frc/robot/Robot.java
+++ developerRobot/src/main/java/frc/robot/Robot.java
@@ -1,18 +1,13 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-  private final PneumaticHub ph = new PneumaticHub();
-  XboxController xbox = new XboxController(0);
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -41,8 +36,5 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during all modes. */
   @Override
-  public void robotPeriodic() {
-    SmartDashboard.putNumber("PHVoltage", ph.getInputVoltage());
-    ph.setSolenoids(1, xbox.getAButton() ? 1 : 0);
-  }
+  public void robotPeriodic() {}
 }
