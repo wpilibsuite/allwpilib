1654acd1ae1245a73c7dbced45b56483ff57994e
Thad House <thadhouse1@gmail.com>
2024-12-09 21:55:20 -0800

    Formatting

--- wpiutil/src/main/java/edu/wpi/first/util/RuntimeLoader.java
+++ wpiutil/src/main/java/edu/wpi/first/util/RuntimeLoader.java
@@ -27,9 +27,9 @@ public final class RuntimeLoader {
         .append(String.format("JVM Location: %s\n", jvmLocation));
     if (System.getProperty("os.name").startsWith("Windows")) {
       msg.append(
-          "A common cause of this error is using a JVM with an incorrect MSVC runtime.\n")
-          .append("Ensure you are using the WPILib JVM (The current running JVM is listed above\n")
-          .append("See https://wpilib.org/jvmruntime for more information\n");
+          "A common cause of this error is using a JVM with an incorrect MSVC runtime.\n"
+            + "Ensure you are using the WPILib JVM (The current running JVM is listed above\n"
+            + "See https://wpilib.org/jvmruntime for more information\n");
     }
     return msg.toString();
   }
