5ee48a2128a859d0df209fe48e88f11b9ca61b45
WispySparks <wispysparks@gmail.com>
2024-12-29 21:57:09 -0600

    silly

--- glass/src/lib/native/cpp/Window.cpp
+++ glass/src/lib/native/cpp/Window.cpp
@@ -61,7 +61,7 @@ void Window::Display() {
   if (m_name.empty()) {
     name = &m_defaultName;
   }
-  std::string label = fmt::format("{}###{}", name, m_id);
+  std::string label = fmt::format("{}###{}", *name, m_id);
 
   // Accounts for size of title, collapse button, and close button
   float minWidth =
