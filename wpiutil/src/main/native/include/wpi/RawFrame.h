4f10089ac307f095f6729d0e2eae5d0ecc108699
Matt <matthew.morley.ca@gmail.com>
2024-12-30 12:17:52 -0800

    run lint on wpiutil

--- wpiutil/src/main/native/include/wpi/RawFrame.h
+++ wpiutil/src/main/native/include/wpi/RawFrame.h
@@ -34,15 +34,15 @@ typedef struct WPI_RawFrame {  // NOLINT
   uint8_t* data;
   // function to free image data (may be NULL)
   void (*freeFunc)(void* cbdata, void* data, size_t capacity);
-  void* freeCbData;   // data passed to freeFunc
-  size_t capacity;    // data buffer capacity, in bytes
-  size_t size;        // actual size of data, in bytes
-  int pixelFormat;    // WPI_PixelFormat
-  int width;          // width of image, in pixels
-  int height;         // height of image, in pixels
-  int stride;         // size of each row of data, in bytes (may be 0)
-  uint64_t timestamp; // image capture timestamp
-  int timestampSrc;   // WPI_TimestampSource
+  void* freeCbData;    // data passed to freeFunc
+  size_t capacity;     // data buffer capacity, in bytes
+  size_t size;         // actual size of data, in bytes
+  int pixelFormat;     // WPI_PixelFormat
+  int width;           // width of image, in pixels
+  int height;          // height of image, in pixels
+  int stride;          // size of each row of data, in bytes (may be 0)
+  uint64_t timestamp;  // image capture timestamp
+  int timestampSrc;    // WPI_TimestampSource
 } WPI_RawFrame;
 
 /**
@@ -64,10 +64,15 @@ enum WPI_PixelFormat {
  * Timestamp metadata. Timebase is the same as wpi::Now
  */
 enum WPI_TimestampSource {
-  WPI_TIMESRC_UNKNOWN = 0,   // unknown
-  WPI_TIMESRC_FRAME_DEQUEUE, // wpi::Now when the new frame was dequeued by CSCore. Does not account for camera exposure time or V4L latency.
-  WPI_TIMESRC_V4L_EOF,       // End of Frame. Same as V4L2_BUF_FLAG_TSTAMP_SRC_EOF, translated into wpi::Now's timebase.
-  WPI_TIMESRC_V4L_SOE,       // Start of Exposure. Same as V4L2_BUF_FLAG_TSTAMP_SRC_SOE, translated into wpi::Now's timebase.
+  WPI_TIMESRC_UNKNOWN = 0,    // unknown
+  WPI_TIMESRC_FRAME_DEQUEUE,  // wpi::Now when the new frame was dequeued by
+                              // CSCore. Does not account for camera exposure
+                              // time or V4L latency.
+  WPI_TIMESRC_V4L_EOF,  // End of Frame. Same as V4L2_BUF_FLAG_TSTAMP_SRC_EOF,
+                        // translated into wpi::Now's timebase.
+  WPI_TIMESRC_V4L_SOE,  // Start of Exposure. Same as
+                        // V4L2_BUF_FLAG_TSTAMP_SRC_SOE, translated into
+                        // wpi::Now's timebase.
 };
 
 // Returns nonzero if the frame data was allocated/reallocated
