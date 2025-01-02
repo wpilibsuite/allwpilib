95e03f1b47e858e01fc26fc793d636ff5bb958cb
Peter Johnson <johnson.peter@gmail.com>
2025-01-01 22:02:59 -0800

    [wpilibc] Reduce SmallString usage

--- wpilibc/src/test/native/cpp/ScopedTracerTest.cpp
+++ wpilibc/src/test/native/cpp/ScopedTracerTest.cpp
@@ -2,10 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <string>
 #include <string_view>
 
 #include <gtest/gtest.h>
-#include <wpi/SmallString.h>
 #include <wpi/StringExtras.h>
 #include <wpi/raw_ostream.h>
 
@@ -13,8 +13,8 @@
 #include "frc/simulation/SimHooks.h"
 
 TEST(ScopedTracerTest, Timing) {
-  wpi::SmallString<128> buf;
-  wpi::raw_svector_ostream os(buf);
+  std::string buf;
+  wpi::raw_string_ostream os(buf);
 
   frc::sim::PauseTiming();
   {
