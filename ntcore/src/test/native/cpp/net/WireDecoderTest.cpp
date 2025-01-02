339f3e91eef37a9f3662c1f8baf43a54dc2c3aad
Peter Johnson <johnson.peter@gmail.com>
2025-01-01 16:42:59 -0800

    Remove headers

--- ntcore/src/test/native/cpp/net/WireDecoderTest.cpp
+++ ntcore/src/test/native/cpp/net/WireDecoderTest.cpp
@@ -2,11 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <string>
+#include <string_view>
 
 #include <gtest/gtest.h>
-#include <wpi/SmallString.h>
-#include <wpi/raw_ostream.h>
 
 #include "../MockLogger.h"
 #include "../PubSubOptionsMatcher.h"
