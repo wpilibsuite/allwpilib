979a5877df464ad1de97c5e12dd40c4cd2ae3762
Peter Johnson <johnson.peter@gmail.com>
2025-01-01 22:32:00 -0800

    [wpinet] hostname: Remove SmallVector version

--- wpinet/src/main/native/include/wpinet/hostname.h
+++ wpinet/src/main/native/include/wpinet/hostname.h
@@ -6,14 +6,9 @@
 #define WPINET_HOSTNAME_H_
 
 #include <string>
-#include <string_view>
 
 namespace wpi {
-template <typename T>
-class SmallVectorImpl;
-
 std::string GetHostname();
-std::string_view GetHostname(SmallVectorImpl<char>& name);
 }  // namespace wpi
 
 #endif  // WPINET_HOSTNAME_H_
