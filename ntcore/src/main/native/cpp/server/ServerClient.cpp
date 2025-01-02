3c8a83fb7111dd8cedb79b2128db61090937e053
Peter Johnson <johnson.peter@gmail.com>
2025-01-01 17:06:44 -0800

    [ntcore] Remove SmallVector and SmallString from most places

--- ntcore/src/main/native/cpp/server/ServerClient.cpp
+++ ntcore/src/main/native/cpp/server/ServerClient.cpp
@@ -45,15 +45,14 @@ void ServerClient::UpdateMetaClientSub() {
   }
 }
 
-std::span<ServerSubscriber*> ServerClient::GetSubscribers(
-    std::string_view name, bool special,
-    wpi::SmallVectorImpl<ServerSubscriber*>& buf) {
-  buf.resize(0);
+std::vector<ServerSubscriber*> ServerClient::GetSubscribers(
+    std::string_view name, bool special) {
+  std::vector<ServerSubscriber*> out;
   for (auto&& subPair : m_subscribers) {
     ServerSubscriber* subscriber = subPair.getSecond().get();
     if (subscriber->Matches(name, special)) {
-      buf.emplace_back(subscriber);
+      out.emplace_back(subscriber);
     }
   }
-  return {buf.data(), buf.size()};
+  return out;
 }
