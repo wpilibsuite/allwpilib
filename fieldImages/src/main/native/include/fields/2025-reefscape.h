086060163f4aac73812d85f64caa5e2aa70b8dc9
Kevin-OConnor <koconnor@usfirst.org>
2025-01-04 12:20:37 -0500

    Update field and april tags
    Column in Field Drawings is labeled X-Rotation, but I believe it should be Y-Rotation so have reflected that here. We'll fix in a TU if this is correct.

---
+++ fieldImages/src/main/native/include/fields/2025-reefscape.h
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string_view>
+
+namespace fields {
+std::string_view GetResource_2025_reefscape_json();
+std::string_view GetResource_2025_field_png();
+}  // namespace fields
