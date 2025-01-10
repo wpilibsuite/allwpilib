612522783618a81b7236224434815404b25d9d66
Elliot Scher <44726531+ElliotScher@users.noreply.github.com>
2025-01-10 02:30:17 -0500

    [wpical] Add JSON Combiner (#7640)
    This new feature allows users to combine multiple Apriltag layouts. This can be useful for fields where the apriltags are split into two or more sections: (red/blue side, grouped together by task, etc.)

--- wpical/src/main/native/include/tagpose.h
+++ wpical/src/main/native/include/tagpose.h
@@ -6,15 +6,19 @@
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <wpi/json.h>
 
 namespace tag {
 class Pose {
  public:
-  Pose(double xpos, double ypos, double zpos, double w, double x, double y,
-       double z, double field_length_meters, double field_width_meters);
+  Pose(int tag_id, double xpos, double ypos, double zpos, double w, double x,
+       double y, double z, double field_length_meters,
+       double field_width_meters);
+  int tagId;
   double xPos, yPos, zPos, yawRot, rollRot, pitchRot;
   Eigen::Quaterniond quaternion;
   Eigen::Matrix3d rotationMatrix;
   Eigen::Matrix4d transformMatrixFmap;
+  wpi::json toJson();
 };
 }  // namespace tag
