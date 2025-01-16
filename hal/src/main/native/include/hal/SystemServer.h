1cad4f64a41b5d19b1aadefeafe84be133cd304b
Thad House <ThadHouse@users.noreply.github.com>
2025-01-16 09:49:40 -0800

    [hal] Add high level way of getting systemserver NT instance (#7683)

---
+++ hal/src/main/native/include/hal/SystemServer.h
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <ntcore_c.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+NT_Inst HAL_GetSystemServerHandle(void);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
