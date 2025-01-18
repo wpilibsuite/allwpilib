1038c51c8d5a2710dfc4bd3c60dd1a90b00fe9bf
yapplejack <jacklf@umich.edu>
2025-01-18 13:14:48 -0500

    requesting kFramework 10 for the Everybot

--- hal/src/generated/main/java/edu/wpi/first/hal/FRCNetComm.java
+++ hal/src/generated/main/java/edu/wpi/first/hal/FRCNetComm.java
@@ -317,6 +317,8 @@ public final class FRCNetComm {
     public static final int kFramework_AdvantageKit = 7;
     /** kFramework_MagicBot = 8. */
     public static final int kFramework_MagicBot = 8;
+    /** kFramework_Everybot = 10. */
+    public static final int kFramework_Everybot = 10;
     /** kRobotDrive_ArcadeStandard = 1. */
     public static final int kRobotDrive_ArcadeStandard = 1;
     /** kRobotDrive_ArcadeButtonSpin = 2. */
