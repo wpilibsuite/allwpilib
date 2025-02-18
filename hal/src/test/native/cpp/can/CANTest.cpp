ca867089a9018827334e994ce7124a71fde40a80
Thad House <thadhouse1@gmail.com>
2025-02-17 16:37:38 -0800

    Fix a few tests

--- hal/src/test/native/cpp/can/CANTest.cpp
+++ hal/src/test/native/cpp/can/CANTest.cpp
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <cstring>
 #include <utility>
 
 #include <gtest/gtest.h>
