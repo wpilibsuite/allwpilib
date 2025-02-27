8ef18465b7a579f5685568601a8ea9fccb7d5a79
Tyler Veness <calcmogul@gmail.com>
2025-02-26 22:35:41 -0800

    [datalog] Fix sorting of related header

--- datalog/src/main/native/cpp/DataLogReader.cpp
+++ datalog/src/main/native/cpp/DataLogReader.cpp
@@ -2,13 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include "wpi/datalog/DataLogReader.h"
+
 #include <bit>
 #include <utility>
 
 #include <wpi/Endian.h>
 
 #include "wpi/datalog/DataLog.h"
-#include "wpi/datalog/DataLogReader.h"
 
 using namespace wpi::log;
 
