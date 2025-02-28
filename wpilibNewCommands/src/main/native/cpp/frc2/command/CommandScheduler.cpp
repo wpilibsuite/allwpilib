0525f525aef0cf9b634bc7c6031ad62019fb4ddd
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-02-27 23:01:05 -0500

    add c++ function(is mutable which will need to be fixed)

--- wpilibNewCommands/src/main/native/cpp/frc2/command/CommandScheduler.cpp
+++ wpilibNewCommands/src/main/native/cpp/frc2/command/CommandScheduler.cpp
@@ -288,6 +288,16 @@ void CommandScheduler::UnregisterSubsystem(
   }
 }
 
+std::vector<Subsystem*> CommandScheduler::GetAllRegisteredSubsystems(){
+  std::vector<frc2::Subsystem*> subsystems;
+  subsystems.reserve(m_impl->subsystems.size());
+  for(wpi::DenseMap<frc2::Subsystem*, std::unique_ptr<Command>>::iterator it = m_impl->subsystems.begin();
+  it != m_impl->subsystems.end(); ++it){
+    subsystems.push_back(it->first);
+  }
+  return subsystems;
+}
+
 void CommandScheduler::UnregisterAllSubsystems() {
   m_impl->subsystems.clear();
 }
