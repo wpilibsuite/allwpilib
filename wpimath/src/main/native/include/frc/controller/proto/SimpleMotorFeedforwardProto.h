71460819bb4b9537846962eb25a46c7fb9cc95c9
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-04-15 23:11:53 -0400

    revert Simple Proto and Test

--- wpimath/src/main/native/include/frc/controller/proto/SimpleMotorFeedforwardProto.h
+++ wpimath/src/main/native/include/frc/controller/proto/SimpleMotorFeedforwardProto.h
@@ -15,30 +15,27 @@
 // frc::SimpleMotorFeedforward<units::meters> or
 // frc::SimpleMotorFeedforward<units::radians>
 
-template <class Distance, class Input>
-  requires(units::length_unit<Distance> || units::angle_unit<Distance>) &&
-          (units::voltage_unit<Input> || units::current_unit<Input>)
-struct wpi::Protobuf<frc::SimpleMotorFeedforward<Distance, Input>> {
+template <class Distance>
+  requires units::length_unit<Distance> || units::angle_unit<Distance>
+struct wpi::Protobuf<frc::SimpleMotorFeedforward<Distance>> {
   using MessageStruct = wpi_proto_ProtobufSimpleMotorFeedforward;
   using InputStream =
-      wpi::ProtoInputStream<frc::SimpleMotorFeedforward<Distance, Input>>;
+      wpi::ProtoInputStream<frc::SimpleMotorFeedforward<Distance>>;
   using OutputStream =
-      wpi::ProtoOutputStream<frc::SimpleMotorFeedforward<Distance, Input>>;
+      wpi::ProtoOutputStream<frc::SimpleMotorFeedforward<Distance>>;
 
-  static std::optional<frc::SimpleMotorFeedforward<Distance, Input>> Unpack(
+  static std::optional<frc::SimpleMotorFeedforward<Distance>> Unpack(
       InputStream& stream) {
-    using BaseDistanceUnit =
+    using BaseUnit =
         units::unit<std::ratio<1>, units::traits::base_unit_of<Distance>>;
-    using BaseInputUnit =
-        units::unit<std::ratio<1>, units::traits::base_unit_of<Input>>;    
-    using BaseFeedforward = frc::SimpleMotorFeedforward<BaseDistanceUnit, BaseInputUnit>;
+    using BaseFeedforward = frc::SimpleMotorFeedforward<BaseUnit>;
     wpi_proto_ProtobufSimpleMotorFeedforward msg;
     if (!stream.Decode(msg)) {
       return {};
     }
 
     return frc::SimpleMotorFeedforward<Distance>{
-        units::unit_t<Input>{msg.ks},
+        units::volt_t{msg.ks},
         units::unit_t<typename BaseFeedforward::kv_unit>{msg.kv},
         units::unit_t<typename BaseFeedforward::ka_unit>{msg.ka},
         units::second_t{msg.dt},
@@ -46,12 +43,10 @@ struct wpi::Protobuf<frc::SimpleMotorFeedforward<Distance, Input>> {
   }
 
   static bool Pack(OutputStream& stream,
-                   const frc::SimpleMotorFeedforward<Distance, Input>& value) {
-    using BaseDistanceUnit =
+                   const frc::SimpleMotorFeedforward<Distance>& value) {
+    using BaseUnit =
         units::unit<std::ratio<1>, units::traits::base_unit_of<Distance>>;
-    using BaseInputUnit =
-        units::unit<std::ratio<1>, units::traits::base_unit_of<Input>>;    
-    using BaseFeedforward = frc::SimpleMotorFeedforward<BaseDistanceUnit, BaseInputUnit>;
+    using BaseFeedforward = frc::SimpleMotorFeedforward<BaseUnit>;
     wpi_proto_ProtobufSimpleMotorFeedforward msg{
         .ks = value.GetKs().value(),
         .kv = units::unit_t<typename BaseFeedforward::kv_unit>{value.GetKv()}
--- wpimath/src/test/native/cpp/controller/SimpleMotorFeedforwardTest.cpp
+++ wpimath/src/test/native/cpp/controller/SimpleMotorFeedforwardTest.cpp
@@ -26,7 +26,7 @@ TEST(SimpleMotorFeedforwardTest, Calculate) {
   constexpr Matrixd<1, 1> B{{1.0 / Ka.value()}};
 
   frc::LinearPlantInversionFeedforward<1, 1> plantInversion{A, B, dt};
-  frc::SimpleMotorFeedforward<units::meter, units::volts> simpleMotor{Ks, Kv, Ka};
+  frc::SimpleMotorFeedforward<units::meter> simpleMotor{Ks, Kv, Ka};
 
   constexpr Vectord<1> r{{2.0}};
   constexpr Vectord<1> nextR{{3.0}};
@@ -47,7 +47,7 @@ TEST(SimpleMotorFeedforwardTest, NegativeGains) {
   constexpr auto Kv = -3_V / 1_mps;
   constexpr auto Ka = -0.6_V / 1_mps_sq;
   constexpr units::second_t dt = 0_ms;
-  frc::SimpleMotorFeedforward<units::meter, units::volts> simpleMotor{Ks, Kv, Ka, dt};
+  frc::SimpleMotorFeedforward<units::meter> simpleMotor{Ks, Kv, Ka, dt};
   EXPECT_EQ(simpleMotor.GetKv().value(), 0);
   EXPECT_EQ(simpleMotor.GetKa().value(), 0);
   EXPECT_EQ(simpleMotor.GetDt().value(), 0.02);
