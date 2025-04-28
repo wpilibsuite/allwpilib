659710a79a1d16c60c1433944ae3a148576153b9
sciencewhiz <sciencewhiz@users.noreply.github.com>
2025-04-28 16:25:49 -0700

    [examples] Add remaining Encoder snippets (#7936)

---
+++ wpilibcExamples/src/main/cpp/snippets/EncoderDrive/cpp/Robot.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <numbers>
+
+#include <frc/Encoder.h>
+#include <frc/TimedRobot.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/motorcontrol/Spark.h>
+
+/**
+ * Encoder drive to distance snippets for frc-docs.
+ * https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/encoders-software.html
+ */
+class Robot : public frc::TimedRobot {
+ public:
+  Robot() {
+    // Configures the encoder's distance-per-pulse
+    // The robot moves forward 1 foot per encoder rotation
+    // There are 256 pulses per encoder rotation
+    m_encoder.SetDistancePerPulse(1.0 / 256.0);
+    // Invert the right side of the drivetrain. You might have to invert the
+    // other side
+    rightLeader.SetInverted(true);
+    // Configure the followers to follow the leaders
+    leftLeader.AddFollower(leftFollower);
+    rightLeader.AddFollower(rightFollower);
+  }
+  void AutonomousPeriodic() override {
+    // Drives forward at half speed until the robot has moved 5 feet, then
+    // stops:
+    if (m_encoder.GetDistance() < 5) {
+      drive.TankDrive(0.5, 0.5);
+    } else {
+      drive.TankDrive(0, 0);
+    }
+  }
+
+ private:
+  // Creates an encoder on DIO ports 0 and 1.
+  frc::Encoder m_encoder{0, 1};
+  // Initialize motor controllers and drive
+  frc::Spark leftLeader{0};
+  frc::Spark leftFollower{1};
+  frc::Spark rightLeader{2};
+  frc::Spark rightFollower{3};
+  frc::DifferentialDrive drive{[&](double output) { leftLeader.Set(output); },
+                               [&](double output) { rightLeader.Set(output); }};
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ wpilibcExamples/src/main/cpp/snippets/EncoderHoming/cpp/Robot.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <numbers>
+
+#include <frc/DigitalInput.h>
+#include <frc/Encoder.h>
+#include <frc/TimedRobot.h>
+#include <frc/motorcontrol/Spark.h>
+
+/**
+ * Encoder mechanism homing snippets for frc-docs.
+ * https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/encoders-software.html
+ */
+class Robot : public frc::TimedRobot {
+ public:
+  void AutonomousPeriodic() override {
+    // Runs the motor backwards at half speed until the limit switch is pressed
+    // then turn off the motor and reset the encoder
+    if (!m_limit.Get()) {
+      m_spark.Set(-0.5);
+    } else {
+      m_spark.Set(0);
+      m_encoder.Reset();
+    }
+  }
+
+ private:
+  frc::Encoder m_encoder{0, 1};
+  frc::Spark m_spark{0};
+  // Limit switch on DIO 2
+  frc::DigitalInput m_limit{2};
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
