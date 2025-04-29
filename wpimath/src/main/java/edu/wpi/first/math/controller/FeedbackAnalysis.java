6c8740ccff10befb00ac63c325b08847480ac6c2
ハイドラント <thecomputer314@gmail.com>
2025-04-29 10:14:47 -0500

    Add Javadocs

--- wpimath/src/main/java/edu/wpi/first/math/controller/FeedbackAnalysis.java
+++ wpimath/src/main/java/edu/wpi/first/math/controller/FeedbackAnalysis.java
@@ -13,6 +13,18 @@ import edu.wpi.first.math.system.LinearSystem;
 public class FeedbackAnalysis {
   public record FeedbackGains(double kP, double kD) {}
 
+  /**
+   * Calculates optimal PD gains for a second-order position system.
+   *
+   * @param system The dynamics function for the system.
+   * @param maxControlEffort Max control effort for the controller.
+   * @param positionTolerance Max position tolerance for the controller.
+   * @param velocityTolerance Max velocity tolerance for the controller.
+   * @param controllerPeriodSeconds Period of the controller.
+   * @param measurementDelaySeconds Measurement delay.
+   * @return Optimal PD gains given the above system and control characteristics.
+   * @see edu.wpi.first.math.system.plant.LinearSystemId
+   */
   public static FeedbackGains getPositionGains(
       LinearSystem<N2, N1, N1> system,
       double maxControlEffort,
@@ -33,6 +45,17 @@ public class FeedbackAnalysis {
     return new FeedbackGains(controller.getK().get(0, 0), controller.getK().get(0, 1));
   }
 
+  /**
+   * Calculates an optimal P gain for a first-order velocity system.
+   *
+   * @param system The dynamics function for the system.
+   * @param maxControlEffort Max control effort for the controller.
+   * @param tolerance Max velocity tolerance for the controller.
+   * @param controllerPeriodSeconds Period of the controller.
+   * @param measurementDelaySeconds Measurement delay.
+   * @return Optimal P gain given the above system and control characteristics.
+   * @see edu.wpi.first.math.system.plant.LinearSystemId
+   */
   public static double getVelocityPGain(
       LinearSystem<N1, N1, N1> system,
       double maxControlEffort,
