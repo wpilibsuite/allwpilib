977634f7d3713c06abc1c8645918a711614804e2
Craftzman7 <zezetta@icloud.com>
2025-05-24 11:56:02 -0700

    Fix WPICal compile error

--- wpical/src/main/native/cpp/WPIcal.cpp
+++ wpical/src/main/native/cpp/WPIcal.cpp
@@ -227,7 +227,7 @@ static void DisplayGui() {
   static double imagerWidth = 1920;
   static double imagerHeight = 1080;
 
-  static constexpr char* charucoDictNames[] = {
+  static constexpr const char* charucoDictNames[] = {
       "DICT_4X4_50", "DICT_4X4_100", "DICT_4X4_250", "DICT_4X4_1000",
       "DICT_5X5_50", "DICT_5X5_100", "DICT_5X5_250", "DICT_5X5_1000",
       "DICT_6X6_50", "DICT_6X6_100", "DICT_6X6_250", "DICT_6X6_1000",
