8e8ea089af3acf5e4352e5af0b061beb9f963779
Craftzman7 <zezetta@icloud.com>
2025-05-24 09:50:19 -0700

    Run wpiformat on WPICal

--- wpical/src/main/native/cpp/cameracalibration.cpp
+++ wpical/src/main/native/cpp/cameracalibration.cpp
@@ -46,10 +46,10 @@ static bool filter(std::vector<cv::Point2f> charuco_corners,
 int cameracalibration::calibrate(const std::string& input_video,
                                  CameraModel& camera_model, float square_width,
                                  float marker_width, int board_width,
-                                 int board_height, bool show_debug_window, int dict) {
+                                 int board_height, bool show_debug_window,
+                                 int dict) {
   // ChArUco Board
-  cv::aruco::Dictionary aruco_dict =
-      cv::aruco::getPredefinedDictionary(dict);
+  cv::aruco::Dictionary aruco_dict = cv::aruco::getPredefinedDictionary(dict);
   cv::Ptr<cv::aruco::CharucoBoard> charuco_board = new cv::aruco::CharucoBoard(
       cv::Size(board_width, board_height), square_width * 0.0254,
       marker_width * 0.0254, aruco_dict);
@@ -159,8 +159,7 @@ int cameracalibration::calibrate(const std::string& input_video,
                                  double imagerHeightPixels,
                                  bool show_debug_window, int dict) {
   // ChArUco Board
-  cv::aruco::Dictionary aruco_dict =
-      cv::aruco::getPredefinedDictionary(dict);
+  cv::aruco::Dictionary aruco_dict = cv::aruco::getPredefinedDictionary(dict);
   cv::Ptr<cv::aruco::CharucoBoard> charuco_board = new cv::aruco::CharucoBoard(
       cv::Size(board_width, board_height), square_width * 0.0254,
       marker_width * 0.0254, aruco_dict);
