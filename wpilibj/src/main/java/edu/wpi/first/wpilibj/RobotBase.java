55701b423798e4506b9e96c01b4538db67e3676d
Peter Johnson <johnson.peter@gmail.com>
2025-06-01 22:22:28 -0700

    Remove unused imports

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/RobotBase.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/RobotBase.java
@@ -15,11 +15,6 @@ import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.util.WPILibVersion;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
