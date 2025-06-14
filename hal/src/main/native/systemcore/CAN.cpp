720b6ad932c3a5eb004b7837eebe5b8e1f19870c
Thad House <thadhouse1@gmail.com>
2025-06-13 19:38:55 -0700

    Review

--- hal/src/main/native/systemcore/CAN.cpp
+++ hal/src/main/native/systemcore/CAN.cpp
@@ -17,9 +17,9 @@
 #include <utility>
 #include <vector>
 
-#include <fmt/format.h>
 #include <wpi/DenseMap.h>
 #include <wpi/mutex.h>
+#include <wpi/print.h>
 #include <wpi/timestamp.h>
 
 #include "PortsInternal.h"
@@ -109,14 +109,15 @@ bool SocketCanState::InitializeBuses() {
     int32_t status = 0;
     HAL_SetCurrentThreadPriority(true, 50, &status);
     if (status != 0) {
-      fmt::print("Failed to set CAN thread priority\n");
+      wpi::print("Failed to set CAN thread priority\n");
     }
 
     for (int i = 0; i < hal::kNumCanBuses; i++) {
       std::scoped_lock lock{writeMutex[i]};
       socketHandle[i] = socket(PF_CAN, SOCK_RAW, CAN_RAW);
       if (socketHandle[i] == -1) {
-        fmt::print("socket() for CAN {} failed with {}\n", i, strerror(errno));
+        wpi::print("socket() for CAN {} failed with {}\n", i,
+                   std::strerror(errno));
         success = false;
         return;
       }
@@ -125,8 +126,8 @@ bool SocketCanState::InitializeBuses() {
       std::snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "can_s%d", i);
 
       if (ioctl(socketHandle[i], SIOCGIFINDEX, &ifr) == -1) {
-        fmt::print("ioctl(SIOCGIFINDEX) for CAN {} failed with {}\n",
-                   ifr.ifr_name, strerror(errno));
+        wpi::print("ioctl(SIOCGIFINDEX) for CAN {} failed with {}\n",
+                   ifr.ifr_name, std::strerror(errno));
         success = false;
         return;
       }
@@ -138,15 +139,15 @@ bool SocketCanState::InitializeBuses() {
 
       if (bind(socketHandle[i], reinterpret_cast<const sockaddr*>(&addr),
                sizeof(addr)) == -1) {
-        fmt::print("bind() for CAN {} failed with {}\n", ifr.ifr_name,
-                   strerror(errno));
+        wpi::print("bind() for CAN {} failed with {}\n", ifr.ifr_name,
+                   std::strerror(errno));
         success = false;
         return;
       }
 
       auto poll = wpi::uv::Poll::Create(loop, socketHandle[i]);
       if (!poll) {
-        fmt::print("wpi::uv::Poll::Create for CAN {} failed\n", ifr.ifr_name);
+        wpi::print("wpi::uv::Poll::Create for CAN {} failed\n", ifr.ifr_name);
         success = false;
         return;
       }
