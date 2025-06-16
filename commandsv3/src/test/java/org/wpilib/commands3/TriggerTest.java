77562b879eb61d78481db9e2db6313a8272fc5d1
Sam Carlberg <sam@slfc.dev>
2025-06-16 10:11:04 -0400

    Checkstyle test

--- commandsv3/src/test/java/org/wpilib/commands3/TriggerTest.java
+++ commandsv3/src/test/java/org/wpilib/commands3/TriggerTest.java
@@ -12,117 +12,129 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TriggerTest {
-  private Scheduler scheduler;
+  private Scheduler m_scheduler;
 
   @BeforeEach
   void setup() {
-    scheduler = new Scheduler();
+    m_scheduler = new Scheduler();
   }
 
   @Test
   void onTrue() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.onTrue(command);
 
     signal.set(true);
-    scheduler.run();
+    m_scheduler.run();
 
-    assertTrue(scheduler.isRunning(command), "Command was not scheduled on rising edge");
+    assertTrue(m_scheduler.isRunning(command), "Command was not scheduled on rising edge");
 
     signal.set(false);
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command), "Command should still be running on falling edge");
+    m_scheduler.run();
+    assertTrue(m_scheduler.isRunning(command), "Command should still be running on falling edge");
   }
 
   @Test
   void onFalse() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.onFalse(command);
 
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command), "Command should be scheduled when signal starts low");
+    m_scheduler.run();
+    assertTrue(
+        m_scheduler.isRunning(command),
+        "Command should be scheduled when signal starts low");
 
     signal.set(true);
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command), "Command should still be running rising falling edge");
+    m_scheduler.run();
+    assertTrue(
+        m_scheduler.isRunning(command),
+        "Command should still be running rising falling edge");
   }
 
   @Test
   void whileTrue() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.whileTrue(command);
 
     signal.set(true);
-    scheduler.run();
+    m_scheduler.run();
 
-    assertTrue(scheduler.isRunning(command), "Command was not scheduled on rising edge");
+    assertTrue(
+        m_scheduler.isRunning(command),
+        "Command was not scheduled on rising edge");
 
     signal.set(false);
-    scheduler.run();
-    assertFalse(scheduler.isRunning(command), "Command should be cancelled on falling edge");
+    m_scheduler.run();
+    assertFalse(
+        m_scheduler.isRunning(command),
+        "Command should be cancelled on falling edge");
   }
 
   @Test
   void whileFalse() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.whileFalse(command);
 
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command), "Command should be scheduled when signal starts low");
+    m_scheduler.run();
+    assertTrue(
+        m_scheduler.isRunning(command),
+        "Command should be scheduled when signal starts low");
 
     signal.set(true);
-    scheduler.run();
-    assertFalse(scheduler.isRunning(command), "Command should be cancelled on rising edge");
+    m_scheduler.run();
+    assertFalse(
+        m_scheduler.isRunning(command),
+        "Command should be cancelled on rising edge");
   }
 
   @Test
   void toggleOnTrue() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.toggleOnTrue(command);
 
-    scheduler.run();
-    assertFalse(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertFalse(m_scheduler.isRunning(command));
 
     signal.set(true);
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertTrue(m_scheduler.isRunning(command));
 
     signal.set(false);
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertTrue(m_scheduler.isRunning(command));
 
     signal.set(true);
-    scheduler.run();
-    assertFalse(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertFalse(m_scheduler.isRunning(command));
   }
 
   @Test
   void toggleOnFalse() {
     var signal = new AtomicBoolean(false);
-    var trigger = new Trigger(scheduler, signal::get);
+    var trigger = new Trigger(m_scheduler, signal::get);
     var command = Command.noRequirements(Coroutine::park).named("Command");
     trigger.toggleOnFalse(command);
 
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertTrue(m_scheduler.isRunning(command));
 
     signal.set(true);
-    scheduler.run();
-    assertTrue(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertTrue(m_scheduler.isRunning(command));
 
     signal.set(false);
-    scheduler.run();
-    assertFalse(scheduler.isRunning(command));
+    m_scheduler.run();
+    assertFalse(m_scheduler.isRunning(command));
   }
 
   @Test
@@ -138,20 +150,20 @@ class TriggerTest {
     }).named("Inner");
 
     var outer = Command.noRequirements(co -> {
-      new Trigger(scheduler, innerSignal::get).onTrue(inner);
+      new Trigger(m_scheduler, innerSignal::get).onTrue(inner);
       co.yield();
     }).named("Outer");
 
-    scheduler.schedule(outer);
-    scheduler.run();
+    m_scheduler.schedule(outer);
+    m_scheduler.run();
     assertFalse(innerRan.get(), "The bound command should not run before the signal is set");
 
     innerSignal.set(true);
-    scheduler.run();
+    m_scheduler.run();
     assertTrue(innerRan.get(), "The nested trigger should have run the bound command");
 
     innerRan.set(false);
-    scheduler.run();
+    m_scheduler.run();
     assertFalse(innerRan.get(), "Trigger should not have fired again");
   }
 }
