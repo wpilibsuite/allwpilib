96b47d108a753577c61b0953275995ac88bb51ec
Peter Johnson <johnson.peter@gmail.com>
2025-06-24 22:27:29 -0700

    Start implementing MockTunableBackend

--- tunable/src/main/java/edu/wpi/first/tunable/MockTunableBackend.java
+++ tunable/src/main/java/edu/wpi/first/tunable/MockTunableBackend.java
@@ -4,6 +4,53 @@
 
 package edu.wpi.first.tunable;
 
-public class MockTunableBackend {
+public class MockTunableBackend implements TunableBackend {
+  private static class MockIntegerTunable implements IntegerTunable {
+    @Override
+    void setDefault(int value);
 
+    @Override
+    void set(int value) {
+      m_value = value;
+    }
+
+    @Override
+    int get() {
+      return m_value;
+    }
+
+    @Override
+    void onChange(IntegerConsumer callback);
+
+    private int m_value;
+  }
+
+  private static class MockTunable<T> implements Tunable<T> {
+    @Override
+    public void setDefault(T value);
+
+    @Override
+    public void set(T value);
+
+    @Override
+    public T get();
+
+    @Override
+    public void onChange(Consumer<T> callback);
+  }
+
+  public void set(String name, int value);
+  public <T> void set(String name, T value);
+
+  @Override
+  public IntegerTunable addInteger(String name, int defaultValue);
+
+  @Override
+  public <T> Tunable<T> addObject(String name, T defaultValue);
+
+  @Override
+  public <T> Tunable<T> addStruct(String name, T defaultValue, Struct<T> struct);
+
+  @Override
+  public <T> Tunable<T> addProtobuf(String name, T defaultValue, Protobuf<T, ?> proto);
 }
