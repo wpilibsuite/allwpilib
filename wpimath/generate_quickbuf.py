aa39620d728630195d74836dd1af2fc9a465dae6
Austin Schuh <austin.linux@gmail.com>
2025-07-01 22:30:11 -0700

    Lint

--- wpimath/generate_quickbuf.py
+++ wpimath/generate_quickbuf.py
@@ -23,7 +23,7 @@ def generate_quickbuf(
                 f"-I{absolute_filename.parent}",
                 absolute_filename,
             ],
-            check = True,
+            check=True,
         )
     java_files = (output_directory / "edu/wpi/first/math/proto").glob("*.java")
     for java_file in java_files:
