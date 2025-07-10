3bf15ca3acff411b15911ca2103b0e0458ae3013
Thad House <thadhouse1@gmail.com>
2025-07-09 22:20:54 -0700

    Another fix

--- hal/src/main/native/sim/DutyCycle.cpp
+++ hal/src/main/native/sim/DutyCycle.cpp
@@ -11,7 +11,6 @@
 #include "PortsInternal.h"
 #include "hal/Errors.h"
 #include "hal/handles/HandlesInternal.h"
-#include "hal/handles/LimitedHandleResource.h"
 #include "hal/handles/IndexedHandleResource.h"
 #include "mockdata/DutyCycleDataInternal.h"
 
