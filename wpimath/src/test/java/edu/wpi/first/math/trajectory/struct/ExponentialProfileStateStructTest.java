a4c307147c5d09eb873cfe0c1ec260a91f50b90e
Daniel1464 <Daniel_Chen@caryacademy.org>
2025-08-10 08:26:35 -0400

    split to separate test files

---
+++ wpimath/src/test/java/edu/wpi/first/math/trajectory/struct/ExponentialProfileStateStructTest.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.math.trajectory.struct;
+
+import edu.wpi.first.math.trajectory.ExponentialProfile;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExponentialProfileStateStructTest {
+    private static final ExponentialProfile.State STATE = new ExponentialProfile.State(4.0, 5.0);
+
+    @Test
+    void testRoundtrip() {
+        ByteBuffer buffer = ByteBuffer.allocate(ExponentialProfile.State.struct.getSize());
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        ExponentialProfile.State.struct.pack(buffer, STATE);
+        buffer.rewind();
+        assertEquals(STATE, ExponentialProfile.State.struct.unpack(buffer));
+    }
+}
