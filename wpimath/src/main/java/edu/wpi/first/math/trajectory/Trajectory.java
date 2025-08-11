3db076c6f6d674f1f50bcf1a52cefc896c859a38
Zach Harel <zach@zharel.me>
2025-08-11 17:44:16 +0200

    put trajectory in the right package
    Signed-off-by: Zach Harel <zach@zharel.me>

---
+++ wpimath/src/main/java/edu/wpi/first/math/trajectory/Trajectory.java
@@ -0,0 +1,45 @@
+package edu.wpi.first.math.trajectory;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.units.measure.Time;
+import java.util.List;
+
+/**
+ * Represents a trajectory consisting of a list of {@link TrajectorySample}s,
+ * kinematically interpolating between them.
+ */
+public class Trajectory {
+  private final InterpolatingTreeMap<Time, TrajectorySample> samples;
+  private final Time duration;
+
+  public Trajectory(List<TrajectorySample> samples) {
+    this.samples =
+        new InterpolatingTreeMap<>(
+                (start, end, q) -> MathUtil.inverseInterpolate(start.in(Seconds), end.in(Seconds), q.in(Seconds)),
+                TrajectorySample::interpolate
+        );
+
+    samples.forEach(
+        sample -> {
+          this.samples.put(sample.timestamp, sample);
+        });
+
+    this.duration = samples.get(samples.size() - 1).timestamp;
+  }
+
+  public Time duration() {
+    return duration;
+  }
+
+  public TrajectorySample sampleAt(Time timestamp) {
+    return samples.get(timestamp);
+  }
+
+  public TrajectorySample sampleAt(double timestamp) {
+    return sampleAt(Seconds.of(timestamp));
+  }
+}
