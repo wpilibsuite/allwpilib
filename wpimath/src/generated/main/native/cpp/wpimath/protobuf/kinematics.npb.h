2bb10aed72cccf029bb3fd43d9192fc42e98b888
Zach Harel <zach@zharel.me>
2025-08-21 17:00:33 -0400

    add c++ versions of the accelerations class/struct/proto
    Signed-off-by: Zach Harel <zach@zharel.me>

--- wpimath/src/generated/main/native/cpp/wpimath/protobuf/kinematics.npb.h
+++ wpimath/src/generated/main/native/cpp/wpimath/protobuf/kinematics.npb.h
@@ -26,6 +26,16 @@ typedef struct _wpi_proto_ProtobufChassisSpeeds {
     double omega;
 } wpi_proto_ProtobufChassisSpeeds;
 
+typedef struct _wpi_proto_ProtobufChassisAccelerations {
+    static const pb_msgdesc_t* msg_descriptor(void) noexcept;
+    static std::string_view msg_name(void) noexcept;
+    static pb_filedesc_t file_descriptor(void) noexcept;
+
+    double ax;
+    double ay;
+    double alpha;
+} wpi_proto_ProtobufChassisAccelerations;
+
 typedef struct _wpi_proto_ProtobufDifferentialDriveKinematics {
     static const pb_msgdesc_t* msg_descriptor(void) noexcept;
     static std::string_view msg_name(void) noexcept;
@@ -42,6 +52,15 @@ typedef struct _wpi_proto_ProtobufDifferentialDriveWheelSpeeds {
     double left;
     double right;
 } wpi_proto_ProtobufDifferentialDriveWheelSpeeds;
+
+typedef struct _wpi_proto_ProtobufDifferentialDriveWheelAccelerations {
+    static const pb_msgdesc_t* msg_descriptor(void) noexcept;
+    static std::string_view msg_name(void) noexcept;
+    static pb_filedesc_t file_descriptor(void) noexcept;
+
+    double left;
+    double right;
+} wpi_proto_ProtobufDifferentialDriveWheelAccelerations;
 
 typedef struct _wpi_proto_ProtobufDifferentialDriveWheelPositions {
     static const pb_msgdesc_t* msg_descriptor(void) noexcept;
@@ -85,6 +104,17 @@ typedef struct _wpi_proto_ProtobufMecanumDriveWheelSpeeds {
     double rear_right;
 } wpi_proto_ProtobufMecanumDriveWheelSpeeds;
 
+typedef struct _wpi_proto_ProtobufMecanumDriveWheelAccelerations {
+    static const pb_msgdesc_t* msg_descriptor(void) noexcept;
+    static std::string_view msg_name(void) noexcept;
+    static pb_filedesc_t file_descriptor(void) noexcept;
+
+    double front_left;
+    double front_right;
+    double rear_left;
+    double rear_right;
+} wpi_proto_ProtobufMecanumDriveWheelAccelerations;
+
 typedef struct _wpi_proto_ProtobufSwerveDriveKinematics {
     static const pb_msgdesc_t* msg_descriptor(void) noexcept;
     static std::string_view msg_name(void) noexcept;
@@ -111,36 +141,58 @@ typedef struct _wpi_proto_ProtobufSwerveModuleState {
     pb_callback_t angle;
 } wpi_proto_ProtobufSwerveModuleState;
 
+typedef struct _wpi_proto_ProtobufSwerveModuleAccelerations {
+    static const pb_msgdesc_t* msg_descriptor(void) noexcept;
+    static std::string_view msg_name(void) noexcept;
+    static pb_filedesc_t file_descriptor(void) noexcept;
+
+    double acceleration;
+    double angular_acceleration;
+} wpi_proto_ProtobufSwerveModuleAccelerations;
+
 
 /* Initializer values for message structs */
 #define wpi_proto_ProtobufChassisSpeeds_init_default {0, 0, 0}
+#define wpi_proto_ProtobufChassisAccelerations_init_default {0, 0, 0}
 #define wpi_proto_ProtobufDifferentialDriveKinematics_init_default {0}
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_init_default {0, 0}
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_init_default {0, 0}
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_init_default {0, 0}
 #define wpi_proto_ProtobufMecanumDriveKinematics_init_default {{{NULL}, NULL}, {{NULL}, NULL}, {{NULL}, NULL}, {{NULL}, NULL}}
 #define wpi_proto_ProtobufMecanumDriveWheelPositions_init_default {0, 0, 0, 0}
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_init_default {0, 0, 0, 0}
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_init_default {0, 0, 0, 0}
 #define wpi_proto_ProtobufSwerveDriveKinematics_init_default {{{NULL}, NULL}}
 #define wpi_proto_ProtobufSwerveModulePosition_init_default {0, {{NULL}, NULL}}
 #define wpi_proto_ProtobufSwerveModuleState_init_default {0, {{NULL}, NULL}}
+#define wpi_proto_ProtobufSwerveModuleAccelerations_init_default {0, 0}
 #define wpi_proto_ProtobufChassisSpeeds_init_zero {0, 0, 0}
+#define wpi_proto_ProtobufChassisAccelerations_init_zero {0, 0, 0}
 #define wpi_proto_ProtobufDifferentialDriveKinematics_init_zero {0}
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_init_zero {0, 0}
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_init_zero {0, 0}
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_init_zero {0, 0}
 #define wpi_proto_ProtobufMecanumDriveKinematics_init_zero {{{NULL}, NULL}, {{NULL}, NULL}, {{NULL}, NULL}, {{NULL}, NULL}}
 #define wpi_proto_ProtobufMecanumDriveWheelPositions_init_zero {0, 0, 0, 0}
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_init_zero {0, 0, 0, 0}
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_init_zero {0, 0, 0, 0}
 #define wpi_proto_ProtobufSwerveDriveKinematics_init_zero {{{NULL}, NULL}}
 #define wpi_proto_ProtobufSwerveModulePosition_init_zero {0, {{NULL}, NULL}}
 #define wpi_proto_ProtobufSwerveModuleState_init_zero {0, {{NULL}, NULL}}
+#define wpi_proto_ProtobufSwerveModuleAccelerations_init_zero {0, 0}
 
 /* Field tags (for use in manual encoding/decoding) */
 #define wpi_proto_ProtobufChassisSpeeds_vx_tag   1
 #define wpi_proto_ProtobufChassisSpeeds_vy_tag   2
 #define wpi_proto_ProtobufChassisSpeeds_omega_tag 3
+#define wpi_proto_ProtobufChassisAccelerations_ax_tag 1
+#define wpi_proto_ProtobufChassisAccelerations_ay_tag 2
+#define wpi_proto_ProtobufChassisAccelerations_alpha_tag 3
 #define wpi_proto_ProtobufDifferentialDriveKinematics_trackwidth_tag 1
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_left_tag 1
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_right_tag 2
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_left_tag 1
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_right_tag 2
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_left_tag 1
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_right_tag 2
 #define wpi_proto_ProtobufMecanumDriveKinematics_front_left_tag 1
@@ -155,11 +207,17 @@ typedef struct _wpi_proto_ProtobufSwerveModuleState {
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_front_right_tag 2
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_rear_left_tag 3
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_rear_right_tag 4
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_front_left_tag 1
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_front_right_tag 2
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_rear_left_tag 3
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_rear_right_tag 4
 #define wpi_proto_ProtobufSwerveDriveKinematics_modules_tag 1
 #define wpi_proto_ProtobufSwerveModulePosition_distance_tag 1
 #define wpi_proto_ProtobufSwerveModulePosition_angle_tag 2
 #define wpi_proto_ProtobufSwerveModuleState_speed_tag 1
 #define wpi_proto_ProtobufSwerveModuleState_angle_tag 2
+#define wpi_proto_ProtobufSwerveModuleAccelerations_acceleration_tag 1
+#define wpi_proto_ProtobufSwerveModuleAccelerations_angular_acceleration_tag 2
 
 /* Struct field encoding specification for nanopb */
 #define wpi_proto_ProtobufChassisSpeeds_FIELDLIST(X, a) \
@@ -168,6 +226,13 @@ X(a, STATIC,   SINGULAR, DOUBLE,   vy,                2) \
 X(a, STATIC,   SINGULAR, DOUBLE,   omega,             3)
 #define wpi_proto_ProtobufChassisSpeeds_CALLBACK NULL
 #define wpi_proto_ProtobufChassisSpeeds_DEFAULT NULL
+
+#define wpi_proto_ProtobufChassisAccelerations_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, DOUBLE,   ax,                1) \
+X(a, STATIC,   SINGULAR, DOUBLE,   ay,                2) \
+X(a, STATIC,   SINGULAR, DOUBLE,   alpha,             3)
+#define wpi_proto_ProtobufChassisAccelerations_CALLBACK NULL
+#define wpi_proto_ProtobufChassisAccelerations_DEFAULT NULL
 
 #define wpi_proto_ProtobufDifferentialDriveKinematics_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, DOUBLE,   trackwidth,        1)
@@ -179,6 +244,12 @@ X(a, STATIC,   SINGULAR, DOUBLE,   left,              1) \
 X(a, STATIC,   SINGULAR, DOUBLE,   right,             2)
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_CALLBACK NULL
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_DEFAULT NULL
+
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, DOUBLE,   left,              1) \
+X(a, STATIC,   SINGULAR, DOUBLE,   right,             2)
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_CALLBACK NULL
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_DEFAULT NULL
 
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, DOUBLE,   left,              1) \
@@ -214,6 +285,14 @@ X(a, STATIC,   SINGULAR, DOUBLE,   rear_right,        4)
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_CALLBACK NULL
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_DEFAULT NULL
 
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, DOUBLE,   front_left,        1) \
+X(a, STATIC,   SINGULAR, DOUBLE,   front_right,       2) \
+X(a, STATIC,   SINGULAR, DOUBLE,   rear_left,         3) \
+X(a, STATIC,   SINGULAR, DOUBLE,   rear_right,        4)
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_CALLBACK NULL
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_DEFAULT NULL
+
 #define wpi_proto_ProtobufSwerveDriveKinematics_FIELDLIST(X, a) \
 X(a, CALLBACK, REPEATED, MESSAGE,  modules,           1)
 #define wpi_proto_ProtobufSwerveDriveKinematics_CALLBACK pb_default_field_callback
@@ -234,18 +313,28 @@ X(a, CALLBACK, OPTIONAL, MESSAGE,  angle,             2)
 #define wpi_proto_ProtobufSwerveModuleState_DEFAULT NULL
 #define wpi_proto_ProtobufSwerveModuleState_angle_MSGTYPE wpi_proto_ProtobufRotation2d
 
+#define wpi_proto_ProtobufSwerveModuleAccelerations_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, DOUBLE,   acceleration,      1) \
+X(a, STATIC,   SINGULAR, DOUBLE,   angular_acceleration,   2)
+#define wpi_proto_ProtobufSwerveModuleAccelerations_CALLBACK NULL
+#define wpi_proto_ProtobufSwerveModuleAccelerations_DEFAULT NULL
+
 /* Maximum encoded size of messages (where known) */
 /* wpi_proto_ProtobufMecanumDriveKinematics_size depends on runtime parameters */
 /* wpi_proto_ProtobufSwerveDriveKinematics_size depends on runtime parameters */
 /* wpi_proto_ProtobufSwerveModulePosition_size depends on runtime parameters */
 /* wpi_proto_ProtobufSwerveModuleState_size depends on runtime parameters */
 #define WPI_PROTO_KINEMATICS_NPB_H_MAX_SIZE      wpi_proto_ProtobufMecanumDriveWheelPositions_size
+#define wpi_proto_ProtobufChassisAccelerations_size 27
 #define wpi_proto_ProtobufChassisSpeeds_size     27
 #define wpi_proto_ProtobufDifferentialDriveKinematics_size 9
+#define wpi_proto_ProtobufDifferentialDriveWheelAccelerations_size 18
 #define wpi_proto_ProtobufDifferentialDriveWheelPositions_size 18
 #define wpi_proto_ProtobufDifferentialDriveWheelSpeeds_size 18
+#define wpi_proto_ProtobufMecanumDriveWheelAccelerations_size 36
 #define wpi_proto_ProtobufMecanumDriveWheelPositions_size 36
 #define wpi_proto_ProtobufMecanumDriveWheelSpeeds_size 36
+#define wpi_proto_ProtobufSwerveModuleAccelerations_size 18
 
 
 #endif
---
+++ wpimath/src/main/native/cpp/kinematics/proto/ChassisAccelerationsProto.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "frc/kinematics/proto/ChassisAccelerationsProto.h"
+
+#include "wpimath/protobuf/kinematics.npb.h"
+
+std::optional<frc::ChassisAccelerations> wpi::Protobuf<frc::ChassisAccelerations>::Unpack(
+    InputStream& stream) {
+  wpi_proto_ProtobufChassisAccelerations msg;
+  if (!stream.Decode(msg)) {
+    return {};
+  }
+
+  return frc::ChassisAccelerations{
+      units::meters_per_second_squared_t{msg.ax},
+      units::meters_per_second_squared_t{msg.ay},
+      units::radians_per_second_squared_t{msg.alpha},
+  };
+}
+
+bool wpi::Protobuf<frc::ChassisAccelerations>::Pack(OutputStream& stream,
+                                                   const frc::ChassisAccelerations& value) {
+  wpi_proto_ProtobufChassisAccelerations msg{
+      .ax = value.ax.value(),
+      .ay = value.ay.value(),
+      .alpha = value.alpha.value(),
+  };
+  return stream.Encode(msg);
+}
