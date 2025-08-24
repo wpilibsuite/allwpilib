899c9ab0ab7283e76f95275de6a46007d7b1c591
MichaelLesirge <michael.lesirge@gmail.com>
2025-08-23 17:39:19 -0700

    Refactor ApplyDeadband and CopySignPow methods to support units

--- wpimath/src/main/native/include/frc/MathUtil.h
+++ wpimath/src/main/native/include/frc/MathUtil.h
@@ -105,12 +105,12 @@ constexpr T ApplyDeadband(T value, T deadband, T maxMagnitude = T{1.0}) {
  * (defaults to 1). Can be infinite.
  * @return The value after the deadband is applied.
  */
-template <int N>
-Eigen::Matrix<double, N, 1> ApplyDeadband(
-    const Eigen::Matrix<double, N, 1>& value, double deadband,
-    double maxMagnitude = 1.0) {
+template <int N, typename T>
+  requires std::is_arithmetic_v<T> || units::traits::is_unit_t_v<T>
+Eigen::Matrix<T, N, 1> ApplyDeadband(const Eigen::Matrix<T, N, 1>& value,
+                                     T deadband, T maxMagnitude = T{1.0}) {
   if (value.norm() < 1e-6) {
-    return Eigen::Matrix<double, N, 1>::Zero();
+    return Eigen::Matrix<T, N, 1>::Zero();
   }
   return value.normalized() *
          ApplyDeadband(value.norm(), deadband, maxMagnitude);
@@ -170,14 +170,14 @@ constexpr T CopySignPow(T value, double exponent, T maxMagnitude = T{1.0}) {
  * @return The transformed value with the same direction and norm scaled to
  * the input range.
  */
-template <int N>
-Eigen::Matrix<double, N, 1> CopySignPow(
-    const Eigen::Matrix<double, N, 1>& value, double deadband,
-    double maxMagnitude = 1.0) {
+template <int N, typename T>
+  requires std::is_arithmetic_v<T> || units::traits::is_unit_t_v<T>
+Eigen::Matrix<T, N, 1> CopySignPow(const Eigen::Matrix<double, N, 1>& value,
+                                   double exponent, T maxMagnitude = T{1.0}) {
   if (value.norm() < 1e-6) {
-    return Eigen::Matrix<double, N, 1>::Zero();
+    return Eigen::Matrix<T, N, 1>::Zero();
   }
-  return value.normalized() * CopySignPow(value.norm(), deadband, maxMagnitude);
+  return value.normalized() * CopySignPow(value.norm(), exponent, maxMagnitude);
 }
 
 /**
