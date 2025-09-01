235434d5929828c9a142589734cfcd7ec6ff1db0
Craftzman7 <zezetta@icloud.com>
2025-08-31 19:31:01 -0700

    Run wpiformat on WPICal

--- wpical/src/main/native/cpp/WPIcal.cpp
+++ wpical/src/main/native/cpp/WPIcal.cpp
@@ -233,24 +233,22 @@ static void DisplayGui() {
   };
 
   static constexpr CharucoDict charucoDicts[] = {
-    {"DICT_4X4_50", cv::aruco::DICT_4X4_50},
-    {"DICT_4X4_100", cv::aruco::DICT_4X4_100},
-    {"DICT_4X4_250", cv::aruco::DICT_4X4_250},
-    {"DICT_4X4_1000", cv::aruco::DICT_4X4_1000},
-    {"DICT_5X5_50", cv::aruco::DICT_5X5_50},
-    {"DICT_5X5_100", cv::aruco::DICT_5X5_100},
-    {"DICT_5X5_250", cv::aruco::DICT_5X5_250},
-    {"DICT_5X5_1000", cv::aruco::DICT_5X5_1000},
-    {"DICT_6X6_50", cv::aruco::DICT_6X6_50},
-    {"DICT_6X6_100", cv::aruco::DICT_6X6_100},
-    {"DICT_6X6_250", cv::aruco::DICT_6X6_250},
-    {"DICT_6X6_1000", cv::aruco::DICT_6X6_1000},
-    {"DICT_7X7_50", cv::aruco::DICT_7X7_50},
-    {"DICT_7X7_100", cv::aruco::DICT_7X7_100},
-    {"DICT_7X7_250", cv::aruco::DICT_7X7_250},
-    {"DICT_7X7_1000", cv::aruco::DICT_7X7_1000}
-  };
-
+      {"DICT_4X4_50", cv::aruco::DICT_4X4_50},
+      {"DICT_4X4_100", cv::aruco::DICT_4X4_100},
+      {"DICT_4X4_250", cv::aruco::DICT_4X4_250},
+      {"DICT_4X4_1000", cv::aruco::DICT_4X4_1000},
+      {"DICT_5X5_50", cv::aruco::DICT_5X5_50},
+      {"DICT_5X5_100", cv::aruco::DICT_5X5_100},
+      {"DICT_5X5_250", cv::aruco::DICT_5X5_250},
+      {"DICT_5X5_1000", cv::aruco::DICT_5X5_1000},
+      {"DICT_6X6_50", cv::aruco::DICT_6X6_50},
+      {"DICT_6X6_100", cv::aruco::DICT_6X6_100},
+      {"DICT_6X6_250", cv::aruco::DICT_6X6_250},
+      {"DICT_6X6_1000", cv::aruco::DICT_6X6_1000},
+      {"DICT_7X7_50", cv::aruco::DICT_7X7_50},
+      {"DICT_7X7_100", cv::aruco::DICT_7X7_100},
+      {"DICT_7X7_250", cv::aruco::DICT_7X7_250},
+      {"DICT_7X7_1000", cv::aruco::DICT_7X7_1000}};
 
   static int currentDictIdx =
       7;  // Default to DICT_5X5_1000, used by default by wpical
