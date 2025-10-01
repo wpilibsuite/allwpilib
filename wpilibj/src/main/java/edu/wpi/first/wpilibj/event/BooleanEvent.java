e604ba5d0cce94488ba490596b50aa84fe471ed7
murat <bob65536@proton.me>
2025-10-01 07:43:54 -0400

    Remove unused import.

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/event/BooleanEvent.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/event/BooleanEvent.java
@@ -8,7 +8,6 @@ import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.util.ErrorMessages.requireNonNullParam;
 
 import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.units.measure.Frequency;
 import edu.wpi.first.units.measure.Time;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiFunction;
