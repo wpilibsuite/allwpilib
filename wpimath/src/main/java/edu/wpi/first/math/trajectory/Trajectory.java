9915cfa9474a142df4d536492d47c69d3e6f0706
Zach Harel <zach@zharel.me>
2025-10-11 21:10:48 -0400

    refactor: convert Trajectory class to abstract and define interpolation methods
    Signed-off-by: Zach Harel <zach@zharel.me>

--- wpimath/src/main/java/edu/wpi/first/math/trajectory/Trajectory.java
+++ wpimath/src/main/java/edu/wpi/first/math/trajectory/Trajectory.java
@@ -18,7 +18,7 @@ import java.util.List;
  * Represents a trajectory consisting of a list of {@link TrajectorySample}s, kinematically
  * interpolating between them.
  */
-public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
+public abstract class Trajectory<SampleType extends TrajectorySample<SampleType>> {
 
   /** The samples this Trajectory is composed of. */
   public final List<SampleType> samples;
@@ -42,7 +42,7 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
         new InterpolatingTreeMap<>(
             (start, end, q) ->
                 MathUtil.inverseLerp(start.in(Seconds), end.in(Seconds), q.in(Seconds)),
-            SampleType::interpolate);
+            this::interpolate);
 
     this.samples.forEach(
         sample -> {
@@ -51,6 +51,17 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
 
     this.duration = this.samples.getLast().timestamp;
   }
+
+  /**
+   * Interpolates between two samples. This method must be implemented by subclasses to provide
+   * drivetrain-specific interpolation logic.
+   *
+   * @param start The starting sample.
+   * @param end The ending sample.
+   * @param t The interpolation parameter between 0 and 1.
+   * @return The interpolated sample.
+   */
+  public abstract SampleType interpolate(SampleType start, SampleType end, double t);
 
   /** Gets the first sample in the trajectory. */
   public SampleType start() {
@@ -84,9 +95,7 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    * @param transform The transform to transform the trajectory by.
    * @return The transformed trajectory.
    */
-  public Trajectory<SampleType> transformBy(Transform2d transform) {
-    return new Trajectory<>(samples.stream().map(s -> s.transform(transform)).toList());
-  }
+  public abstract Trajectory<SampleType> transformBy(Transform2d transform);
 
   /**
    * Concatenates this trajectory with another trajectory. If the other trajectory is empty, this
@@ -96,19 +105,7 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    * @param other the other trajectory to concatenate with this one.
    * @return a new trajectory that is the concatenation of this trajectory and the other trajectory.
    */
-  public Trajectory<SampleType> concatenate(Trajectory<SampleType> other) {
-    if (other.samples.isEmpty()) {
-      return this;
-    }
-
-    var combinedSamples = new ArrayList<>(this.samples);
-    combinedSamples.addAll(
-        other.samples.stream()
-            .map(s -> s.withNewTimestamp(s.timestamp.plus(this.duration)))
-            .toList());
-
-    return new Trajectory<>(combinedSamples);
-  }
+  public abstract Trajectory<SampleType> concatenate(Trajectory<SampleType> other);
 
   /**
    * Returns a new trajectory that is relative to the given pose. This is useful for converting a
@@ -120,9 +117,16 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    *     starting pose.
    * @return a new trajectory that is relative to the given pose.
    */
-  public Trajectory<SampleType> relativeTo(Pose2d other) {
-    return new Trajectory<>(samples.stream().map(s -> s.relativeTo(other)).toList());
-  }
+  public abstract Trajectory<SampleType> relativeTo(Pose2d other);
+
+  /**
+   * Returns a new trajectory with the direction of travel reversed. All poses, velocities, and
+   * accelerations are negated along the direction of travel. The returned trajectory represents
+   * the same path but traversed in the opposite direction.
+   *
+   * @return a new trajectory with the direction of travel reversed.
+   */
+  public abstract Trajectory<SampleType> reversed();
 
   /**
    * Converts this trajectory to a differential trajectory, allowing for easier following by
@@ -131,9 +135,8 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    * @param kinematics DifferentialDriveKinematics object representing the drivetrain.
    * @return the trajectory with differential samples.
    */
-  public Trajectory<DifferentialSample> toDifferentialTrajectory(
-      DifferentialDriveKinematics kinematics) {
-    return new Trajectory<>(
+  public DifferentialTrajectory toDifferentialTrajectory(DifferentialDriveKinematics kinematics) {
+    return new DifferentialTrajectory(
         samples.stream().map(s -> new DifferentialSample(s, kinematics)).toList());
   }
 
@@ -144,8 +147,9 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    * @param kinematics MecanumDriveKinematics object representing the drivetrain.
    * @return the trajectory with mecanum samples.
    */
-  public Trajectory<MecanumSample> toMecanumTrajectory(MecanumDriveKinematics kinematics) {
-    return new Trajectory<>(samples.stream().map(s -> new MecanumSample(s, kinematics)).toList());
+  public MecanumTrajectory toMecanumTrajectory(MecanumDriveKinematics kinematics) {
+    return new MecanumTrajectory(
+        samples.stream().map(s -> new MecanumSample(s, kinematics)).toList());
   }
 
   /**
@@ -155,7 +159,8 @@ public class Trajectory<SampleType extends TrajectorySample<SampleType>> {
    * @param kinematics SwerveDriveKinematics object representing the drivetrain.
    * @return the trajectory with swerve samples.
    */
-  public Trajectory<SwerveSample> toSwerveTrajectory(SwerveDriveKinematics kinematics) {
-    return new Trajectory<>(samples.stream().map(s -> new SwerveSample(s, kinematics)).toList());
+  public SwerveTrajectory toSwerveTrajectory(SwerveDriveKinematics kinematics) {
+    return new SwerveTrajectory(
+        samples.stream().map(s -> new SwerveSample(s, kinematics)).toList());
   }
 }
