662d7768486dba0083e5c69cb735ea479b9417e3
Thad House <thadhouse1@gmail.com>
2025-10-24 16:46:13 -0700

    Remove Jaguar (and other) motor controllers

--- hal/src/main/native/sim/DigitalInternal.h
+++ hal/src/main/native/sim/DigitalInternal.h
@@ -29,9 +29,8 @@ constexpr int32_t kExpectedLoopTiming = 40;
  *   reliably down to 10.0 ms; starting at about 8.5ms, the servo sometimes hums
  *   and get hot; by 5.0ms the hum is nearly continuous
  * - 10ms periods work well for Victor 884
- * - 5ms periods allows higher update rates for Luminary Micro Jaguar motor
- *   controllers. Due to the shipping firmware on the Jaguar, we can't run the
- *   update period less than 5.05 ms.
+ * - 5ms periods allows higher update rates for Motorn CAN based motor
+ *   controllers.
  *
  * kDefaultPwmPeriod is the 1x period (5.05 ms).  In hardware, the period
  * scaling is implemented as an output squelch to get longer periods for old
