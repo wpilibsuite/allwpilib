9d7581b4669e9a9ea907e9dd05aa2a26fd72f9ae
PJ Reiniger <pj.reiniger@gmail.com>
2025-10-27 01:54:20 -0400

    SCRIPT: Pregenerate files

--- commandsv2/src/generated/main/java/org/wpilib/commands2/button/CommandXboxController.java
+++ commandsv2/src/generated/main/java/org/wpilib/commands2/button/CommandXboxController.java
@@ -6,9 +6,9 @@
 
 package org.wpilib.commands2.button;
 
+import org.wpilib.commands2.CommandScheduler;
 import org.wpilib.driverstation.XboxController;
 import org.wpilib.event.EventLoop;
-import org.wpilib.commands2.CommandScheduler;
 
 /**
  * A version of {@link XboxController} with {@link Trigger} factories for command-based.
