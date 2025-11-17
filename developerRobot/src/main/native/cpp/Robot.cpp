02e2273df23b3098c84d39b13e9bbfc188356bd4
Thad House <thadhouse1@gmail.com>
2025-11-17 00:15:27 -0800

    Remove DeveloperRobot changes

--- developerRobot/src/main/native/cpp/Robot.cpp
+++ developerRobot/src/main/native/cpp/Robot.cpp
@@ -3,11 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "wpi/framework/TimedRobot.hpp"
-#include "wpi/driverstation/Gamepad.hpp"
 
 class Robot : public wpi::TimedRobot {
  public:
-  wpi::Gamepad m_gamepad{0};
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -42,25 +40,7 @@ class Robot : public wpi::TimedRobot {
   /**
    * This function is called periodically during all modes
    */
-  void RobotPeriodic() override {
-    double leftTrigger = m_gamepad.GetLeftTriggerAxis();
-    double rightTrigger = m_gamepad.GetRightTriggerAxis();
-
-    m_gamepad.SetRumble(wpi::GenericHID::kLeftRumble, leftTrigger);
-    m_gamepad.SetRumble(wpi::GenericHID::kRightRumble, rightTrigger);
-
-    if (m_gamepad.GetSouthFaceButton()) {
-      m_gamepad.SetLeds(0, 255, 0);  // Green
-    } else if (m_gamepad.GetNorthFaceButton()) {
-      m_gamepad.SetLeds(255, 0, 0);  // Red
-    } else if (m_gamepad.GetEastFaceButton()) {
-      m_gamepad.SetLeds(0, 0, 255);  // Blue
-    } else if (m_gamepad.GetWestFaceButton()) {
-      m_gamepad.SetLeds(255, 255, 0);  // Yellow
-    } else {
-      m_gamepad.SetLeds(0, 0, 0);  // Off
-    }
-  }
+  void RobotPeriodic() override {}
 };
 
 int main() {
