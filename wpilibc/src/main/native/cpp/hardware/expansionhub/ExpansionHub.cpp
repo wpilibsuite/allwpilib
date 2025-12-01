feea24251f850e14280494661742ce1aadd5d9f3
Peter Johnson <johnson.peter@gmail.com>
2025-12-01 12:47:01 -0800

    [wpilib] Move ExpansionHub to hardware/expansionhub (#8440)
    These need to be moved out of the org.wpilib root for Java
    modularization, and in general it's cleaner.

--- wpilibc/src/main/native/cpp/hardware/expansionhub/ExpansionHub.cpp
+++ wpilibc/src/main/native/cpp/hardware/expansionhub/ExpansionHub.cpp
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "wpi/ExpansionHub.hpp"
+#include "wpi/hardware/expansionhub/ExpansionHub.hpp"
 
 #include <memory>
 
-#include "wpi/ExpansionHubMotor.hpp"
-#include "wpi/ExpansionHubServo.hpp"
 #include "wpi/hal/UsageReporting.h"
+#include "wpi/hardware/expansionhub/ExpansionHubMotor.hpp"
+#include "wpi/hardware/expansionhub/ExpansionHubServo.hpp"
 #include "wpi/nt/BooleanTopic.hpp"
 #include "wpi/system/Errors.hpp"
 #include "wpi/system/SystemServer.hpp"
--- wpilibj/src/main/java/org/wpilib/hardware/expansionhub/ExpansionHubMotor.java
+++ wpilibj/src/main/java/org/wpilib/hardware/expansionhub/ExpansionHubMotor.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.wpilib;
+package org.wpilib.hardware.expansionhub;
 
 import static org.wpilib.units.Units.Amps;
 import static org.wpilib.units.Units.Volts;
