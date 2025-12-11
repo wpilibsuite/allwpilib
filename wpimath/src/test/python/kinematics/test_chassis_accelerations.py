ca460e09a655ad7993b47a61603d7bb90dfddd30
PJ Reiniger <pj.reiniger@gmail.com>
2025-12-11 13:09:12 -0500

    Add feet converters and __repr__

--- wpimath/src/test/python/kinematics/test_chassis_accelerations.py
+++ wpimath/src/test/python/kinematics/test_chassis_accelerations.py
@@ -2,8 +2,10 @@ import math
 import pytest
 from wpimath.kinematics import ChassisAccelerations
 from wpimath.geometry import Rotation2d
+from wpimath.units import feetToMeters
 
 K_EPSILON = 1e-9
+
 
 def test_default_constructor():
     accelerations = ChassisAccelerations()
@@ -11,6 +13,7 @@ def test_default_constructor():
     assert accelerations.ax == pytest.approx(0.0, abs=K_EPSILON)
     assert accelerations.ay == pytest.approx(0.0, abs=K_EPSILON)
     assert accelerations.alpha == pytest.approx(0.0, abs=K_EPSILON)
+
 
 def test_parameterized_constructor():
     # 1.0 m/s^2, 2.0 m/s^2, 3.0 rad/s^2
@@ -20,18 +23,20 @@ def test_parameterized_constructor():
     assert accelerations.ay == pytest.approx(2.0, abs=K_EPSILON)
     assert accelerations.alpha == pytest.approx(3.0, abs=K_EPSILON)
 
+
 def test_to_robot_relative():
     start_accel = ChassisAccelerations(1.0, 0.0, 0.5)
-    
+
     chassis_accelerations = start_accel.toRobotRelative(Rotation2d.fromDegrees(-90))
 
     assert chassis_accelerations.ax == pytest.approx(0.0, abs=K_EPSILON)
     assert chassis_accelerations.ay == pytest.approx(1.0, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(0.5, abs=K_EPSILON)
 
+
 def test_to_field_relative():
     start_accel = ChassisAccelerations(1.0, 0.0, 0.5)
-    
+
     chassis_accelerations = start_accel.toFieldRelative(Rotation2d.fromDegrees(45))
 
     expected_val = 1.0 / math.sqrt(2.0)
@@ -39,6 +44,7 @@ def test_to_field_relative():
     assert chassis_accelerations.ax == pytest.approx(expected_val, abs=K_EPSILON)
     assert chassis_accelerations.ay == pytest.approx(expected_val, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(0.5, abs=K_EPSILON)
+
 
 def test_plus():
     left = ChassisAccelerations(1.0, 0.5, 0.75)
@@ -50,6 +56,7 @@ def test_plus():
     assert chassis_accelerations.ay == pytest.approx(2.0, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(1.0, abs=K_EPSILON)
 
+
 def test_minus():
     left = ChassisAccelerations(1.0, 0.5, 0.75)
     right = ChassisAccelerations(2.0, 0.5, 0.25)
@@ -60,29 +67,53 @@ def test_minus():
     assert chassis_accelerations.ay == pytest.approx(0.0, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(0.5, abs=K_EPSILON)
 
+
 def test_unary_minus():
     accel = ChassisAccelerations(1.0, 0.5, 0.75)
-    
+
     chassis_accelerations = -accel
 
     assert chassis_accelerations.ax == pytest.approx(-1.0, abs=K_EPSILON)
     assert chassis_accelerations.ay == pytest.approx(-0.5, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(-0.75, abs=K_EPSILON)
 
+
 def test_multiplication():
     accel = ChassisAccelerations(1.0, 0.5, 0.75)
-    
+
     chassis_accelerations = accel * 2.0
 
     assert chassis_accelerations.ax == pytest.approx(2.0, abs=K_EPSILON)
     assert chassis_accelerations.ay == pytest.approx(1.0, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(1.5, abs=K_EPSILON)
 
+
 def test_division():
     accel = ChassisAccelerations(2.0, 1.0, 1.5)
-    
+
     chassis_accelerations = accel / 2.0
 
     assert chassis_accelerations.ax == pytest.approx(1.0, abs=K_EPSILON)
     assert chassis_accelerations.ay == pytest.approx(0.5, abs=K_EPSILON)
     assert chassis_accelerations.alpha == pytest.approx(0.75, abs=K_EPSILON)
+
+
+def test_feet_constructor():
+    chassis_accelerations = ChassisAccelerations.fromFpsSq(10, 11, math.radians(45))
+
+    assert chassis_accelerations.ax == pytest.approx(feetToMeters(10), abs=K_EPSILON)
+    assert chassis_accelerations.ay == pytest.approx(feetToMeters(11), abs=K_EPSILON)
+    assert chassis_accelerations.alpha == pytest.approx(math.radians(45), abs=K_EPSILON)
+
+    assert chassis_accelerations.ax_fpss == pytest.approx(10, abs=K_EPSILON)
+    assert chassis_accelerations.ay_fpss == pytest.approx(11, abs=K_EPSILON)
+    assert chassis_accelerations.alpha_dpss == pytest.approx(45, abs=K_EPSILON)
+
+
+def test_repr():
+    chassis_accelerations = ChassisAccelerations(1, 2, 3)
+
+    assert (
+        str(chassis_accelerations)
+        == "ChassisAccelerations(ax=1.000000, ay=2.000000, alpha=3.000000)"
+    )
--- wpimath/src/test/python/kinematics/test_differential_drive_wheel_accelerations.py
+++ wpimath/src/test/python/kinematics/test_differential_drive_wheel_accelerations.py
@@ -1,7 +1,9 @@
 import pytest
 from wpimath.kinematics import DifferentialDriveWheelAccelerations
+from wpimath.units import feetToMeters
 
 K_EPSILON = 1e-9
+
 
 def test_default_constructor():
     wheel_accelerations = DifferentialDriveWheelAccelerations()
@@ -9,11 +11,13 @@ def test_default_constructor():
     assert wheel_accelerations.left == pytest.approx(0.0, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(0.0, abs=K_EPSILON)
 
+
 def test_parameterized_constructor():
     wheel_accelerations = DifferentialDriveWheelAccelerations(1.5, 2.5)
 
     assert wheel_accelerations.left == pytest.approx(1.5, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(2.5, abs=K_EPSILON)
+
 
 def test_plus():
     left = DifferentialDriveWheelAccelerations(1.0, 0.5)
@@ -24,6 +28,7 @@ def test_plus():
     assert wheel_accelerations.left == pytest.approx(3.0, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(2.0, abs=K_EPSILON)
 
+
 def test_minus():
     left = DifferentialDriveWheelAccelerations(1.0, 0.5)
     right = DifferentialDriveWheelAccelerations(2.0, 0.5)
@@ -33,26 +38,48 @@ def test_minus():
     assert wheel_accelerations.left == pytest.approx(-1.0, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(0.0, abs=K_EPSILON)
 
+
 def test_unary_minus():
     accel = DifferentialDriveWheelAccelerations(1.0, 0.5)
-    
+
     wheel_accelerations = -accel
 
     assert wheel_accelerations.left == pytest.approx(-1.0, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(-0.5, abs=K_EPSILON)
 
+
 def test_multiplication():
     accel = DifferentialDriveWheelAccelerations(1.0, 0.5)
-    
+
     wheel_accelerations = accel * 2.0
 
     assert wheel_accelerations.left == pytest.approx(2.0, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(1.0, abs=K_EPSILON)
 
+
 def test_division():
     accel = DifferentialDriveWheelAccelerations(1.0, 0.5)
-    
+
     wheel_accelerations = accel / 2.0
 
     assert wheel_accelerations.left == pytest.approx(0.5, abs=K_EPSILON)
     assert wheel_accelerations.right == pytest.approx(0.25, abs=K_EPSILON)
+
+
+def test_feet_constructor():
+    accel = DifferentialDriveWheelAccelerations.fromFpsSq(10, 11)
+
+    assert accel.left == pytest.approx(feetToMeters(10), abs=K_EPSILON)
+    assert accel.right == pytest.approx(feetToMeters(11), abs=K_EPSILON)
+
+    assert accel.left_fpss == pytest.approx(10, abs=K_EPSILON)
+    assert accel.right_fpss == pytest.approx(11, abs=K_EPSILON)
+
+
+def test_repr():
+    accel = DifferentialDriveWheelAccelerations(1, 2)
+
+    assert (
+        str(accel)
+        == "DifferentialDriveWheelAccelerations(left=1.000000, right=2.000000)"
+    )
