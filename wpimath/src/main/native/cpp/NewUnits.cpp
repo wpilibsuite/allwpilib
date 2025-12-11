abff46d7aefa53605c4428cdee01331d102275b2
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2025-12-11 13:23:37 -0800

    Add short units test

---
+++ wpimath/src/main/native/cpp/NewUnits.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <units/curvature.h>
+
+using namespace wpi::units::literals;
+
+inline static constexpr wpi::units::curvature_t x = 1_rad / 1_m;
+
+static_assert(x.value() == 1.0);
