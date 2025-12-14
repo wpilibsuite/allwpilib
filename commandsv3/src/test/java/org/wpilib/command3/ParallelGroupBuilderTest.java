cda675c1d041101649824bdad9354ba888a543ec
Sam Carlberg <sam@slfc.dev>
2025-12-14 15:31:11 -0500

    Fix commandv3 builders and add tests
    Sequential group builder had an inverted if condition causing NPEs
    
    Parallel group builder was building parallel groups using an old ctor signature and creating groups that didn't match what was specified in the builder
    
    Test coverage has been added for both builder types

---
+++ commandsv3/src/test/java/org/wpilib/command3/ParallelGroupBuilderTest.java
@@ -0,0 +1,79 @@
+package org.wpilib.command3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class ParallelGroupBuilderTest {
+  @Test
+  void optionalVarargNullArray() {
+    var builder = new ParallelGroupBuilder();
+    assertThrows(NullPointerException.class, () -> builder.optional((Command[]) null));
+  }
+
+  @Test
+  void optionalNullInVarargs() {
+    var builder = new ParallelGroupBuilder();
+    assertThrows(NullPointerException.class, () -> builder.optional((Command) null));
+  }
+
+  @Test
+  void optionalPopulatesOptionalOnly() {
+    var a = Command.noRequirements().executing(Coroutine::park).named("A");
+    var b = Command.noRequirements().executing(Coroutine::park).named("B");
+
+    var group = new ParallelGroupBuilder().optional(a, b).withAutomaticName();
+
+    assertEquals(Set.of(a, b), group.getOptionalCommands());
+    assertTrue(group.getRequiredCommands().isEmpty());
+    assertEquals("(A | B)", group.name());
+  }
+
+  @Test
+  void requiringVarargNullArray() {
+    var builder = new ParallelGroupBuilder();
+    assertThrows(NullPointerException.class, () -> builder.requiring((Command[]) null));
+  }
+
+  @Test
+  void requiringNullInVarargs() {
+    var builder = new ParallelGroupBuilder();
+    assertThrows(NullPointerException.class, () -> builder.requiring((Command) null));
+  }
+
+  @Test
+  void requiringPopulatesRequiredOnly() {
+    var a = Command.noRequirements().executing(Coroutine::park).named("A");
+    var b = Command.noRequirements().executing(Coroutine::park).named("B");
+
+    var group = new ParallelGroupBuilder().requiring(a, b).withAutomaticName();
+
+    assertEquals(Set.of(a, b), group.getRequiredCommands());
+    assertTrue(group.getOptionalCommands().isEmpty());
+    assertEquals("(A & B)", group.name());
+  }
+
+  @Test
+  void mixedRequiredAndOptional() {
+    var reqA = Command.noRequirements().executing(Coroutine::park).named("ReqA");
+    var reqB = Command.noRequirements().executing(Coroutine::park).named("ReqB");
+    var optX = Command.noRequirements().executing(Coroutine::park).named("OptX");
+    var optY = Command.noRequirements().executing(Coroutine::park).named("OptY");
+
+    var group =
+        new ParallelGroupBuilder().requiring(reqA, reqB).optional(optX, optY).withAutomaticName();
+
+    assertEquals(Set.of(reqA, reqB), group.getRequiredCommands());
+    assertEquals(Set.of(optX, optY), group.getOptionalCommands());
+    assertEquals("[(ReqA & ReqB) * (OptX | OptY)]", group.name());
+  }
+
+  @Test
+  void namedWithNull() {
+    var builder = new ParallelGroupBuilder();
+    assertThrows(NullPointerException.class, () -> builder.named(null));
+  }
+}
