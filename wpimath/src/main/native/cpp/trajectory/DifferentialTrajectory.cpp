5a017fe8059e833c362cd09bb2af511a8447db06
Zach Harel <zach@zharel.me>
2025-12-24 19:53:09 -0500

    [wpimath] Add DifferentialTrajectory class for differential drive-specific trajectory interpolation and operations

---
+++ wpimath/src/main/native/cpp/trajectory/DifferentialTrajectory.cpp
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "wpi/math/trajectory/DifferentialTrajectory.hpp"
+
+#include "wpi/util/MathExtras.hpp"
+
+using namespace wpi::math;
+
+DifferentialSample DifferentialTrajectory::Interpolate(
+    const DifferentialSample& start, const DifferentialSample& end,
+    double t) const {
+  // Use kinematic interpolation for base TrajectorySample fields
+  auto baseSample = KinematicInterpolate(start, end, t);
+
+  // Interpolate wheel speeds
+  auto leftSpeed = wpi::util::Lerp(start.leftSpeed, end.leftSpeed, t);
+  auto rightSpeed = wpi::util::Lerp(start.rightSpeed, end.rightSpeed, t);
+
+  return DifferentialSample(baseSample.timestamp, baseSample.pose,
+                            baseSample.velocity, baseSample.acceleration,
+                            leftSpeed, rightSpeed);
+}
+
+DifferentialTrajectory DifferentialTrajectory::TransformBy(
+    const Transform2d& transform) const {
+  const Pose2d& firstPose = Start().pose;
+  Pose2d transformedFirstPose = firstPose.TransformBy(transform);
+
+  std::vector<DifferentialSample> transformedSamples;
+  transformedSamples.reserve(m_samples.size());
+
+  // Transform first sample
+  transformedSamples.push_back(DifferentialSample(
+      Start().timestamp, transformedFirstPose, Start().velocity,
+      Start().acceleration, Start().leftSpeed, Start().rightSpeed));
+
+  // Transform remaining samples
+  for (size_t i = 1; i < m_samples.size(); ++i) {
+    const auto& sample = m_samples[i];
+    transformedSamples.push_back(DifferentialSample(
+        sample.timestamp, transformedFirstPose + (sample.pose - firstPose),
+        sample.velocity, sample.acceleration, sample.leftSpeed,
+        sample.rightSpeed));
+  }
+
+  return DifferentialTrajectory(std::move(transformedSamples));
+}
+
+DifferentialTrajectory DifferentialTrajectory::RelativeTo(
+    const Pose2d& pose) const {
+  std::vector<DifferentialSample> relativeSamples;
+  relativeSamples.reserve(m_samples.size());
+
+  for (const auto& sample : m_samples) {
+    relativeSamples.push_back(sample.RelativeTo(pose));
+  }
+
+  return DifferentialTrajectory(std::move(relativeSamples));
+}
+
+DifferentialTrajectory DifferentialTrajectory::Concatenate(
+    const Trajectory<DifferentialSample, DifferentialTrajectory>& other) const {
+  if (other.GetSamples().empty()) {
+    return *this;
+  }
+
+  std::vector<DifferentialSample> combined;
+  combined.reserve(m_samples.size() + other.GetSamples().size());
+
+  // Add this trajectory's samples
+  combined.insert(combined.end(), m_samples.begin(), m_samples.end());
+
+  // Add other trajectory's samples with adjusted timestamps
+  for (const auto& sample : other.GetSamples()) {
+    combined.push_back(sample.WithNewTimestamp(sample.timestamp + m_duration));
+  }
+
+  return DifferentialTrajectory(std::move(combined));
+}
---
+++ wpimath/src/main/native/include/wpi/math/trajectory/DifferentialTrajectory.hpp
@@ -0,0 +1,114 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <vector>
+
+#include "wpi/math/geometry/Pose2d.hpp"
+#include "wpi/math/geometry/Rotation2d.hpp"
+#include "wpi/math/geometry/Transform2d.hpp"
+#include "wpi/math/kinematics/ChassisAccelerations.hpp"
+#include "wpi/math/kinematics/ChassisSpeeds.hpp"
+#include "wpi/math/kinematics/DifferentialDriveKinematics.hpp"
+#include "wpi/math/trajectory/DifferentialSample.hpp"
+#include "wpi/math/trajectory/Trajectory.hpp"
+#include "wpi/util/MathExtras.hpp"
+#include "wpi/util/SymbolExports.hpp"
+
+namespace wpi::math {
+
+/**
+ * A trajectory for differential drive robots with drivetrain-specific
+ * interpolation.
+ *
+ * This trajectory uses numerical integration of the differential drive
+ * differential equation for accurate interpolation.
+ */
+class WPILIB_DLLEXPORT DifferentialTrajectory
+    : public Trajectory<DifferentialSample, DifferentialTrajectory> {
+ public:
+  /**
+   * Constructs a DifferentialTrajectory from a vector of samples.
+   *
+   * @param samples The samples of the trajectory. Order does not matter as
+   *                they will be sorted internally.
+   * @throws std::invalid_argument if the vector of samples is empty.
+   */
+  explicit DifferentialTrajectory(std::vector<DifferentialSample> samples)
+      : Trajectory<DifferentialSample, DifferentialTrajectory>(
+            std::move(samples)) {}
+
+  /**
+   * Constructs a DifferentialTrajectory from a vector of generic samples
+   * and kinematics.
+   *
+   * @param kinematics The kinematics of the drivetrain.
+   * @param samples The samples to convert.
+   */
+  template <typename SampleType>
+  DifferentialTrajectory(const DifferentialDriveKinematics& kinematics,
+                         const std::vector<SampleType>& samples) {
+    std::vector<DifferentialSample> diffSamples;
+    diffSamples.reserve(samples.size());
+
+    for (const auto& sample : samples) {
+      diffSamples.emplace_back(sample, kinematics);
+    }
+
+    *this = DifferentialTrajectory(std::move(diffSamples));
+  }
+
+  /**
+   * Interpolates between two samples using numerical integration of the
+   * differential drive differential equation.
+   *
+   * @param start The starting sample.
+   * @param end The ending sample.
+   * @param t The interpolation parameter between 0 and 1.
+   * @return The interpolated sample.
+   */
+  DifferentialSample Interpolate(const DifferentialSample& start,
+                                 const DifferentialSample& end,
+                                 double t) const override;
+
+  /**
+   * Transforms all poses in the trajectory by the given transform.
+   *
+   * @param transform The transform to apply to the poses.
+   * @return The transformed trajectory.
+   */
+  DifferentialTrajectory TransformBy(
+      const Transform2d& transform) const override;
+
+  /**
+   * Transforms all poses so they are relative to the given pose.
+   *
+   * @param pose The pose to make the trajectory relative to.
+   * @return The transformed trajectory.
+   */
+  DifferentialTrajectory RelativeTo(const Pose2d& pose) const override;
+
+  /**
+   * Concatenates another trajectory to this trajectory.
+   *
+   * @param other The trajectory to concatenate.
+   * @return The concatenated trajectory.
+   */
+  DifferentialTrajectory Concatenate(
+      const Trajectory<DifferentialSample, DifferentialTrajectory>& other)
+      const override;
+
+  /**
+   * Concatenates another trajectory to this trajectory.
+   *
+   * @param other The trajectory to concatenate.
+   * @return The concatenated trajectory.
+   */
+  DifferentialTrajectory operator+(const DifferentialTrajectory& other) const {
+    return Concatenate(other);
+  }
+};
+
+}  // namespace wpi::math
