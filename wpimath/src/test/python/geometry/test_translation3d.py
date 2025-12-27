325b676ab155a1c4af2e9164237cf7c13a1b9473
PJ Reiniger <pj.reiniger@gmail.com>
2025-12-27 00:52:18 -0500

    Remove named args for translation, removed unused import

--- wpimath/src/test/python/geometry/test_translation3d.py
+++ wpimath/src/test/python/geometry/test_translation3d.py
@@ -10,8 +10,8 @@ from wpimath.geometry import (
 
 
 def test_sum():
-    one = Translation3d(x=1, y=3, z=5)
-    two = Translation3d(x=2, y=5, z=8)
+    one = Translation3d(1, 3, 5)
+    two = Translation3d(2, 5, 8)
 
     sum_ = one + two
 
@@ -21,8 +21,8 @@ def test_sum():
 
 
 def test_difference():
-    one = Translation3d(x=1, y=3, z=5)
-    two = Translation3d(x=2, y=5, z=8)
+    one = Translation3d(1, 3, 5)
+    two = Translation3d(2, 5, 8)
 
     difference = one - two
 
@@ -36,7 +36,7 @@ def test_rotate_by():
     y_axis = np.array([0.0, 1.0, 0.0])
     z_axis = np.array([0.0, 0.0, 1.0])
 
-    translation = Translation3d(x=1, y=2, z=3)
+    translation = Translation3d(1, 2, 3)
 
     rotated1 = translation.rotateBy(Rotation3d(x_axis, math.radians(90)))
     assert rotated1.x == pytest.approx(1.0, abs=1e-9)
@@ -59,8 +59,8 @@ def test_rotate_around():
     y_axis = np.array([0.0, 1.0, 0.0])
     z_axis = np.array([0.0, 0.0, 1.0])
 
-    translation = Translation3d(x=1, y=2, z=3)
-    around = Translation3d(x=3, y=2, z=1)
+    translation = Translation3d(1, 2, 3)
+    around = Translation3d(3, 2, 1)
 
     rotated1 = translation.rotateAround(around, Rotation3d(x_axis, math.radians(90)))
     assert rotated1.x == pytest.approx(1.0, abs=1e-9)
@@ -79,14 +79,14 @@ def test_rotate_around():
 
 
 def test_to_translation2d():
-    translation = Translation3d(x=1, y=2, z=3)
-    expected = Translation2d(x=1, y=2)
+    translation = Translation3d(1, 2, 3)
+    expected = Translation2d(1, 2)
 
     assert expected == translation.toTranslation2d()
 
 
 def test_multiplication():
-    original = Translation3d(x=3, y=5, z=7)
+    original = Translation3d(3, 5, 7)
     mult = original * 3
 
     assert mult.x == pytest.approx(9.0, abs=1e-9)
@@ -95,7 +95,7 @@ def test_multiplication():
 
 
 def test_division():
-    original = Translation3d(x=3, y=5, z=7)
+    original = Translation3d(3, 5, 7)
     div = original / 2
 
     assert div.x == pytest.approx(1.5, abs=1e-9)
@@ -104,29 +104,29 @@ def test_division():
 
 
 def test_norm():
-    one = Translation3d(x=3, y=5, z=7)
+    one = Translation3d(3, 5, 7)
     assert one.norm() == pytest.approx(math.hypot(3, 5, 7), abs=1e-9)
 
 
 def test_squared_norm():
-    one = Translation3d(x=3, y=5, z=7)
+    one = Translation3d(3, 5, 7)
     assert one.squaredNorm() == pytest.approx(83.0, abs=1e-9)
 
 
 def test_distance():
-    one = Translation3d(x=1, y=1, z=1)
-    two = Translation3d(x=6, y=6, z=6)
+    one = Translation3d(1, 1, 1)
+    two = Translation3d(6, 6, 6)
     assert one.distance(two) == pytest.approx(5 * math.sqrt(3), abs=1e-9)
 
 
 def test_squared_distance():
-    one = Translation3d(x=1, y=1, z=1)
-    two = Translation3d(x=6, y=6, z=6)
+    one = Translation3d(1, 1, 1)
+    two = Translation3d(6, 6, 6)
     assert one.squaredDistance(two) == pytest.approx(75.0, abs=1e-9)
 
 
 def test_unary_minus():
-    original = Translation3d(x=-4.5, y=7, z=9)
+    original = Translation3d(-4.5, 7, 9)
     inverted = -original
 
     assert inverted.x == pytest.approx(4.5, abs=1e-9)
@@ -135,14 +135,14 @@ def test_unary_minus():
 
 
 def test_equality():
-    one = Translation3d(x=9, y=5.5, z=3.5)
-    two = Translation3d(x=9, y=5.5, z=3.5)
+    one = Translation3d(9, 5.5, 3.5)
+    two = Translation3d(9, 5.5, 3.5)
     assert one == two
 
 
 def test_inequality():
-    one = Translation3d(x=9, y=5.5, z=3.5)
-    two = Translation3d(x=9, y=5.7, z=3.5)
+    one = Translation3d(9, 5.5, 3.5)
+    two = Translation3d(9, 5.7, 3.5)
     assert one != two
 
 
@@ -172,16 +172,16 @@ def test_to_vector():
 
 
 def test_nearest():
-    origin = Translation3d(x=0, y=0, z=0)
+    origin = Translation3d(0, 0, 0)
 
     # Distance sort
     # translations are in order of closest to farthest away from the origin at
     # various positions in 3D space.
-    translation1 = Translation3d(x=1, y=0, z=0)
-    translation2 = Translation3d(x=0, y=2, z=0)
-    translation3 = Translation3d(x=0, y=0, z=3)
-    translation4 = Translation3d(x=2, y=2, z=2)
-    translation5 = Translation3d(x=3, y=3, z=3)
+    translation1 = Translation3d(1, 0, 0)
+    translation2 = Translation3d(0, 2, 0)
+    translation3 = Translation3d(0, 0, 3)
+    translation4 = Translation3d(2, 2, 2)
+    translation5 = Translation3d(3, 3, 3)
 
     nearest1 = origin.nearest([translation5, translation3, translation4])
     assert nearest1.x == pytest.approx(translation3.x)
@@ -200,6 +200,6 @@ def test_nearest():
 
 
 def test_dot():
-    one = Translation3d(x=1, y=2, z=3)
-    two = Translation3d(x=4, y=5, z=6)
+    one = Translation3d(1, 2, 3)
+    two = Translation3d(4, 5, 6)
     assert one.dot(two) == pytest.approx(32.0, abs=1e-9)
