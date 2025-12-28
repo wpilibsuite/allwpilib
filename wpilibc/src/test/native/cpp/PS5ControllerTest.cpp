b89e5f7090dfad36531d20bd7be69d84ca60d3f2
Thad House <thadhouse1@gmail.com>
2025-12-27 16:52:45 -0800

    Fix sim tests

--- wpilibc/src/test/native/cpp/PS5ControllerTest.cpp
+++ wpilibc/src/test/native/cpp/PS5ControllerTest.cpp
@@ -12,28 +12,28 @@
 using namespace wpi;
 using namespace wpi::nids;
 
-BUTTON_TEST(PS5Controller, SquareButton)
-BUTTON_TEST(PS5Controller, CrossButton)
-BUTTON_TEST(PS5Controller, CircleButton)
-BUTTON_TEST(PS5Controller, TriangleButton)
+BUTTON_TEST_NIDS(PS5Controller, SquareButton)
+BUTTON_TEST_NIDS(PS5Controller, CrossButton)
+BUTTON_TEST_NIDS(PS5Controller, CircleButton)
+BUTTON_TEST_NIDS(PS5Controller, TriangleButton)
 
-BUTTON_TEST(PS5Controller, L1Button)
-BUTTON_TEST(PS5Controller, R1Button)
-BUTTON_TEST(PS5Controller, L2Button)
-BUTTON_TEST(PS5Controller, R2Button)
+BUTTON_TEST_NIDS(PS5Controller, L1Button)
+BUTTON_TEST_NIDS(PS5Controller, R1Button)
+BUTTON_TEST_NIDS(PS5Controller, L2Button)
+BUTTON_TEST_NIDS(PS5Controller, R2Button)
 
-BUTTON_TEST(PS5Controller, CreateButton)
-BUTTON_TEST(PS5Controller, OptionsButton)
+BUTTON_TEST_NIDS(PS5Controller, CreateButton)
+BUTTON_TEST_NIDS(PS5Controller, OptionsButton)
 
-BUTTON_TEST(PS5Controller, L3Button)
-BUTTON_TEST(PS5Controller, R3Button)
+BUTTON_TEST_NIDS(PS5Controller, L3Button)
+BUTTON_TEST_NIDS(PS5Controller, R3Button)
 
-BUTTON_TEST(PS5Controller, PSButton)
-BUTTON_TEST(PS5Controller, TouchpadButton)
+BUTTON_TEST_NIDS(PS5Controller, PSButton)
+BUTTON_TEST_NIDS(PS5Controller, TouchpadButton)
 
-AXIS_TEST(PS5Controller, LeftX)
-AXIS_TEST(PS5Controller, RightX)
-AXIS_TEST(PS5Controller, LeftY)
-AXIS_TEST(PS5Controller, RightY)
-AXIS_TEST(PS5Controller, L2Axis)
-AXIS_TEST(PS5Controller, R2Axis)
+AXIS_TEST_NIDS(PS5Controller, LeftX)
+AXIS_TEST_NIDS(PS5Controller, RightX)
+AXIS_TEST_NIDS(PS5Controller, LeftY)
+AXIS_TEST_NIDS(PS5Controller, RightY)
+AXIS_TEST_NIDS(PS5Controller, L2Axis)
+AXIS_TEST_NIDS(PS5Controller, R2Axis)
