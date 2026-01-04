0e4f7db7913256717b1fe864801321fa274493ad
Austin Schuh <austin.linux@gmail.com>
2026-01-03 18:47:31 -0800

    Roll back rules_java

--- epilogue-processor/src/test/java/org/wpilib/epilogue/processor/AnnotationProcessorTest.java
+++ epilogue-processor/src/test/java/org/wpilib/epilogue/processor/AnnotationProcessorTest.java
@@ -2300,7 +2300,7 @@ class AnnotationProcessorTest {
                 JavaFileObjects.forSourceString("example.package-info", packageInfo));
 
     assertThat(compilation).succeeded();
-    var unused = compilation.generatedSourceFiles().stream()
+    compilation.generatedSourceFiles().stream()
         .filter(jfo -> jfo.getName().contains("Example"))
         .findFirst()
         .orElseThrow(() -> new IllegalStateException("Logger file was not generated!"));
