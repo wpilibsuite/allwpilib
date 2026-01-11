92d96ff18590c4bd87ae44f72ca642ac352227a9
Charlotte Wilson <251223230+cswilson252@users.noreply.github.com>
2026-01-10 21:56:10 -0500

    ...

--- xrpVendordep/src/main/native/cpp/xrp/XRPGyro.cpp
+++ xrpVendordep/src/main/native/cpp/xrp/XRPGyro.cpp
@@ -29,7 +29,8 @@ XRPGyro::XRPGyro() : m_simDevice("Gyro:XRPGyro") {
     m_simAngleZ =
         m_simDevice.CreateDouble("angle_z", hal::SimDevice::kInput, 0.0);
   }
-  fmt::print("Name of connected device: ", wpi::hal::SimDevice::GetName());
+  fmt::print("Name of connected device: ",
+             (std::string wpi::hal::SimDevice::GetName() const;));
   fmt::print("Robot Enable Status: ", wpi::DriverStation::IsEnabled());
   fmt::print("E-Stop Status: ", wpi::DriverStation::IsEStopped());
 }
