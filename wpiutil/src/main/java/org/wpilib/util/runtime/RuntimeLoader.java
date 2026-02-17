2a0f53135651cb9bf3333f131abdb1726233a0b2
Thad House <thadhouse1@gmail.com>
2026-02-17 15:04:25 -0800

    Remove CombinedRuntimeLoader
    It was only ever for Java tools, which we do not support anymore

--- wpiutil/src/main/java/org/wpilib/util/runtime/RuntimeLoader.java
+++ wpiutil/src/main/java/org/wpilib/util/runtime/RuntimeLoader.java
@@ -4,10 +4,53 @@
 
 package org.wpilib.util.runtime;
 
+import java.io.File;
 import java.io.IOException;
 
 /** Loads a native library at runtime. */
 public final class RuntimeLoader {
+  /**
+   * Returns platform name.
+   *
+   * @return The current platform name.
+   * @throws IllegalStateException Thrown if the operating system is unknown.
+   */
+  public static String getPlatformName() {
+    String filePath;
+    String arch = System.getProperty("os.arch");
+
+    boolean intel32 = "x86".equals(arch) || "i386".equals(arch);
+    boolean intel64 = "amd64".equals(arch) || "x86_64".equals(arch);
+
+    if (System.getProperty("os.name").startsWith("Windows")) {
+      if (intel32) {
+        filePath = "windows-x86";
+      } else {
+        filePath = "windows-x86-64";
+      }
+    } else if (System.getProperty("os.name").startsWith("Mac")) {
+      filePath = "osx-universal";
+    } else if (System.getProperty("os.name").startsWith("Linux")) {
+      if (intel32) {
+        filePath = "linux-x86";
+      } else if (intel64) {
+        filePath = "linux-x86-64";
+      } else if (new File("/usr/local/frc/bin/frcRunRobot.sh").exists()) {
+        filePath = "linux-athena";
+      } else if ("arm".equals(arch) || "arm32".equals(arch)) {
+        filePath = "linux-arm32";
+      } else if ("aarch64".equals(arch) || "arm64".equals(arch)) {
+        filePath = "linux-arm64";
+      } else {
+        filePath = "linux-nativearm";
+      }
+    } else {
+      throw new IllegalStateException();
+    }
+
+    return filePath;
+  }
+
   /**
    * Returns a load error message given the information in the provided UnsatisfiedLinkError.
    *
@@ -20,7 +63,7 @@ public final class RuntimeLoader {
     StringBuilder msg = new StringBuilder(512);
     msg.append(libraryName)
         .append(" could not be loaded from path.\n" + "\tattempted to load for platform ")
-        .append(CombinedRuntimeLoader.getPlatformPath())
+        .append(getPlatformName())
         .append("\nLast Load Error: \n")
         .append(ule.getMessage())
         .append('\n')
