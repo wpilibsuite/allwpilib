c3c0b7d8819e5decb834178862e74a9635957dd1
Thad House <thadhouse1@gmail.com>
2026-02-18 14:51:59 -0800

    Add to backend

--- wpilibj/src/main/java/org/wpilib/driverstation/DriverStationBase.java
+++ wpilibj/src/main/java/org/wpilib/driverstation/DriverStationBase.java
@@ -1,5 +1,21 @@
 package org.wpilib.driverstation;
 
-public abstract class DriverStationBase {
+import java.util.Optional;
+import java.util.OptionalInt;
 
+import org.wpilib.driverstation.backend.DriverStationBackend;
+import org.wpilib.driverstation.backend.DriverStationBackend.Alliance;
+
+public abstract class DriverStationBase {
+  public Optional<Alliance> getAlliance() {
+    return DriverStationBackend.getAlliance();
+  }
+
+  public OptionalInt getLocation() {
+    return DriverStationBackend.getLocation();
+  }
+
+  public Optional<String> getGameData() {
+    return DriverStationBackend.getGameData();
+  }
 }
