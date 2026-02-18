24fa744d3d943034e10d66da45558ad362cd5051
Thad House <thadhouse1@gmail.com>
2026-02-18 14:48:28 -0800

    Add gamepads to default

--- wpilibj/src/main/java/org/wpilib/driverstation/DefaultDriverStation.java
+++ wpilibj/src/main/java/org/wpilib/driverstation/DefaultDriverStation.java
@@ -1,5 +1,18 @@
 package org.wpilib.driverstation;
 
-public class DefaultDriverStation extends DriverStationBase {
+import org.wpilib.driverstation.backend.DriverStationBackend;
 
+public class DefaultDriverStation extends DriverStationBase {
+    private final Gamepad[] m_gamepads;
+
+    public DefaultDriverStation() {
+        m_gamepads = new Gamepad[DriverStationBackend.kJoystickPorts];
+        for (int i = 0; i < m_gamepads.length; i++) {
+            m_gamepads[i] = new Gamepad(i);
+        }
+    }
+
+    public Gamepad getGamepad(int port) {
+        return m_gamepads[port];
+    }
 }
