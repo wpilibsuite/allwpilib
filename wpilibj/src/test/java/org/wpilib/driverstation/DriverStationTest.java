12cb1f98240c1daeccb20af75decb0666f169aa8
Thad House <thadhouse1@gmail.com>
2026-02-19 18:53:53 -0800

    Fix DS test

--- wpilibj/src/test/java/org/wpilib/driverstation/DriverStationTest.java
+++ wpilibj/src/test/java/org/wpilib/driverstation/DriverStationTest.java
@@ -11,6 +11,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.wpilib.driverstation.internal.DriverStationBackend;
 import org.wpilib.simulation.DriverStationSim;
 
 class DriverStationTest {
