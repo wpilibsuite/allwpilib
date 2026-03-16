531b3b45df5ca83915f5c0d68c1f2ee557a42fca
Matt Morley <matthew.morley.ca@gmail.com>
2026-03-16 08:23:33 -0700

    Failing test

---
+++ wpinet/src/test/java/org/wpilib/net/MulticastServiceAnnouncerTest.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.wpilib.net;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MulticastServiceAnnouncerTest {
+
+    @Test
+    public void emptyText() throws InterruptedException, UnknownHostException {
+        final String serviceName = "TestServiceNoText";
+        final String serviceType = "_wpinotxt._tcp";
+        final int port = new Random().nextInt(65535);
+
+        try (MulticastServiceAnnouncer announcer =
+                    new MulticastServiceAnnouncer(serviceName, serviceType, port);
+             MulticastServiceResolver resolver =
+                    new MulticastServiceResolver(serviceType)) {
+
+            assertTrue(announcer.hasImplementation() && resolver.hasImplementation());
+
+            announcer.start();
+            resolver.start();
+
+            List<ServiceData> allData = new ArrayList<>();
+
+            for (int i = 0; i < 15; i++) {
+                ServiceData[] data = resolver.getData();
+                if (data == null) {
+                    System.err.println("null data");
+                    continue;
+                }
+
+                System.out.printf("Got %d data%n", data.length);
+
+                for (ServiceData it : data) {
+                    String ipString = InetAddress.getByAddress(
+                        ByteBuffer.allocate(4).putInt((int) it.getIpv4Address()).array()
+                    ).getHostAddress();
+
+                    System.out.printf("service %s at host %s ipv4 %s%n",
+                            it.getServiceName(), it.getHostName(), ipString);
+                    allData.add(it);
+                }
+
+                if (!allData.isEmpty()) {
+                    break;
+                }
+
+                Thread.sleep(1000);
+            }
+
+            System.out.printf("Ending with %d%n", allData.size());
+            assertFalse(allData.isEmpty(),
+                    "Expected at least one service entry, but got none");
+
+            resolver.stop();
+            announcer.stop();
+        }
+    }
+}
