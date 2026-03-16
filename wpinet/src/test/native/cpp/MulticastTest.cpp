33b168026fa03fd89446f19d7a8219a7e8c0bb12
Matt Morley <matthew.morley.ca@gmail.com>
2026-03-16 08:12:32 -0700

    Add new test to make sure things actually work

--- wpinet/src/test/native/cpp/MulticastTest.cpp
+++ wpinet/src/test/native/cpp/MulticastTest.cpp
@@ -8,6 +8,7 @@
 #include <string_view>
 #include <thread>
 #include <utility>
+#include <arpa/inet.h>
 
 #include <gtest/gtest.h>
 
@@ -17,7 +18,7 @@
 
 TEST(MulticastServiceAnnouncerTest, EmptyText) {
   const std::string_view serviceName = "TestServiceNoText";
-  const std::string_view serviceType = "_wpinotxt";
+  const std::string_view serviceType = "_wpinotxt._tcp";
   const int port = std::rand();
   wpi::net::MulticastServiceAnnouncer announcer(serviceName, serviceType, port);
   wpi::net::MulticastServiceResolver resolver(serviceType);
@@ -26,7 +27,33 @@ TEST(MulticastServiceAnnouncerTest, EmptyText) {
     announcer.Start();
     resolver.Start();
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::vector<wpi::net::MulticastServiceResolver::ServiceData> allData {};
+
+    for (int i = 0; i < 15; i++) {
+      auto data = resolver.GetData();
+      
+      printf("Got %lu data\n", data.size());
+      for (const auto& it : data) {
+        char ip_string[INET_ADDRSTRLEN];
+        struct in_addr ip_addr;
+        ip_addr.s_addr = htonl(it.ipv4Address); 
+        inet_ntop(AF_INET, &ip_addr, ip_string, INET_ADDRSTRLEN);
+
+        printf("service %s at host %s ipv4 %s\n", it.serviceName.c_str(), 
+            it.hostName.c_str(), ip_string);
+
+        allData.push_back(it);
+      }
+
+      if (!allData.empty()) {
+        break;
+      }
+
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    printf("Ending with %lu\n", allData.size());
+    ASSERT_GT(allData.size(), 0ul);
 
     resolver.Stop();
     announcer.Stop();
