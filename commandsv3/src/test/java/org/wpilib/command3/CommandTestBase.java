466d449854c4ae9e8c52f95be540ffd048a01f3a
Sam Carlberg <sam@slfc.dev>
2026-03-25 10:52:41 -0400

    Enforce command lifetimes across all opmode and command scopes
    Commands are no longer able to outlive their schedule-site's scope, regardless of how they were scheduled (set as a default command, bound to a trigger, or manually scheduled)
    
    As a consequence, default commands need better tracking so the default command setting can be released when their scope exits and the next-most appropriate default command can be rescheduled (eg, an opmode sets a default command, then the globally-scoped default is restored when the opmode exits). Some complexity is required here to make it work well for edge cases.

--- commandsv3/src/test/java/org/wpilib/command3/CommandTestBase.java
+++ commandsv3/src/test/java/org/wpilib/command3/CommandTestBase.java
@@ -6,12 +6,15 @@ package org.wpilib.command3;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.wpilib.system.RobotController;
 
 class CommandTestBase {
   protected Scheduler m_scheduler;
   protected List<SchedulerEvent> m_events;
+  protected long m_opModeId = 0;
+  protected String m_opModeName = "";
 
   @BeforeEach
   void initScheduler() {
@@ -19,5 +22,27 @@ class CommandTestBase {
     m_scheduler = Scheduler.createIndependentScheduler();
     m_events = new ArrayList<>();
     m_scheduler.addEventListener(m_events::add);
+  }
+
+  @BeforeEach
+  void initOpmodeFetcher() {
+    OpModeFetcher.setFetcher(
+        new OpModeFetcher() {
+          @Override
+          long getOpModeId() {
+            return m_opModeId;
+          }
+
+          @Override
+          String getOpModeName() {
+            return m_opModeName;
+          }
+        });
+  }
+
+  @AfterEach
+  void resetOpmodeFetcher() {
+    m_opModeId = 0;
+    m_opModeName = "";
   }
 }
