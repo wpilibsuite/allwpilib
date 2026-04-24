429aadef7b0fd6d1abacc7df91e5cbde5364545c
ハイドラ <thecomputer314@gmail.com>
2026-04-23 20:53:00 -0500

    Write Javadocs for DoubleFollowerWheelOdometry and DoubleFollowerWheelOdometry3d

--- wpimath/src/main/java/org/wpilib/math/kinematics/DoubleFollowerWheelOdometry.java
+++ wpimath/src/main/java/org/wpilib/math/kinematics/DoubleFollowerWheelOdometry.java
@@ -3,7 +3,25 @@ package org.wpilib.math.kinematics;
 import org.wpilib.math.geometry.Pose2d;
 import org.wpilib.math.geometry.Rotation2d;
 
+// TODO: Should I do an impl that doesn't extend Odometry<> since the super methods that take
+//  in redundant gyro angles could be confusing?
+//  But also that would be duplicated code for similar purposes
+//  Maybe it's fine since the formulation would be different enough?
+/**
+ * Class for double follower wheel odometry. Odometry allows you to track the robot's position on
+ * the field over the course of a match using readings from 2 encoders and a gyroscope.
+ *
+ * <p>Teams can use odometry during the autonomous period for complex tasks like path following.
+ * Furthermore, odometry can be used for latency compensation when using computer-vision systems.
+ */
 public class DoubleFollowerWheelOdometry extends Odometry<DoubleFollowerWheelPositions> {
+  /**
+   * Constructs a DoubleFollowerWheelOdometry object.
+   * @param kinematics The double follower wheel kinematics.
+   * @param wheelPositions The distances driven by each wheel and the angle reported by the
+   *                       gyroscope.
+   * @param initialPose The starting position of the robot on the field.
+   */
   public DoubleFollowerWheelOdometry(
       DoubleFollowerWheelKinematics kinematics,
       DoubleFollowerWheelPositions wheelPositions,
@@ -11,17 +29,50 @@ public class DoubleFollowerWheelOdometry extends Odometry<DoubleFollowerWheelPos
     super(kinematics, wheelPositions.theta, wheelPositions, initialPose);
   }
 
-  // TODO: Should I do an impl that doesn't extend Odometry<> since the super methods that take
-  //  in redundant gyro angles could be confusing?
-  //  But also that would be duplicated, redundant code
+  /**
+   * Constructs a DoubleFollowerWheelOdometry object.
+   * @param kinematics The double follower wheel kinematics for your drivetrain.
+   * @param gyroAngle The angle reported by the gyroscope.
+   * @param x The distance traveled by the forward-facing wheel in meters.
+   * @param y The distance traveled by the left-facing wheel in meters.
+   * @param initialPose The starting position of the robot on the field.
+   */
+  public DoubleFollowerWheelOdometry(DoubleFollowerWheelKinematics kinematics,
+                                     Rotation2d gyroAngle, double x, double y, Pose2d initialPose) {
+    this(kinematics, new DoubleFollowerWheelPositions(x, y, gyroAngle), initialPose);
+  }
+
+  /**
+   * Resets the robot's position on the field.
+   *
+   * <p>The gyroscope angle does not need to be reset here on the user's robot code. The library
+   * automatically takes care of offsetting the gyro angle.
+   * @param wheelPositions The current encoder and gyroscope readings.
+   * @param pose The position on the field that your robot is at.
+   */
   public void resetPosition(DoubleFollowerWheelPositions wheelPositions, Pose2d pose) {
     super.resetPosition(wheelPositions.theta, wheelPositions, pose);
   }
 
+  /**
+   * Updates the robot's position on the field using forward kinematics and integration of the pose
+   * over time.
+   *
+   * @param wheelPositions The current encoder and gyroscope readings.
+   * @return The new pose of the robot.
+   */
   public Pose2d update(DoubleFollowerWheelPositions wheelPositions) {
     return super.update(wheelPositions.theta, wheelPositions);
   }
 
+  /**
+   * Updates the robot's position on the field using forward kinematics and integration of the pose
+   * over time.
+   * @param x The distance traveled by the forward-facing wheel in meters.
+   * @param y The distance traveled by the left-facing wheel in meters.
+   * @param gyroAngle The angle reported by the gyroscope.
+   * @return The new pose of the robot.
+   */
   public Pose2d update(double x, double y, Rotation2d gyroAngle) {
     return update(new DoubleFollowerWheelPositions(x, y, gyroAngle));
   }
--- wpimath/src/main/java/org/wpilib/math/kinematics/DoubleFollowerWheelOdometry3d.java
+++ wpimath/src/main/java/org/wpilib/math/kinematics/DoubleFollowerWheelOdometry3d.java
@@ -1,9 +1,28 @@
 package org.wpilib.math.kinematics;
 
-import org.wpilib.math.geometry.Pose3d;
-import org.wpilib.math.geometry.Rotation3d;
+import org.wpilib.math.geometry.*;
 
+/**
+ * Class for double follower wheel odometry. Odometry allows you to track the robot's position on
+ * the field over a course of a match using course of a match using readings from 2 encoders and a
+ * gyroscope.
+ *
+ * <p>This class is meant to be an easy replacement for {@link DoubleFollowerWheelOdometry}, only
+ * requiring the addition of appropriate conversions between 2D and 3D versions of geometry
+ * classes. (See {@link Pose3d#Pose3d(Pose2d)}, {@link Rotation3d#Rotation3d(Rotation2d)}, {@link
+ * Translation3d#Translation3d(Translation2d)}, and {@link Pose3d#toPose2d()}.)
+ *
+ * <p>Teams can use odometry during the autonomous period for complex tasks like path following.
+ * Furthermore, odometry can be used for latency compensation when using computer-vision systems.
+ */
 public class DoubleFollowerWheelOdometry3d extends Odometry3d<DoubleFollowerWheelPositions> {
+  /**
+   * Constructs a DoubleFollowerWheelOdometry3d object.
+   * @param kinematics The double follower wheel kinematics.
+   * @param gyroAngle The angle reported by the gyroscope.
+   * @param wheelPositions The distances driven by each wheel and the angle reported by the gyroscope.
+   * @param initialPose The starting position of the robot on the field.
+   */
   public DoubleFollowerWheelOdometry3d(
       DoubleFollowerWheelKinematics kinematics,
       Rotation3d gyroAngle,
@@ -12,6 +31,14 @@ public class DoubleFollowerWheelOdometry3d extends Odometry3d<DoubleFollowerWhee
     super(kinematics, gyroAngle, wheelPositions, initialPose);
   }
 
+  /**
+   * Updates the robot's position on the field using forward kinematics and integration of the pose
+   * over time.
+   * @param x The distance traveled by the forward-facing wheel in meters.
+   * @param y The distance traveled by the left-facing wheel in meters.
+   * @param gyroAngle The angle reported by the gyroscope.
+   * @return The new pose of the robot.
+   */
   public Pose3d update(double x, double y, Rotation3d gyroAngle) {
     return update(gyroAngle,new DoubleFollowerWheelPositions(x, y, gyroAngle.toRotation2d()));
   }
