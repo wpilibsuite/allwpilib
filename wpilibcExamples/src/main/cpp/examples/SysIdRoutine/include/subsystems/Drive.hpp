76ee5a2e14506227d122f835abf7509e34674892
DeltaDizzy <EJBraswell@gmail.com>
2026-05-01 23:13:18 -0400

    fix sysidroutine example

--- wpilibcExamples/src/main/cpp/examples/SysIdRoutine/include/subsystems/Drive.hpp
+++ wpilibcExamples/src/main/cpp/examples/SysIdRoutine/include/subsystems/Drive.hpp
@@ -44,18 +44,18 @@ class Drive : public wpi::cmd::SubsystemBase {
                               nullptr},
       wpi::cmd::sysid::Mechanism{
           [this](wpi::units::volt_t driveVoltage) {
-            leftMotor.SetVoltage(driveVoltage);
-            rightMotor.SetVoltage(driveVoltage);
+            leftLeader.SetVoltage(driveVoltage);
+            rightLeader.SetVoltage(driveVoltage);
           },
           [this](wpi::sysid::SysIdRoutineLog* log) {
             log->Motor("drive-left")
-                .voltage(leftMotor.GetThrottle() *
+                .voltage(leftLeader.GetThrottle() *
                          wpi::RobotController::GetBatteryVoltage())
                 .position(wpi::units::meter_t{leftEncoder.GetDistance()})
                 .velocity(
                     wpi::units::meters_per_second_t{leftEncoder.GetRate()});
             log->Motor("drive-right")
-                .voltage(rightMotor.GetThrottle() *
+                .voltage(rightLeader.GetThrottle() *
                          wpi::RobotController::GetBatteryVoltage())
                 .position(wpi::units::meter_t{rightEncoder.GetDistance()})
                 .velocity(
