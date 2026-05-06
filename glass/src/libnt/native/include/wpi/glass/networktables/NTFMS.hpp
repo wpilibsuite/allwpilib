dd83323b57e6ffae3d6b49d3996a6664ef41294f
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-05-06 19:29:07 -0400

    [wpilib] Publish AllianceStationID

--- glass/src/libnt/native/include/wpi/glass/networktables/NTFMS.hpp
+++ glass/src/libnt/native/include/wpi/glass/networktables/NTFMS.hpp
@@ -53,8 +53,7 @@ class NTFMSModel : public FMSModel {
  private:
   wpi::nt::NetworkTableInstance m_inst;
   wpi::nt::StringSubscriber m_gameDataSubscriber;
-  wpi::nt::BooleanSubscriber m_alliance;
-  wpi::nt::IntegerSubscriber m_station;
+  wpi::nt::IntegerSubscriber m_allianceStation;
   wpi::nt::RawSubscriber m_controlWord;
 
   BooleanSource m_fmsAttached;
