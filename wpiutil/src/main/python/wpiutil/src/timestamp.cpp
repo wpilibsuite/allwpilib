ed11f5f4e9cfa39fdd0444d7cfef4c46637b7a96
PJ Reiniger <pj.reiniger@gmail.com>
2026-05-12 02:13:10 -0400

    Add exception handling to trampoline

--- wpiutil/src/main/python/wpiutil/src/timestamp.cpp
+++ wpiutil/src/main/python/wpiutil/src/timestamp.cpp
@@ -12,8 +12,17 @@ py::object &get_now_impl_ref() {
 
 // Helper function to massage the python callback into a C api
 uint64_t now_impl_trampoline() {
-    py::gil_scoped_acquire acquire;
-    return get_now_impl_ref()().cast<uint64_t>();
+  py::gil_scoped_acquire acquire;
+  try {
+    auto &hook = get_now_impl_ref();
+    if (hook) {
+      return hook().cast<uint64_t>();
+    }
+  } catch (py::error_already_set &e) {
+    e.discard_as_unraisable("wpiutil.now_impl_trampoline");
+  }
+
+  return wpi::util::NowDefault();
 }
 
 void set_now_impl(py::object func) {
