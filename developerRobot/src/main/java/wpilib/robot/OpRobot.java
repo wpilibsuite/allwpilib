34eba45c9af15788136e862fe6cbe6783eda34fa
Thad House <thadhouse1@gmail.com>
2026-05-13 12:26:09 -0700

    Formatting

--- developerRobot/src/main/java/wpilib/robot/OpRobot.java
+++ developerRobot/src/main/java/wpilib/robot/OpRobot.java
@@ -8,9 +8,7 @@ import org.wpilib.driverstation.DefaultUserControls;
 import org.wpilib.driverstation.UserControlsInstance;
 import org.wpilib.framework.OpModeRobot;
 
-/**
- * This is a dev program for testing OpModeRobot.
- */
+/** This is a dev program for testing OpModeRobot. */
 @UserControlsInstance(DefaultUserControls.class)
 public class OpRobot extends OpModeRobot {
   /** Called once at the beginning of the robot program. */
