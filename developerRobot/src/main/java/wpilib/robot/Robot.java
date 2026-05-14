3ec0ebc64908c9803100c9af21e8fe05133b2dbc
Thad House <thadhouse1@gmail.com>
2026-05-14 12:09:05 -0700

    Fix file

--- developerRobot/src/main/java/wpilib/robot/Robot.java
+++ developerRobot/src/main/java/wpilib/robot/Robot.java
@@ -4,25 +4,15 @@
 
 package wpilib.robot;
 
-import org.wpilib.driverstation.Gamepad;
 import org.wpilib.framework.TimedRobot;
-import org.wpilib.hardware.expansionhub.ExpansionHubMotor;
-import org.wpilib.hardware.expansionhub.ExpansionHubMotor.FollowDirection;
 
 public class Robot extends TimedRobot {
-  ExpansionHubMotor motor = new ExpansionHubMotor(0, 0);
-  ExpansionHubMotor motor2 = new ExpansionHubMotor(0, 1);
-  ExpansionHubMotor motor3 = new ExpansionHubMotor(0, 2);
-  Gamepad gamepad = new Gamepad(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
-  public Robot() {
-    motor2.follow(motor3, FollowDirection.Opposed);
-    motor3.follow(motor, FollowDirection.Opposed);
-  }
+  public Robot() {}
 
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
@@ -46,7 +36,5 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during all modes. */
   @Override
-  public void robotPeriodic() {
-    motor.setThrottle(gamepad.getLeftY());
-  }
+  public void robotPeriodic() {}
 }
