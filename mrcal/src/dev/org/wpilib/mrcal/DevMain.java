4b66e9d27a2db846fa7710d1d1b037c02c36e911
Jade Turner <spacey-sooty@proton.me>
2026-05-26 18:54:25 +0800

    move devmain

--- mrcal/src/dev/org/wpilib/mrcal/DevMain.java
+++ mrcal/src/dev/org/wpilib/mrcal/DevMain.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.mrcal;
+package org.wpilib.mrcal;
 
 public final class DevMain {
   public static void main(String[] args) {}
