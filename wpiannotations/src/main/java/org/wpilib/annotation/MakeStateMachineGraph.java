7e82a004e859e25527c69481013b6e6b64b3aee0
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-06-03 18:17:57 -0400

    Add option to use custom state machine class

--- wpiannotations/src/main/java/org/wpilib/annotation/MakeStateMachineGraph.java
+++ wpiannotations/src/main/java/org/wpilib/annotation/MakeStateMachineGraph.java
@@ -1,10 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package org.wpilib.annotation;
 
 import java.lang.annotation.Target;
 
 /**
- * Generates a mermaid graph for a state machine encapsulated within a method.
- * Note that this doesn't support multiple state machines per method.
+ * Generates a mermaid graph for a state machine encapsulated within a method. Note that this
+ * doesn't support multiple state machines per method.
  */
 @Target(java.lang.annotation.ElementType.METHOD)
-public @interface MakeStateMachineGraph {}
+public @interface MakeStateMachineGraph {
+  /**
+   * An advanced configuration option that allows you to generate diagrams for a custom state
+   * machine class sharing the same syntax as WPILib's state machine.
+   */
+  String stateMachineType() default "StateMachine";
+}
