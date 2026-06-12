863600e7e8292e95dd5692b4bd0b299a1db2b13e
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-06-11 23:06:04 -0400

    [apriltag] Apriltag Protobuf/Struct build

---
+++ apriltag/generate_apriltag.py
@@ -0,0 +1,87 @@
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+from apriltag.generate_nanopb import generate_nanopb
+from apriltag.generate_quickbuf import generate_quickbuf
+
+
+def main(argv):
+    script_path = Path(__file__).resolve()
+    dirname = script_path.parent
+
+    root_path = dirname.parent
+    nanopb_path = os.path.join(
+        root_path,
+        "wpiutil",
+        "src",
+        "main",
+        "native",
+        "thirdparty",
+        "nanopb",
+        "generator",
+        "nanopb_generator.py",
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_directory",
+        help="Optional. If set, will output the generated files to this directory, otherwise it will use a path relative to the script",
+        default=dirname / "src/generated",
+        type=Path,
+    )
+    parser.add_argument(
+        "--template_root",
+        help="Optional. If set, will use this directory as the root for the jinja templates",
+        default=dirname / "src/generate",
+        type=Path,
+    )
+    parser.add_argument(
+        "--proto_directory",
+        help="Optional. If set, will use this directory to glob for protobuf files",
+        default=dirname / "src/main/proto",
+        type=Path,
+    )
+    parser.add_argument(
+        "--proto_path",
+        help="Optional. Specifies additional directories in which to search for protobuf imports",
+        default=root_path,
+        type=Path,
+    )
+    parser.add_argument(
+        "--protoc",
+        help="Protoc executable command",
+        default="protoc",
+    )
+    parser.add_argument(
+        "--nanopb_generator", help="Nanopb generator command", default=nanopb_path
+    )
+    parser.add_argument(
+        "--quickbuf_plugin",
+        help="Path to the quickbuf protoc plugin",
+        required=True,
+    )
+    args = parser.parse_args(argv)
+
+    generate_nanopb(
+        args.nanopb_generator,
+        args.output_directory / "main/native/cpp/apriltag/protobuf",
+        args.proto_directory,
+        args.proto_path,
+    )
+    generate_quickbuf(
+        args.protoc,
+        args.quickbuf_plugin,
+        args.output_directory / "main/java",
+        args.proto_directory,
+        args.proto_path,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
