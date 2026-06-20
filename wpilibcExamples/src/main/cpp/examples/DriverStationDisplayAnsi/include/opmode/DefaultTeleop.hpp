f1c9d82d506637ca786d6c32bf933813522dffe6
Thad House <ThadHouse@users.noreply.github.com>
2026-06-20 10:28:24 -0700

    [hal,wpilib] Add DS Display API (#8975)

---
+++ wpilibcExamples/src/main/cpp/examples/DriverStationDisplayAnsi/include/opmode/DefaultTeleop.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include "wpi/opmode/PeriodicOpMode.hpp"
+#include "wpi/system/Timer.hpp"
+
+class DefaultTeleop : public wpi::PeriodicOpMode {
+ public:
+  void Start() override;
+  void Periodic() override;
+
+ private:
+  void UpdateSecondsDisplay();
+
+  wpi::Timer m_timer;
+  int m_seconds = 0;
+};
