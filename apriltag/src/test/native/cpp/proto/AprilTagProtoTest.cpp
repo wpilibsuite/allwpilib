053c466362522089cc4731d9e9a390a03b693c23
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-06-29 15:45:27 -0700

    [apriltag] Change tests to use Cache2

--- apriltag/src/test/native/cpp/proto/AprilTagProtoTest.cpp
+++ apriltag/src/test/native/cpp/proto/AprilTagProtoTest.cpp
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
 
 #include "wpi/apriltag/AprilTag.hpp"
 #include "wpi/util/SmallVector.hpp"
@@ -16,14 +16,14 @@ const AprilTag kExpectedData =
                                   wpi::math::Rotation3d{0_deg, 0_deg, 0_deg}}};
 }  // namespace
 
-TEST(AprilTagProtoTest, Roundtrip) {
+TEST_CASE("AprilTagProtoTest Roundtrip", "[apriltag]") {
   wpi::util::ProtobufMessage<decltype(kExpectedData)> message;
   wpi::util::SmallVector<uint8_t, 64> buf;
 
-  ASSERT_TRUE(message.Pack(buf, kExpectedData));
+  REQUIRE(message.Pack(buf, kExpectedData));
   auto unpacked_data = message.Unpack(buf);
-  ASSERT_TRUE(unpacked_data.has_value());
+  REQUIRE(unpacked_data.has_value());
 
-  EXPECT_EQ(kExpectedData.ID, unpacked_data->ID);
-  EXPECT_EQ(kExpectedData.pose, unpacked_data->pose);
+  REQUIRE(unpacked_data->ID == kExpectedData.ID);
+  REQUIRE(unpacked_data->pose == kExpectedData.pose);
 }
