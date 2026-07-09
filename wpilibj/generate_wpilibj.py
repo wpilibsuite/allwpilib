35854b1fb0d4e126707cdffa30411209e3440e68
Peter Johnson <johnson.peter@gmail.com>
2026-07-08 17:11:42 -0700

    Add shebangs

--- wpilibj/generate_wpilibj.py
+++ wpilibj/generate_wpilibj.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+
 import argparse
 import os
 from pathlib import Path
