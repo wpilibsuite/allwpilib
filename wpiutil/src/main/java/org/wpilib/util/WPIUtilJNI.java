56dd8e1bea51c2207028dc25b7ad5685c9283bb4
Peter Johnson <johnson.peter@gmail.com>
2026-07-09 19:50:42 -0700

    Update tests

--- wpiutil/src/main/java/org/wpilib/util/WPIUtilJNI.java
+++ wpiutil/src/main/java/org/wpilib/util/WPIUtilJNI.java
@@ -74,7 +74,9 @@ public class WPIUtilJNI {
    */
   public static native void writeStderr(String str);
 
-  /** Enable mock time. The mock time is initially set to 0. */
+  /**
+   * Enable mock time. The mock time is initially set to 0. This also sets program start time to 0.
+   */
   public static native void enableMockTime();
 
   /** Disable mock time. */
--- wpiutil/src/test/python/test_timestamp.py
+++ wpiutil/src/test/python/test_timestamp.py
@@ -14,6 +14,13 @@ def test_default():
 NOW_TIMESTAMP_S = 0
 
 
+@pytest.fixture(autouse=True)
+def reset_timestamp():
+    wpiutil.set_now_impl(None)
+    yield
+    wpiutil.set_now_impl(None)
+
+
 def custom_now_getter():
     global NOW_TIMESTAMP_S
     return int(NOW_TIMESTAMP_S * 1e6)
@@ -21,24 +28,47 @@ def custom_now_getter():
 
 @pytest.fixture
 def custom_fixture():
+    global NOW_TIMESTAMP_S
+    NOW_TIMESTAMP_S = 0
     wpiutil.set_now_impl(custom_now_getter)
     yield
-    wpiutil.set_now_impl(None)
 
 
 def test_custom_timestamp(custom_fixture):
     global NOW_TIMESTAMP_S
 
     assert 0 == wpiutil.now()
+    assert 0 == wpiutil.get_program_start_time()
 
     NOW_TIMESTAMP_S = 1.5
     assert 1_500_000 == wpiutil.now()
+    assert 0 == wpiutil.get_program_start_time()
 
     NOW_TIMESTAMP_S = 100
     assert 100_000_000 == wpiutil.now()
+    assert 0 == wpiutil.get_program_start_time()
 
     # Set it back to the standard implementation and expect monotonic time.
     wpiutil.set_now_impl(None)
     now = wpiutil.now()
     assert wpiutil.get_program_start_time() <= now
     assert now <= wpiutil.now()
+
+
+def test_set_now_impl_updates_program_start_time():
+    global NOW_TIMESTAMP_S
+    original_start_time = wpiutil.get_program_start_time()
+
+    NOW_TIMESTAMP_S = 1.5
+    wpiutil.set_now_impl(custom_now_getter)
+
+    assert 1_500_000 == wpiutil.now()
+    assert 1_500_000 == wpiutil.get_program_start_time()
+
+    NOW_TIMESTAMP_S = 100
+    assert 100_000_000 == wpiutil.now()
+    assert 1_500_000 == wpiutil.get_program_start_time()
+
+    wpiutil.set_now_impl(None)
+
+    assert original_start_time == wpiutil.get_program_start_time()
