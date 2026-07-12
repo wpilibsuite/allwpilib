bd5682bf47658dd2ef989803dc4e39d2ba4e7d2d
Maxwell Morgan <itsmaxwellm@gmail.com>
2026-07-12 16:04:20 -0400

    test(wpimath): use unnamed namespace and remove wpi::math scope

--- wpimath/src/test/native/cpp/controller/LinearSystemLoopTest.cpp
+++ wpimath/src/test/native/cpp/controller/LinearSystemLoopTest.cpp
@@ -25,50 +25,56 @@
 #include "wpi/units/velocity.hpp"
 #include "wpi/units/voltage.hpp"
 
-namespace wpi::math {
+inline constexpr wpi::units::second_t kDt = 5_ms;
+inline constexpr double kPositionStddev = 0.0001;
 
-constexpr units::second_t kDt = 5_ms;
-constexpr double kPositionStddev = 0.0001;
-std::default_random_engine generator{};
-std::normal_distribution<double> distribution(0.0, 1.0);
+namespace {
+std::default_random_engine generator;
+std::normal_distribution<double> distribution{0.0, 1.0};
+}  // namespace
 
 TEST(LinearSystemLoopTest, StateSpaceEnabled) {
-  LinearSystem<2, 1, 2> plant{Models::ElevatorFromPhysicalConstants(
-      DCMotor::Vex775Pro(2), 5_kg, 0.0181864_m, 1.0)};
+  wpi::math::LinearSystem<2, 1, 2> plant{
+      wpi::math::Models::ElevatorFromPhysicalConstants(
+          wpi::math::DCMotor::Vex775Pro(2), 5_kg, 0.0181864_m, 1.0)};
 
-  LinearSystem<2, 1, 1> slicedPlant{plant.Slice(0)};
+  wpi::math::LinearSystem<2, 1, 1> slicedPlant{plant.Slice(0)};
 
-  KalmanFilter<2, 1, 1> observer{
+  wpi::math::KalmanFilter<2, 1, 1> observer{
       slicedPlant, {0.05, 1.0}, {kPositionStddev}, kDt};
 
-  LinearQuadraticRegulator<2, 1> controller{
+  wpi::math::LinearQuadraticRegulator<2, 1> controller{
       slicedPlant, {0.02, 0.4}, {12.0}, kDt};
 
-  LinearSystemLoop<2, 1, 1> loop{slicedPlant, controller, observer, 12_V, kDt};
+  wpi::math::LinearSystemLoop<2, 1, 1> loop{slicedPlant, controller, observer,
+                                            12_V, kDt};
   loop.Reset({0, 0});
 
-  Vectord<2> references{2.0, 0.0};
+  wpi::math::Vectord<2> references{2.0, 0.0};
   loop.SetNextR(references);
 
-  TrapezoidProfile<units::meters>::Constraints constraints{4_mps, 3_mps_sq};
+  wpi::math::TrapezoidProfile<wpi::units::meters>::Constraints constraints{
+      4_mps, 3_mps_sq};
 
   for (int i = 0; i < 1000; ++i) {
-    TrapezoidProfile<units::meters> profile{constraints};
+    wpi::math::TrapezoidProfile<wpi::units::meters> profile{constraints};
 
-    TrapezoidProfile<units::meters>::State current{
-        units::meter_t{loop.Xhat(0)}, units::meters_per_second_t{loop.Xhat(1)}};
+    wpi::math::TrapezoidProfile<wpi::units::meters>::State current{
+        wpi::units::meter_t{loop.Xhat(0)},
+        wpi::units::meters_per_second_t{loop.Xhat(1)}};
 
-    TrapezoidProfile<units::meters>::State goal{
-        units::meter_t{references(0)},
-        units::meters_per_second_t{references(1)}};
+    wpi::math::TrapezoidProfile<wpi::units::meters>::State goal{
+        wpi::units::meter_t{references(0)},
+        wpi::units::meters_per_second_t{references(1)}};
 
-    TrapezoidProfile<units::meters>::State state{
+    wpi::math::TrapezoidProfile<wpi::units::meters>::State state{
         profile.Calculate(kDt, current, goal)};
 
     loop.SetNextR({state.position.value(), state.velocity.value()});
 
-    Matrixd<1, 1> y{slicedPlant.CalculateY(loop.Xhat(), loop.U()) +
-                    Vectord<1>{distribution(generator) * kPositionStddev}};
+    wpi::math::Matrixd<1, 1> y{
+        slicedPlant.CalculateY(loop.Xhat(), loop.U()) +
+        wpi::math::Vectord<1>{distribution(generator) * kPositionStddev}};
 
     loop.Correct(y);
     loop.Predict(kDt);
@@ -84,26 +90,31 @@ TEST(LinearSystemLoopTest, StateSpaceEnabled) {
 }
 
 TEST(LinearSystemLoopTest, FlywheelEnabled) {
-  LinearSystem<1, 1, 1> plant{Models::FlywheelFromPhysicalConstants(
-      DCMotor::NEO(2), 0.00289_kg_sq_m, 1.0)};
+  wpi::math::LinearSystem<1, 1, 1> plant{
+      wpi::math::Models::FlywheelFromPhysicalConstants(
+          wpi::math::DCMotor::NEO(2), 0.00289_kg_sq_m, 1.0)};
 
-  KalmanFilter<1, 1, 1> observer{plant, {1.0}, {kPositionStddev}, kDt};
+  wpi::math::KalmanFilter<1, 1, 1> observer{
+      plant, {1.0}, {kPositionStddev}, kDt};
 
-  LinearQuadraticRegulator<1, 1> controller{plant, {9.0}, {12.0}, kDt};
+  wpi::math::LinearQuadraticRegulator<1, 1> controller{
+      plant, {9.0}, {12.0}, kDt};
 
-  LinearPlantInversionFeedforward feedforward{plant, kDt};
+  wpi::math::LinearPlantInversionFeedforward feedforward{plant, kDt};
 
-  LinearSystemLoop<1, 1, 1> loop{controller, feedforward, observer, 12_V};
-  loop.Reset(Vectord<1>{0.0});
+  wpi::math::LinearSystemLoop<1, 1, 1> loop{controller, feedforward, observer,
+                                            12_V};
+  loop.Reset(wpi::math::Vectord<1>{0.0});
 
-  Vectord<1> references{3000.0 / 60.0 * 2.0 * std::numbers::pi};
+  wpi::math::Vectord<1> references{3000.0 / 60.0 * 2.0 * std::numbers::pi};
   loop.SetNextR(references);
 
   wpi::units::second_t time = 0_s;
   while (time < 10_s) {
     loop.SetNextR(references);
-    Matrixd<1, 1> y{plant.CalculateY(loop.Xhat(), loop.U()) +
-                    Vectord<1>{distribution(generator) * kPositionStddev}};
+    wpi::math::Matrixd<1, 1> y{
+        plant.CalculateY(loop.Xhat(), loop.U()) +
+        wpi::math::Vectord<1>{distribution(generator) * kPositionStddev}};
 
     loop.Correct(y);
     loop.Predict(kDt);
@@ -118,5 +129,3 @@ TEST(LinearSystemLoopTest, FlywheelEnabled) {
 
   EXPECT_NEAR(0.0, loop.Error().value(), 0.1);
 }
-
-}  // namespace wpi::math
