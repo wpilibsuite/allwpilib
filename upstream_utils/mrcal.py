737885722bc71711ea9f26c398966f6fa3395882
Tyler Veness <calcmogul@gmail.com>
2026-07-16 22:02:19 -0700

    Give mrcal .pl files execute permissions

--- upstream_utils/mrcal.py
+++ upstream_utils/mrcal.py
@@ -54,6 +54,8 @@ def copy_upstream_src(wpilib_root: Path):
         content = content.replace("__attribute__((unused))", "")
         with open(f, "w") as file:
             file.write(content)
+        if f.suffix == ".pl":
+            f.chmod(0o755)
 
 
 def main():
