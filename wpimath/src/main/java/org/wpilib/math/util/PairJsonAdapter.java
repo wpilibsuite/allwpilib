1e8aa34fd86d22592bb5b7cc59d5b9fb76ca34fa
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-07-22 22:58:05 -0400

    Add custom JsonAdapter for Pair

---
+++ wpimath/src/main/java/org/wpilib/math/util/PairJsonAdapter.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.wpilib.math.util;
+
+import io.avaje.json.JsonAdapter;
+import io.avaje.json.JsonReader;
+import io.avaje.json.JsonWriter;
+import io.avaje.jsonb.AdapterFactory;
+import io.avaje.jsonb.CustomAdapter;
+import io.avaje.jsonb.Jsonb;
+import io.avaje.jsonb.Types;
+import java.lang.reflect.Type;
+
+@CustomAdapter
+public class PairJsonAdapter implements JsonAdapter<Pair<?, ?>> {
+  private JsonAdapter<Object> firstAdapter;
+  private JsonAdapter<Object> secondAdapter;
+
+  public static final AdapterFactory FACTORY =
+      (type, jsonb) -> {
+        if (Types.isGenericTypeOf(type, Pair.class)) {
+          Type[] args = Types.typeArguments(type);
+          return new PairJsonAdapter(jsonb, args[0], args[1]);
+        }
+        return null;
+      };
+
+  public PairJsonAdapter(Jsonb jsonb, Type first, Type second) {
+    firstAdapter = jsonb.adapter(first);
+    secondAdapter = jsonb.adapter(second);
+  }
+
+  @Override
+  public Pair<?, ?> fromJson(JsonReader reader) {
+    reader.beginArray();
+    Object first = firstAdapter.fromJson(reader);
+    Object second = secondAdapter.fromJson(reader);
+    reader.endArray();
+    return Pair.of(first, second);
+  }
+
+  @Override
+  public void toJson(JsonWriter writer, Pair<?, ?> value) {
+    writer.beginArray();
+    firstAdapter.toJson(writer, value.getFirst());
+    secondAdapter.toJson(writer, value.getSecond());
+    writer.endArray();
+  }
+}
