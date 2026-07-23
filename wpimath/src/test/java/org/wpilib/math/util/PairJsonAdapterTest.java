931d4d05ea312fe68d026d61f18a7459bfd0cff6
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-07-23 10:20:06 -0400

    Add test for Pair JsonAdapter

---
+++ wpimath/src/test/java/org/wpilib/math/util/PairJsonAdapterTest.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.wpilib.math.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.avaje.jsonb.Jsonb;
+import io.avaje.jsonb.Types;
+import org.junit.jupiter.api.Test;
+
+class PairJsonAdapterTest {
+  PairJsonAdapterTest() {}
+
+  @Test
+  void testJsonRoundtrip() {
+    var expected = new Pair<>("Test", 2);
+    var pairJsonb =
+        Jsonb.instance().type(Types.newParameterizedType(Pair.class, String.class, Integer.class));
+    var json = pairJsonb.toJson(expected);
+
+    assertEquals("[\"Test\",2]", json);
+
+    var actual = pairJsonb.fromJson(json);
+
+    assertEquals(expected, actual);
+  }
+}
