34d0689c0098ae0bb608a086619eff9a16d60bac
Kevin-OConnor <koconnor@usfirst.org>
2026-07-29 15:50:51 -0400

    Fix lint errors

--- hal/src/main/native/systemcore/PWM.cpp
+++ hal/src/main/native/systemcore/PWM.cpp
@@ -32,8 +32,8 @@ HAL_DigitalHandle HAL_InitializePWMPort(int32_t channel,
 
   if (channel < 0 || channel >= kNumSmartIo) {
     *status = MakeErrorIndexOutOfRange(HAL_RESOURCE_OUT_OF_RANGE,
-                                       "Invalid Index for PWM", 0, kNumSmartIo - 1,
-                                       channel);
+                                       "Invalid Index for PWM", 0, 
+                                       kNumSmartIo - 1, channel);
     return HAL_INVALID_HANDLE;
   }
 
