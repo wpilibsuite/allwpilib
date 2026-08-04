035f436df76a7568954510398cddb4e32692b939
Tyler Veness <calcmogul@gmail.com>
2026-08-03 22:20:33 -0700

    Fix ruff warnings

--- ntcoreffi/generate_exported_symbols.py
+++ ntcoreffi/generate_exported_symbols.py
@@ -4,10 +4,8 @@ from pathlib import Path
 
 
 def generate_symbol_text(output_directory, symbols):
-    with open(output_directory, "w") as out:
-        with open(symbols, "r") as f:
-            for line in f:
-                out.write(f"AddFunctionToLink({line.strip()});\n")
+    with open(output_directory, "w") as out, open(symbols, "r") as f:
+        out.writelines(f"AddFunctionToLink({line.strip()});\n" for line in f)
 
 
 def main():
