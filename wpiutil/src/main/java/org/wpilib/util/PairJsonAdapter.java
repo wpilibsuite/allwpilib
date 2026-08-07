131518ea689d3ac03246dd8e0581256f668ce332
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-08-07 19:38:13 -0400

    Simplify Pair adapter with forceSerialize

--- wpiutil/src/main/java/org/wpilib/util/PairJsonAdapter.java
+++ wpiutil/src/main/java/org/wpilib/util/PairJsonAdapter.java
@@ -12,11 +12,6 @@ import io.avaje.jsonb.CustomAdapter;
 import io.avaje.jsonb.Jsonb;
 import io.avaje.jsonb.Types;
 import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
-import java.util.OptionalLong;
 
 /**
  * For automated Jsonb use.
@@ -65,50 +60,10 @@ public class PairJsonAdapter implements JsonAdapter<Pair<?, ?>> {
   @Override
   public void toJson(JsonWriter writer, Pair<?, ?> value) {
     writer.beginArray();
-    if (writer.serializeNulls()
-        && (writer.serializeEmpty()
-            || !(value.getFirst() instanceof Collection
-                || value.getSecond() instanceof Collection))) {
-      // If nulls and collections are serialized, the default behavior works
-      firstAdapter.toJson(writer, value.getFirst());
-      secondAdapter.toJson(writer, value.getSecond());
-    } else {
-      boolean wasSerializingNulls = writer.serializeNulls();
-      boolean wasSerializingEmpty = writer.serializeEmpty();
-      // If nulls or collections are not serialized, override the flags to still put fields at the
-      // correct index
-      if (value.getFirst() == null || isEmpty(value.getFirst())) {
-        writer.serializeNulls(true);
-        writer.serializeEmpty(true);
-      }
-      firstAdapter.toJson(writer, value.getFirst());
-      if (value.getSecond() == null || isEmpty(value.getSecond())) {
-        writer.serializeNulls(true);
-        writer.serializeEmpty(true);
-      } else {
-        writer.serializeNulls(wasSerializingNulls);
-        writer.serializeEmpty(wasSerializingEmpty);
-      }
-      secondAdapter.toJson(writer, value.getSecond());
-      writer.serializeNulls(wasSerializingNulls);
-      writer.serializeEmpty(wasSerializingEmpty);
-    }
+    writer.forceSerialize(); // Fields are position dependent, so they can't be skipped
+    firstAdapter.toJson(writer, value.getFirst());
+    writer.forceSerialize();
+    secondAdapter.toJson(writer, value.getSecond());
     writer.endArray();
-  }
-
-  private static boolean isEmpty(Object value) {
-    if (value instanceof Optional) {
-      return ((Optional<?>) value).isEmpty();
-    } else if (value instanceof OptionalInt) {
-      return ((OptionalInt) value).isEmpty();
-    } else if (value instanceof OptionalLong) {
-      return ((OptionalLong) value).isEmpty();
-    } else if (value instanceof OptionalDouble) {
-      return ((OptionalDouble) value).isEmpty();
-    } else if (value instanceof Collection) {
-      return ((Collection<?>) value).isEmpty();
-    } else {
-      return false;
-    }
   }
 }
