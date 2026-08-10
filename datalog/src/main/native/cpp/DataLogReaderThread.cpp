b3109e1b2de040cae0b3be42795490959686d1e2
Dustin Spicuzza <dustin@virtualroadside.com>
2026-08-10 06:10:02 +0000

    [datalog] Prevent malformed protobuf descriptor crashes

--- datalog/src/main/native/cpp/DataLogReaderThread.cpp
+++ datalog/src/main/native/cpp/DataLogReaderThread.cpp
@@ -118,14 +118,21 @@ void DataLogReaderThread::ReadMain() {
     } else if (auto filename =
                    wpi::util::remove_prefix(name, "/.schema/proto:")) {
       // protobuf descriptor handling
+      if (!m_protoPool || !m_arena) {
+        wpi::util::print("could not allocate protobuf schema resources\n");
+        continue;
+      }
+      auto* descriptor = google_protobuf_FileDescriptorProto_parse(
+          reinterpret_cast<const char*>(data.data()), data.size(), m_arena);
+      if (!descriptor) {
+        wpi::util::print("could not decode protobuf '{}' filename '{}'\n", name,
+                         *filename);
+        continue;
+      }
       upb_Status status;
-      status.ok = true;
-      upb_DefPool_AddFile(
-          m_protoPool,
-          google_protobuf_FileDescriptorProto_parse(
-              reinterpret_cast<const char*>(data.data()), data.size(), m_arena),
-          &status);
-      if (!status.ok) {
+      upb_Status_Clear(&status);
+      upb_DefPool_AddFile(m_protoPool, descriptor, &status);
+      if (!upb_Status_IsOk(&status)) {
         wpi::util::print("could not decode protobuf '{}' filename '{}'\n", name,
                          *filename);
       }
