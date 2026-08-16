61e6294758611518bb766e56611d12602c3491c8
Dustin Spicuzza <dustin@virtualroadside.com>
2026-08-16 16:19:49 -0400

    Add documentation

--- wpinet/src/main/native/include/wpi/net/HttpWebSocketServerConnection.hpp
+++ wpinet/src/main/native/include/wpi/net/HttpWebSocketServerConnection.hpp
@@ -33,6 +33,7 @@ class HttpWebSocketServerConnection
    *
    * @param stream network stream
    * @param protocols Acceptable subprotocols
+   * @param requestTimeout Timeout for server handshake to be established
    */
   HttpWebSocketServerConnection(std::shared_ptr<uv::Stream> stream,
                                 std::span<const std::string_view> protocols,
