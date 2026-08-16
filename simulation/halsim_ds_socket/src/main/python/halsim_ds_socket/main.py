86e0935e28be08a70b03d73e706bc88c709bdc40
Thad House <thadhouse1@gmail.com>
2026-08-15 22:00:05 -0700

    Fix python socket

--- simulation/halsim_ds_socket/src/main/python/halsim_ds_socket/main.py
+++ simulation/halsim_ds_socket/src/main/python/halsim_ds_socket/main.py
@@ -18,6 +18,10 @@ def load_extension():
 
     root = join(abspath(dirname(__file__)), "lib")
     ext = join(root, os.listdir(root)[0])
+
+    # Load MrcLib globally before loading the extension that links to it.
+    import native.wpihal._init_robotpy_native_mrclib  # noqa: F401
+
     retval = hal.load_one_extension(ext)
     if retval != 0:
         logger.warn("loading extension may have failed (error=%d)", retval)
