569c5e62d22e356f7d908d082c319f2d7899e292
Thad House <thadhouse1@gmail.com>
2026-08-21 11:18:05 -0700

    Fix an issue

--- wpilibj/src/main/java/org/wpilib/driverstation/DSGamepadChooser.java
+++ wpilibj/src/main/java/org/wpilib/driverstation/DSGamepadChooser.java
@@ -8,7 +8,6 @@ import static org.wpilib.util.ErrorMessages.requireNonNullParam;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
