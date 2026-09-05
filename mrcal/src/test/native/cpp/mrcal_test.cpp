448cb94400fd8ee432f69dabc19a4657bc4c2084
Jade Turner <spacey-sooty@proton.me>
2026-09-05 14:44:00 +0800

    remove test todo, its easier to let the wpical tests handle this
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- mrcal/src/test/native/cpp/mrcal_test.cpp
+++ mrcal/src/test/native/cpp/mrcal_test.cpp
@@ -7,10 +7,6 @@
 
 #include "mrcal_wrapper.hpp"
 
-// TODO add end-to-end calibration tests. tools/wpical currently covers that via
-// test_result_is_exact.cpp; those cases should move here once the corner-cache
-// fixtures are shared.
-
 TEST_CASE("undistort_mrcal rejects a bad point array", "[mrcal]") {
   cv::Mat cameraMat{{3, 3},
                     {1000.0, 0.0, 640.0, 0.0, 1000.0, 360.0, 0.0, 0.0, 1.0}};
