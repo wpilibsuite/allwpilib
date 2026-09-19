41e20cc8d841e29f62528ecbfd17d7ab3e56eba1
Sam Carlberg <sam@slfc.dev>
2026-09-18 20:56:57 -0400

    Sort requires and exports directives consistently
    - `requires transitive` always before `requires`
    
    - All entries sorted alphabetically

--- wpiutil/src/main/java/module-info.java
+++ wpiutil/src/main/java/module-info.java
@@ -7,10 +7,9 @@
  * and deserialization, native library interactions, and error reporting.
  */
 open module wpilib.util {
-  requires io.avaje.json;
   requires transitive us.hebi.quickbuf.runtime;
+  requires io.avaje.json;
 
-  exports org.wpilib.util;
   exports org.wpilib.util.cleanup;
   exports org.wpilib.util.collections.prefixmap;
   exports org.wpilib.util.collections;
@@ -20,6 +19,7 @@ open module wpilib.util {
   exports org.wpilib.util.option;
   exports org.wpilib.util.protobuf;
   exports org.wpilib.util.runtime;
-  exports org.wpilib.util.struct;
   exports org.wpilib.util.struct.parser;
+  exports org.wpilib.util.struct;
+  exports org.wpilib.util;
 }
