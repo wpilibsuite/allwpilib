7790d748aed880faaa9df4d6cdb666ee2508affd
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-09-25 14:57:29 -0400

    Add Python support

--- wpilibc/src/test/python/event/test_event_loop.py
+++ wpilibc/src/test/python/event/test_event_loop.py
@@ -3,6 +3,31 @@ import pytest
 from wpilib import EventLoop
 
 
+def test_bind_unbind():
+    loop = EventLoop()
+
+    pollCount = 0
+    def count_poll():
+        nonlocal pollCount
+        pollCount += 1
+
+    task1 = loop.bind(count_poll)
+    task2 = loop.bind(count_poll)
+    loop.poll()
+
+    assert pollCount == 2
+
+    loop.unbind(task1)
+    loop.poll()
+
+    assert pollCount == 3
+
+    loop.unbind(task2)
+
+    loop.poll()
+
+    assert pollCount == 3
+
 def test_concurrent_modification():
     loop = EventLoop()
 
@@ -11,6 +36,17 @@ def test_concurrent_modification():
             loop.bind(lambda: None)
 
     loop.bind(bind_during_poll)
+    loop.poll()
+
+    loop.clear()
+
+    taskId = None
+
+    def unbind_during_poll():
+        with pytest.raises(RuntimeError):
+            loop.unbind(taskId)
+
+    taskId = loop.bind(unbind_during_poll)
     loop.poll()
 
     loop.clear()
